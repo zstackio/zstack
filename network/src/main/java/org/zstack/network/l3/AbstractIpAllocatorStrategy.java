91b2a9043d597b835336e1e61ec95d879ae41c90
Frank Zhang <xing5820@gmail.com>
2015-06-28 15:20:27 -0700

    https://github.com/zstackorg/zstack/issues/59
    IP arrange judgment has bug

--- network/src/main/java/org/zstack/network/l3/AbstractIpAllocatorStrategy.java
+++ network/src/main/java/org/zstack/network/l3/AbstractIpAllocatorStrategy.java
@@ -1,13 +1,15 @@
 package org.zstack.network.l3;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
 import org.zstack.core.errorcode.ErrorFacade;
 import org.zstack.header.errorcode.OperationFailureException;
 import org.zstack.header.network.l3.*;
+import org.zstack.utils.CollectionUtils;
+import org.zstack.utils.function.Function;
+import org.zstack.utils.network.NetworkUtils;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
@@ -20,27 +22,28 @@ public abstract class AbstractIpAllocatorStrategy implements IpAllocatorStrategy
     @Autowired
     protected ErrorFacade errf;
 
-    @Transactional
     protected UsedIpInventory allocateRequiredIp(IpAllocateMessage msg) {
-        String sql = "select ipr from IpRangeVO ipr where ipr.startIp <= :ip and ipr.endIp >= :ip and ipr.l3NetworkUuid = :l3uuid";
-        TypedQuery<IpRangeVO> q = dbf.getEntityManager().createQuery(sql, IpRangeVO.class);
-        q.setParameter("ip", msg.getRequiredIp());
-        q.setParameter("l3uuid", msg.getL3NetworkUuid());
-        List<IpRangeVO> iprs = q.getResultList();
+        SimpleQuery<IpRangeVO> q = dbf.createQuery(IpRangeVO.class);
+        q.add(IpRangeVO_.l3NetworkUuid, SimpleQuery.Op.EQ, msg.getL3NetworkUuid());
+        List<IpRangeVO> iprs = q.list();
 
-        if (iprs.isEmpty()) {
+        final long rip = NetworkUtils.ipv4StringToLong(msg.getRequiredIp());
+
+        IpRangeVO ipr = CollectionUtils.find(iprs, new Function<IpRangeVO, IpRangeVO>() {
+            @Override
+            public IpRangeVO call(IpRangeVO arg) {
+                long s = NetworkUtils.ipv4StringToLong(arg.getStartIp());
+                long e = NetworkUtils.ipv4StringToLong(arg.getEndIp());
+                return s <= rip && rip <= e ? arg : null;
+            }
+        });
+
+        if (ipr == null) {
             throw new OperationFailureException(errf.instantiateErrorCode(L3Errors.ALLOCATE_IP_ERROR,
                     String.format("cannot find ip range that has ip[%s] in l3Network[uuid:%s]", msg.getRequiredIp(), msg.getL3NetworkUuid())
             ));
         }
 
-        if (iprs.size() > 1) {
-            throw new OperationFailureException(errf.instantiateErrorCode(L3Errors.ALLOCATE_IP_ERROR,
-                    String.format("find more than one ip range that has ip[%s] in l3Network[uuid:%s]", msg.getRequiredIp(), msg.getL3NetworkUuid())
-            ));
-        }
-
-        IpRangeVO ipr = iprs.get(0);
         return l3NwMgr.reserveIp(IpRangeInventory.valueOf(ipr), msg.getRequiredIp());
     }
 }
---
+++ test/src/test/java/org/zstack/test/network/TestAcquireRequiredVip1.java
@@ -0,0 +1,43 @@
+package org.zstack.test.network;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.network.l3.L3NetworkInventory;
+import org.zstack.network.service.vip.VipInventory;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.deployer.Deployer;
+
+public class TestAcquireRequiredVip1 {
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        deployer = new Deployer("deployerXml/network/TestAcquireIp.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+    }
+    
+    @Test
+    public void test() throws ApiSenderException {
+        String rip = "192.168.1.5";
+        L3NetworkInventory l3nw = deployer.l3Networks.get("TestL3Network1");
+        api.addIpRange(l3nw.getUuid(), "192.168.1.2", "192.168.1.254", "192.168.1.1", "255.255.255.0");
+        VipInventory ip = api.acquireIp(l3nw.getUuid(), rip);
+        Assert.assertEquals(ip.getIp(), rip);
+    }
+
+}
