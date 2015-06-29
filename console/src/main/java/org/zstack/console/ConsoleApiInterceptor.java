941825719730950a787f602f4f6efeed9a3dc113
Frank Zhang <xing5820@gmail.com>
2015-06-29 16:19:39 -0700

    only allow console access when the VM is Runnning

--- console/src/main/java/org/zstack/console/ConsoleApiInterceptor.java
+++ console/src/main/java/org/zstack/console/ConsoleApiInterceptor.java
@@ -2,12 +2,20 @@ package org.zstack.console;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
+import org.zstack.core.db.SimpleQuery.Op;
+import org.zstack.core.errorcode.ErrorFacade;
 import org.zstack.header.apimediator.ApiMessageInterceptionException;
 import org.zstack.header.apimediator.ApiMessageInterceptor;
 import org.zstack.header.apimediator.StopRoutingException;
 import org.zstack.header.console.APIRequestConsoleAccessMsg;
 import org.zstack.header.console.ConsoleConstants;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.vm.VmInstance;
+import org.zstack.header.vm.VmInstanceState;
+import org.zstack.header.vm.VmInstanceVO;
+import org.zstack.header.vm.VmInstanceVO_;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,13 +26,30 @@ import org.zstack.header.message.APIMessage;
 public class ConsoleApiInterceptor implements ApiMessageInterceptor {
     @Autowired
     private CloudBus bus;
+    @Autowired
+    private DatabaseFacade dbf;
+    @Autowired
+    private ErrorFacade errf;
 
     @Override
     public APIMessage intercept(APIMessage msg) throws ApiMessageInterceptionException {
         if (msg instanceof APIRequestConsoleAccessMsg) {
-            bus.makeTargetServiceIdByResourceUuid(msg, ConsoleConstants.SERVICE_ID, ((APIRequestConsoleAccessMsg) msg).getVmInstanceUuid());
+            validate((APIRequestConsoleAccessMsg) msg);
         }
 
         return msg;
+    }
+
+    private void validate(APIRequestConsoleAccessMsg msg) {
+        SimpleQuery<VmInstanceVO> q = dbf.createQuery(VmInstanceVO.class);
+        q.select(VmInstanceVO_.state);
+        q.add(VmInstanceVO_.uuid, Op.EQ, msg.getVmInstanceUuid());
+        VmInstanceState state = q.findValue();
+        if (VmInstanceState.Running != state) {
+            throw new ApiMessageInterceptionException(errf.stringToOperationError(
+                    String.format("Console is only available when the VM[uuid:%s] is Running, but the current state is %s", msg.getVmInstanceUuid(), state)
+            ));
+        }
+        bus.makeTargetServiceIdByResourceUuid(msg, ConsoleConstants.SERVICE_ID, ((APIRequestConsoleAccessMsg) msg).getVmInstanceUuid());
     }
 }
