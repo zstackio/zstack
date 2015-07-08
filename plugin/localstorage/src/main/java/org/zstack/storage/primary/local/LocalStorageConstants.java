2e61f34b7d72bb43e805f559a237dd6c5a74108a
Frank Zhang <xing5820@gmail.com>
2015-07-07 23:57:13 -0700

    update inventory.py

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageConstants.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageConstants.java
@@ -1,9 +1,13 @@
 package org.zstack.storage.primary.local;
 
+import org.zstack.header.configuration.PythonClass;
+
 /**
  * Created by frank on 6/30/2015.
  */
-public interface LocalStorageConstants {
-    String LOCAL_STORAGE_TYPE = "LocalStorage";
-    String LOCAL_STORAGE_ALLOCATOR_STRATEGY = "LocalPrimaryStorageStrategy";
+@PythonClass
+public class LocalStorageConstants {
+    @PythonClass
+    public static final String LOCAL_STORAGE_TYPE = "LocalStorage";
+    public static final String LOCAL_STORAGE_ALLOCATOR_STRATEGY = "LocalPrimaryStorageStrategy";
 }
