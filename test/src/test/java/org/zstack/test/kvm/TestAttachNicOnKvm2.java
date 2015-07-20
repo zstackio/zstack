124867a29202e19503bc597e168e8596522bc746
Frank Zhang <xing5820@gmail.com>
2015-07-20 13:50:23 -0700

    add one more unit test case

---
+++ test/src/test/java/org/zstack/test/kvm/TestAttachNicOnKvm2.java
@@ -0,0 +1,76 @@
+package org.zstack.test.kvm;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.network.l3.APIGetIpAddressCapacityReply;
+import org.zstack.header.network.l3.L3NetworkInventory;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.vm.VmInstanceVO;
+import org.zstack.header.vm.VmNicInventory;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.test.storage.backup.sftp.TestSftpBackupStorageDeleteImage2;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+/**
+ * 1. attach a nic to vm
+ * 2. make attaching fails
+ *
+ * confirm the IP is returned
+ */
+public class TestAttachNicOnKvm2 {
+    CLogger logger = Utils.getLogger(TestSftpBackupStorageDeleteImage2.class);
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    KVMSimulatorConfig config;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/kvm/TestCreateVmOnKvm.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(KVMSimulatorConfig.class);
+        session = api.loginAsAdmin();
+    }
+
+    @Test
+    public void test() throws ApiSenderException {
+        final L3NetworkInventory l3 = deployer.l3Networks.get("TestL3Network4");
+        VmInstanceInventory vm = deployer.vms.get("TestVm");
+
+        APIGetIpAddressCapacityReply r = api.getIpAddressCapacityByAll();
+        long avail = r.getAvailableCapacity();
+
+        config.attachNicSuccess = false;
+        try {
+            vm = api.attachNic(vm.getUuid(), l3.getUuid());
+        } catch (ApiSenderException e) {
+        }
+
+        r = api.getIpAddressCapacityByAll();
+        Assert.assertEquals(avail, r.getAvailableCapacity());
+
+        VmInstanceVO vmvo = dbf.findByUuid(vm.getUuid(), VmInstanceVO.class);
+        Assert.assertEquals(3, vmvo.getVmNics().size());
+    }
+}
