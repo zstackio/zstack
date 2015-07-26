89f6e69281758d5bbf365ce3afbd5d0df1491893
Frank Zhang <xing5820@gmail.com>
2015-07-26 13:39:13 -0700

    upgrade agent package name to 0.8

--- plugin/sftpBackupStorage/src/main/java/org/zstack/storage/backup/sftp/SftpBackupStorageGlobalProperty.java
+++ plugin/sftpBackupStorage/src/main/java/org/zstack/storage/backup/sftp/SftpBackupStorageGlobalProperty.java
@@ -8,7 +8,7 @@ import org.zstack.core.GlobalPropertyDefinition;
 
 @GlobalPropertyDefinition
 public class SftpBackupStorageGlobalProperty {
-    @GlobalProperty(name="SftpBackupStorage.agentPackageName", defaultValue = "sftpbackupstorage-0.7.tar.gz")
+    @GlobalProperty(name="SftpBackupStorage.agentPackageName", defaultValue = "sftpbackupstorage-0.8.tar.gz")
     public static String AGENT_PACKAGE_NAME;
     @GlobalProperty(name="SftpBackupStorage.agentPort", defaultValue = "7171")
     public static int AGENT_PORT;
