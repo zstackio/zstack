0cd3e0edcac291d851c1ec5e1f124f01989d2ac9
youyk <yongkang.you@gmail.com>
2015-08-09 16:37:50 +0800

    add ceph constant defination in python inventory.py

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/CephConstants.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/CephConstants.java
@@ -1,10 +1,15 @@
 package org.zstack.storage.ceph;
-
 /**
  * Created by frank on 7/27/2015.
  */
+
+import org.zstack.header.configuration.PythonClass;
+
+@PythonClass
 public interface CephConstants {
+    @PythonClass
     String CEPH_BACKUP_STORAGE_TYPE = "Ceph";
+    @PythonClass
     String CEPH_PRIMARY_STORAGE_TYPE = "Ceph";
 
     String MON_PARAM_MON_PORT = "monPort";
