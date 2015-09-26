eed73e979909c43d5cb1b54152ef8f6f5e841613
Frank Zhang <xing5820@gmail.com>
2015-09-26 23:32:14 +0800

    working on premium

---
+++ test/src/test/java/org/zstack/test/mevoco/TestMevoco3.java
@@ -0,0 +1,86 @@
+package org.zstack.test.mevoco;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.allocator.HostAllocatorConstant;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.vm.VmNicInventory;
+import org.zstack.header.volume.VolumeInventory;
+import org.zstack.mevoco.MevocoConstants;
+import org.zstack.mevoco.MevocoGlobalConfig;
+import org.zstack.network.service.flat.FlatNetworkServiceSimulatorConfig;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig.Capacity;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.utils.Utils;
+import org.zstack.utils.data.SizeUnit;
+import org.zstack.utils.logging.CLogger;
+
+/**
+ * 1. create a vm with mevoco host allocator strategy
+ *
+ * confirm the vm created with the strategy
+ *
+ */
+public class TestMevoco3 {
+    CLogger logger = Utils.getLogger(TestMevoco3.class);
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    LocalStorageSimulatorConfig config;
+    FlatNetworkServiceSimulatorConfig fconfig;
+    KVMSimulatorConfig kconfig;
+    long totalSize = SizeUnit.GIGABYTE.toByte(100);
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/mevoco/TestMevoco3.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.addSpringConfig("mevoco.xml");
+        deployer.addSpringConfig("localStorage.xml");
+        deployer.addSpringConfig("localStorageSimulator.xml");
+        deployer.addSpringConfig("flatNetworkServiceSimulator.xml");
+        deployer.load();
+
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(LocalStorageSimulatorConfig.class);
+        fconfig = loader.getComponent(FlatNetworkServiceSimulatorConfig.class);
+        kconfig = loader.getComponent(KVMSimulatorConfig.class);
+
+        Capacity c = new Capacity();
+        c.total = totalSize;
+        c.avail = totalSize;
+
+        config.capacityMap.put("host1", c);
+
+        deployer.build();
+        api = deployer.getApi();
+        session = api.loginAsAdmin();
+    }
+    
+	@Test
+	public void test() throws ApiSenderException {
+        VmInstanceInventory vm = deployer.vms.get("TestVm");
+        Assert.assertEquals(MevocoConstants.MEVOCO_HOST_ALLOCATOR_STRATEGY, vm.getAllocatorStrategy());
+
+        MevocoGlobalConfig.HOST_ALLOCATOR_STRATEGY.updateValue(HostAllocatorConstant.LEAST_VM_PREFERRED_HOST_ALLOCATOR_STRATEGY_TYPE);
+        api.createVmFromClone(vm);
+    }
+}
