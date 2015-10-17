01c3bea81700a2db749bb6f669b9e1cc91bb2e3d
Frank Zhang <xing5820@gmail.com>
2015-10-16 17:14:49 -0700

    working on premium

--- header/src/main/java/org/zstack/header/storage/backup/BackupStorageConstant.java
+++ header/src/main/java/org/zstack/header/storage/backup/BackupStorageConstant.java
@@ -6,6 +6,7 @@ public interface BackupStorageConstant {
 	public static final String SCHEME_NFS = "nfs";
 	public static final String SCHEME_HTTP = "http";
 	public static final String SCHEME_HTTPS = "https";
+    public static final String SCHEME_FILE = "file";
 
     public static final String DEFAULT_ALLOCATOR_STRATEGY = "defaultAllocatorStrategy";
     public static final BackupStorageAllocatorStrategyType DEFAULT_ALLOCATOR_STRATEGY_TYPE = new BackupStorageAllocatorStrategyType(DEFAULT_ALLOCATOR_STRATEGY);
