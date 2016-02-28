c5040999428ab13879ff452c48f5ad65d163dd52
Frank Zhang <xing5820@gmail.com>
2016-02-28 22:02:29 +0800

    check resource type in APIAttachPolicyToUserGroupMsg

--- header/src/main/java/org/zstack/header/identity/APIAttachPolicyToUserGroupMsg.java
+++ header/src/main/java/org/zstack/header/identity/APIAttachPolicyToUserGroupMsg.java
@@ -4,9 +4,9 @@ import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.APIParam;
 @Action(category = AccountConstant.ACTION_CATEGORY, accountOnly = true)
 public class APIAttachPolicyToUserGroupMsg extends APIMessage implements AccountMessage {
-    @APIParam(checkAccount = true, operationTarget = true)
+    @APIParam(resourceType = PolicyVO.class, checkAccount = true, operationTarget = true)
     private String policyUuid;
-    @APIParam(checkAccount = true, operationTarget = true)
+    @APIParam(resourceType = UserGroupVO.class, checkAccount = true, operationTarget = true)
     private String groupUuid;
     
     @Override
