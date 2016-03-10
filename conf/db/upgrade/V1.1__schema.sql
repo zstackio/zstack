b1e429b3b46a98e6b09e948ce8f2480a20a673b7
Frank Zhang <xing5820@gmail.com>
2016-03-10 18:23:47 +0800

    fix https://github.com/zstackorg/zstack/issues/224

--- conf/db/upgrade/V1.1__schema.sql
+++ conf/db/upgrade/V1.1__schema.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `zstack`.`UserGroupPolicyRefVO` ADD CONSTRAINT uqUserGroupPolicyVO UNIQUE (policyUuid, groupUuid);
+ALTER TABLE `zstack`.`L2NetworkClusterRefVO` ADD CONSTRAINT uqL2NetworkClusterRefVO UNIQUE (l2NetworkUuid, clusterUuid);
+ALTER TABLE `zstack`.`UserPolicyRefVO` ADD CONSTRAINT uqUserPolicyRefVO UNIQUE (policyUuid, userUuid);
