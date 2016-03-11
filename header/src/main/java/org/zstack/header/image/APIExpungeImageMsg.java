906324eb1f55a3438414e82c27a93d47961ab6cf
Frank Zhang <xing5820@gmail.com>
2016-03-11 15:00:55 +0800

    fix https://github.com/zxwing/premium/issues/61

--- header/src/main/java/org/zstack/header/image/APIExpungeImageMsg.java
+++ header/src/main/java/org/zstack/header/image/APIExpungeImageMsg.java
@@ -1,14 +1,21 @@
 package org.zstack.header.image;
 
+import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APIParam;
+import org.zstack.header.storage.backup.BackupStorage;
+import org.zstack.header.storage.backup.BackupStorageVO;
 
 import java.util.List;
 
 /**
  * Created by frank on 11/15/2015.
  */
+@Action(category = ImageConstant.ACTION_CATEGORY)
 public class APIExpungeImageMsg extends APIMessage implements ImageMessage {
+    @APIParam(resourceType = ImageVO.class, checkAccount = true, operationTarget = true)
     private String imageUuid;
+    @APIParam(required = false, nonempty = true, resourceType = BackupStorageVO.class)
     private List<String> backupStorageUuids;
 
     @Override
