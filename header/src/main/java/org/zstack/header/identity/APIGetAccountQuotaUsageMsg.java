f9246eb847d5626ca2c1a93b37e1b543023725e9
Frank Zhang <xing5820@gmail.com>
2016-03-11 11:46:58 +0800

    allow user to query account quota

--- header/src/main/java/org/zstack/header/identity/APIGetAccountQuotaUsageMsg.java
+++ header/src/main/java/org/zstack/header/identity/APIGetAccountQuotaUsageMsg.java
@@ -6,7 +6,7 @@ import org.zstack.header.message.APISyncCallMessage;
 /**
  * Created by frank on 2/22/2016.
  */
-@Action(category = AccountConstant.ACTION_CATEGORY, accountOnly = true)
+@Action(category = AccountConstant.ACTION_CATEGORY, names = {"read"})
 public class APIGetAccountQuotaUsageMsg extends APISyncCallMessage implements AccountMessage {
     @APIParam(resourceType = AccountVO.class, checkAccount = true, operationTarget = true, required = false)
     private String uuid;
