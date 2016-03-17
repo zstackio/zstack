65513a8743193e5409b7c640a7ef20a941aa0782
Frank Zhang <xing5820@gmail.com>
2016-03-17 21:48:24 +0800

    allow normal account to query hosts

--- header/src/main/java/org/zstack/header/host/APIQueryHostMsg.java
+++ header/src/main/java/org/zstack/header/host/APIQueryHostMsg.java
@@ -4,7 +4,7 @@ import org.zstack.header.identity.Action;
 import org.zstack.header.query.APIQueryMessage;
 import org.zstack.header.query.AutoQuery;
 
-@Action(category = HostConstant.ACTION_CATEGORY, accountControl = true)
+@Action(category = HostConstant.ACTION_CATEGORY, names = {"read"})
 @AutoQuery(replyClass = APIQueryHostReply.class, inventoryClass = HostInventory.class)
 public class APIQueryHostMsg extends APIQueryMessage {
 
