6de3364c025d4dd4ceed33c4ad23285f16d8426a
Frank Zhang <xing5820@gmail.com>
2016-03-21 11:08:16 +0800

    fix https://github.com/zstackorg/zstack/issues/237

--- header/src/main/java/org/zstack/header/vm/APIChangeInstanceOfferingMsg.java
+++ header/src/main/java/org/zstack/header/vm/APIChangeInstanceOfferingMsg.java
@@ -1,16 +1,18 @@
 package org.zstack.header.vm;
 
 import org.zstack.header.configuration.InstanceOfferingVO;
+import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.APIParam;
 
 /**
  * Created by frank on 7/16/2015.
  */
+@Action(category = VmInstanceConstant.ACTION_CATEGORY)
 public class APIChangeInstanceOfferingMsg extends APIMessage implements VmInstanceMessage {
     @APIParam(resourceType = VmInstanceVO.class, checkAccount = true, operationTarget = true)
     private String vmInstanceUuid;
-    @APIParam(resourceType = InstanceOfferingVO.class, checkAccount = true, operationTarget = true)
+    @APIParam(resourceType = InstanceOfferingVO.class, checkAccount = true)
     private String instanceOfferingUuid;
 
     @Override
