34e352de40a7939c032239fdb1159b0aa2877fe5
Frank Zhang <xing5820@gmail.com>
2016-03-24 22:15:01 +0800

    for https://github.com/zxwing/premium/issues/114

---
+++ test/src/test/java/org/zstack/test/kvm/TestDetachNicOnKvm10.java
@@ -0,0 +1,66 @@
+package org.zstack.test.kvm;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.network.l3.APIGetIpAddressCapacityReply;
+import org.zstack.header.network.l3.L3NetworkInventory;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.vm.VmInstanceVO;
+import org.zstack.header.vm.VmNicInventory;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.test.storage.backup.sftp.TestSftpBackupStorageDeleteImage2;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 1. delete a l3
+ *
+ * confirm vm's nic of the l3 is deleted
+ */
+public class TestDetachNicOnKvm10 {
+    CLogger logger = Utils.getLogger(TestSftpBackupStorageDeleteImage2.class);
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    KVMSimulatorConfig config;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/kvm/TestDetachNicOnKvm10.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(KVMSimulatorConfig.class);
+        session = api.loginAsAdmin();
+    }
+    
+	@Test
+	public void test() throws ApiSenderException, InterruptedException {
+        final L3NetworkInventory l3 = deployer.l3Networks.get("TestL3Network1");
+        VmInstanceInventory vm = deployer.vms.get("TestVm");
+        VmNicInventory nic = vm.getVmNics().get(0);
+        api.deleteL3Network(l3.getUuid());
+        VmInstanceVO vmvo = dbf.findByUuid(vm.getUuid(), VmInstanceVO.class);
+        Assert.assertEquals(0, vmvo.getVmNics().size());
+    }
+}
