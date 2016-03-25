769c1b4472f21c586d5f45e8a391c94efc87c578
youyk <yongkang.you@gmail.com>
2016-03-25 16:39:24 +0800

    bounce agent from 1.0 to 1.1

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMGlobalProperty.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMGlobalProperty.java
@@ -7,7 +7,7 @@ import org.zstack.core.GlobalPropertyDefinition;
  */
 @GlobalPropertyDefinition
 public class KVMGlobalProperty {
-    @GlobalProperty(name="KvmAgent.agentPackageName", defaultValue = "kvmagent-1.0.tar.gz")
+    @GlobalProperty(name="KvmAgent.agentPackageName", defaultValue = "kvmagent-1.1.tar.gz")
     public static String AGENT_PACKAGE_NAME;
     @GlobalProperty(name="KvmAgent.agentUrlRootPath", defaultValue = "")
     public static String AGENT_URL_ROOT_PATH;
