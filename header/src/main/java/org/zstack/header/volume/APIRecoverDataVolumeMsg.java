ae7c2260500cc44ad6af7f5d279aa8b8661e9a8d
Frank Zhang <xing5820@gmail.com>
2016-03-28 14:34:59 +0800

    add permission control to APIRecoverDataVolumeMsg
    for https://github.com/zxwing/premium/issues/125

--- header/src/main/java/org/zstack/header/volume/APIRecoverDataVolumeMsg.java
+++ header/src/main/java/org/zstack/header/volume/APIRecoverDataVolumeMsg.java
@@ -1,11 +1,15 @@
 package org.zstack.header.volume;
 
+import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APIParam;
 
 /**
  * Created by frank on 11/12/2015.
  */
+@Action(category = VolumeConstant.ACTION_CATEGORY)
 public class APIRecoverDataVolumeMsg extends APIMessage implements VolumeMessage {
+    @APIParam(resourceType = VolumeVO.class, checkAccount = true, operationTarget = true)
     private String uuid;
 
     public String getUuid() {
