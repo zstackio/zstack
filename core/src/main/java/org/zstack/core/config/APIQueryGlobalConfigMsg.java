0434a1cbb6e104049c3a19751bbe967ee2a8c956
Frank Zhang <xing5820@gmail.com>
2016-04-20 11:28:53 +0800

    for destroyed VM, directly delete the nic when receiving a DetachNicMsg

--- core/src/main/java/org/zstack/core/config/APIQueryGlobalConfigMsg.java
+++ core/src/main/java/org/zstack/core/config/APIQueryGlobalConfigMsg.java
@@ -1,10 +1,12 @@
 package org.zstack.core.config;
 
+import org.zstack.header.identity.Action;
 import org.zstack.header.query.APIQueryMessage;
 import org.zstack.header.query.AutoQuery;
 
 /**
  */
 @AutoQuery(replyClass = APIQueryGlobalConfigReply.class, inventoryClass = GlobalConfigInventory.class)
+@Action(category = "configuration", names = {"read"})
 public class APIQueryGlobalConfigMsg extends APIQueryMessage {
 }
