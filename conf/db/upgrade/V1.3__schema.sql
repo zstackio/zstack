8711b09388a597bd14f072cd8f0268f8c6c139ef
Frank Zhang <xing5820@gmail.com>
2016-05-10 12:55:57 +0800

    [vm] delete vm fails
    update ImageEO instead of ImageVO in 1.3 DB upgrade
    
    for https://github.com/zxwing/premium/issues/302

--- conf/db/upgrade/V1.3__schema.sql
+++ conf/db/upgrade/V1.3__schema.sql
@@ -71,7 +71,7 @@ ALTER TABLE FusionstorPrimaryStorageVO ADD CONSTRAINT fkFusionstorPrimaryStorage
 ALTER TABLE ImageEO ADD actualSize bigint unsigned DEFAULT NULL;
 DROP VIEW IF EXISTS `zstack`.`ImageVO`;
 CREATE VIEW `zstack`.`ImageVO` AS SELECT uuid, name, description, status, state, size, actualSize, md5Sum, platform, type, format, url, system, mediaType, createDate, lastOpDate, guestOsType FROM `zstack`.`ImageEO` WHERE deleted IS NULL;
-UPDATE ImageVO set actualSize = size;
+UPDATE ImageEO set actualSize = size;
 
 ALTER TABLE VolumeEO ADD actualSize bigint unsigned DEFAULT NULL;
 DROP VIEW IF EXISTS `zstack`.`VolumeVO`;
