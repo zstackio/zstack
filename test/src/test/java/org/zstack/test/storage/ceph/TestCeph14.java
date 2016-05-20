f139449f259f12a5af22c6732ada1209b5db7e82
Frank Zhang <xing5820@gmail.com>
2016-05-20 19:58:33 +0800

    if stop one osd, the ceph actual available capacity doesn't minus
    for https://github.com/zxwing/premium/issues/379

---
+++ test/src/test/java/org/zstack/test/storage/ceph/TestCeph14.java
@@ -0,0 +1,71 @@
+package org.zstack.test.storage.ceph;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.storage.primary.PrimaryStorageCapacityVO;
+import org.zstack.header.storage.primary.PrimaryStorageInventory;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.storage.ceph.backup.CephBackupStorageSimulatorConfig;
+import org.zstack.storage.ceph.primary.CephPrimaryStorageSimulatorConfig;
+import org.zstack.storage.ceph.primary.CephPrimaryStorageSimulatorConfig.CephPrimaryStorageConfig;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.utils.data.SizeUnit;
+
+/**
+ * 1. an empty ceph primary storage
+ * 2. resize it
+ *
+ * confirm the total/available capacity changed correctly
+ */
+public class TestCeph14 {
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    CephPrimaryStorageSimulatorConfig config;
+    KVMSimulatorConfig kconfig;
+    CephBackupStorageSimulatorConfig bconfig;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/ceph/TestCeph14.xml", con);
+        deployer.addSpringConfig("ceph.xml");
+        deployer.addSpringConfig("cephSimulator.xml");
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(CephPrimaryStorageSimulatorConfig.class);
+        kconfig = loader.getComponent(KVMSimulatorConfig.class);
+        bconfig = loader.getComponent(CephBackupStorageSimulatorConfig.class);
+        session = api.loginAsAdmin();
+    }
+    
+	@Test
+	public void test() throws ApiSenderException {
+        PrimaryStorageInventory ps = deployer.primaryStorages.get("ceph-pri");
+        CephPrimaryStorageConfig c = config.config.get(ps.getName());
+        c.totalCapacity = SizeUnit.GIGABYTE.toByte(500);
+
+        api.reconnectPrimaryStorage(ps.getUuid());
+
+        PrimaryStorageCapacityVO cap = dbf.findByUuid(ps.getUuid(), PrimaryStorageCapacityVO.class);
+        Assert.assertEquals(c.totalCapacity, cap.getTotalCapacity());
+        Assert.assertEquals(c.totalCapacity, cap.getAvailableCapacity());
+    }
+}
