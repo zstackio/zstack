16aa9add55d757d0d70afa2986196b53a5624c3f
Frank Zhang <xing5820@gmail.com>
2016-05-25 16:43:04 +0800

    If the user is deleted, the original has been logged in user should be invalid
    for https://github.com/zxwing/premium/issues/394

--- header/src/main/java/org/zstack/header/identity/IdentityCanonicalEvents.java
+++ header/src/main/java/org/zstack/header/identity/IdentityCanonicalEvents.java
@@ -9,6 +9,7 @@ import java.util.Date;
  */
 public class IdentityCanonicalEvents {
     public static final String ACCOUNT_DELETED_PATH = "/account/delete";
+    public static final String USER_DELETED_PATH = "/user/delete";
 
     @NeedJsonSchema
     public static class AccountDeletedData {
@@ -29,6 +30,37 @@ public class IdentityCanonicalEvents {
         }
 
         public void setInventory(AccountInventory inventory) {
+            this.inventory = inventory;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+    }
+
+    @NeedJsonSchema
+    public static class UserDeletedData {
+        private String userUuid;
+        private UserInventory inventory;
+        private Date date = new Date();
+
+        public String getUserUuid() {
+            return userUuid;
+        }
+
+        public void setUserUuid(String userUuid) {
+            this.userUuid = userUuid;
+        }
+
+        public UserInventory getInventory() {
+            return inventory;
+        }
+
+        public void setInventory(UserInventory inventory) {
             this.inventory = inventory;
         }
 
