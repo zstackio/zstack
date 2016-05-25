68c776452ab09892157847a4fce5e60b54ec2dc6
Mei Lei <meilei007@gmail.com>
2016-05-25 15:17:18 +0800

    Add update kvmhost and sftpbackup storage api
    for https://github.com/zxwing/premium/issues/378#event-666739705
    
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- test/src/test/java/org/zstack/test/Api.java
+++ test/src/test/java/org/zstack/test/Api.java
@@ -3210,6 +3210,7 @@ public class Api implements CloudBusEventListener {
         msg.setUuid(inv.getUuid());
         msg.setUsername(inv.getUsername());
         msg.setPassword(password);
+        msg.setSshport(inv.getSshport());
         ApiSender sender = new ApiSender();
         sender.setTimeout(timeout);
         APIUpdateBackupStorageEvent evt = sender.send(msg, APIUpdateBackupStorageEvent.class);
@@ -3450,6 +3451,7 @@ public class Api implements CloudBusEventListener {
         msg.setUuid(inv.getUuid());
         msg.setUsername(inv.getUsername());
         msg.setPassword(password);
+        msg.setSshport(inv.getSshPort());
         ApiSender sender = new ApiSender();
         sender.setTimeout(timeout);
         APIUpdateHostEvent evt = sender.send(msg, APIUpdateHostEvent.class);
