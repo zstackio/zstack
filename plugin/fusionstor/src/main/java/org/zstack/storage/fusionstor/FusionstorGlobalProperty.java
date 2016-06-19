2e6fb9492e3d3ddb12b3443657c4513105e251e4
tianyang <tianyang@huayunwangji.com>
2016-06-19 22:08:16 +0800

    automatically create stor cluster When add Fusionstor Primary Storage

--- plugin/fusionstor/src/main/java/org/zstack/storage/fusionstor/FusionstorGlobalProperty.java
+++ plugin/fusionstor/src/main/java/org/zstack/storage/fusionstor/FusionstorGlobalProperty.java
@@ -24,4 +24,6 @@ public class FusionstorGlobalProperty {
     public static String PRIMARY_STORAGE_PLAYBOOK_NAME;
     @GlobalProperty(name="Fusionstor.primaryStorage.ansibleModulePath", defaultValue = "ansible/fusionstorp")
     public static String PRIMARY_STORAGE_MODULE_PATH;
+    @GlobalProperty(name="Fusionstor.type", defaultValue = "Fusionstor")
+    public static String FUSIONSTOR_PRIMARY_STORAGE_TYPE;
 }
