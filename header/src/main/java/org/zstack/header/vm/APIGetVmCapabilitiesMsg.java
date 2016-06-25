990fefdcbe8f632c2f61f90026c0c6ce7344aece
Frank Zhang <xing5820@gmail.com>
2016-06-25 22:38:44 +0800

    User needs permission to call APIGetVmCapabilitiesMsg()
    for https://github.com/zxwing/premium/issues/497

--- header/src/main/java/org/zstack/header/vm/APIGetVmCapabilitiesMsg.java
+++ header/src/main/java/org/zstack/header/vm/APIGetVmCapabilitiesMsg.java
@@ -7,7 +7,7 @@ import org.zstack.header.message.APISyncCallMessage;
 /**
  * Created by xing5 on 2016/5/17.
  */
-@Action(category = VmInstanceConstant.ACTION_CATEGORY)
+@Action(category = VmInstanceConstant.ACTION_CATEGORY, names = {"read"})
 public class APIGetVmCapabilitiesMsg extends APISyncCallMessage implements VmInstanceMessage {
     @APIParam(resourceType = VmInstanceVO.class, checkAccount = true, operationTarget = true)
     private String uuid;
