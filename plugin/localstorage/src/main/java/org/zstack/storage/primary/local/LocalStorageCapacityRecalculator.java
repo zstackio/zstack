13a03064be5916c107adc5f3ab9d0b71ccec6ed1
Mei Lei <meilei007@gmail.com>
2016-07-06 16:07:43 +0800

    Fix recaculate primary storage didn't return issue
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageCapacityRecalculator.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageCapacityRecalculator.java
@@ -122,7 +122,7 @@ public class LocalStorageCapacityRecalculator {
                     cap.setTotalCapacity(total);
                     cap.setTotalPhysicalCapacity(tp);
                     cap.setAvailablePhysicalCapacity(pa);
-                    return null;
+                    return cap;
                 }
             });
         }
