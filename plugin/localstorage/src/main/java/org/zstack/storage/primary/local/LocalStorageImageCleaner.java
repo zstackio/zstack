6c41eaa84b08443be30f1e13d3b25fd11286aa44
Frank Zhang <xing5820@gmail.com>
2016-07-22 20:19:32 +0800

    fix image cache cleaner for local storage

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageImageCleaner.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageImageCleaner.java
@@ -1,12 +1,33 @@
 package org.zstack.storage.primary.local;
 
+import org.springframework.transaction.annotation.Transactional;
+import org.zstack.core.cloudbus.CloudBusCallBack;
 import org.zstack.header.managementnode.ManagementNodeReadyExtensionPoint;
+import org.zstack.header.message.MessageReply;
+import org.zstack.header.storage.primary.ImageCacheShadowVO;
+import org.zstack.header.storage.primary.ImageCacheVO;
+import org.zstack.header.storage.primary.PrimaryStorageConstant;
+import org.zstack.header.volume.VolumeType;
+import org.zstack.header.volume.VolumeVO;
 import org.zstack.storage.primary.ImageCacheCleaner;
+import org.zstack.storage.primary.local.LocalStorageKvmBackend.CacheInstallPath;
+import org.zstack.utils.CollectionUtils;
+import org.zstack.utils.Utils;
+import org.zstack.utils.function.Function;
+import org.zstack.utils.logging.CLogger;
+
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xing5 on 2016/7/20.
  */
 public class LocalStorageImageCleaner extends ImageCacheCleaner implements ManagementNodeReadyExtensionPoint {
+    private static final CLogger logger = Utils.getLogger(LocalStorageImageCleaner.class);
+
     @Override
     public void managementNodeReady() {
         startGC();
@@ -15,5 +36,129 @@ public class LocalStorageImageCleaner extends ImageCacheCleaner implements Manag
     @Override
     protected String getPrimaryStorageType() {
         return LocalStorageConstants.LOCAL_STORAGE_TYPE;
+    }
+
+    @Transactional
+    protected List<ImageCacheShadowVO> createShadowImageCacheVOs() {
+        String sql = "select count(*) from VolumeVO vol, PrimaryStorageVO pri where vol.primaryStorageUuid = pri.uuid" +
+                " and vol.type = :volType and vol.rootImageUuid is null and pri.type = :psType";
+        TypedQuery<Long> q = dbf.getEntityManager().createQuery(sql, Long.class);
+        q.setParameter("volType", VolumeType.Root);
+        q.setParameter("psType", getPrimaryStorageType());
+        Long count = q.getSingleResult();
+        if (count != 0) {
+            logger.warn(String.format("found %s volumes on the primary storage[type:%s] has NULL rootImageUuid. Please do following:\n" +
+                    "1. zstack-ctl stop_node\n" +
+                    "2. zstack-ctl start_node -DfixImageCacheUuid=true -DrootVolumeFindMissingImageUuid=true\n" +
+                    "to fix the problem. For the data safety, we won't clean the image cache of the primary storage", count, getPrimaryStorageType()));
+            return null;
+        }
+
+        sql = "select c from ImageCacheVO c, PrimaryStorageVO pri, ImageEO i where i.uuid = c.imageUuid and i.deleted is not null and pri.type = :ptype";
+        TypedQuery<ImageCacheVO> cq = dbf.getEntityManager().createQuery(sql, ImageCacheVO.class);
+        cq.setParameter("ptype", getPrimaryStorageType());
+        List<ImageCacheVO> deleted = cq.getResultList();
+
+        sql = "select c from ImageCacheVO c, PrimaryStorageVO pri where c.imageUuid not in (select img.uuid from ImageVO img) and" +
+                " c.primaryStorageUuid = pri.uuid and pri.type = :psType";
+
+        cq = dbf.getEntityManager().createQuery(sql, ImageCacheVO.class);
+        cq.setParameter("psType", getPrimaryStorageType());
+        deleted.addAll(cq.getResultList());
+
+        if (deleted.isEmpty()) {
+            return null;
+        }
+
+
+        Map<String, List<ImageCacheVO>> refMap = new HashMap<String, List<ImageCacheVO>>();
+        for (ImageCacheVO c : deleted) {
+            CacheInstallPath p = new CacheInstallPath();
+            p.fullPath = c.getInstallUrl();
+            p.disassemble();
+            String hostUuid = p.hostUuid;
+
+            List<ImageCacheVO> refs = refMap.get(hostUuid);
+            if (refs == null) {
+                refs = new ArrayList<ImageCacheVO>();
+                refMap.put(hostUuid, refs);
+            }
+            refs.add(c);
+        }
+
+        List<ImageCacheVO> stale = new ArrayList<ImageCacheVO>();
+        for (Map.Entry<String, List<ImageCacheVO>> e : refMap.entrySet()) {
+            String hostUuid = e.getKey();
+            List<ImageCacheVO> refs = e.getValue();
+            List<Long> cacheIds = CollectionUtils.transformToList(refs, new Function<Long, ImageCacheVO>() {
+                @Override
+                public Long call(ImageCacheVO arg) {
+                    return arg.getId();
+                }
+            });
+
+            sql = "select c from ImageCacheVO c where c.imageUuid not in (select vol.rootImageUuid from VolumeVO vol, LocalStorageResourceRefVO ref" +
+                    " where vol.uuid = ref.resourceUuid and ref.resourceType = :rtype and ref.hostUuid = :huuid) and c.id in (:ids)";
+            cq = dbf.getEntityManager().createQuery(sql, ImageCacheVO.class);
+            cq.setParameter("rtype", VolumeVO.class.getSimpleName());
+            cq.setParameter("huuid", hostUuid);
+            cq.setParameter("ids", cacheIds);
+            stale.addAll(cq.getResultList());
+        }
+
+        if (stale.isEmpty()) {
+            return null;
+        }
+
+        logger.debug(String.format("found %s stale images in cache on the primary storage[type:%s], they are about to be cleaned up",
+                stale.size(), getPrimaryStorageType()));
+
+        for (ImageCacheVO vo : stale) {
+            dbf.getEntityManager().persist(new ImageCacheShadowVO(vo));
+            dbf.getEntityManager().remove(vo);
+        }
+
+        sql = "select s from ImageCacheShadowVO s";
+        TypedQuery<ImageCacheShadowVO> sq = dbf.getEntityManager().createQuery(sql, ImageCacheShadowVO.class);
+        return sq.getResultList();
+    }
+
+    @Override
+    public void cleanup() {
+        List<ImageCacheShadowVO> shadowVOs = createShadowImageCacheVOs();
+        if (shadowVOs == null || shadowVOs.isEmpty()) {
+            return;
+        }
+
+        for (final ImageCacheShadowVO vo : shadowVOs) {
+            if (!destMaker.isManagedByUs(vo.getImageUuid())) {
+                continue;
+            }
+
+            CacheInstallPath p = new CacheInstallPath();
+            p.fullPath = vo.getInstallUrl();
+            p.disassemble();
+
+            LocalStorageDeleteImageCacheOnPrimaryStorageMsg msg = new LocalStorageDeleteImageCacheOnPrimaryStorageMsg();
+            msg.setHostUuid(p.hostUuid);
+            msg.setImageUuid(vo.getImageUuid());
+            msg.setInstallPath(p.installPath);
+            msg.setPrimaryStorageUuid(vo.getPrimaryStorageUuid());
+            bus.makeTargetServiceIdByResourceUuid(msg, PrimaryStorageConstant.SERVICE_ID, vo.getPrimaryStorageUuid());
+            bus.send(msg, new CloudBusCallBack() {
+                @Override
+                public void run(MessageReply reply) {
+                    if (!reply.isSuccess()) {
+                        logger.warn(String.format("failed to delete the stale image cache[%s] on the primary storage[%s], %s," +
+                                "will re-try later", vo.getInstallUrl(), vo.getPrimaryStorageUuid(), reply.getError()));
+                        return;
+                    }
+
+                    logger.debug(String.format("successfully deleted the stale image cache[%s] on the primary storage[%s]",
+                            vo.getInstallUrl(), vo.getPrimaryStorageUuid()));
+                    dbf.remove(vo);
+                }
+            });
+        }
     }
 }
