13c2e3b942e750b9d221f8abb6c49dddf432c0ec
David Lee <live4thee@gmail.com>
2016-07-22 13:36:26 +0800

    Update the method signature of 'uploadBits()'
    Signed-off-by: David Lee <live4thee@gmail.com>

--- plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/BackupStorageKvmUploader.java
+++ plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/BackupStorageKvmUploader.java
@@ -1,10 +1,13 @@
 package org.zstack.storage.primary.smp;
 
-import org.zstack.header.core.Completion;
+import org.zstack.header.core.ReturnValueCompletion;
 
 /**
  * Created by xing5 on 2016/3/27.
  */
 public abstract class BackupStorageKvmUploader {
-    public abstract void uploadBits(String bsPath, String psPath, Completion completion);
+    // When uploading an image from 'psPath' to backup storage, the 'bsPath'
+    // might be allocated by the backup storage and returned by the completion,
+    // instead of being known ahead of time.
+    public abstract void uploadBits(String bsPath, String psPath, ReturnValueCompletion<String> completion);
 }
