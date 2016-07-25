347f139979380250b912b48512c6dbd9fef1e6f9
Mei Lei <meilei007@gmail.com>
2016-07-26 00:23:28 +0800

    Revert "fix missing reply download image format"
    This reverts commit d5819451fc8493c125b38552c8efb836a633ae8d.

--- plugin/sftpBackupStorage/src/main/java/org/zstack/storage/backup/sftp/SftpBackupStorage.java
+++ plugin/sftpBackupStorage/src/main/java/org/zstack/storage/backup/sftp/SftpBackupStorage.java
@@ -81,7 +81,6 @@ public class SftpBackupStorage extends BackupStorageBase {
 
     private class DownloadResult {
         String md5sum;
-        String format;
         long size;
         long actualSize;
     }
@@ -182,7 +181,6 @@ public class SftpBackupStorage extends BackupStorageBase {
                 reply.setSize(res.size);
                 reply.setActualSize(res.actualSize);
                 reply.setMd5sum(res.md5sum);
-                reply.setFormat(res.format);
                 bus.reply(msg, reply);
             }
 
