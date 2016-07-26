b438d1b9817b77b676b438fabc65823655f92e53
miao.DengSheng <xuexuemiao@yeah.net>
2016-07-26 15:43:29 +0800

    add unitTest for detecting Database Cascade Deletion Problem #616

---
+++ test/src/test/java/org/zstack/test/kvm/TestCreateVmOnKvm1FailureAtEnd.java
@@ -0,0 +1,86 @@
+package org.zstack.test.kvm;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.configuration.DiskOfferingInventory;
+import org.zstack.header.configuration.InstanceOfferingInventory;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.image.ImageInventory;
+import org.zstack.header.network.l3.L3NetworkInventory;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.volume.VolumeInventory;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.simulator.storage.backup.sftp.SftpBackupStorageSimulatorConfig;
+import org.zstack.test.*;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.test.storage.backup.sftp.TestSftpBackupStorageDeleteImage2;
+import org.zstack.utils.SizeUtils;
+import org.zstack.utils.Utils;
+import org.zstack.utils.data.SizeUnit;
+import org.zstack.utils.logging.CLogger;
+
+/**
+ * Created by miao on 16-7-26.
+ */
+
+
+/**
+ * 1 create a vm from a data volume template
+ * <p>
+ * confirm the vm failed to create
+ */
+public class TestCreateVmOnKvm1FailureAtEnd {
+    CLogger logger = Utils.getLogger(TestSftpBackupStorageDeleteImage2.class);
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    SftpBackupStorageSimulatorConfig config;
+    KVMSimulatorConfig kconfig;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/kvm/TestCreateVmOnKvmFailureAtEnd.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(SftpBackupStorageSimulatorConfig.class);
+        kconfig = loader.getComponent(KVMSimulatorConfig.class);
+        session = api.loginAsAdmin();
+    }
+
+    @Test
+    public void test() throws ApiSenderException {
+        ImageInventory img = deployer.images.get("TestImage");
+        InstanceOfferingInventory ioinv = deployer.instanceOfferings.get("TestInstanceOffering");
+        L3NetworkInventory l3 = deployer.l3Networks.get("TestL3Network1");
+
+        kconfig.startVmSuccess = false;
+        //kconfig.totalMemory = SizeUnit.GIGABYTE.toByte(7);
+        VmCreator creator = new VmCreator(api);
+        creator.addL3Network(l3.getUuid());
+        creator.name = "vm";
+        creator.imageUuid = img.getUuid();
+        creator.instanceOfferingUuid = ioinv.getUuid();
+        boolean s = false;
+        try {
+            creator.create();
+        } catch (ApiSenderException e) {
+            s = true;
+        }
+
+        Assert.assertTrue(s);
+    }
+
+}
