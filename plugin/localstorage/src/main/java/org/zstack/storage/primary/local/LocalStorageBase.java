zstackio/zstack

commit 3d75e48b2a1a24165bac2b59f6b9f51e2c803487

do reference check before cleaning up image cache on local storage

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageBase.java
@@ -2,7 +2,6 @@ package org.zstack.storage.primary.local;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.zstack.core.cloudbus.CloudBusCallBack;
 import org.zstack.core.componentloader.PluginRegistry;
 import org.zstack.core.db.SimpleQuery;
 import org.zstack.core.db.SimpleQuery.Op;
@@ -28,7 +27,6 @@ import org.zstack.header.host.HostVO_;
 import org.zstack.header.image.ImageVO;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.Message;
-import org.zstack.header.message.MessageReply;
 import org.zstack.header.storage.primary.*;
 import org.zstack.header.storage.primary.VolumeSnapshotCapability.VolumeSnapshotArrangementType;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;
@@ -425,20 +423,18 @@ public class LocalStorageBase extends PrimaryStorageBase {
 
 
     private void handle(final LocalStorageDeleteImageCacheOnPrimaryStorageMsg msg) {
-        LocalStorageDirectlyDeleteBitsMsg dmsg = new LocalStorageDirectlyDeleteBitsMsg();
-        dmsg.setPrimaryStorageUuid(msg.getPrimaryStorageUuid());
-        dmsg.setPath(msg.getInstallPath());
-        dmsg.setHostUuid(msg.getHostUuid());
-        bus.makeTargetServiceIdByResourceUuid(dmsg, PrimaryStorageConstant.SERVICE_ID, self.getUuid());
-        bus.send(dmsg, new CloudBusCallBack(msg) {
+        LocalStorageHypervisorBackend bkd = getHypervisorBackendFactoryByHostUuid(msg.getHostUuid()).getHypervisorBackend(self);
+        bkd.handle(msg, msg.getHostUuid(), new ReturnValueCompletion<DeleteImageCacheOnPrimaryStorageReply>(msg) {
             @Override
-            public void run(MessageReply reply) {
-                DeleteImageCacheOnPrimaryStorageReply r = new DeleteImageCacheOnPrimaryStorageReply();
-                r.setSuccess(reply.isSuccess());
-                if (reply.getError() != null) {
-                    r.setError(reply.getError());
-                }
-                bus.reply(msg, r);
+            public void success(DeleteImageCacheOnPrimaryStorageReply reply) {
+                bus.reply(msg, reply);
+            }
+
+            @Override
+            public void fail(ErrorCode errorCode) {
+                DeleteImageCacheOnPrimaryStorageReply reply = new DeleteImageCacheOnPrimaryStorageReply();
+                reply.setError(errorCode);
+                bus.reply(msg, reply);
             }
         });
     }

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageHypervisorBackend.java
@@ -61,6 +61,8 @@ public abstract class LocalStorageHypervisorBackend extends LocalStorageBase {
 
     abstract void downloadImageToCache(ImageInventory img, String hostUuid, ReturnValueCompletion<String> completion);
 
+    abstract void handle(LocalStorageDeleteImageCacheOnPrimaryStorageMsg msg, String hostUuid, ReturnValueCompletion<DeleteImageCacheOnPrimaryStorageReply> completion);
+
     abstract List<Flow> createMigrateBitsFlow(MigrateBitsStruct struct);
 
     abstract void deleteBits(String path, String hostUuid, Completion completion);

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageImageCleaner.java
@@ -38,7 +38,7 @@ public class LocalStorageImageCleaner extends ImageCacheCleaner implements Manag
     }
 
     @Transactional
-    protected List<ImageCacheShadowVO> createShadowImageCacheVOs(String psUUid) {
+    protected List<ImageCacheShadowVO> createShadowImageCacheVOsForNewDeletedAndOld(String psUUid) {
         List<Long> staleImageCacheIds = getStaleImageCacheIds(psUUid);
         if (staleImageCacheIds == null || staleImageCacheIds.isEmpty()) {
             return null;

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageKvmBackend.java
@@ -1,5 +1,6 @@
 package org.zstack.storage.primary.local;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.compute.vm.ImageBackupStorageSelector;
 import org.zstack.core.cloudbus.CloudBusCallBack;
@@ -539,6 +540,15 @@ public class LocalStorageKvmBackend extends LocalStorageHypervisorBackend {
         public long size;
     }
 
+    public static class GetQCOW2ReferenceCmd extends AgentCommand {
+        public String path;
+        public String searchingDir;
+    }
+
+    public static class GetQCOW2ReferenceRsp extends AgentResponse {
+        List<String> referencePaths;
+    }
+
     public static final String INIT_PATH = "/localstorage/init";
     public static final String GET_PHYSICAL_CAPACITY_PATH = "/localstorage/getphysicalcapacity";
     public static final String CREATE_EMPTY_VOLUME_PATH = "/localstorage/volume/createempty";
@@ -555,6 +565,7 @@ public class LocalStorageKvmBackend extends LocalStorageHypervisorBackend {
     public static final String GET_BACKING_FILE_PATH = "/localstorage/volume/getbackingfile";
     public static final String GET_VOLUME_SIZE = "/localstorage/volume/getsize";
     public static final String GET_BASE_IMAGE_PATH = "/localstorage/volume/getbaseimagepath";
+    public static final String GET_QCOW2_REFERENCE = "/localstorage/getqcow2reference";
 
 
     public LocalStorageKvmBackend(PrimaryStorageVO self) {
@@ -1675,6 +1686,83 @@ public class LocalStorageKvmBackend extends LocalStorageHypervisorBackend {
                 completion.fail(errorCode);
             }
         });
+    }
+
+    @Override
+    void handle(final LocalStorageDeleteImageCacheOnPrimaryStorageMsg msg, String hostUuid, final ReturnValueCompletion<DeleteImageCacheOnPrimaryStorageReply> completion) {
+        FlowChain chain = FlowChainBuilder.newShareFlowChain();
+        chain.setName(String.format("clean-up-image-cache-on-local-storage-%s", self.getUuid()));
+        chain.then(new ShareFlow() {
+            @Override
+            public void setup() {
+                flow(new NoRollbackFlow() {
+                    String __name__ = "ensure-image-is-not-referenced";
+
+                    @Override
+                    public void run(final FlowTrigger trigger, Map data) {
+                        GetQCOW2ReferenceCmd cmd = new GetQCOW2ReferenceCmd();
+                        cmd.searchingDir = self.getUrl();
+                        cmd.path = msg.getInstallPath();
+
+                        new KvmCommandSender(msg.getHostUuid()).send(cmd, GET_QCOW2_REFERENCE, new KvmCommandFailureChecker() {
+                            @Override
+                            public ErrorCode getError(KvmResponseWrapper wrapper) {
+                                GetQCOW2ReferenceRsp rsp = wrapper.getResponse(GetQCOW2ReferenceRsp.class);
+                                return rsp.isSuccess() ? null : errf.stringToOperationError(rsp.getError());
+                            }
+                        }, new ReturnValueCompletion<KvmResponseWrapper>(trigger) {
+                            @Override
+                            public void success(KvmResponseWrapper w) {
+                                GetQCOW2ReferenceRsp rsp = w.getResponse(GetQCOW2ReferenceRsp.class);
+                                if (rsp.referencePaths == null || rsp.referencePaths.isEmpty()) {
+                                    trigger.next();
+                                } else  {
+                                    trigger.fail(errf.stringToInternalError(String.format("[THIS IS A BUG NEEDED TO BE FIXED RIGHT NOW, PLEASE REPORT TO US ASAP] the image cache file[%s] is still referenced by" +
+                                            " below QCOW2 files:\n%s", msg.getInstallPath(), StringUtils.join(rsp.referencePaths, "\n"))));
+                                }
+                            }
+
+                            @Override
+                            public void fail(ErrorCode errorCode) {
+                                trigger.fail(errorCode);
+                            }
+                        });
+                    }
+                });
+
+                flow(new NoRollbackFlow() {
+                    @Override
+                    public void run(final FlowTrigger trigger, Map data) {
+                        deleteBits(msg.getInstallPath(), msg.getHostUuid(), new Completion(trigger) {
+                            @Override
+                            public void success() {
+                                trigger.next();
+                            }
+
+                            @Override
+                            public void fail(ErrorCode errorCode) {
+                                trigger.fail(errorCode);
+                            }
+                        });
+                    }
+                });
+
+                done(new FlowDoneHandler(completion) {
+                    @Override
+                    public void handle(Map data) {
+                        DeleteImageCacheOnPrimaryStorageReply reply = new DeleteImageCacheOnPrimaryStorageReply();
+                        completion.success(reply);
+                    }
+                });
+
+                error(new FlowErrorHandler(completion) {
+                    @Override
+                    public void handle(ErrorCode errCode, Map data) {
+                        completion.fail(errCode);
+                    }
+                });
+            }
+        }).start();
     }
 
     @Override

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSimulator.java
@@ -61,6 +61,17 @@ public class LocalStorageSimulator {
         restf.getRESTTemplate().exchange(callbackUrl, HttpMethod.POST, rreq, String.class);
     }
 
+    @RequestMapping(value=LocalStorageKvmBackend.GET_QCOW2_REFERENCE, method= RequestMethod.POST)
+    public @ResponseBody
+    String getQcow2Reference(HttpEntity<String> entity) {
+        GetQCOW2ReferenceCmd cmd = JSONObjectUtil.toObject(entity.getBody(), GetQCOW2ReferenceCmd.class);
+        GetQCOW2ReferenceRsp rsp = new GetQCOW2ReferenceRsp();
+        config.getQCOW2ReferenceCmds.add(cmd);
+        rsp.referencePaths = config.getQCOW2ReferenceCmdReference;
+        reply(entity, rsp);
+        return null;
+    }
+
     @RequestMapping(value=LocalStorageKvmBackend.GET_BASE_IMAGE_PATH, method= RequestMethod.POST)
     public @ResponseBody
     String getVolumeBaseImagePath(HttpEntity<String> entity) {

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSimulatorConfig.java
@@ -55,4 +55,7 @@ public class LocalStorageSimulatorConfig {
     public Map<String, Long> getVolumeSizeCmdSize = new HashMap<String, Long>();
 
     public Map<String, String> getVolumeBaseImagePaths = new HashMap();
+
+    public List<GetQCOW2ReferenceCmd> getQCOW2ReferenceCmds = new ArrayList<GetQCOW2ReferenceCmd>();
+    public List<String> getQCOW2ReferenceCmdReference = new ArrayList<String>();
 }

storage/src/main/java/org/zstack/storage/primary/ImageCacheCleaner.java
@@ -188,7 +188,7 @@ public abstract class ImageCacheCleaner {
     }
 
     @Transactional
-    protected List<ImageCacheShadowVO> createShadowImageCacheVOs(String psUuid) {
+    protected List<ImageCacheShadowVO> createShadowImageCacheVOsForNewDeletedAndOld(String psUuid) {
         List<Long> staleImageCacheIds = getStaleImageCacheIds(psUuid);
         if (staleImageCacheIds == null || staleImageCacheIds.isEmpty()) {
             return null;
@@ -214,5 +214,18 @@ public abstract class ImageCacheCleaner {
         sql = "select s from ImageCacheShadowVO s";
         TypedQuery<ImageCacheShadowVO> sq = dbf.getEntityManager().createQuery(sql, ImageCacheShadowVO.class);
         return sq.getResultList();
+    }
+
+    @Transactional
+    protected List<ImageCacheShadowVO> createShadowImageCacheVOs(String psUuid) {
+        List<ImageCacheShadowVO> newDeletedAndOld = createShadowImageCacheVOsForNewDeletedAndOld(psUuid);
+        if (newDeletedAndOld == null) {
+            // no new deleted images, let's check if there any old that failed to be deleted last time
+            String sql = "select s from ImageCacheShadowVO s";
+            TypedQuery<ImageCacheShadowVO> sq = dbf.getEntityManager().createQuery(sql, ImageCacheShadowVO.class);
+            return sq.getResultList();
+        } else {
+            return newDeletedAndOld;
+        }
     }
 }

test/src/test/java/org/zstack/test/storage/primary/local/TestLocalStorage53.java
@@ -1,0 +1,109 @@
+package org.zstack.test.storage.primary.local;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.compute.vm.VmGlobalConfig;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
+import org.zstack.core.db.SimpleQuery.Op;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.image.ImageDeletionPolicyManager.ImageDeletionPolicy;
+import org.zstack.header.image.ImageInventory;
+import org.zstack.header.storage.primary.ImageCacheShadowVO;
+import org.zstack.header.storage.primary.ImageCacheVO;
+import org.zstack.header.storage.primary.ImageCacheVO_;
+import org.zstack.header.vm.VmInstanceDeletionPolicyManager.VmInstanceDeletionPolicy;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.image.ImageGlobalConfig;
+import org.zstack.storage.primary.PrimaryStorageGlobalConfig;
+import org.zstack.storage.primary.local.LocalStorageKvmBackend.CacheInstallPath;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig.Capacity;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.utils.data.SizeUnit;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 1. one host, one vm
+ * 2. delete the image the vm created from
+ * 3. delete the vm
+ * 4. set the qcow2 reference
+ *
+ * confirm the image is not garbage collected
+ */
+public class TestLocalStorage53 {
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    LocalStorageSimulatorConfig config;
+    long totalSize = SizeUnit.GIGABYTE.toByte(100);
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/localStorage/TestLocalStorage50.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.addSpringConfig("localStorageSimulator.xml");
+        deployer.addSpringConfig("localStorage.xml");
+        deployer.load();
+
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(LocalStorageSimulatorConfig.class);
+
+        Capacity c = new Capacity();
+        c.total = totalSize;
+        c.avail = totalSize;
+
+        config.capacityMap.put("host1", c);
+
+        deployer.build();
+        api = deployer.getApi();
+        session = api.loginAsAdmin();
+    }
+    
+	@Test
+	public void test() throws ApiSenderException, InterruptedException {
+        ImageGlobalConfig.DELETION_POLICY.updateValue(ImageDeletionPolicy.Direct.toString());
+        ImageInventory image1 = deployer.images.get("TestImage");
+        api.deleteImage(image1.getUuid());
+
+        SimpleQuery<ImageCacheVO> q = dbf.createQuery(ImageCacheVO.class);
+        q.add(ImageCacheVO_.imageUuid, Op.EQ, image1.getUuid());
+        ImageCacheVO c = q.find();
+
+        VmGlobalConfig.VM_DELETION_POLICY.updateValue(VmInstanceDeletionPolicy.Direct.toString());
+        VmInstanceInventory vm1 = deployer.vms.get("TestVm");
+        api.destroyVmInstance(vm1.getUuid());
+
+        CacheInstallPath path = new CacheInstallPath();
+        path.fullPath = c.getInstallUrl();
+        path.disassemble();
+
+        config.getQCOW2ReferenceCmdReference.add("/test/test.qcow2");
+        config.deleteBitsCmds.clear();
+        PrimaryStorageGlobalConfig.IMAGE_CACHE_GARBAGE_COLLECTOR_INTERVAL.updateValue(1);
+        TimeUnit.SECONDS.sleep(3);
+
+        Assert.assertEquals(0, config.deleteBitsCmds.size());
+        // image cache is cleaned
+        c = q.find();
+        Assert.assertNull(c);
+        // the shadow is created
+        ImageCacheShadowVO shadowVO = dbf.listAll(ImageCacheShadowVO.class).get(0);
+        Assert.assertEquals(image1.getUuid(), shadowVO.getImageUuid());
+    }
+}

commit d3bc0b8829c0dae470ad88f8877dd047ff3ac005

Fix build error introduced by commit 3d75e48b2a
Signed-off-by: David Lee <live4thee@gmail.com>

plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageBase.java
@@ -2,6 +2,7 @@ package org.zstack.storage.primary.local;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.zstack.core.cloudbus.CloudBusCallBack;
 import org.zstack.core.componentloader.PluginRegistry;
 import org.zstack.core.db.SimpleQuery;
 import org.zstack.core.db.SimpleQuery.Op;
@@ -27,6 +28,7 @@ import org.zstack.header.host.HostVO_;
 import org.zstack.header.image.ImageVO;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.Message;
+import org.zstack.header.message.MessageReply;
 import org.zstack.header.storage.primary.*;
 import org.zstack.header.storage.primary.VolumeSnapshotCapability.VolumeSnapshotArrangementType;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;