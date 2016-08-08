23361dd2c247dc2db7c14af1fced09649cac9557
miao.DengSheng <xuexuemiao@yeah.net>
2016-08-08 17:35:37 +0800

    APIGetL3NetworkDhcpIpAddressMsg extends APISyncCallMessage

--- plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/APIGetL3NetworkDhcpIpAddressMsg.java
+++ plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/APIGetL3NetworkDhcpIpAddressMsg.java
@@ -2,6 +2,7 @@ package org.zstack.network.service.flat;
 
 import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APISyncCallMessage;
 import org.zstack.header.network.l3.L3NetworkConstant;
 import org.zstack.header.network.l3.L3NetworkMessage;
 
@@ -9,7 +10,7 @@ import org.zstack.header.network.l3.L3NetworkMessage;
  * Created by miao on 16-7-19.
  */
 @Action(category = L3NetworkConstant.ACTION_CATEGORY)
-public class APIGetL3NetworkDhcpIpAddressMsg extends APIMessage implements L3NetworkMessage {
+public class APIGetL3NetworkDhcpIpAddressMsg extends APISyncCallMessage implements L3NetworkMessage {
     private String l3NetworkUuid;
 
     @Override
