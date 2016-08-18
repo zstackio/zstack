6ffefc0a63dd35277317a5a0cadb44c8abf90b7a
miao.DengSheng <xuexuemiao@yeah.net>
2016-08-18 10:31:42 +0800

    [description]failed when length of Image description is over 1024 #736 Data Format Normalization for fields(name,description) of all tables;

--- conf/db/upgrade/V1.7__schema.sql
+++ conf/db/upgrade/V1.7__schema.sql
@@ -9,4 +9,14 @@ CREATE TABLE  `zstack`.`KeystoreVO` (
     PRIMARY KEY  (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE zstack.AccountResourceRefVO ADD UNIQUE INDEX(resourceUuid,resourceType);
+ALTER TABLE `zstack`.`AccountResourceRefVO` ADD UNIQUE INDEX(resourceUuid,resourceType);
+
+ALTER TABLE `zstack`.`ConsoleProxyAgentVO` modify column description varchar(2048) DEFAULT NULL;
+ALTER TABLE `zstack`.`ImageEO` modify column description varchar(2048) DEFAULT NULL COMMENT 'image description';
+ALTER TABLE `zstack`.`InstanceOfferingEO` column description varchar(2048) DEFAULT NULL  COMMENT 'instance offering description';
+ALTER TABLE `zstack`.`DiskOfferingEO` column description varchar(2048) DEFAULT NULL COMMENT 'disk offering description';
+ALTER TABLE `zstack`.`VolumeEO` column description varchar(2048) DEFAULT NULL;
+ALTER TABLE `zstack`.`VipVO` column description varchar(2048) DEFAULT NULL;
+ALTER TABLE `zstack`.`GlobalConfigVO` column description varchar(2048) DEFAULT NULL;
+ALTER TABLE `zstack`.`NetworkServiceProviderVO` column description varchar(2048) DEFAULT NULL;
+ALTER TABLE `zstack`.`SecurityGroupVO` column description varchar(2048) DEFAULT NULL;
