6ad449065acc450ce1c2f14f5651776d9529c26d
David Lee <live4thee@gmail.com>
2016-08-22 15:56:46 +0800

    [cloneVm] in case of instanceOfferingUuid is null.
    for https://github.com/zxwing/premium/issues/775
    
    Signed-off-by: David Lee <live4thee@gmail.com>

--- header/src/main/java/org/zstack/header/vm/CreateVmInstanceMsg.java
+++ header/src/main/java/org/zstack/header/vm/CreateVmInstanceMsg.java
@@ -7,11 +7,14 @@ import java.util.List;
 /**
  * Created by david on 8/4/16.
  */
-public class CreateVmInstanceMsg extends NeedReplyMessage {
+public class CreateVmInstanceMsg extends NeedReplyMessage implements CreateVmInstanceMessage {
     private String accountUuid;
     private String name;
-    private String instanceOfferingUuid;
     private String imageUuid;
+    private String instanceOfferingUuid;
+    private int cpuNum;
+    private long cpuSpeed;
+    private long memorySize;
     private List<String> l3NetworkUuids;
     private String type;
     private String rootDiskOfferingUuid;
@@ -22,89 +25,9 @@ public class CreateVmInstanceMsg extends NeedReplyMessage {
     private String description;
     private String resourceUuid;
     private String defaultL3NetworkUuid;
+    private String allocatorStrategy;
 
-
-    public String getDefaultL3NetworkUuid() {
-        return defaultL3NetworkUuid;
-    }
-
-    public void setDefaultL3NetworkUuid(String defaultL3NetworkUuid) {
-        this.defaultL3NetworkUuid = defaultL3NetworkUuid;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getHostUuid() {
-        return hostUuid;
-    }
-
-    public void setHostUuid(String hostUuid) {
-        this.hostUuid = hostUuid;
-    }
-
-    public String getClusterUuid() {
-        return clusterUuid;
-    }
-
-    public void setClusterUuid(String clusterUuid) {
-        this.clusterUuid = clusterUuid;
-    }
-
-    public String getZoneUuid() {
-        return zoneUuid;
-    }
-
-    public void setZoneUuid(String zoneUuid) {
-        this.zoneUuid = zoneUuid;
-    }
-
-    public List<String> getDataDiskOfferingUuids() {
-        return dataDiskOfferingUuids;
-    }
-
-    public void setDataDiskOfferingUuids(List<String> dataDiskOfferingUuids) {
-        this.dataDiskOfferingUuids = dataDiskOfferingUuids;
-    }
-
-    public String getRootDiskOfferingUuid() {
-        return rootDiskOfferingUuid;
-    }
-
-    public void setRootDiskOfferingUuid(String rootDiskOfferingUuid) {
-        this.rootDiskOfferingUuid = rootDiskOfferingUuid;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public List<String> getL3NetworkUuids() {
-        return l3NetworkUuids;
-    }
-
-    public void setL3NetworkUuids(List<String> l3NetworkUuids) {
-        this.l3NetworkUuids = l3NetworkUuids;
-    }
-
-
-    public String getImageUuid() {
-        return imageUuid;
-    }
-
-    public void setImageUuid(String imageUuid) {
-        this.imageUuid = imageUuid;
-    }
-
+    @Override
     public String getInstanceOfferingUuid() {
         return instanceOfferingUuid;
     }
@@ -113,6 +36,133 @@ public class CreateVmInstanceMsg extends NeedReplyMessage {
         this.instanceOfferingUuid = instanceOfferingUuid;
     }
 
+    @Override
+    public String getDefaultL3NetworkUuid() {
+        return defaultL3NetworkUuid;
+    }
+
+    public void setDefaultL3NetworkUuid(String defaultL3NetworkUuid) {
+        this.defaultL3NetworkUuid = defaultL3NetworkUuid;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String getHostUuid() {
+        return hostUuid;
+    }
+
+    public void setHostUuid(String hostUuid) {
+        this.hostUuid = hostUuid;
+    }
+
+    @Override
+    public String getClusterUuid() {
+        return clusterUuid;
+    }
+
+    public void setClusterUuid(String clusterUuid) {
+        this.clusterUuid = clusterUuid;
+    }
+
+    @Override
+    public String getZoneUuid() {
+        return zoneUuid;
+    }
+
+    public void setZoneUuid(String zoneUuid) {
+        this.zoneUuid = zoneUuid;
+    }
+
+    @Override
+    public List<String> getDataDiskOfferingUuids() {
+        return dataDiskOfferingUuids;
+    }
+
+    public void setDataDiskOfferingUuids(List<String> dataDiskOfferingUuids) {
+        this.dataDiskOfferingUuids = dataDiskOfferingUuids;
+    }
+
+    @Override
+    public String getRootDiskOfferingUuid() {
+        return rootDiskOfferingUuid;
+    }
+
+    public void setRootDiskOfferingUuid(String rootDiskOfferingUuid) {
+        this.rootDiskOfferingUuid = rootDiskOfferingUuid;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public List<String> getL3NetworkUuids() {
+        return l3NetworkUuids;
+    }
+
+    public void setL3NetworkUuids(List<String> l3NetworkUuids) {
+        this.l3NetworkUuids = l3NetworkUuids;
+    }
+
+    @Override
+    public String getImageUuid() {
+        return imageUuid;
+    }
+
+    public void setImageUuid(String imageUuid) {
+        this.imageUuid = imageUuid;
+    }
+
+    @Override
+    public int getCpuNum() {
+        return cpuNum;
+    }
+
+    public void setCpuNum(int cpuNum) {
+        this.cpuNum = cpuNum;
+    }
+
+    @Override
+    public long getCpuSpeed() {
+        return cpuSpeed;
+    }
+
+    public void setCpuSpeed(long cpuSpeed) {
+        this.cpuSpeed = cpuSpeed;
+    }
+
+    @Override
+    public long getMemorySize() {
+        return memorySize;
+    }
+
+    public void setMemorySize(long memorySize) {
+        this.memorySize = memorySize;
+    }
+
+    @Override
+    public String getAllocatorStrategy() {
+        return allocatorStrategy;
+    }
+
+    public void setAllocatorStrategy(String allocatorStrategy) {
+        this.allocatorStrategy = allocatorStrategy;
+    }
+
+    @Override
     public String getName() {
         return name;
     }
@@ -121,6 +171,7 @@ public class CreateVmInstanceMsg extends NeedReplyMessage {
         this.name = name;
     }
 
+    @Override
     public String getAccountUuid() {
         return accountUuid;
     }
@@ -129,31 +180,12 @@ public class CreateVmInstanceMsg extends NeedReplyMessage {
         this.accountUuid = accountUuid;
     }
 
+    @Override
     public String getResourceUuid() {
         return resourceUuid;
     }
 
     public void setResourceUuid(String resourceUuid) {
         this.resourceUuid = resourceUuid;
-    }
-
-    public static CreateVmInstanceMsg valueOf(final APICreateVmInstanceMsg msg) {
-        CreateVmInstanceMsg cmsg = new CreateVmInstanceMsg();
-
-        cmsg.setAccountUuid(msg.getSession().getAccountUuid());
-        cmsg.setName(msg.getName());
-        cmsg.setInstanceOfferingUuid(msg.getInstanceOfferingUuid());
-        cmsg.setImageUuid(msg.getImageUuid());
-        cmsg.setL3NetworkUuids(msg.getL3NetworkUuids());
-        cmsg.setType(msg.getType());
-        cmsg.setRootDiskOfferingUuid(msg.getRootDiskOfferingUuid());
-        cmsg.setDataDiskOfferingUuids(msg.getDataDiskOfferingUuids());
-        cmsg.setZoneUuid(msg.getZoneUuid());
-        cmsg.setClusterUuid(msg.getClusterUuid());
-        cmsg.setHostUuid(msg.getHostUuid());
-        cmsg.setDescription(msg.getDescription());
-        cmsg.setResourceUuid(msg.getResourceUuid());
-        cmsg.setDefaultL3NetworkUuid(msg.getDefaultL3NetworkUuid());
-        return cmsg;
     }
 }
