8ff018ab1dc9b5b4c05a588b412cb01d416d251c
miao.DengSheng <xuexuemiao@yeah.net>
2016-08-23 22:34:48 +0800

    [sg] duplicate rule for iptables should be not allowed #658 For https://github.com/zxwing/premium/issues/658

---
+++ test/src/test/java/org/zstack/test/securitygroup/TestAddSecurityGroupDuplicateRule.java
@@ -0,0 +1,38 @@
+package org.zstack.test.securitygroup;
+
+import junit.framework.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.network.securitygroup.SecurityGroupRuleVO;
+import org.zstack.test.Api;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+public class TestAddSecurityGroupDuplicateRule {
+    static CLogger logger = Utils.getLogger(TestAddSecurityGroupDuplicateRule.class);
+    static Deployer deployer;
+    static Api api;
+    static ComponentLoader loader;
+    static DatabaseFacade dbf;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/securityGroup/TestAddSecurityGroupDuplicateRule.xml", con);
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        dbf = loader.getComponent(DatabaseFacade.class);
+    }
+    @Test
+    public void test() {
+        long count = dbf.count(SecurityGroupRuleVO.class);
+        Assert.assertEquals(2, count);
+    }
+}
