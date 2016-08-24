3109c95cf41f998b7b33ad63c340012f80d7bd11
Mei Lei <meilei007@gmail.com>
2016-08-24 17:46:36 +0800

    Add unit test for volume status changed
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- test/src/test/java/org/zstack/test/core/scheduler/TestSchedulerChangeVolumeStatus.java
+++ test/src/test/java/org/zstack/test/core/scheduler/TestSchedulerChangeVolumeStatus.java
@@ -3,75 +3,60 @@ package org.zstack.test.core.scheduler;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.componentloader.ComponentLoader;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.scheduler.SchedulerFacade;
 import org.zstack.header.configuration.DiskOfferingInventory;
-import org.zstack.header.identity.SessionInventory;
 import org.zstack.header.storage.snapshot.VolumeSnapshotVO;
-import org.zstack.header.vm.VmInstanceInventory;
 import org.zstack.header.volume.VolumeInventory;
 import org.zstack.header.volume.VolumeStatus;
 import org.zstack.header.volume.VolumeType;
-import org.zstack.simulator.kvm.VolumeSnapshotKvmSimulator;
-import org.zstack.test.Api;
-import org.zstack.test.ApiSenderException;
-import org.zstack.test.DBUtil;
-import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.*;
 import org.zstack.test.deployer.Deployer;
+import org.zstack.test.storage.volume.TestCreateDataVolume;
+import org.zstack.utils.Utils;
 import org.zstack.utils.data.SizeUnit;
+import org.zstack.utils.logging.CLogger;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by root on 7/11/16.
+ * Created by root on 8/24/16.
  */
-public class TestSchedulerCreateVolumeSnapshot {
-    ComponentLoader loader;
+public class TestSchedulerChangeVolumeStatus {
+    CLogger logger = Utils.getLogger(TestCreateDataVolume.class);
     Api api;
-    @Autowired
-    SchedulerFacade scheduler;
+    ComponentLoader loader;
     DatabaseFacade dbf;
     CloudBus bus;
     Deployer deployer;
-    SessionInventory session;
-    VolumeSnapshotKvmSimulator snapshotKvmSimulator;
+    @Autowired
+    SchedulerFacade scheduler;
 
     @Before
     public void setUp() throws Exception {
         DBUtil.reDeployDB();
-        WebBeanConstructor con = new WebBeanConstructor();
+        BeanConstructor con = new WebBeanConstructor();
+        /* This loads spring application context */
         deployer = new Deployer("deployerXml/kvm/TestCreateVmOnKvm.xml", con);
         deployer.addSpringConfig("KVMRelated.xml");
         deployer.addSpringConfig("SchedulerFacade.xml");
-        deployer.build();
-        api = deployer.getApi();
-        loader = deployer.getComponentLoader();
-        bus = loader.getComponent(CloudBus.class);
+        loader = con.addXml("PortalForUnitTest.xml").addXml("Simulator.xml").addXml("ZoneManager.xml")
+                .addXml("PrimaryStorageManager.xml").addXml("ConfigurationManager.xml").addXml("VolumeManager.xml")
+                .addXml("AccountManager.xml").build();
         dbf = loader.getComponent(DatabaseFacade.class);
+        bus = loader.getComponent(CloudBus.class);
         scheduler = loader.getComponent(SchedulerFacade.class);
-        snapshotKvmSimulator = loader.getComponent(VolumeSnapshotKvmSimulator.class);
-        session = api.loginAsAdmin();
+        api = new Api();
+        api.startServer();
     }
 
     @Test
-    public void test() throws InterruptedException, ApiSenderException, SchedulerException {
+    public void test() throws ApiSenderException, InterruptedException {
+        api = deployer.getApi();
         Assert.assertNotNull(scheduler);
-        VmInstanceInventory vm = deployer.vms.get("TestVm");
-        String volUuid = vm.getRootVolumeUuid();
-        Integer interval = 3;
-        String type="simple";
-        Long startDate=0L;
-        String schedulerUuid = api.createVolumeSnapshotScheduler(volUuid, session, type, startDate, interval, null);
-        TimeUnit.SECONDS.sleep(2);
-        long record = dbf.count(VolumeSnapshotVO.class);
-        Assert.assertEquals(1,record);
-
-        //test change volume status
-        api.deleteScheduler(schedulerUuid, null);
         DiskOfferingInventory dinv = new DiskOfferingInventory();
         dinv.setDiskSize(SizeUnit.GIGABYTE.toByte(10));
         dinv.setName("Test");
@@ -81,33 +66,30 @@ public class TestSchedulerCreateVolumeSnapshot {
         Assert.assertEquals(VolumeStatus.NotInstantiated.toString(), vinv.getStatus());
         Assert.assertEquals(VolumeType.Data.toString(), vinv.getType());
         Assert.assertFalse(vinv.isAttached());
-        // create scheduler for data volume snapshot
-        String DataVolUuid = vinv.getUuid();
-        api.attachVolumeToVm(vm.getUuid(), DataVolUuid);
-        api.createVolumeSnapshotScheduler(DataVolUuid, null, type, startDate, interval, null);
+        Integer interval = 3;
+        String type="simple";
+        Long startDate=0L;
+        api.createVolumeSnapshotScheduler(vinv.getUuid(), null, type, startDate, interval, null);
         //destroy volume
         TimeUnit.SECONDS.sleep(2);
-        long record1 = dbf.count(VolumeSnapshotVO.class);
-        Assert.assertEquals(2,record1);
+        long record = dbf.count(VolumeSnapshotVO.class);
+        Assert.assertEquals(1,record);
         api.deleteDataVolume(vinv.getUuid());
-        TimeUnit.SECONDS.sleep(4);
+        TimeUnit.SECONDS.sleep(3);
         long record2 = dbf.count(VolumeSnapshotVO.class);
-        Assert.assertEquals(2,record2);
+        Assert.assertEquals(1,record2);
 
         //recover volume
         api.recoverVolume(vinv.getUuid(), null);
-        TimeUnit.SECONDS.sleep(2);
+        TimeUnit.SECONDS.sleep(4);
         long record3 = dbf.count(VolumeSnapshotVO.class);
-        Assert.assertEquals(3,record3);
+        Assert.assertEquals(2,record3);
 
         //expunge volume
-        api.deleteDataVolume(vinv.getUuid());
         api.expungeDataVolume(vinv.getUuid(), null);
         TimeUnit.SECONDS.sleep(4);
         long record4 = dbf.count(VolumeSnapshotVO.class);
-        Assert.assertEquals(1,record4);
-
-
+        Assert.assertEquals(2,record4);
     }
 
 }
