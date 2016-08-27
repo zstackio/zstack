94a25c16ad9ca162b9d596c5aeb6fbe2bf9a203a
miao.DengSheng <xuexuemiao@yeah.net>
2016-08-27 10:29:14 +0800

    Revert "GetCandidateVmNicForSecurityGroup cannot work under normal account #818"
    This reverts commit 302e42dd7685cc403f4e046fc10fe665594fb7ef.

--- header/src/main/java/org/zstack/header/cluster/APIListClusterMsg.java
+++ header/src/main/java/org/zstack/header/cluster/APIListClusterMsg.java
@@ -1,11 +1,9 @@
 package org.zstack.header.cluster;
 
-import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIListMessage;
 
 import java.util.List;
 
-@Action(category = ClusterConstant.CATEGORY, names = {"read"})
 public class APIListClusterMsg extends APIListMessage {
     public APIListClusterMsg() {
     }
--- header/src/main/java/org/zstack/header/cluster/APISearchClusterMsg.java
+++ header/src/main/java/org/zstack/header/cluster/APISearchClusterMsg.java
@@ -1,8 +1,6 @@
 package org.zstack.header.cluster;
 
-import org.zstack.header.identity.Action;
 import org.zstack.header.search.APISearchMessage;
 
-@Action(category = ClusterConstant.CATEGORY, names = {"read"})
 public class APISearchClusterMsg extends APISearchMessage {
 }
