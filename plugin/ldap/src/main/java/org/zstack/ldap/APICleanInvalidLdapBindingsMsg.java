69d18642418e95ccf7fb243eb1c99ec6139da80e
miao.DengSheng <xuexuemiao@yeah.net>
2016-09-22 15:32:12 +0800

    [normal account]wrong quota check #994 For https://github.com/zxwing/premium/issues/994 [ldap]still keep the old bind relation after changed the ldap server #972 For https://github.com/zxwing/premium/issues/972

---
+++ plugin/ldap/src/main/java/org/zstack/ldap/APICleanInvalidLdapBindingsMsg.java
@@ -0,0 +1,9 @@
+package org.zstack.ldap;
+
+import org.zstack.header.message.APIMessage;
+
+/**
+ * Created by miao on 16-9-22.
+ */
+public class APICleanInvalidLdapBindingsMsg extends APIMessage {
+}
