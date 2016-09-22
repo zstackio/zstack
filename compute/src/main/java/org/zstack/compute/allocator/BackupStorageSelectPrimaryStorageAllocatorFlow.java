7132d0cc6269e67f0245339f2522b54f1ebdd9e5
Frank Zhang <xing5820@gmail.com>
2016-09-22 23:04:45 +0800

    [API]GetCandidateZonesClustersHostsForCreatingVm doesn't get the right cluster #893
    for https://github.com/zxwing/premium/issues/893

--- compute/src/main/java/org/zstack/compute/allocator/BackupStorageSelectPrimaryStorageAllocatorFlow.java
+++ compute/src/main/java/org/zstack/compute/allocator/BackupStorageSelectPrimaryStorageAllocatorFlow.java
@@ -12,6 +12,7 @@ import org.zstack.core.errorcode.ErrorFacade;
 import org.zstack.header.allocator.AbstractHostAllocatorFlow;
 import org.zstack.header.errorcode.OperationFailureException;
 import org.zstack.header.host.HostVO;
+import org.zstack.header.storage.backup.BackupStorageType;
 import org.zstack.header.storage.backup.BackupStorageVO;
 import org.zstack.header.storage.backup.BackupStorageVO_;
 
@@ -43,30 +44,61 @@ public class BackupStorageSelectPrimaryStorageAllocatorFlow extends AbstractHost
         q.select(BackupStorageVO_.type);
         q.add(BackupStorageVO_.uuid, Op.EQ, spec.getRequiredBackupStorageUuid());
         String type = q.findValue();
+        BackupStorageType bsType = BackupStorageType.valueOf(type);
 
-        List<String> possiblePrimaryStorageTypes = spec.getBackupStoragePrimaryStorageMetrics().get(type);
-        if (possiblePrimaryStorageTypes == null) {
-            throw new OperationFailureException(errf.stringToInternalError(
-                    String.format("the image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that doesn't have metrics defined" +
-                            " in conf/springConfigXml/HostAllocatorManager.xml. The developer should add its primary storage metrics",
-                            spec.getImage().getUuid(), spec.getRequiredBackupStorageUuid(), type)
-            ));
+        List<String> psUuids = bsType.findRelatedPrimaryStorage(spec.getRequiredBackupStorageUuid());
+        List<HostVO> result;
+        if (psUuids == null) {
+            List<String> possiblePrimaryStorageTypes = spec.getBackupStoragePrimaryStorageMetrics().get(type);
+            if (possiblePrimaryStorageTypes == null) {
+                throw new OperationFailureException(errf.stringToInternalError(
+                        String.format("the image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that doesn't have metrics defined" +
+                                        " in conf/springConfigXml/HostAllocatorManager.xml. The developer should add its primary storage metrics",
+                                spec.getImage().getUuid(), spec.getRequiredBackupStorageUuid(), type)
+                ));
+            }
+
+            result = findHostsByPrimaryStorageTypes(possiblePrimaryStorageTypes);
+            if (result.isEmpty()) {
+                throw new OperationFailureException(errf.stringToOperationError(
+                        String.format("The image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that requires to work with primary storage[types:%s]," +
+                                        "however, no host found suitable to work with those primary storage", spec.getImage().getUuid(),
+                                spec.getRequiredBackupStorageUuid(), type, possiblePrimaryStorageTypes)
+                ));
+            }
+        } else if (!psUuids.isEmpty()) {
+            result = findHostsByPrimaryStorageUuids(psUuids);
+
+            if (result.isEmpty()) {
+                throw new OperationFailureException(errf.stringToOperationError(
+                        String.format("The image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that requires to work with primary storage[uuids:%s]," +
+                                        "however, no host found suitable to work with those primary storage", spec.getImage().getUuid(),
+                                spec.getRequiredBackupStorageUuid(), type, psUuids)
+                ));
+            }
+        } else {
+            throw new OperationFailureException(errf.stringToOperationError(String.format("the backup storage[uuid:%s, type:%s] requires bound" +
+                    " primary storage, however, the primary storage has not been added", spec.getRequiredBackupStorageUuid(), bsType)));
         }
 
-        List<HostVO> result = findHostsByPrimaryStorageUuids(possiblePrimaryStorageTypes);
-        if (result.isEmpty()) {
-            throw new OperationFailureException(errf.stringToOperationError(
-                    String.format("The image[uuid:%s] is on the backup storage[uuid:%s, type:%s] that requires to work with primary storage[types:%s]," +
-                            "however, no host found suitable to work with those primary storage", spec.getImage().getUuid(),
-                            spec.getRequiredBackupStorageUuid(), type, possiblePrimaryStorageTypes)
-            ));
-        }
 
         next(result);
     }
 
     @Transactional(readOnly = true)
-    private List<HostVO> findHostsByPrimaryStorageUuids(List<String> psTypes) {
+    private List<HostVO> findHostsByPrimaryStorageUuids(List<String> psUuids) {
+        String sql = "select h from HostVO h, PrimaryStorageClusterRefVO ref, PrimaryStorageVO ps where ref.clusterUuid = h.clusterUuid" +
+                " and ref.primaryStorageUuid = ps.uuid and ps.uuid in (:psUuids) and h.uuid in (:huuids)";
+
+        TypedQuery<HostVO> q = dbf.getEntityManager().createQuery(sql, HostVO.class);
+        q.setParameter("psUuids", psUuids);
+        q.setParameter("huuids", getHostUuidsFromCandidates());
+
+        return q.getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    private List<HostVO> findHostsByPrimaryStorageTypes(List<String> psTypes) {
         String sql = "select h from HostVO h, PrimaryStorageClusterRefVO ref, PrimaryStorageVO ps where ref.clusterUuid = h.clusterUuid" +
                 " and ref.primaryStorageUuid = ps.uuid and ps.type in (:psTypes) and h.uuid in (:huuids)";
 
--- identity/src/main/java/org/zstack/identity/QuotaChecker.java
+++ identity/src/main/java/org/zstack/identity/QuotaChecker.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Created by frank on 7/30/2015.
