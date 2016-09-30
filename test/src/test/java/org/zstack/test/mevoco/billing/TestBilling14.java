68a1a4bdec35ac627ab882a6a82a83bb81c7c910
Frank Zhang <xing5820@gmail.com>
2016-09-30 14:47:49 +0800

    [billing] after change VM's owner, cost keeps increasing for two accounts #1049
    for https://github.com/zxwing/premium/issues/1049

---
+++ test/src/test/java/org/zstack/test/mevoco/billing/TestBilling14.java
@@ -0,0 +1,125 @@
+package org.zstack.test.mevoco.billing;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.billing.*;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.header.allocator.HostCapacityOverProvisioningManager;
+import org.zstack.header.identity.AccountConstant;
+import org.zstack.header.identity.AccountInventory;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.storage.primary.PrimaryStorageOverProvisioningManager;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.network.service.flat.FlatNetworkServiceSimulatorConfig;
+import org.zstack.simulator.kvm.KVMSimulatorConfig;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig.Capacity;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.test.identity.IdentityCreator;
+import org.zstack.utils.Utils;
+import org.zstack.utils.data.SizeUnit;
+import org.zstack.utils.logging.CLogger;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * after changing owner, the billing of the old owner should not increase
+ */
+public class TestBilling14 {
+    CLogger logger = Utils.getLogger(TestBilling14.class);
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    LocalStorageSimulatorConfig config;
+    FlatNetworkServiceSimulatorConfig fconfig;
+    KVMSimulatorConfig kconfig;
+    PrimaryStorageOverProvisioningManager psRatioMgr;
+    HostCapacityOverProvisioningManager hostRatioMgr;
+    long totalSize = SizeUnit.GIGABYTE.toByte(100);
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/mevoco/TestMevoco.xml", con);
+        deployer.addSpringConfig("mevocoRelated.xml");
+        deployer.addSpringConfig("billing.xml");
+        deployer.load();
+
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(LocalStorageSimulatorConfig.class);
+        fconfig = loader.getComponent(FlatNetworkServiceSimulatorConfig.class);
+        kconfig = loader.getComponent(KVMSimulatorConfig.class);
+        psRatioMgr = loader.getComponent(PrimaryStorageOverProvisioningManager.class);
+        hostRatioMgr = loader.getComponent(HostCapacityOverProvisioningManager.class);
+
+        Capacity c = new Capacity();
+        c.total = totalSize;
+        c.avail = totalSize;
+
+        config.capacityMap.put("host1", c);
+
+        deployer.build();
+        api = deployer.getApi();
+        session = api.loginAsAdmin();
+    }
+
+    @Test
+    public void test() throws ApiSenderException, InterruptedException {
+        BillingGlobalConfig.SAMPLING_INTERVAL.updateValue(1);
+
+        VmInstanceInventory vm = deployer.vms.get("TestVm");
+        api.stopVmInstance(vm.getUuid());
+
+        TimeUnit.SECONDS.sleep(1);
+
+        APICreateResourcePriceMsg msg = new APICreateResourcePriceMsg();
+        msg.setTimeUnit("s");
+        msg.setPrice(100f);
+        msg.setResourceName(BillingConstants.SPENDING_CPU);
+        PriceInventory priceinv = api.createPrice(msg);
+        Assert.assertTrue(dbf.isExist(priceinv.getUuid(), PriceVO.class));
+
+        msg = new APICreateResourcePriceMsg();
+        msg.setTimeUnit("s");
+        msg.setPrice(10f);
+        msg.setResourceName(BillingConstants.SPENDING_MEMORY);
+        msg.setResourceUnit("m");
+        api.createPrice(msg);
+
+        msg = new APICreateResourcePriceMsg();
+        msg.setTimeUnit("s");
+        msg.setPrice(9f);
+        msg.setResourceName(BillingConstants.SPENDING_ROOT_VOLUME);
+        msg.setResourceUnit("m");
+        api.createPrice(msg);
+
+        long startTime = System.currentTimeMillis();
+        long during = 5;
+        api.startVmInstance(vm.getUuid());
+        TimeUnit.SECONDS.sleep(during);
+
+        IdentityCreator identityCreator = new IdentityCreator(api);
+        AccountInventory account = identityCreator.createAccount("root", "password");
+        api.changeResourceOwner(vm.getUuid(), account.getUuid());
+
+        TimeUnit.SECONDS.sleep(2);
+        //APICalculateAccountSpendingReply reply1 = api.calculateSpending(AccountConstant.INITIAL_SYSTEM_ADMIN_UUID, startTime, System.currentTimeMillis(), null);
+        APICalculateAccountSpendingReply reply1 = api.calculateSpending(AccountConstant.INITIAL_SYSTEM_ADMIN_UUID, startTime, null, null);
+        TimeUnit.SECONDS.sleep(3);
+        APICalculateAccountSpendingReply reply2 = api.calculateSpending(AccountConstant.INITIAL_SYSTEM_ADMIN_UUID, startTime, System.currentTimeMillis(), null);
+        Assert.assertEquals(reply1.getTotal(), reply2.getTotal(), 0.2);
+    }
+}
