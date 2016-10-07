a8cac698173af5ebb22fe3f796ac8dc97a394763
David Lee <live4thee@gmail.com>
2016-10-07 10:45:35 +0800

    Added an internal VolumeCreateSnapshotMsg which queues on volume
    for https://github.com/zxwing/premium/issues/1058
    
    Signed-off-by: David Lee <live4thee@gmail.com>

---
+++ header/src/main/java/org/zstack/header/volume/VolumeCreateSnapshotMsg.java
@@ -0,0 +1,57 @@
+package org.zstack.header.volume;
+
+import org.zstack.header.core.ApiTimeout;
+import org.zstack.header.message.NeedReplyMessage;
+
+/**
+ * Created by david on 10/7/16.
+ */
+@ApiTimeout(apiClasses = {APICreateVolumeSnapshotMsg.class})
+public class VolumeCreateSnapshotMsg extends NeedReplyMessage implements VolumeMessage {
+    private String accountUuid;
+    private String resourceUuid;
+    private String name;
+    private String description;
+    private String volumeUuid;
+
+    @Override
+    public String getVolumeUuid() {
+        return volumeUuid;
+    }
+
+    public void setVolumeUuid(String volumeUuid) {
+        this.volumeUuid = volumeUuid;
+    }
+
+    public String getAccountUuid() {
+        return accountUuid;
+    }
+
+    public void setAccountUuid(String accountUuid) {
+        this.accountUuid = accountUuid;
+    }
+
+    public String getResourceUuid() {
+        return resourceUuid;
+    }
+
+    public void setResourceUuid(String resourceUuid) {
+        this.resourceUuid = resourceUuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
