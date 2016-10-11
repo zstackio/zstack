95049eebda6352762775c0b51d699877821ffb61
miao.DengSheng <xuexuemiao@yeah.net>
2016-10-11 19:44:31 +0800

    add ut for snapshot quota check

--- test/src/test/java/org/zstack/test/applianceVm/TestChangeOwnerOfVm.java
+++ test/src/test/java/org/zstack/test/applianceVm/TestChangeOwnerOfVm.java
@@ -1,5 +1,6 @@
 package org.zstack.test.applianceVm;
 
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.zstack.core.cloudbus.CloudBus;
@@ -8,7 +9,10 @@ import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.db.SimpleQuery;
 import org.zstack.header.configuration.DiskOfferingInventory;
 import org.zstack.header.identity.AccountInventory;
+import org.zstack.header.identity.Quota;
+import org.zstack.header.identity.QuotaInventory;
 import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.storage.snapshot.VolumeSnapshotConstant;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;
 import org.zstack.header.vm.VmInstanceInventory;
 import org.zstack.header.vm.VmNicVO;
@@ -20,7 +24,9 @@ import org.zstack.test.DBUtil;
 import org.zstack.test.WebBeanConstructor;
 import org.zstack.test.deployer.Deployer;
 import org.zstack.test.identity.IdentityCreator;
+import org.zstack.utils.CollectionUtils;
 import org.zstack.utils.Utils;
+import org.zstack.utils.function.Function;
 import org.zstack.utils.logging.CLogger;
 
 import java.util.ArrayList;
@@ -56,21 +62,41 @@ public class TestChangeOwnerOfVm {
     @Test
     public void test() throws ApiSenderException {
         IdentityCreator identityCreator = new IdentityCreator(api);
-        identityCreator.useAccount("test");
+        AccountInventory test = identityCreator.useAccount("test");
         VmInstanceInventory vm = deployer.vms.get("TestVm");
         DiskOfferingInventory dinv = deployer.diskOfferings.get("TestDataDiskOffering1");
         VolumeInventory vi = api.createDataVolume("data", dinv.getUuid());
         api.attachVolumeToVm(vm.getUuid(), vi.getUuid());
         // create volume snapshot
         String volUuid = vm.getRootVolumeUuid();
-        VolumeSnapshotInventory inv = api.createSnapshot(volUuid);
+        VolumeSnapshotInventory inv = api.createSnapshot(volUuid, identityCreator.getAccountSession());
 
+        // share resource
         ArrayList<String> sharedResources = new ArrayList<>();
         ArrayList<String> accountList = new ArrayList<>();
         accountList.add(deployer.accounts.get("test2").getUuid());
         deployer.l3Networks.forEach(
                 (s, l3NetworkInventory) -> sharedResources.add(l3NetworkInventory.getUuid()));
         api.shareResource(sharedResources, accountList, false);
+
+        //
+        List<Quota.QuotaUsage> usages = api.getQuotaUsage(test.getUuid(), null);
+        {
+            Quota.QuotaUsage snapshotNum = CollectionUtils.find(usages, new Function<Quota.QuotaUsage, Quota.QuotaUsage>() {
+                @Override
+                public Quota.QuotaUsage call(Quota.QuotaUsage arg) {
+                    return VolumeSnapshotConstant.QUOTA_VOLUME_SNAPSHOT_NUM.equals(arg.getName()) ? arg : null;
+                }
+            });
+            Assert.assertNotNull(snapshotNum);
+
+            QuotaInventory quotaInventory = api.getQuota(
+                    VolumeSnapshotConstant.QUOTA_VOLUME_SNAPSHOT_NUM,
+                    test.getUuid(),
+                    identityCreator.getAccountSession());
+            Assert.assertEquals(quotaInventory.getValue(), snapshotNum.getTotal().longValue());
+            Assert.assertEquals(1, snapshotNum.getUsed().longValue());
+        }
 
         // change owner for the same account
         try {
