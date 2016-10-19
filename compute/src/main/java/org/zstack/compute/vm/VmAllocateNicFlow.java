ffa5edbe536f1397a364ced2f3393e5bfa4e35bb
David Lee <live4thee@gmail.com>
2016-10-19 17:00:42 +0800

    Fix a BAT failure caused by vCenter enabling
    Signed-off-by: David Lee <live4thee@gmail.com>

--- compute/src/main/java/org/zstack/compute/vm/VmAllocateNicFlow.java
+++ compute/src/main/java/org/zstack/compute/vm/VmAllocateNicFlow.java
@@ -86,11 +86,13 @@ public class VmAllocateNicFlow implements Flow {
     public void run(final FlowTrigger trigger, final Map data) {
         final VmInstanceSpec spec = (VmInstanceSpec) data.get(VmInstanceConstant.Params.VmInstanceSpec.toString());
 
+        /*
         if (spec.getVmInventory().getVmNics().isEmpty()) {
             logger.info(String.format("VM [%s] doesn't have any NIC", spec.getVmInventory().getUuid()));
             trigger.next();
             return;
         }
+        */
 
         List<AllocateIpMsg> msgs = new ArrayList<AllocateIpMsg>();
         Map<String, String> vmStaticIps = new StaticIpOperator().getStaticIpbyVmUuid(spec.getVmInventory().getUuid());
