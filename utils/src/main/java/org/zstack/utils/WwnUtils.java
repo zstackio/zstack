5bd7215c92c65928ed15235e4c09c01226161d75
miao.DengSheng <xuexuemiao@yeah.net>
2016-11-03 10:14:50 +0800

    modify wwn match format

--- utils/src/main/java/org/zstack/utils/WwnUtils.java
+++ utils/src/main/java/org/zstack/utils/WwnUtils.java
@@ -10,11 +10,11 @@ public class WwnUtils {
 
     public String getRandomWwn() {
         String uuid = UUID.randomUUID().toString().replace("-", "");
-        String wwn = oui + uuid.substring(0, 12);
+        String wwn = "0x" + oui + uuid.substring(0, 12);
         return wwn;
     }
 
     public boolean isValidWwn(final String wwn) {
-        return wwn.toLowerCase().matches(oui + "[0-9a-f]{12}");
+        return wwn.toLowerCase().matches("(0x)?" + oui + "[0-9a-f]{12}");
     }
 }
