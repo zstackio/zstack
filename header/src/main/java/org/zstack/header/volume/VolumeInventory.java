c042cc3b230d1ff0c5b56ca1bf592e1bfac853c6
miao.DengSheng <xuexuemiao@yeah.net>
2016-11-08 10:21:05 +0800

    Fix issue #1243;format some code;

--- header/src/main/java/org/zstack/header/volume/VolumeInventory.java
+++ header/src/main/java/org/zstack/header/volume/VolumeInventory.java
@@ -15,39 +15,34 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
 /**
- * @inventory
- * inventory for volume
- *
+ * @inventory inventory for volume
  * @category volume
- *
- * @example
- *
+ * @example {
+ * "inventory": {
+ * "uuid": "d4910ee8def241e7afcb55ca1ee685c9",
+ * "name": "d1",
+ * "primaryStorageUuid": "29ea91d6cfb544a392b24f84a43de154",
+ * "vmInstanceUuid": "0135cb45094f4f6fb84375e13d4a1cb8",
+ * "installPath": "/opt/zstack/nfsprimarystorage/prim-29ea91d6cfb544a392b24f84a43de154/dataVolumes/acct-36c27e8ff05c4780bf6d2fa65700f22e/vol-d4910ee8def241e7afcb55ca1ee685c9/d4910ee8def241e7afcb55ca1ee685c9.qcow2",
+ * "type": "Data",
+ * "hypervisorType": "KVM",
+ * "size": 32212254720,
+ * "deviceId": 1,
+ * "state": "Enabled",
+ * "status": "Ready",
+ * "createDate": "May 2, 2014 7:55:15 PM",
+ * "lastOpDate": "May 2, 2014 7:55:15 PM",
+ * "backupStorageRefs": [
  * {
-"inventory": {
-"uuid": "d4910ee8def241e7afcb55ca1ee685c9",
-"name": "d1",
-"primaryStorageUuid": "29ea91d6cfb544a392b24f84a43de154",
-"vmInstanceUuid": "0135cb45094f4f6fb84375e13d4a1cb8",
-"installPath": "/opt/zstack/nfsprimarystorage/prim-29ea91d6cfb544a392b24f84a43de154/dataVolumes/acct-36c27e8ff05c4780bf6d2fa65700f22e/vol-d4910ee8def241e7afcb55ca1ee685c9/d4910ee8def241e7afcb55ca1ee685c9.qcow2",
-"type": "Data",
-"hypervisorType": "KVM",
-"size": 32212254720,
-"deviceId": 1,
-"state": "Enabled",
-"status": "Ready",
-"createDate": "May 2, 2014 7:55:15 PM",
-"lastOpDate": "May 2, 2014 7:55:15 PM",
-"backupStorageRefs": [
-{
-"volumeUuid": "d4910ee8def241e7afcb55ca1ee685c9",
-"backupStorageUuid": "e028f12592fa40359b9af5b8946b1c53",
-"installPath": "nfs:/test1/volumeSnapshots/acct-36c27e8ff05c4780bf6d2fa65700f22e/d4910ee8def241e7afcb55ca1ee685c9/d4910ee8def241e7afcb55ca1ee685c9.qcow2"
-}
-]
-}
-}
- *
+ * "volumeUuid": "d4910ee8def241e7afcb55ca1ee685c9",
+ * "backupStorageUuid": "e028f12592fa40359b9af5b8946b1c53",
+ * "installPath": "nfs:/test1/volumeSnapshots/acct-36c27e8ff05c4780bf6d2fa65700f22e/d4910ee8def241e7afcb55ca1ee685c9/d4910ee8def241e7afcb55ca1ee685c9.qcow2"
+ * }
+ * ]
+ * }
+ * }
  * @since 0.1.0
  */
 @Inventory(mappingVOClass = VolumeVO.class)
@@ -64,55 +59,45 @@ import java.util.List;
         @ExpandedQuery(expandedField = "image", inventoryClass = ImageInventory.class,
                 foreignKey = "rootImageUuid", expandedInventoryKey = "uuid"),
 })
-public class VolumeInventory implements Serializable{
+public class VolumeInventory implements Serializable {
     /**
-     * @desc
-     * volume uuid
+     * @desc volume uuid
      */
     private String uuid;
     /**
-     * @desc
-     * max length of 255 characters
+     * @desc max length of 255 characters
      */
     private String name;
     /**
-     * @desc
-     * max length of 2048 characters
+     * @desc max length of 2048 characters
      * @nullable
      */
     private String description;
     /**
-     * @desc
-     * uuid of primary storage the volume is on. See :ref:`PrimaryStorageInventory`
+     * @desc uuid of primary storage the volume is on. See :ref:`PrimaryStorageInventory`
      */
     private String primaryStorageUuid;
     /**
-     * @desc
-     * uuid of vm the volume is attached to. If null, the volume isn't attached
+     * @desc uuid of vm the volume is attached to. If null, the volume isn't attached
      * @nullable
      */
     private String vmInstanceUuid;
 
     private String diskOfferingUuid;
     /**
-     * @desc
-     * uuid of image from which the volume is created when type = 'Root'. Null when type = 'Data'
+     * @desc uuid of image from which the volume is created when type = 'Root'. Null when type = 'Data'
      * @nullable
      */
     private String rootImageUuid;
     /**
-     * @desc
-     * path the volume locates on primary storage. Depending on primary storage type, this field may have various meanings.
+     * @desc path the volume locates on primary storage. Depending on primary storage type, this field may have various meanings.
      * For example, for nfs primary storage it is filesystem path
      */
     private String installPath;
     /**
-     * @desc
-     * - Root: vm's root volume where operating system was installed
+     * @desc - Root: vm's root volume where operating system was installed
      * - Data: data volume that can be attached/detached to/from vm
-     *
-     * @choices
-     * - Root
+     * @choices - Root
      * - Data
      */
     private String type;
@@ -130,20 +115,18 @@ public class VolumeInventory implements Serializable{
      */
     private Integer deviceId;
     /**
-     * @desc
-     * - Enabled: the volume is ok for operations
+     * @desc - Enabled: the volume is ok for operations
      * - Disabled: the volume can not be attached to vm
-     *
+     * <p>
      * .. note:: state is only meaningful for data volume. Root volume always has state Enabled that can not be changed
      */
     private String state;
     /**
-     * @desc
-     * - Creating: volume is being created from other resource, for example, volume snapshot.
+     * @desc - Creating: volume is being created from other resource, for example, volume snapshot.
      * - Ready: the volume is ok for operations
      * - NotInstantiated: volume is created in database but has not initialized on any primary storage. The volume will be initialized
-     *   at first time it attaches to vm and change to status Ready then.
-     *
+     * at first time it attaches to vm and change to status Ready then.
+     * <p>
      * .. note:: status is only meaningful for data volume. Root volume always has status Ready that can not be changed
      */
     private String status;
@@ -181,30 +164,30 @@ public class VolumeInventory implements Serializable{
 
 
     public static VolumeInventory valueOf(VolumeVO vo) {
-    	VolumeInventory inv = new VolumeInventory();
-    	inv.setRootImageUuid(vo.getRootImageUuid());
-    	inv.setCreateDate(vo.getCreateDate());
-    	inv.setDescription(vo.getDescription());
-    	inv.setInstallPath(vo.getInstallPath());
-    	inv.setName(vo.getName());
-    	inv.setPrimaryStorageUuid(vo.getPrimaryStorageUuid());
-    	inv.setSize(vo.getSize());
+        VolumeInventory inv = new VolumeInventory();
+        inv.setRootImageUuid(vo.getRootImageUuid());
+        inv.setCreateDate(vo.getCreateDate());
+        inv.setDescription(vo.getDescription());
+        inv.setInstallPath(vo.getInstallPath());
+        inv.setName(vo.getName());
+        inv.setPrimaryStorageUuid(vo.getPrimaryStorageUuid());
+        inv.setSize(vo.getSize());
         inv.setActualSize(vo.getActualSize());
-    	inv.setState(vo.getState().toString());
-    	inv.setUuid(vo.getUuid());
-    	inv.setVmInstanceUuid(vo.getVmInstanceUuid());
-    	inv.setType(vo.getType().toString());
+        inv.setState(vo.getState().toString());
+        inv.setUuid(vo.getUuid());
+        inv.setVmInstanceUuid(vo.getVmInstanceUuid());
+        inv.setType(vo.getType().toString());
         inv.setDiskOfferingUuid(vo.getDiskOfferingUuid());
-    	inv.setCreateDate(vo.getCreateDate());
-    	inv.setLastOpDate(vo.getLastOpDate());
-    	inv.setDeviceId(vo.getDeviceId());
+        inv.setCreateDate(vo.getCreateDate());
+        inv.setLastOpDate(vo.getLastOpDate());
+        inv.setDeviceId(vo.getDeviceId());
         inv.setStatus(vo.getStatus().toString());
         inv.setFormat(vo.getFormat());
-    	return inv;
+        return inv;
     }
-    
+
     public static List<VolumeInventory> valueOf(Collection<VolumeVO> vos) {
-        List<VolumeInventory> invs = new ArrayList<VolumeInventory>(vos.size());
+        List<VolumeInventory> invs = new ArrayList<>(vos.size());
         for (VolumeVO vo : vos) {
             invs.add(VolumeInventory.valueOf(vo));
         }
@@ -236,63 +219,79 @@ public class VolumeInventory implements Serializable{
     }
 
     public String getUuid() {
-    	return uuid;
-    }
-	public void setUuid(String uuid) {
-    	this.uuid = uuid;
-    }
-	public String getName() {
-    	return name;
-    }
-	public void setName(String name) {
-    	this.name = name;
-    }
-	public String getDescription() {
-    	return description;
-    }
-	public void setDescription(String description) {
-    	this.description = description;
-    }
-	public String getPrimaryStorageUuid() {
-    	return primaryStorageUuid;
-    }
-	public void setPrimaryStorageUuid(String primaryStorageUuid) {
-    	this.primaryStorageUuid = primaryStorageUuid;
-    }
-	public String getVmInstanceUuid() {
-    	return vmInstanceUuid;
-    }
-	public void setVmInstanceUuid(String vmInstanceUuid) {
-    	this.vmInstanceUuid = vmInstanceUuid;
-    }
-	public String getInstallPath() {
-    	return installPath;
-    }
-	public void setInstallPath(String installPath) {
-    	this.installPath = installPath;
-    }
-	public String getType() {
-    	return type;
-    }
-	public void setType(String volumeType) {
-    	this.type = volumeType;
-    }
-	public long getSize() {
-    	return size;
-    }
-	public void setSize(long size) {
-    	this.size = size;
-    }
-	
-	public boolean isAttached() {
-    	return this.vmInstanceUuid != null;
+        return uuid;
     }
 
-	public String getState() {
-    	return state;
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
     }
-	public void setState(String state) {
-    	this.state = state;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrimaryStorageUuid() {
+        return primaryStorageUuid;
+    }
+
+    public void setPrimaryStorageUuid(String primaryStorageUuid) {
+        this.primaryStorageUuid = primaryStorageUuid;
+    }
+
+    public String getVmInstanceUuid() {
+        return vmInstanceUuid;
+    }
+
+    public void setVmInstanceUuid(String vmInstanceUuid) {
+        this.vmInstanceUuid = vmInstanceUuid;
+    }
+
+    public String getInstallPath() {
+        return installPath;
+    }
+
+    public void setInstallPath(String installPath) {
+        this.installPath = installPath;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String volumeType) {
+        this.type = volumeType;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public boolean isAttached() {
+        return this.vmInstanceUuid != null;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 
     public String getRootImageUuid() {
