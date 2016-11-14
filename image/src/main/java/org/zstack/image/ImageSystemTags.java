0dfada76e0030de3fe0b8c32a35b3f4cf8d683f5
mingjian2049 <mingjian.deng@mevoco.com>
2016-11-14 17:04:11 +0800

    support ImageSystemTags

--- image/src/main/java/org/zstack/image/ImageSystemTags.java
+++ image/src/main/java/org/zstack/image/ImageSystemTags.java
@@ -11,7 +11,8 @@ import org.zstack.tag.PatternedSystemTag;
 public class ImageSystemTags {
     public static String IMAGE_NAME_TOKEN = "imageName";
     public static String PRIMARY_STORAGE_UUID_TOKEN = "primaryStorageUuid";
-    public static String IMAGE_INJECT_QEMUGA_TOKEN = "qemu-ga";
+    public static String IMAGE_INJECT_QEMUGA_TOKEN = "qemuga";
+    public static PatternedSystemTag IMAGE_INJECT_QEMUGA = new PatternedSystemTag(String.format("qemu-ga::{%s}", IMAGE_INJECT_QEMUGA_TOKEN), ImageVO.class);
     public static PatternedSystemTag DELETED_IMAGE_CACHE = new PatternedSystemTag(
             String.format("imageName::{%s}::primaryStorageUuid::{%s}", IMAGE_NAME_TOKEN, PRIMARY_STORAGE_UUID_TOKEN),
             ImageVO.class
--- tag/src/main/java/org/zstack/tag/TagManagerImpl.java
+++ tag/src/main/java/org/zstack/tag/TagManagerImpl.java
@@ -703,6 +703,9 @@ public class TagManagerImpl extends AbstractService implements TagManager,
 
             for (String tag : cmsg.getSystemTags()) {
                 boolean checked = false;
+                for(SystemTag inittag: systemTags) {
+                    logger.debug(String.format("systemTags init contains: %s", inittag.getClass().getName()));
+                }
                 for (SystemTag stag : systemTags) {
                     if (stag.isMatch(tag)) {
                         checked = true;
