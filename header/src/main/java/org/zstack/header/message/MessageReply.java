da2ed5c5b8bc310a61ba438195dd399787bc60f8
miao.DengSheng <xuexuemiao@yeah.net>
2016-11-16 13:35:49 +0800

    adjust ut;

--- header/src/main/java/org/zstack/header/message/MessageReply.java
+++ header/src/main/java/org/zstack/header/message/MessageReply.java
@@ -10,32 +10,32 @@ public class MessageReply extends Message {
      * - true
      * - false
      */
-	private boolean success = true;
+    private boolean success = true;
     /**
      * @desc indicate the reason of api failure. It presents only if success = false
      * @nullable
      */
     @NoJsonSchema
-	private ErrorCode error;
-	
-	public boolean isSuccess() {
-    	return success;
+    private ErrorCode error;
+
+    public boolean isSuccess() {
+        return success;
     }
 
-	public void setSuccess(boolean success) {
-    	this.success = success;
+    public void setSuccess(boolean success) {
+        this.success = success;
     }
 
-	public ErrorCode getError() {
-    	return error;
+    public ErrorCode getError() {
+        return error;
     }
 
-	public void setError(ErrorCode error) {
-    	this.error = error;
-    	this.setSuccess(false);
+    public void setError(ErrorCode error) {
+        this.error = error;
+        this.setSuccess(false);
     }
 
     public <T> T castReply() {
-        return (T)this;
+        return (T) this;
     }
 }
