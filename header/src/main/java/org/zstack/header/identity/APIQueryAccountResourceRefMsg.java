07c685696b3397fa2a943b6cab7d5d107f80c864
Frank.Zhang <xin.zhang@mevoco.com>
2016-11-25 00:14:18 -0500

    fix typo in APIQueryAccountResourceRefMsg

--- header/src/main/java/org/zstack/header/identity/APIQueryAccountResourceRefMsg.java
+++ header/src/main/java/org/zstack/header/identity/APIQueryAccountResourceRefMsg.java
@@ -7,6 +7,6 @@ import org.zstack.header.query.AutoQuery;
  * Created by frank on 2/25/2016.
  */
 @AutoQuery(replyClass = APIQueryAccountResourceRefReply.class, inventoryClass = AccountResourceRefInventory.class)
-@Action(category = AccountConstant.ACTION_CATEGORY, names = {"*.read"})
+@Action(category = AccountConstant.ACTION_CATEGORY, names = {"read"})
 public class APIQueryAccountResourceRefMsg extends APIQueryMessage {
 }
