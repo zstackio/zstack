a8eea11f82b5908df8378c66e87b3dfe64c49051
mingjian2049 <mingjian.deng@mevoco.com>
2016-11-28 17:35:06 +0800

    add TestUnit for SystemTags

--- test/src/test/java/org/zstack/test/mevoco/imageStore/TestImageStoreCommitVolume.java
+++ test/src/test/java/org/zstack/test/mevoco/imageStore/TestImageStoreCommitVolume.java
@@ -7,10 +7,14 @@ import org.zstack.core.MessageCommandRecorder;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.componentloader.ComponentLoader;
 import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
 import org.zstack.header.image.ImageInventory;
 import org.zstack.header.storage.backup.BackupStorageInventory;
 import org.zstack.header.storage.primary.APICommitVolumeAsImageMsg;
+import org.zstack.header.tag.SystemTagVO;
+import org.zstack.header.tag.SystemTagVO_;
 import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.vm.VmInstanceVO;
 import org.zstack.header.volume.VolumeVO;
 import org.zstack.image.ImageGlobalConfig;
 import org.zstack.storage.backup.imagestore.ImageStoreBackupStorageSimulatorConfig;
@@ -19,6 +23,7 @@ import org.zstack.test.ApiSenderException;
 import org.zstack.test.DBUtil;
 import org.zstack.test.WebBeanConstructor;
 import org.zstack.test.deployer.Deployer;
+import org.zstack.test.tag.TestQemuAgentSystemTag;
 import org.zstack.utils.Utils;
 import org.zstack.utils.logging.CLogger;
 
@@ -53,6 +58,8 @@ public class TestImageStoreCommitVolume {
     @Test
     public void test() throws ApiSenderException, InterruptedException {
         VmInstanceInventory vm = deployer.vms.get("TestVm");
+        api.createSystemTag(vm.getUuid(), TestQemuAgentSystemTag.TestSystemTags.qemu.getTagFormat(), VmInstanceVO.class);
+        Assert.assertEquals(TestQemuAgentSystemTag.TestSystemTags.qemu.getTagFormat(), getResourceUuidTag(vm.getUuid()));
 
         // Commit a volume as image
         MessageCommandRecorder.reset();
@@ -71,6 +78,10 @@ public class TestImageStoreCommitVolume {
 
         Assert.assertTrue(inv != null);
 
+        // make sure the commited-image has the same SystemTags as vmInstance
+        String tag = getResourceUuidTag(inv.getUuid());
+        Assert.assertEquals(TestQemuAgentSystemTag.TestSystemTags.qemu.getTagFormat(), tag);
+
         // Expunge the root image of the VM and try to commit again
         final VolumeVO vol = dbf.findByUuid(vm.getRootVolumeUuid(), VolumeVO.class);
         Assert.assertTrue("Root volume is missing!", vol != null);
@@ -86,5 +97,13 @@ public class TestImageStoreCommitVolume {
                 bsUuids
         );
         Assert.assertTrue(inv != null);
+    }
+
+    String getResourceUuidTag(String resourceUuid) {
+        SimpleQuery<SystemTagVO> pq = dbf.createQuery(SystemTagVO.class);
+        pq.select(SystemTagVO_.tag);
+        pq.add(SystemTagVO_.resourceUuid, SimpleQuery.Op.EQ, resourceUuid);
+        String tag = pq.findValue();
+        return tag;
     }
 }
