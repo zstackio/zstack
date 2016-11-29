b018a2c830a133b89cb6568183413bc239ec6bd4
miao.DengSheng <xuexuemiao@yeah.net>
2016-11-29 11:03:46 +0800

    define error codes for re-image ops

--- storage/src/main/java/org/zstack/storage/snapshot/VolumeSnapshotErrors.java
+++ storage/src/main/java/org/zstack/storage/snapshot/VolumeSnapshotErrors.java
@@ -3,11 +3,14 @@ package org.zstack.storage.snapshot;
 /**
  */
 public enum VolumeSnapshotErrors {
-    NOT_IN_CORRECT_STATE(1000);
+    NOT_IN_CORRECT_STATE(1000),
+    RE_IMAGE_VM_NOT_IN_STOPPED_STATE(1001),
+    RE_IMAGE_IMAGE_MEDIA_TYPE_SHOULD_NOT_BE_ISO(1002),
+    RE_IMAGE_CANNOT_FIND_IMAGE_CACHE(1003);
 
     private String code;
 
-    private VolumeSnapshotErrors(int id) {
+    VolumeSnapshotErrors(int id) {
         code = String.format("VOLUME_SNAPSHOT.%s", id);
     }
 
