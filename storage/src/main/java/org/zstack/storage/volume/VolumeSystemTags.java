297cc1f3f8652bc69e4db4363f1ee9b3b98a2460
miao.DengSheng <xuexuemiao@yeah.net>
2016-12-25 21:26:04 +0800

    fix:tagdefinition

--- storage/src/main/java/org/zstack/storage/volume/VolumeSystemTags.java
+++ storage/src/main/java/org/zstack/storage/volume/VolumeSystemTags.java
@@ -1,10 +1,12 @@
 package org.zstack.storage.volume;
 
+import org.zstack.header.tag.TagDefinition;
 import org.zstack.tag.EphemeralSystemTag;
 
 /**
  * Created by miao on 12/23/16.
  */
+@TagDefinition
 public class VolumeSystemTags {
     public static EphemeralSystemTag SHAREABLE = new EphemeralSystemTag("shareable");
 }
