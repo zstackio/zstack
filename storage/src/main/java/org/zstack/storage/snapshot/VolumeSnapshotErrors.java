c5725a9632825a14eadf6a86c8fd3642d7207590
miao.DengSheng <xuexuemiao@yeah.net>
2017-01-05 11:00:21 +0800

    move reimagevminstance to vm module from volume snapshot module

--- storage/src/main/java/org/zstack/storage/snapshot/VolumeSnapshotErrors.java
+++ storage/src/main/java/org/zstack/storage/snapshot/VolumeSnapshotErrors.java
@@ -3,10 +3,7 @@ package org.zstack.storage.snapshot;
 /**
  */
 public enum VolumeSnapshotErrors {
-    NOT_IN_CORRECT_STATE(1000),
-    RE_IMAGE_VM_NOT_IN_STOPPED_STATE(1001),
-    RE_IMAGE_IMAGE_MEDIA_TYPE_SHOULD_NOT_BE_ISO(1002),
-    RE_IMAGE_CANNOT_FIND_IMAGE_CACHE(1003);
+    NOT_IN_CORRECT_STATE(1000);
 
     private String code;
 
