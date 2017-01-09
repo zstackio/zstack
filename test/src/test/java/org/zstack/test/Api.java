b8e13c458a54c05eb80502a1d2c102602132cd59
mingjian2049 <mingjian.deng@mevoco.com>
2017-01-09 13:44:49 +0800

    improved ut for more cases

--- test/src/test/java/org/zstack/test/Api.java
+++ test/src/test/java/org/zstack/test/Api.java
@@ -3975,6 +3975,28 @@ public class Api implements CloudBusEventListener {
         return reply;
     }
 
+    public APISetNicQosEvent setVmNicQos(String vmUuid, String vmNicUuid, long outbound) throws ApiSenderException {
+        APISetNicQosMsg msg = new APISetNicQosMsg();
+        msg.setUuid(vmNicUuid);
+        msg.setOutboundBandwidth(outbound);
+        msg.setSession(adminSession);
+        ApiSender sender = new ApiSender();
+        sender.setTimeout(timeout);
+        APISetNicQosEvent evt = sender.send(msg, APISetNicQosEvent.class);
+        return evt;
+    }
+
+    public APISetNicQosEvent setVmNicQos(String vmUuid, String vmNicUuid, long outbound) throws ApiSenderException {
+        APISetNicQosMsg msg = new APISetNicQosMsg();
+        msg.setUuid(vmNicUuid);
+        msg.setOutboundBandwidth(outbound);
+        msg.setSession(adminSession);
+        ApiSender sender = new ApiSender();
+        sender.setTimeout(timeout);
+        APISetNicQosEvent evt = sender.send(msg, APISetNicQosEvent.class);
+        return evt;
+    }
+
     public APISetNicQosEvent setVmNicQos(String vmUuid, String vmNicUuid, long inbound, long outbound) throws ApiSenderException {
         APISetNicQosMsg msg = new APISetNicQosMsg();
         msg.setUuid(vmNicUuid);
