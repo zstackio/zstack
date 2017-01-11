1bc5821719ae013cbaa04a7468f0f3d74ea89845
Frank.Zhang <xin.zhang@mevoco.com>
2017-01-11 10:04:26 -0500

    [VM]exception happens when query vminstance after delete the vm from LS when host is deleted #1660
    https://github.com/zxwing/premium/issues/1660

---
+++ test/src/test/java/org/zstack/test/storage/primary/local/TestLocalStorage56.java
@@ -0,0 +1,86 @@
+package org.zstack.test.storage.primary.local;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
+import org.zstack.core.db.SimpleQuery.Op;
+import org.zstack.header.host.HostInventory;
+import org.zstack.header.identity.SessionInventory;
+import org.zstack.header.storage.primary.PrimaryStorageInventory;
+import org.zstack.header.vm.VmInstanceConstant.Capability;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.vm.VmInstanceVO;
+import org.zstack.header.volume.VolumeConstant;
+import org.zstack.header.volume.VolumeVO;
+import org.zstack.storage.primary.local.*;
+import org.zstack.storage.primary.local.LocalStorageSimulatorConfig.Capacity;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
+import org.zstack.test.deployer.Deployer;
+import org.zstack.utils.data.SizeUnit;
+
+import java.util.Map;
+
+/**
+ * 1. use local storage
+ * 2. create a vm
+ * <p>
+ * confirm all local storage related commands, VOs are set
+ * <p>
+ * 3. attach another local storage to the cluster
+ * <p>
+ * confirm able to attach
+ */
+public class TestLocalStorage56 {
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+    SessionInventory session;
+    LocalStorageSimulatorConfig config;
+    long totalSize = SizeUnit.GIGABYTE.toByte(100);
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        WebBeanConstructor con = new WebBeanConstructor();
+        deployer = new Deployer("deployerXml/localStorage/TestLocalStorage1.xml", con);
+        deployer.addSpringConfig("KVMRelated.xml");
+        deployer.addSpringConfig("localStorageSimulator.xml");
+        deployer.addSpringConfig("localStorage.xml");
+        deployer.load();
+
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+        config = loader.getComponent(LocalStorageSimulatorConfig.class);
+
+        Capacity c = new Capacity();
+        c.total = totalSize;
+        c.avail = totalSize;
+
+        config.capacityMap.put("host1", c);
+        config.capacityMap.put("host4", c);
+
+        deployer.build();
+        api = deployer.getApi();
+        session = api.loginAsAdmin();
+    }
+
+    @Test
+    public void test() throws ApiSenderException {
+        VmInstanceInventory vm = deployer.vms.get("TestVm");
+        HostInventory host = deployer.hosts.get("host1");
+
+        api.deleteHost(host.getUuid());
+        Assert.assertFalse(dbf.isExist(vm.getUuid(), VmInstanceVO.class));
+        Assert.assertFalse(dbf.isExist(vm.getRootVolumeUuid(), VolumeVO.class));
+    }
+}
