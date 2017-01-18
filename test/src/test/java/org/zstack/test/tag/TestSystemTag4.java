626aec9af0b2a460b2038c396a74c0b8011b1ea1
Camile <camilesing@gmail.com>
2017-01-18 20:07:52 +0800

    try fix test case

--- test/src/test/java/org/zstack/test/tag/TestSystemTag4.java
+++ test/src/test/java/org/zstack/test/tag/TestSystemTag4.java
@@ -2,27 +2,21 @@ package org.zstack.test.tag;
 
 import junit.framework.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.zstack.core.Platform;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.componentloader.ComponentLoader;
 import org.zstack.core.db.DatabaseFacade;
-import org.zstack.header.errorcode.OperationFailureException;
-import org.zstack.header.query.QueryOp;
-import org.zstack.header.tag.SystemTagVO;
 import org.zstack.header.tag.TagDefinition;
-import org.zstack.header.zone.APIQueryZoneMsg;
-import org.zstack.header.zone.APIQueryZoneReply;
-import org.zstack.header.zone.ZoneInventory;
 import org.zstack.header.zone.ZoneVO;
 import org.zstack.tag.SystemTag;
-import org.zstack.tag.TagSubQueryExtension;
 import org.zstack.test.Api;
 import org.zstack.test.ApiSenderException;
 import org.zstack.test.DBUtil;
 import org.zstack.test.deployer.Deployer;
 
-import java.util.List;
 
 /**
  */
@@ -32,6 +26,9 @@ public class TestSystemTag4 {
     ComponentLoader loader;
     CloudBus bus;
     DatabaseFacade dbf;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @TagDefinition
     public static class TestSystemTags {
@@ -52,12 +49,8 @@ public class TestSystemTag4 {
     @Test
     public void test() throws ApiSenderException {
         String randomUUID = Platform.getUuid();
-        try {
-            api.createSystemTag(randomUUID, "big", ZoneVO.class);
-        } catch (ApiSenderException e) {
-            Assert.assertEquals(e instanceof ApiSenderException, true);
-        } catch (Throwable t) {
-            Assert.assertEquals(t.toString(), "Unknown error");
-        }
+        thrown.expect(ApiSenderException.class);
+        api.createSystemTag(randomUUID, "big", ZoneVO.class);
+
     }
 }
