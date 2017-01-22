fb3d0960fd06ee3855f7002631634e304ab67b7a
mingjian2049 <mingjian.deng@mevoco.com>
2017-01-22 12:21:30 +0800

    add some docs

--- header/src/main/java/org/zstack/header/core/progress/APIGetTaskProgressReply.java
+++ header/src/main/java/org/zstack/header/core/progress/APIGetTaskProgressReply.java
@@ -1,11 +1,13 @@
 package org.zstack.header.core.progress;
 
 import org.zstack.header.message.APIReply;
+import org.zstack.header.rest.RestResponse;
 
 import java.sql.Timestamp;
 /**
  * Created by mingjian.deng on 16/12/8.
  */
+@RestResponse(fieldsTo = {"all"})
 public class APIGetTaskProgressReply extends APIReply {private String progress;
     private String resourceUuid;
     private String processType;
@@ -54,8 +56,11 @@ public class APIGetTaskProgressReply extends APIReply {private String progress;
  
     public static APIGetTaskProgressReply __example__() {
         APIGetTaskProgressReply reply = new APIGetTaskProgressReply();
-
-
+        reply.setProcessType("AddImage");
+        reply.setResourceUuid("f16661c706ae403883f5e4cca6f1f3f4");
+        reply.setProgress("99");
+        reply.setLastOpDate(new Timestamp(2017, 1, 22, 11, 26, 30, 5000));
+        reply.setCreateDate(new Timestamp(2017, 1, 22, 11, 26, 30, 5000));
         return reply;
     }
 
