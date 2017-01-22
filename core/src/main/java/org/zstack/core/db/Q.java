4b37995fd52bd6267e25d74d641c2389fd994bd1
miao.DengSheng <xuexuemiao@yeah.net>
2017-01-22 16:51:02 +0800

    check if exists before create resource ref

--- core/src/main/java/org/zstack/core/db/Q.java
+++ core/src/main/java/org/zstack/core/db/Q.java
@@ -48,6 +48,10 @@ public class Q {
         return this;
     }
 
+    public boolean isExists() {
+        return q.count() > 0;
+    }
+
     public Long count() {
         return q.count();
     }
