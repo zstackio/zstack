62067543a91de73a4b032cd2814e2a9db6a07e78
Mei Lei <meilei007@gmail.com>
2017-01-24 15:06:36 +0800

    fix vm state from paused to stop issue
    for https://github.com/zxwing/premium/issues/1954
    
    Signed-off-by: Mei Lei <meilei007@gmail.com>

--- header/src/main/java/org/zstack/header/vm/VmAbnormalLifeCycleStruct.java
+++ header/src/main/java/org/zstack/header/vm/VmAbnormalLifeCycleStruct.java
@@ -12,6 +12,7 @@ public class VmAbnormalLifeCycleStruct {
         VmRunningFromUnknownStateHostNotChanged,
         VmRunningFromUnknownStateHostChanged,
         VmStoppedFromUnknownStateHostNotChanged,
+        VmStoppedFromPausedStateHostNotChanged,
         VmMigrateToAnotherHost,
         VmRunningOnTheHost,
         VmPausedFromRunningStateHostNotChanged,
