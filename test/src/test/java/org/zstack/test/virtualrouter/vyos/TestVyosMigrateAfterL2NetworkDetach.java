7fdb68e8200abafc00ae1ac4cc2181b54148e4dd
Wei Wang <wei.wang@mevoco.com>
2017-02-22 18:16:31 +0800

    Add test case for validate migrate in detach l2network from cluster
    When a l2network detach from a cluster, applicance vm in this cluster
    will try to migrate to another cluster, this patch add testcase to
    validate it.
    
    For zstackio/issues#1800

--- test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosMigrateAfterL2NetworkDetach.java
+++ test/src/test/java/org/zstack/test/virtualrouter/vyos/TestVyosMigrateAfterL2NetworkDetach.java
@@ -9,27 +9,27 @@ import org.zstack.core.db.DatabaseFacade;
 import org.zstack.header.identity.SessionInventory;
 import org.zstack.header.network.l3.L3NetworkInventory;
 import org.zstack.header.vm.VmInstanceInventory;
-import org.zstack.network.service.portforwarding.PortForwardingProtocolType;
-import org.zstack.network.service.portforwarding.PortForwardingRuleInventory;
-import org.zstack.network.service.vip.VipInventory;
 import org.zstack.network.service.virtualrouter.VirtualRouterVmVO;
-import org.zstack.network.service.virtualrouter.portforwarding.PortForwardingRuleTO;
 import org.zstack.simulator.appliancevm.ApplianceVmSimulatorConfig;
 import org.zstack.simulator.virtualrouter.VirtualRouterSimulatorConfig;
-import org.zstack.test.*;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.WebBeanConstructor;
 import org.zstack.test.deployer.Deployer;
-import org.zstack.test.virtualrouter.PortForwardingRuleTestValidator;
-import org.zstack.test.virtualrouter.VipTestValidator;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
 
 /**
  * @author WeiW
  * @condition use vyos provider
  * <p>
  * 1. detach a l2network from a cluster
- * 2. check the router delete for no other cluster
+ * 2. check the router migrate to another cluster
  * @test confirm port forwarding rules on vm are correct
  */
-public class TestVyosAfterL2NetworkDetach {
+public class TestVyosMigrateAfterL2NetworkDetach {
+    private static final CLogger logger = Utils.getLogger(TestVyosMigrateAfterL2NetworkDetach.class);
     Deployer deployer;
     Api api;
     ComponentLoader loader;
@@ -43,7 +43,7 @@ public class TestVyosAfterL2NetworkDetach {
     public void setUp() throws Exception {
         DBUtil.reDeployDB();
         WebBeanConstructor con = new WebBeanConstructor();
-        deployer = new Deployer("deployerXml/virtualRouter/TestVyosAfterL2NetworkDetach.xml", con);
+        deployer = new Deployer("deployerXml/virtualRouter/TestVyosMigrateAfterL2NetworkDetach.xml", con);
         deployer.addSpringConfig("VirtualRouter.xml");
         deployer.addSpringConfig("VirtualRouterSimulator.xml");
         deployer.addSpringConfig("KVMRelated.xml");
@@ -62,15 +62,14 @@ public class TestVyosAfterL2NetworkDetach {
 
     @Test
     public void test() throws ApiSenderException {
-        L3NetworkInventory publicNw = deployer.l3Networks.get("PublicNetwork");
         String l2Uuid = deployer.l2Networks.get("TestL2Network").getUuid();
-        String clusterUuid = deployer.clusters.get("Cluster1").getUuid();
-        VmInstanceInventory vm = deployer.vms.get("TestVm");
         VirtualRouterVmVO vr = dbf.listAll(VirtualRouterVmVO.class).get(0);
-        Assert.assertNotNull(vr);
+        String preClusterUuid = vr.getClusterUuid();
 
-        api.detachL2NetworkFromCluster(l2Uuid, clusterUuid);
+        api.detachL2NetworkFromCluster(l2Uuid, preClusterUuid);
+        vr = dbf.listAll(VirtualRouterVmVO.class).get(0);
+        String afterClusterUuid = vr.getClusterUuid();
 
-        Assert.assertTrue(dbf.listAll(VirtualRouterVmVO.class).isEmpty());
+        Assert.assertNotSame(preClusterUuid, afterClusterUuid);
     }
 }
