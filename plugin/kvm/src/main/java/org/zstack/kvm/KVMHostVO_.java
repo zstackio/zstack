d6b8d5ebdb947f3cd2a59a24aa6bf1ae47c43891
hhjuliet <280157545@qq.com>
2017-02-22 12:50:29 +0800

    change all APIGetQgaEnable to APIGetQga

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHostVO_.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHostVO_.java
@@ -10,6 +10,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(KVMHostVO.class)
 public class KVMHostVO_ extends HostVO_ {
     public static volatile SingularAttribute<KVMHostVO, String> username;
+    //cannot get password by using this method,because password is encrypted
     public static volatile SingularAttribute<KVMHostVO, String> password;
     public static volatile SingularAttribute<KVMHostVO, Integer> port;
 }
