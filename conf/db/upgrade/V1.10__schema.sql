47732048bcc302abbf1e6686267d555196535ba7
David Lee <live4thee@gmail.com>
2017-02-24 19:07:01 +0800

    Enhance the test code for issue 2289 and fix trigger
    for https://github.com/zstackio/issues/issues/2289
    
    Signed-off-by: David Lee <live4thee@gmail.com>

--- conf/db/upgrade/V1.10__schema.sql
+++ conf/db/upgrade/V1.10__schema.sql
@@ -29,10 +29,10 @@ DELIMITER $$
 CREATE TRIGGER trigger_cleanup_for_VmInstanceEO_hard_delete AFTER DELETE ON `VmInstanceEO`
 FOR EACH ROW
     BEGIN
-            DELETE FROM `AccountResourceRefVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceEO';
-            DELETE FROM `SystemTagVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceEO';
-            DELETE FROM `UserTagVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceEO';
-            DELETE FROM ImageEO WHERE `deleted` IS NOT NULL AND `uuid` NOT IN (SELECT imageUuid FROM VmInstanceVO WHERE imageUuid IS NOT NULL);
+            DELETE FROM `AccountResourceRefVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceVO';
+            DELETE FROM `SystemTagVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceVO';
+            DELETE FROM `UserTagVO` WHERE `resourceUuid` = OLD.`uuid` AND `resourceType` = 'VmInstanceVO';
+            DELETE FROM ImageEO WHERE `deleted` IS NOT NULL AND `uuid` NOT IN (SELECT imageUuid FROM VmInstanceEO WHERE imageUuid IS NOT NULL);
     END $$
 DELIMITER ;
 
