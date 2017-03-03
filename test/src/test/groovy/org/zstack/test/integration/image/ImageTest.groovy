4ed5622df91a981f06fb20379523bbf1db767a55
Frank.Zhang <xin.zhang@mevoco.com>
2017-03-03 09:50:33 -0500

    add ImageTest

---
+++ test/src/test/groovy/org/zstack/test/integration/image/ImageTest.groovy
@@ -0,0 +1,37 @@
+package org.zstack.test.integration.image
+
+import org.zstack.testlib.SpringSpec
+import org.zstack.testlib.Test
+
+/**
+ * Created by xing5 on 2017/3/3.
+ */
+class ImageTest extends Test {
+    SpringSpec springSpec = makeSpring {
+        localStorage()
+        nfsPrimaryStorage()
+        sftpBackupStorage()
+        smp()
+        ceph()
+        virtualRouter()
+        vyos()
+        kvm()
+        securityGroup()
+    }
+
+    @Override
+    void setup() {
+        useSpring(springSpec)
+    }
+
+    @Override
+    void environment() {
+    }
+
+    @Override
+    void test() {
+        runSubCases([
+
+        ])
+    }
+}
