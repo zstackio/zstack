cd2e736239aae49e2c5c2374a21fe19a4dd6b6ff
David Lee <live4thee@gmail.com>
2017-03-05 21:49:18 +0800

    Enhance the leastVmPreferredHostAllocator
    for https://github.com/zstackio/issues/issues/829
    
    Signed-off-by: David Lee <live4thee@gmail.com>

---
+++ test/src/test/groovy/org/zstack/test/integration/kvm/hostallocator/AllocatorTest.groovy
@@ -0,0 +1,32 @@
+package org.zstack.test.integration.kvm.hostallocator
+
+import org.zstack.testlib.SpringSpec
+import org.zstack.testlib.Test
+
+/**
+ * Created by david on 3/6/17.
+ */
+class AllocatorTest extends Test {
+    static SpringSpec springSpec = makeSpring {
+        localStorage()
+        sftpBackupStorage()
+        kvm()
+        securityGroup()
+    }
+
+    @Override
+    void setup() {
+        useSpring(springSpec)
+    }
+
+    @Override
+    void environment() {
+    }
+
+    @Override
+    void test() {
+        runSubCases([
+                new LeastVmPreferredAllocatorCase()
+        ])
+    }
+}
