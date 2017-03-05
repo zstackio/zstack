5fbc504928c7fad16f6aa3351af7d0a751da79d3
David Lee <live4thee@gmail.com>
2017-03-05 21:49:18 +0800

    Enhance the leastVmPreferredHostAllocator
    for https://github.com/zstackio/issues/issues/829
    
    Signed-off-by: David Lee <live4thee@gmail.com>

--- compute/src/main/java/org/zstack/compute/allocator/LeastVmPreferredAllocatorFlow.java
+++ compute/src/main/java/org/zstack/compute/allocator/LeastVmPreferredAllocatorFlow.java
@@ -7,19 +7,15 @@ import org.springframework.transaction.annotation.Transactional;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.header.allocator.AbstractHostAllocatorFlow;
 import org.zstack.header.host.HostVO;
-import org.zstack.utils.CollectionDSL;
-import org.zstack.utils.CollectionUtils;
 import org.zstack.utils.Utils;
-import org.zstack.utils.function.Function;
 import org.zstack.utils.logging.CLogger;
 
 import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
+ * This flow returns a list of host candidates sorted by the number of their VMs.
  */
 @Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
 public class LeastVmPreferredAllocatorFlow extends AbstractHostAllocatorFlow {
@@ -27,21 +23,15 @@ public class LeastVmPreferredAllocatorFlow extends AbstractHostAllocatorFlow {
     @Autowired
     private DatabaseFacade dbf;
 
-    class VmNumHost {
-        long vmNum;
-        String hostUuid;
-    }
-
     @Transactional(readOnly = true)
     private List<Tuple> findLeastVmHost(List<String> huuids) {
-        String sql = "select count(vm), host.uuid" +
+        String sql = "select count(vm) as cnt, host.uuid" +
                 " from VmInstanceVO vm, HostVO host" +
                 " where vm.hostUuid = host.uuid" +
                 " and host.uuid in (:huuids)" +
-                " group by host.uuid";
+                " group by host.uuid order by cnt";
         TypedQuery<Tuple> q = dbf.getEntityManager().createQuery(sql, Tuple.class);
         q.setParameter("huuids", huuids);
-        logger.debug(huuids.stream().toString());
         return q.getResultList();
     }
 
@@ -49,13 +39,13 @@ public class LeastVmPreferredAllocatorFlow extends AbstractHostAllocatorFlow {
     public void allocate() {
         throwExceptionIfIAmTheFirstFlow();
 
-        List<String> huuids = getHostUuidsFromCandidates();
-        List<Tuple> tuples = findLeastVmHost(huuids);
-
         if (spec.isListAllHosts()) {
             next(candidates);
             return;
         }
+
+        List<String> huuids = getHostUuidsFromCandidates();
+        List<Tuple> tuples = findLeastVmHost(huuids);
 
         // no VM running on any candidate host(s)
         if (tuples.isEmpty()) {
@@ -63,46 +53,33 @@ public class LeastVmPreferredAllocatorFlow extends AbstractHostAllocatorFlow {
             return;
         }
 
+        List<HostVO> sorted = new ArrayList<>(candidates.size());
 
-        //
-        Map<String, VmNumHost> mp = new HashMap<>();
+        Map<String, HostVO> dict = new HashMap<>();
+        for (HostVO hvo: candidates) {
+            dict.put(hvo.getUuid(), hvo);
+        }
+
+        if (huuids.size() > tuples.size()) {
+            HashSet<String> hostsWithVMs = new HashSet<>();
+            for (Tuple t : tuples) {
+                String hostUuid = t.get(1, String.class);
+                hostsWithVMs.add(hostUuid);
+            }
+
+            for (String huuid : huuids) {
+                if (!hostsWithVMs.contains(huuid)) {
+                    sorted.add(dict.get(huuid));
+                }
+            }
+        }
+
+        // Note: the query result is ordered.
         for (Tuple t : tuples) {
-            long num = t.get(0, Long.class);
             String hostUuid = t.get(1, String.class);
-            VmNumHost vh = new VmNumHost();
-            vh.vmNum = num;
-            vh.hostUuid = hostUuid;
-            mp.put(hostUuid, vh);
+            sorted.add(dict.get(hostUuid));
         }
 
-        // for host not having vm running, put vm number to zero
-        for (String huuid : huuids) {
-            VmNumHost vh = mp.get(huuid);
-            if (vh == null) {
-                vh = new VmNumHost();
-                vh.hostUuid = huuid;
-                vh.vmNum = 0;
-                mp.put(huuid, vh);
-            }
-        }
-
-        long max = Integer.MAX_VALUE;
-        String huuid = null;
-        for (VmNumHost vh : mp.values()) {
-            if (vh.vmNum < max) {
-                huuid = vh.hostUuid;
-                max = vh.vmNum;
-            }
-        }
-
-        final String finalHuuid = huuid;
-        HostVO target = CollectionUtils.find(candidates, new Function<HostVO, HostVO>() {
-            @Override
-            public HostVO call(HostVO arg) {
-                return arg.getUuid().equals(finalHuuid) ? arg : null;
-            }
-        });
-
-        next(CollectionDSL.list(target));
+        next(sorted);
     }
 }
---
+++ test/src/test/groovy/org/zstack/test/integration/kvm/hostallocator/LeastVmPreferredAllocatorCase.groovy
@@ -0,0 +1,78 @@
+package org.zstack.test.integration.kvm.hostallocator
+
+import org.zstack.kvm.KVMGlobalConfig
+import org.zstack.testlib.*
+/**
+ * Created by david on 3/6/17.
+ */
+class LeastVmPreferredAllocatorCase extends SubCase {
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(AllocatorTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = Env.fourVmThreeHostEnv()
+    }
+
+    @Override
+    void test() {
+        env.create {
+            testCreateMulitpleVMs(8)
+        }
+    }
+
+    void testCreateMulitpleVMs(Long num) {
+        KVMGlobalConfig.RESERVED_MEMORY_CAPACITY.updateValue("2G")
+
+        // We have three hosts, each has 8 CPU and 10G memory.
+        // The reserved memory for each host is 1GB.
+        //   host1 has 4 VMs  (instance offering: 2 CPU, 2GB)
+        //   host2 has no VM
+        //   host3 has no VM
+        // We can still create 8 VMs in all, on host2 and host3, with the instance offering.
+        def thisImageUuid = (env.specByName("image1") as ImageSpec).inventory.uuid
+        def instOffering = (env.specByName("instanceOffering") as InstanceOfferingSpec).inventory.uuid
+        def l3uuid = (env.specByName("pubL3") as L3NetworkSpec).inventory.uuid
+
+        def threads = []
+        1.upto(num, {
+            def vmName = "VM-${it}"
+            def thread = Thread.start {
+                createVmInstance {
+                    name = vmName
+                    instanceOfferingUuid = instOffering
+                    imageUuid = thisImageUuid
+                    l3NetworkUuids = [l3uuid]
+                }
+            }
+
+            threads.add(thread)
+        })
+
+        threads.each{it.join()}
+
+        // We shall not be able to create another VM instance now
+        try {
+            createVmInstance {
+                name = "VMM"
+                instanceOfferingUuid = instOffering
+                imageUuid = thisImageUuid
+                l3NetworkUuids = [l3uuid]
+            }
+        } catch (AssertionError ignored) {
+            // Upon API failure, we have an assertion error in ApiHelper.groovy
+            return
+        }
+
+        assert false
+    }
+}
