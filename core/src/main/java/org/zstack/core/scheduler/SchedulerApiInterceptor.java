695d945c23f177b41abc5e44f27e439448e9f80d
AlanJager <873863981@qq.com>
2017-03-07 15:16:01 +0800

    Cast data type from Integer to long to avoid passive value Add test case to reproduce job scheduler
    update test

--- core/src/main/java/org/zstack/core/scheduler/SchedulerApiInterceptor.java
+++ core/src/main/java/org/zstack/core/scheduler/SchedulerApiInterceptor.java
@@ -127,6 +127,18 @@ public class SchedulerApiInterceptor implements ApiMessageInterceptor {
                         String.format("repeatCount must be positive integer")
                 ));
             }
+            
+            if (msg.getInterval() * msg.getRepeatCount() * 1000 < 0) {
+                throw new ApiMessageInterceptionException(errf.instantiateErrorCode(SysErrors.INVALID_ARGUMENT_ERROR,
+                        String.format("schedule job time out of range")
+                ));
+            }
+
+            if ((msg.getStartTime() + msg.getInterval() * msg.getRepeatCount() * 1000) > 2147454847) {
+                throw new ApiMessageInterceptionException(errf.instantiateErrorCode(SysErrors.INVALID_ARGUMENT_ERROR,
+                        String.format("stop time out of range")
+                ));
+            }
         }
 
         if (msg.getType().equals("cron")) {
