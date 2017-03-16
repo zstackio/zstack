70e05647a35d31ff40b64830d32fc6b29cd58f3f
Camile <camilesing@gmail.com>
2017-03-16 10:56:21 +0800

    delect old case

--- test/src/test/groovy/org/zstack/test/integration/kvm/vm/VmConsoleCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/kvm/vm/VmConsoleCase.groovy
@@ -5,6 +5,7 @@ import org.zstack.header.vm.VmInstanceState
 import org.zstack.header.vm.VmInstanceVO
 import org.zstack.kvm.KVMAgentCommands
 import org.zstack.kvm.KVMConstant
+import org.zstack.sdk.GetVmConsolePasswordAction
 import org.zstack.sdk.SetVmConsolePasswordAction
 import org.zstack.sdk.VmInstanceInventory
 import org.zstack.test.integration.kvm.Env
@@ -17,6 +18,8 @@ import org.zstack.utils.gson.JSONObjectUtil
 
 /**
  * Created by Camile on 2017/3/13.
+ *
+ * base on BAT test : TestVmConsolePasswordOnKvm
  */
 class VmConsoleCase extends SubCase {
     EnvSpec env
@@ -38,25 +41,33 @@ class VmConsoleCase extends SubCase {
     @Override
     void test() {
         env.create {
-            testSetConsolePwd()
+            testSetConsolePasswordSuccess()
+            testSetConsolePasswordFailure()
         }
     }
 
-
-    void testSetConsolePwd() {
+    void testSetConsolePasswordSuccess() {
         VmSpec spec = env.specByName("vm")
         def action = new SetVmConsolePasswordAction()
         action.uuid = spec.inventory.uuid
         action.consolePassword = "password2"
-        action.sessionId = Test.currentEnvSpec.session.uuid
+        action.sessionId = adminSession()
         SetVmConsolePasswordAction.Result res = action.call()
         assert res.error != null
 
+    }
+
+    void testSetConsolePasswordFailure() {
         VmInstanceInventory inv = setVmConsolePassword {
             uuid = spec.inventory.uuid
             consolePassword = "123456789"
-            sessionId = Test.currentEnvSpec.session.uuid
+            sessionId = adminSession()
         }
+        def action  = new GetVmConsolePasswordAction()
+        action.uuid = spec.inventory.uuid
+        action.sessionId = adminSession()
+        GetVmConsolePasswordAction.Result res = action.call()
+        assert res.value.getPassword() == "123456789"
 
     }
 
