40d660e646489ac332fbf802f5623a7510c55e6c
Frank.Zhang <xin.zhang@mevoco.com>
2017-03-17 18:16:05 -0400

    working on notification

--- header/src/main/java/org/zstack/header/image/APIDeleteImageMsg.java
+++ header/src/main/java/org/zstack/header/image/APIDeleteImageMsg.java
@@ -3,7 +3,10 @@ package org.zstack.header.image;
 import org.springframework.http.HttpMethod;
 import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIDeleteMessage;
+import org.zstack.header.message.APIEvent;
+import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.APIParam;
+import org.zstack.header.notification.ApiNotification;
 import org.zstack.header.rest.RestRequest;
 import org.zstack.header.storage.backup.BackupStorageVO;
 
@@ -60,6 +63,18 @@ public class APIDeleteImageMsg extends APIDeleteMessage implements ImageMessage 
         msg.setUuid(uuid());
 
         return msg;
+    }
+
+    public ApiNotification __notification__() {
+        APIMessage that = this;
+
+        return new ApiNotification() {
+            @Override
+            public void after(APIEvent evt) {
+                ntfy("Deleting").resource(uuid, ImageVO.class.getSimpleName())
+                        .messageAndEvent(that, evt).done();
+            }
+        };
     }
 
 }
--- header/src/main/java/org/zstack/header/image/APIExpungeImageMsg.java
+++ header/src/main/java/org/zstack/header/image/APIExpungeImageMsg.java
@@ -2,8 +2,10 @@ package org.zstack.header.image;
 
 import org.springframework.http.HttpMethod;
 import org.zstack.header.identity.Action;
+import org.zstack.header.message.APIEvent;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.APIParam;
+import org.zstack.header.notification.ApiNotification;
 import org.zstack.header.rest.RestRequest;
 import org.zstack.header.storage.backup.BackupStorageVO;
 
@@ -53,4 +55,15 @@ public class APIExpungeImageMsg extends APIMessage implements ImageMessage {
         return msg;
     }
 
+    public ApiNotification __notification__() {
+        APIMessage that = this;
+
+        return new ApiNotification() {
+            @Override
+            public void after(APIEvent evt) {
+                ntfy("Expunged").resource(imageUuid, ImageVO.class.getSimpleName())
+                        .messageAndEvent(that, evt).done();
+            }
+        };
+    }
 }
