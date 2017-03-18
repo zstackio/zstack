7253cde48af6bc1c774c518b94ce30f4047ecf2f
aias_for_win10 <aiassing@outlook.com>
2017-03-18 21:42:41 +0800

    fix bug 2270

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageFactory.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageFactory.java
@@ -436,6 +436,7 @@ public class LocalStorageFactory implements PrimaryStorageFactory, Component,
                     DeleteVolumeMsg msg = new DeleteVolumeMsg();
                     msg.setUuid(uuid);
                     msg.setDetachBeforeDeleting(true);
+                    msg.setDeletionPolicy(VolumeDeletionPolicyManager.VolumeDeletionPolicy.DBOnly.toString());
                     bus.makeTargetServiceIdByResourceUuid(msg, VolumeConstant.SERVICE_ID, uuid);
                     return msg;
                 }
