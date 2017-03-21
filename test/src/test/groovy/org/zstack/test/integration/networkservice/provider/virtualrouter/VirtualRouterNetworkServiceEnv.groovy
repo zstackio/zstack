386783997a214af9ebf5ee176a415995493c2d48
David Lee <live4thee@gmail.com>
2017-03-21 18:53:13 +0800

    Add a case to check VR creation process
    for https://github.com/zstackio/issues/issues/2567
    
    Signed-off-by: David Lee <live4thee@gmail.com>

--- test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/VirtualRouterNetworkServiceEnv.groovy
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/VirtualRouterNetworkServiceEnv.groovy
@@ -2,10 +2,8 @@ package org.zstack.test.integration.networkservice.provider.virtualrouter
 
 import org.zstack.header.network.service.NetworkServiceType
 import org.zstack.network.service.eip.EipConstant
-import org.zstack.network.service.flat.FlatNetworkServiceConstant
 import org.zstack.network.service.lb.LoadBalancerConstants
 import org.zstack.network.service.portforwarding.PortForwardingConstant
-import org.zstack.network.service.userdata.UserdataConstant
 import org.zstack.network.service.virtualrouter.vyos.VyosConstants
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.Test
@@ -124,6 +122,141 @@ class VirtualRouterNetworkServiceEnv {
                 useImage("image")
                 useL3Networks("l3")
                 useInstanceOffering("instanceOffering")
+            }
+        }
+    }
+
+    static EnvSpec ForHostsVyosOnEipEnv() {
+        Test.makeEnv {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.MEGABYTE.toByte(512)
+                cpu = 5
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+
+                image {
+                    name = "vr-image"
+                    url  = "http://zstack.org/download/vr.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm-1"
+                        managementIp = "127.0.0.1"
+                        username = "root"
+                        password = "password"
+                        totalCpu = 2
+                        totalMem = SizeUnit.GIGABYTE.toByte(4)
+                    }
+
+                    kvm {
+                        name = "kvm-2"
+                        managementIp = "127.0.0.2"
+                        username = "root"
+                        password = "password"
+                        totalCpu = 2
+                        totalMem = SizeUnit.GIGABYTE.toByte(4)
+                    }
+
+                    kvm {
+                        name = "kvm-3"
+                        managementIp = "127.0.0.3"
+                        username = "root"
+                        password = "password"
+                        totalCpu = 2
+                        totalMem = SizeUnit.GIGABYTE.toByte(4)
+                    }
+
+                    kvm {
+                        name = "kvm-4"
+                        managementIp = "127.0.0.4"
+                        username = "root"
+                        password = "password"
+                        totalCpu = 2
+                        totalMem = SizeUnit.GIGABYTE.toByte(4)
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        service {
+                            provider = VyosConstants.VYOS_ROUTER_PROVIDER_TYPE
+                            types = [NetworkServiceType.DHCP.toString(),
+                                     NetworkServiceType.DNS.toString(),
+                                     NetworkServiceType.SNAT.toString(),
+                                     PortForwardingConstant.PORTFORWARDING_NETWORK_SERVICE_TYPE,
+                                     LoadBalancerConstants.LB_NETWORK_SERVICE_TYPE_STRING,
+                                     EipConstant.EIP_NETWORK_SERVICE_TYPE]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                    l3Network {
+                        name = "pubL3"
+
+                        ip {
+                            startIp = "11.168.100.10"
+                            endIp = "11.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "11.168.100.1"
+                        }
+                    }
+                }
+
+                attachBackupStorage("sftp")
+
+                eip {
+                    name = "eip"
+                    useVip("pubL3")
+                }
+
+                virtualRouterOffering {
+                    name = "vr"
+                    memory = SizeUnit.MEGABYTE.toByte(512)
+                    cpu = 2
+                    useManagementL3Network("pubL3")
+                    usePublicL3Network("pubL3")
+                    useImage("vr-image")
+                }
             }
         }
     }
