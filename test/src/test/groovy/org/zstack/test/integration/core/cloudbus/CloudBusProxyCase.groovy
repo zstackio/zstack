191d61a67ff1375707fffc10dff49729aa404f0f
HeathHose <l96dou@outlook.com>
2017-03-22 20:04:35 +0800

    [Unittest] migrate TestCloudBusProxy
    https://github.com/zstackio/issues/issues/2375

---
+++ test/src/test/groovy/org/zstack/test/integration/core/cloudbus/CloudBusProxyCase.groovy
@@ -0,0 +1,89 @@
+package org.zstack.test.integration.core.cloudbus
+
+import groovy.transform.TypeChecked
+import junit.framework.Assert
+import org.zstack.core.cloudbus.CloudBus
+import org.zstack.core.cloudbus.CloudBusCallBack
+import org.zstack.header.message.MessageReply
+import org.zstack.header.message.NeedReplyMessage
+import org.zstack.test.aop.CloudBusAopProxy
+import org.zstack.test.core.cloudbus.FakeNeedReplyMessage
+import org.zstack.testlib.SubCase
+import org.zstack.utils.Utils
+import org.zstack.utils.logging.CLogger
+
+/**
+ * Created by heathhose on 17-3-22.
+ */
+class CloudBusProxyCase extends SubCase{
+
+    CloudBus bus
+    CloudBusAopProxy aop
+    boolean isSuccess = false
+
+    @Override
+    void setup() {
+    }
+
+    @Override
+    void environment() {
+    }
+
+    @Override
+    void test() {
+        bus = bean(CloudBus.class)
+        aop = bean(CloudBusAopProxy.class)
+
+        testCloudBusProxy()
+    }
+
+    void testCloudBusProxy(){
+        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.FAIL)
+
+        List<FakeNeedReplyMessage> msgs = new ArrayList<FakeNeedReplyMessage>(2)
+        FakeNeedReplyMessage msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        msgs.add(msg)
+        msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        msgs.add(msg)
+        bus.send(msgs)
+
+        msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        bus.send(msg, new CloudBusCallBack(null) {
+            @Override
+            public void run(MessageReply reply) {
+                if (!reply.isSuccess()) {
+                    isSuccess = true
+                }
+            }
+        })
+
+        msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        MessageReply reply = bus.call(msg)
+        Assert.assertFalse(reply.isSuccess())
+        Assert.assertTrue(isSuccess)
+
+        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.TIMEOUT)
+        msgs.clear()
+        msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        msg.setTimeout(1)
+        msgs.add(msg)
+        msg = new FakeNeedReplyMessage()
+        msg.setServiceId("A fake service id not needed")
+        msg.setTimeout(1)
+        msgs.add(msg)
+        List<MessageReply> replies = bus.call(msgs)
+        for (MessageReply r : replies) {
+            Assert.assertFalse(r.isSuccess())
+        }
+    }
+
+    @Override
+    void clean() {
+    }
+
+}
