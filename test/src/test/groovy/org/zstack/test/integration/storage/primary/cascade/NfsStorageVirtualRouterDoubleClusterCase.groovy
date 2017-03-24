a948ebd16851284aba6a97ff7bcc72278e4b2896
HeathHose <l96dou@outlook.com>
2017-03-24 18:30:47 +0800

    [Unittest] migrate TestCascadeDeletion33/31/29/27

---
+++ test/src/test/groovy/org/zstack/test/integration/storage/primary/cascade/NfsStorageVirtualRouterDoubleClusterCase.groovy
@@ -0,0 +1,208 @@
+package org.zstack.test.integration.storage.primary.cascade
+
+import groovy.transform.TypeChecked
+import junit.framework.Assert
+import org.zstack.appliancevm.ApplianceVmVO
+import org.zstack.core.db.DatabaseFacade
+import org.zstack.header.network.service.NetworkServiceType
+import org.zstack.header.vm.VmInstanceState
+import org.zstack.header.vm.VmInstanceVO
+import org.zstack.network.securitygroup.SecurityGroupConstant
+import org.zstack.network.service.eip.EipConstant
+import org.zstack.network.service.lb.LoadBalancerConstants
+import org.zstack.network.service.portforwarding.PortForwardingConstant
+import org.zstack.network.service.virtualrouter.VirtualRouterConstant
+import org.zstack.network.service.virtualrouter.vyos.VyosConstants
+import org.zstack.sdk.ClusterInventory
+import org.zstack.sdk.PrimaryStorageInventory
+import org.zstack.storage.primary.nfs.NfsPrimaryStorage
+import org.zstack.test.integration.networkservice.provider.NetworkServiceProviderTest
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.utils.data.SizeUnit
+
+/**
+ * Created by heathhose on 17-3-24.
+ */
+class NfsStorageVirtualRouterDoubleClusterCase extends SubCase{
+    def DOC = """
+use:
+1. have two clusters
+2. create a vm with virtual router
+3. detach primary storage to another cluster that vr is not running
+4. detach primary storage from cluster vr is running
+5. confirm vr is stopped
+"""
+    EnvSpec env
+    DatabaseFacade dbf
+    @Override
+    void setup() {
+        useSpring(StorageTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env{
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+
+            diskOffering {
+                name = "diskOffering"
+                diskSize = SizeUnit.GIGABYTE.toByte(20)
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image1"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+
+                image {
+                    name = "vr"
+                    url  = "http://zstack.org/download/vr.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster1"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm1"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("nfs")
+                    attachL2Network("l2")
+                }
+
+                cluster {
+                    name = "cluster2"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm2"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("nfs")
+                    attachL2Network("l2")
+                }
+
+                nfsPrimaryStorage {
+                    name = "nfs"
+                    url = "localhost:/nfs"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        service {
+                            provider = VirtualRouterConstant.PROVIDER_TYPE
+                            types = [NetworkServiceType.DHCP.toString(), NetworkServiceType.DNS.toString()]
+                        }
+
+                        service {
+                            provider = SecurityGroupConstant.SECURITY_GROUP_PROVIDER_TYPE
+                            types = [SecurityGroupConstant.SECURITY_GROUP_NETWORK_SERVICE_TYPE]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                    l3Network {
+                        name = "pubL3"
+
+                        ip {
+                            startIp = "12.16.10.10"
+                            endIp = "12.16.10.100"
+                            netmask = "255.255.255.0"
+                            gateway = "12.16.10.1"
+                        }
+                    }
+                }
+
+                virtualRouterOffering {
+                    name = "vr"
+                    memory = SizeUnit.MEGABYTE.toByte(512)
+                    cpu = 2
+                    useManagementL3Network("pubL3")
+                    usePublicL3Network("pubL3")
+                    useImage("vr")
+                }
+
+                attachBackupStorage("sftp")
+            }
+
+            vm {
+                name = "vm"
+                useInstanceOffering("instanceOffering")
+                useImage("image1")
+                useL3Networks("l3")
+                useRootDiskOffering("diskOffering")
+                useHost("kvm1")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        dbf = bean(DatabaseFacade.class)
+        env.create{
+            DetachPrimaryStorageAndCheckVRState()
+        }
+    }
+
+    void DetachPrimaryStorageAndCheckVRState(){
+        ClusterInventory cluster1 = env.inventoryByName("cluster1")
+        ClusterInventory cluster2 = env.inventoryByName("cluster2")
+        PrimaryStorageInventory nfs = env.inventoryByName("nfs")
+        ApplianceVmVO vr = dbf.listAll(ApplianceVmVO.class).get(0);
+
+        String lastClusterUuid = vr.getClusterUuid();
+        String targetClusterUuid = cluster1.getUuid().equals(lastClusterUuid) ? cluster2.getUuid() : cluster1.getUuid();
+
+        detachPrimaryStorageFromCluster {
+            primaryStorageUuid = nfs.uuid
+            clusterUuid = lastClusterUuid
+        }
+        detachPrimaryStorageFromCluster {
+            primaryStorageUuid = nfs.uuid
+            clusterUuid = targetClusterUuid
+        }
+
+        long count = dbf.count(VmInstanceVO.class)
+        Assert.assertEquals(1, count)
+    }
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+}
