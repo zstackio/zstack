8565ef107de64aa76f5b50f423e5fcbbd27640e3
HeathHose <l96dou@outlook.com>
2017-03-24 19:22:41 +0800

    [Unittest] migrate TestCascadeDeletion33/31/29/27

---
+++ test/src/test/groovy/org/zstack/test/integration/network/l2network/cascade/L2NetworkVirtualRouterLocalStorageCase.groovy
@@ -0,0 +1,70 @@
+package org.zstack.test.integration.network.l2network.cascade
+
+import junit.framework.Assert
+import org.zstack.appliancevm.ApplianceVmVO
+import org.zstack.core.db.DatabaseFacade
+import org.zstack.header.vm.VmInstanceState
+import org.zstack.header.vm.VmInstanceVO
+import org.zstack.sdk.ClusterInventory
+import org.zstack.sdk.L2NetworkInventory
+import org.zstack.test.integration.networkservice.provider.NetworkServiceProviderTest
+import org.zstack.test.integration.networkservice.provider.virtualrouter.VirtualRouterNetworkServiceEnv
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+
+/**
+ * Created by heathhose on 17-3-24.
+ */
+class L2NetworkVirtualRouterLocalStorageCase extends SubCase{
+    def DOC = """
+use:
+1. have two clusters
+2. create a vm with virtual router
+3. make vr nowhere to migrate(in deploy configuration)
+4. detach l2 which vr has l3 on
+5. confirm vr is stopped
+"""
+    EnvSpec env
+    DatabaseFacade dbf
+
+    @Override
+    void setup() {
+        useSpring(NetworkServiceProviderTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = VirtualRouterNetworkServiceEnv.oneVmOneHostVyosOnEipEnv()
+    }
+
+    @Override
+    void test() {
+        dbf = bean(DatabaseFacade.class)
+        env.create {
+            detachL2AndCheckVRState()
+        }
+    }
+
+    void detachL2AndCheckVRState(){
+        L2NetworkInventory l2i = env.inventoryByName("l2")
+        ClusterInventory cluster = env.inventoryByName("cluster")
+
+        ApplianceVmVO vr = dbf.listAll(ApplianceVmVO.class).get(0)
+        Assert.assertEquals(VmInstanceState.Running, vr.getState()) 
+        long count = dbf.count(VmInstanceVO.class) 
+        Assert.assertEquals(2, count) 
+
+        detachL2NetworkFromCluster {
+            l2NetworkUuid = l2i.uuid
+            clusterUuid = cluster.uuid
+        }
+
+        count = dbf.count(VmInstanceVO.class) 
+        Assert.assertEquals(1, count) 
+    }
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+}
