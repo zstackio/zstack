998cb314f550a5c98ba45dff127aa21fceb429af
Camile <camilesing@gmail.com>
2017-03-24 15:32:23 +0800

    fix bug 2411

--- test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/eip/VirtualRouterOperationCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/eip/VirtualRouterOperationCase.groovy
@@ -1,8 +1,9 @@
 package org.zstack.test.integration.networkservice.provider.virtualrouter.eip
 
+import org.zstack.appliancevm.ApplianceVmVO
+import org.zstack.core.db.DatabaseFacade
 import org.springframework.http.HttpEntity
-import org.zstack.appliancevm.ApplianceVmInventory
-import org.zstack.appliancevm.ApplianceVmSpec
+import org.zstack.sdk.ApplianceVmInventory
 import org.zstack.core.db.Q
 import org.zstack.network.service.virtualrouter.VirtualRouterCommands
 import org.zstack.network.service.virtualrouter.VirtualRouterConstant
@@ -11,10 +12,8 @@ import org.zstack.network.service.virtualrouter.VirtualRouterVmVO_
 import org.zstack.sdk.EipInventory
 import org.zstack.sdk.L3NetworkInventory
 import org.zstack.sdk.VmInstanceInventory
-import org.zstack.storage.backup.sftp.SftpBackupStorageCommands
 import org.zstack.test.integration.networkservice.provider.NetworkServiceProviderTest
 import org.zstack.test.integration.networkservice.provider.virtualrouter.VirtualRouterNetworkServiceEnv
-import org.zstack.testlib.BackupStorageSpec
 import org.zstack.testlib.EipSpec
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.L3NetworkSpec
@@ -22,17 +21,28 @@ import org.zstack.testlib.SubCase
 import org.zstack.testlib.VmSpec
 import org.zstack.utils.gson.JSONObjectUtil
 
-import java.util.concurrent.TimeUnit
-
 /**
- * Created by Camile on 2017/3/22.
+ * Created by Camile on 2017/3/24.
  */
+
 class VirtualRouterOperationCase extends SubCase {
     EnvSpec env
 
     VmInstanceInventory vm
     L3NetworkInventory publicL3
     EipInventory eip
+
+    @Override
+    void test() {
+        env.create {
+            vm = (env.specByName("vm") as VmSpec).inventory
+            eip = (env.specByName("eip") as EipSpec).inventory
+            publicL3 = (env.specByName("pubL3") as L3NetworkSpec).inventory
+
+            testVirtualRouterReconnect()
+            testUpdateGlobalConfigAndPingVirtualRouter()
+        }
+    }
 
     @Override
     void clean() {
@@ -49,16 +59,15 @@ class VirtualRouterOperationCase extends SubCase {
         env = VirtualRouterNetworkServiceEnv.oneVmOneHostVyosOnEipEnv()
     }
 
-    @Override
-    void test() {
-        env.create {
-            vm = (env.specByName("vm") as VmSpec).inventory
-            eip = (env.specByName("eip") as EipSpec).inventory
-            publicL3 = (env.specByName("pubL3") as L3NetworkSpec).inventory
-
-            testUpdateGlobalConfigAndPingVirtualRouter()
+    void testVirtualRouterReconnect() {
+        DatabaseFacade dbf = bean(DatabaseFacade.class)
+        ApplianceVmVO vr = dbf.listAll(ApplianceVmVO.class).get(0)
+        ApplianceVmInventory inv = reconnectVirtualRouter {
+            vmInstanceUuid = vr.uuid
+            sessionId = adminSession()
         }
     }
+
 
     void testUpdateGlobalConfigAndPingVirtualRouter() {
         assert (Q.New(VirtualRouterVmVO.class).select(VirtualRouterVmVO_.uuid).listValues().get(0)) != null
