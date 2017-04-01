3e0756603bb43a291229bbf7b203a3f1150b4f90
Camile <camilesing@gmail.com>
2017-04-01 15:17:43 +0800

    add new case for 2410

--- test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/VirtualRouterNetworkServiceEnv.groovy
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/virtualrouter/VirtualRouterNetworkServiceEnv.groovy
@@ -14,7 +14,7 @@ import org.zstack.utils.data.SizeUnit
  */
 class VirtualRouterNetworkServiceEnv {
     static EnvSpec oneVmOneHostVyosOnEipEnv() {
-        Test.makeEnv {
+        return Test.makeEnv {
             instanceOffering {
                 name = "instanceOffering"
                 memory = SizeUnit.GIGABYTE.toByte(8)
@@ -30,12 +30,12 @@ class VirtualRouterNetworkServiceEnv {
 
                 image {
                     name = "image"
-                    url  = "http://zstack.org/download/test.qcow2"
+                    url = "http://zstack.org/download/test.qcow2"
                 }
 
                 image {
                     name = "vr"
-                    url  = "http://zstack.org/download/vr.qcow2"
+                    url = "http://zstack.org/download/vr.qcow2"
                 }
             }
 
@@ -127,7 +127,7 @@ class VirtualRouterNetworkServiceEnv {
     }
 
     static EnvSpec ForHostsVyosOnEipEnv() {
-        Test.makeEnv {
+        return Test.makeEnv {
             instanceOffering {
                 name = "instanceOffering"
                 memory = SizeUnit.MEGABYTE.toByte(512)
@@ -143,12 +143,12 @@ class VirtualRouterNetworkServiceEnv {
 
                 image {
                     name = "image"
-                    url  = "http://zstack.org/download/test.qcow2"
+                    url = "http://zstack.org/download/test.qcow2"
                 }
 
                 image {
                     name = "vr-image"
-                    url  = "http://zstack.org/download/vr.qcow2"
+                    url = "http://zstack.org/download/vr.qcow2"
                 }
             }
 
@@ -257,6 +257,158 @@ class VirtualRouterNetworkServiceEnv {
                     usePublicL3Network("pubL3")
                     useImage("vr-image")
                 }
+            }
+        }
+    }
+
+    static EnvSpec twoVmOneHostThreePortForwardingVyosOnEipEnv() {
+        return Test.makeEnv {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image"
+                    url = "http://zstack.org/download/test.qcow2"
+                }
+
+                image {
+                    name = "vr"
+                    url = "http://zstack.org/download/vr.qcow2"
+                }
+            }
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2")
+                    attachL2Network("L2-2")
+                }
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "PublicNetwork"
+                        ip {
+                            startIp = "192.168.1.10"
+                            endIp = "192.168.1.100"
+                            gateway = "192.168.1.1"
+                            netmask = "255.255.255.0"
+                        }
+                    }
+
+                    l3Network {
+                        name = "GuestNetwork"
+                        service {
+                            provider = VyosConstants.VYOS_ROUTER_PROVIDER_TYPE
+                            types = [NetworkServiceType.SNAT.toString(),
+                                     PortForwardingConstant.PORTFORWARDING_NETWORK_SERVICE_TYPE]
+                        }
+                        ip {
+                            startIp = "10.10.2.100"
+                            endIp = "10.20.2.200"
+                            gateway = "10.10.2.1"
+                            netmask = "255.0.0.0"
+                        }
+                    }
+                }
+                l2NoVlanNetwork {
+                    name = "L2-2"
+                    physicalInterface = "eth1"
+
+                    l3Network {
+                        name = "GuestNetwork2"
+                        ip {
+                            startIp = "10.10.2.100"
+                            endIp = "10.20.2.200"
+                            gateway = "10.10.2.1"
+                            netmask = "255.0.0.0"
+                        }
+                        service {
+                            provider = VyosConstants.VYOS_ROUTER_PROVIDER_TYPE
+                            types = [NetworkServiceType.SNAT.toString(),
+                                     PortForwardingConstant.PORTFORWARDING_NETWORK_SERVICE_TYPE]
+                        }
+                    }
+                }
+                attachBackupStorage("sftp")
+
+                portForwarding {
+                    name = "pfRule1"
+                    allowedCidr = "77.10.3.1/24"
+                    privatePortStart = 22
+                    privatePortEnd = 100
+                    protocolType = "TCP"
+                    vipPortStart = 22
+                    vipPortEnd = 100
+                    useVip("PublicNetwork")
+                }
+                portForwarding {
+                    name = "pfRule2"
+                    allowedCidr = "0.0.0.0/0"
+                    privatePortStart = 50
+                    privatePortEnd = 80
+                    protocolType = "TCP"
+                    vipPortStart = 50
+                    vipPortEnd = 80
+                    useVip("PublicNetwork")
+                }
+                portForwarding {
+                    name = "pfRule3"
+                    allowedCidr = "0.0.0.0/0"
+                    privatePortStart = 50
+                    privatePortEnd = 80
+                    protocolType = "UDP"
+                    vipPortEnd = 50
+                    vipPortStart = 80
+                    useVip("PublicNetwork")
+                }
+                virtualRouterOffering {
+                    name = "vro"
+                    memory = SizeUnit.MEGABYTE.toByte(512)
+                    cpu = 2
+                    useManagementL3Network("PublicNetwork")
+                    usePublicL3Network("PublicNetwork")
+                    useImage("vr")
+                    isDefault = true
+                }
+            }
+            vm {
+                name = "vm"
+                useImage("image")
+                useL3Networks("GuestNetwork")
+                useInstanceOffering("instanceOffering")
+            }
+            vm {
+                name = "vm2"
+                useImage("image")
+                useL3Networks("GuestNetwork2")
+                useInstanceOffering("instanceOffering")
             }
         }
     }
