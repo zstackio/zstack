1010e0bb97d9d8d2fa43b253f78644dc6a269fef
HeathHose <l96dou@outlook.com>
2017-04-06 09:49:59 +0800

    [Unittest]migrate premium test

---
+++ test/src/test/groovy/org/zstack/test/integration/network/l3network/multil3/ChangeDefaultL3Case.groovy
@@ -0,0 +1,194 @@
+package org.zstack.test.integration.network.l3network.multil3
+
+import junit.framework.Assert
+import org.springframework.http.HttpEntity
+import org.zstack.header.network.service.NetworkServiceType
+import org.zstack.network.service.eip.EipConstant
+import org.zstack.network.service.flat.BridgeNameFinder
+import org.zstack.network.service.flat.FlatDhcpBackend
+import org.zstack.network.service.flat.FlatNetworkServiceConstant
+import org.zstack.network.service.userdata.UserdataConstant
+import org.zstack.sdk.L3NetworkInventory
+import org.zstack.sdk.VmInstanceInventory
+import org.zstack.sdk.VmNicInventory
+
+import org.zstack.test.integration.network.NetworkTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.utils.CollectionUtils
+import org.zstack.utils.data.SizeUnit
+import org.zstack.utils.function.Function
+import org.zstack.utils.gson.JSONObjectUtil
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by heathhose on 17-3-25.
+ */
+class ChangeDefaultL3Case extends SubCase{
+    def DOC = """
+use:
+1. create a vm with 2 flat L3 networks
+2. change the vm's default L3 network from one to another
+3. confirm the ResetDefaultGatewayCmd sent to the backend
+"""
+    EnvSpec env
+    @Override
+    void setup() {
+        useSpring(NetworkTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2-1")
+                    attachL2Network("l2-2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2-1"
+                    physicalInterface = "eth1"
+
+                    l3Network {
+                        name = "l3-1"
+
+                        service {
+                            provider = FlatNetworkServiceConstant.FLAT_NETWORK_SERVICE_TYPE_STRING
+                            types = [NetworkServiceType.DHCP.toString(), EipConstant.EIP_NETWORK_SERVICE_TYPE, UserdataConstant.USERDATA_TYPE_STRING]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2-2"
+                    physicalInterface = "eth2"
+
+                    l3Network {
+                        name = "l3-2"
+
+                        service {
+                            provider = FlatNetworkServiceConstant.FLAT_NETWORK_SERVICE_TYPE_STRING
+                            types = [NetworkServiceType.DHCP.toString(), EipConstant.EIP_NETWORK_SERVICE_TYPE, UserdataConstant.USERDATA_TYPE_STRING]
+                        }
+
+                        ip {
+                            startIp = "192.168.200.10"
+                            endIp = "192.168.200.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.200.1"
+                        }
+                    }
+
+                }
+
+                attachBackupStorage("sftp")
+
+            }
+
+            vm {
+                name = "vm"
+                useImage("image")
+                useDefaultL3Network("l3-1")
+                useL3Networks("l3-1","l3-2")
+                useInstanceOffering("instanceOffering")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+            changeDefaultL3NetworkToAnother()
+        }
+    }
+
+    void changeDefaultL3NetworkToAnother(){
+        L3NetworkInventory l31i = env.inventoryByName("l3-1")
+        L3NetworkInventory l32i = env.inventoryByName("l3-2")
+        VmInstanceInventory vmi = env.inventoryByName("vm")
+
+        VmNicInventory nic1 = CollectionUtils.find(vmi.getVmNics(), new Function<VmNicInventory, VmNicInventory>() {
+            @Override
+            public VmNicInventory call(VmNicInventory arg) {
+                return arg.getL3NetworkUuid().equals(l31i.getUuid()) ? arg : null;
+            }
+        });
+        VmNicInventory nic2 = CollectionUtils.find(vmi.getVmNics(), new Function<VmNicInventory, VmNicInventory>() {
+            @Override
+            public VmNicInventory call(VmNicInventory arg) {
+                return arg.getL3NetworkUuid().equals(l32i.getUuid()) ? arg : null;
+            }
+        });
+
+        FlatDhcpBackend.ResetDefaultGatewayCmd cmd = null
+        env.afterSimulator(FlatDhcpBackend.RESET_DEFAULT_GATEWAY_PATH){ rsp, HttpEntity<String> e ->
+            cmd = JSONObjectUtil.toObject(e.body,FlatDhcpBackend.ResetDefaultGatewayCmd.class)
+            return rsp
+        }
+        updateVmInstance {
+            uuid = vmi.uuid
+            defaultL3NetworkUuid = l32i.uuid
+        }
+
+        TimeUnit.SECONDS.sleep(2);
+        Assert.assertEquals(nic1.getMac(), cmd.macOfGatewayToRemove);
+        Assert.assertEquals(nic1.getGateway(), cmd.gatewayToRemove);
+        Assert.assertEquals(new BridgeNameFinder().findByL3Uuid(l31i.getUuid()), cmd.bridgeNameOfGatewayToRemove);
+
+        Assert.assertEquals(nic2.getMac(), cmd.macOfGatewayToAdd);
+        Assert.assertEquals(nic2.getGateway(), cmd.gatewayToAdd);
+        Assert.assertEquals(new BridgeNameFinder().findByL3Uuid(l32i.getUuid()), cmd.bridgeNameOfGatewayToAdd);
+    }
+    @Override
+    void clean() {
+        env.delete()
+    }
+}
---
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/flat/dhcp/CheckFlatDhcpWorkCase.groovy
@@ -0,0 +1,192 @@
+package org.zstack.test.integration.networkservice.provider.flat.dhcp
+
+import junit.framework.Assert
+import org.springframework.http.HttpEntity
+import org.zstack.header.network.service.NetworkServiceType
+import org.zstack.network.service.eip.EipConstant
+import org.zstack.network.service.flat.FlatDhcpBackend
+import org.zstack.network.service.flat.FlatNetworkServiceConstant
+import org.zstack.network.service.userdata.UserdataConstant
+import org.zstack.sdk.L3NetworkInventory
+import org.zstack.sdk.VmInstanceInventory
+import org.zstack.sdk.VmNicInventory
+import org.zstack.test.integration.networkservice.provider.NetworkServiceProviderTest
+import org.zstack.test.integration.networkservice.provider.flat.FlatNetworkServiceEnv
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.utils.CollectionUtils
+import org.zstack.utils.data.SizeUnit
+import org.zstack.utils.function.Function
+import org.zstack.utils.gson.JSONObjectUtil
+
+/**
+ * Created by heathhose on 17-4-1.
+ */
+class CheckFlatDhcpWorkCase extends SubCase{
+
+    EnvSpec env
+    @Override
+    void setup() {
+        useSpring(NetworkServiceProviderTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = FlatNetworkServiceEnv.oneFlatEipEnv()
+        env = env{
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2-1")
+                    attachL2Network("l2-2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2-1"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3-1"
+
+                        service {
+                            provider = FlatNetworkServiceConstant.FLAT_NETWORK_SERVICE_TYPE_STRING
+                            types = [NetworkServiceType.DHCP.toString(), EipConstant.EIP_NETWORK_SERVICE_TYPE, UserdataConstant.USERDATA_TYPE_STRING]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+                    
+                }
+                l2NoVlanNetwork {
+                    name = "l2-2"
+                    physicalInterface = "eth1"
+
+                    l3Network {
+                        name = "l3-2"
+
+                        service {
+                            provider = FlatNetworkServiceConstant.FLAT_NETWORK_SERVICE_TYPE_STRING
+                            types = [NetworkServiceType.DHCP.toString(), EipConstant.EIP_NETWORK_SERVICE_TYPE, UserdataConstant.USERDATA_TYPE_STRING]
+                        }
+
+                        ip {
+                            startIp = "192.168.200.10"
+                            endIp = "192.168.200.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.200.1"
+                        }
+                    }
+                }
+
+                attachBackupStorage("sftp")
+            }
+
+            vm {
+                name = "vm"
+                useImage("image")
+                useDefaultL3Network("l3-1")
+                useL3Networks("l3-1","l3-2")
+                useInstanceOffering("instanceOffering")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+            checkDhcpWork()
+        }
+    }
+
+    void checkDhcpWork(){
+        VmInstanceInventory vm = env.inventoryByName("vm")
+        final L3NetworkInventory l31 = env.inventoryByName("l3-1")
+        final L3NetworkInventory l32 = env.inventoryByName("l3-2");
+
+        VmNicInventory n = CollectionUtils.find(vm.getVmNics(), new Function<VmNicInventory, VmNicInventory>() {
+            @Override
+            public VmNicInventory call(VmNicInventory arg) {
+                return arg.getL3NetworkUuid().equals(l31.getUuid()) ? arg : null;
+            }
+        });
+        Assert.assertEquals(0, n.getDeviceId());
+
+        n = CollectionUtils.find(vm.getVmNics(), new Function<VmNicInventory, VmNicInventory>() {
+            @Override
+            public VmNicInventory call(VmNicInventory arg) {
+                return arg.getL3NetworkUuid().equals(l32.getUuid()) ? arg : null;
+            }
+        });
+        Assert.assertEquals(1, n.getDeviceId());
+
+        List<FlatDhcpBackend.DhcpInfo> dhcpInfoList = new ArrayList<FlatDhcpBackend.DhcpInfo>()
+        env.afterSimulator(FlatDhcpBackend.APPLY_DHCP_PATH) { rsp, HttpEntity<String> e1 ->
+            FlatDhcpBackend.ApplyDhcpCmd cmd = JSONObjectUtil.toObject(e1.body, FlatDhcpBackend.ApplyDhcpCmd.class)
+            assert null != cmd
+            dhcpInfoList.addAll(cmd.dhcp)
+            return rsp
+        }
+        goOn:
+        for (FlatDhcpBackend.ApplyDhcpCmd cmd : dhcpInfoList) {
+            FlatDhcpBackend.DhcpInfo info = cmd.dhcp.get(0);
+            if (!info.isDefaultL3Network && info.hostname != null) {
+                Assert.fail(String.format("wrong hostname set. %s", JSONObjectUtil.toJsonString(info)));
+            }
+
+            for (VmNicInventory nic : vm.getVmNics()) {
+                if (info.ip.equals(nic.getIp()) && info.gateway.equals(nic.getGateway()) && info.netmask.equals(nic.getNetmask())) {
+                    break goOn;
+                }
+            }
+
+            Assert.fail(String.format("nic %s", JSONObjectUtil.toJsonString(cmd.dhcp)));
+        }
+    }
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+}
