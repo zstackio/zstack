17076ad77614df767845f91ed53f24c4512a80d8
HeathHose <l96dou@outlook.com>
2017-04-08 17:46:56 +0800

    [fix Bug2005]cannot maintain the disconnected host

---
+++ test/src/test/groovy/org/zstack/test/integration/kvm/host/ChangeDisconncetedHostToMaintainCase.groovy
@@ -0,0 +1,118 @@
+package org.zstack.test.integration.kvm.host
+
+import junit.framework.Assert
+import org.springframework.http.HttpEntity
+import org.zstack.compute.host.HostGlobalConfig
+import org.zstack.core.db.DatabaseFacade
+import org.zstack.header.apimediator.ApiMessageInterceptionException
+import org.zstack.header.host.HostState
+import org.zstack.header.host.HostStateEvent
+import org.zstack.header.host.HostStatus
+import org.zstack.header.host.HostVO
+import org.zstack.header.vm.VmInstanceState
+import org.zstack.header.vm.VmInstanceStateEvent
+import org.zstack.header.vm.VmInstanceVO
+import org.zstack.kvm.KVMAgentCommands
+import org.zstack.kvm.KVMConstant
+import org.zstack.sdk.ChangeHostStateAction
+import org.zstack.sdk.HostInventory
+import org.zstack.sdk.VmInstanceInventory
+import org.zstack.simulator.kvm.KVMSimulatorConfig
+import org.zstack.test.integration.kvm.Env
+import org.zstack.test.integration.kvm.KvmTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by heathhose on 17-4-7.
+ */
+class ChangeDisconncetedHostToMaintainCase extends SubCase{
+
+    EnvSpec env
+    DatabaseFacade dbf
+    HostInventory host
+    VmInstanceInventory vm
+    @Override
+    void setup() {
+        useSpring(KvmTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = Env.oneVmBasicEnv()
+    }
+
+    @Override
+    void test() {
+        dbf = bean(DatabaseFacade.class)
+        env.create {
+            host = env.inventoryByName("kvm")
+            vm = env.inventoryByName("vm")
+            testChangeStateIntoMaintainOneVMUnkown()
+            testChangeStateIntoMaintainNoneVm()
+        }
+
+    }
+
+    void testChangeStateIntoMaintainOneVMUnkown(){
+        VmInstanceInventory vm = env.inventoryByName("vm")
+        assert vm.state == VmInstanceState.Running.name()
+
+        env.simulator(KVMConstant.KVM_CONNECT_PATH){HttpEntity<String> entity,EnvSpec spec ->
+            def rsp = new KVMAgentCommands.ConnectResponse()
+            rsp.success = false
+            return rsp
+        }
+
+        expect(AssertionError.class){
+            reconnectHost {
+                uuid = host.uuid
+            }
+        }
+        retryInSecs(3,1){
+            HostVO hvo = dbf.findByUuid(host.getUuid(), HostVO.class)
+            assert HostStatus.Disconnected == hvo.getStatus()
+            VmInstanceVO vmvo = dbf.findByUuid(vm.getUuid(), VmInstanceVO.class)
+            assert VmInstanceState.Unknown == vmvo.getState()
+        }
+
+        expect(ApiMessageInterceptionException.class){
+            ChangeHostStateAction action = new ChangeHostStateAction()
+            action.uuid = host.uuid
+            action.stateEvent = HostStateEvent.maintain
+            action.sessionId = loginAsAdmin()
+            action.call()
+        }
+
+        assert dbFindByUuid(host.uuid,HostVO.class).state == HostState.Enabled
+
+    }
+    
+    void testChangeStateIntoMaintainNoneVm(){
+        destroyVmInstance {
+            uuid = vm.uuid
+        }
+        retryInSecs(1,1){
+            HostVO host = dbf.findByUuid(host.getUuid(), HostVO.class)
+            Assert.assertEquals(HostStatus.Disconnected, host.getStatus())
+        }
+
+        expect(ApiMessageInterceptionException.class){
+            ChangeHostStateAction action = new ChangeHostStateAction()
+            action.uuid = host.uuid
+            action.stateEvent = HostStateEvent.maintain
+            action.sessionId = loginAsAdmin()
+            action.call()
+        }
+
+        assert dbFindByUuid(host.uuid,HostVO.class).state == HostState.Enabled
+
+    }
+    
+    @Override
+    void clean() {
+        env.delete()
+    }
+}
