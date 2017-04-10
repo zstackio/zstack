8a4178b9241cb695543cbd41cf2ff4eeb8644db1
Camile <camilesing@gmail.com>
2017-04-10 16:09:45 +0800

    fix bug 2704

--- test/src/test/groovy/org/zstack/test/integration/kvm/hostallocator/Env.groovy
+++ test/src/test/groovy/org/zstack/test/integration/kvm/hostallocator/Env.groovy
@@ -250,4 +250,110 @@ This environment is modeled with the issue reported in #829.
             }
         }
     }
+
+    static EnvSpec noVmThreeHostEnv() {
+        return Test.makeEnv {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(2)
+                cpu = 2
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image1"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm1"
+                        managementIp = "127.0.0.2"
+                        username = "root"
+                        password = "password"
+
+                        totalCpu = 8
+                        totalMem = SizeUnit.GIGABYTE.toByte(10)
+                    }
+
+                    kvm {
+                        name = "kvm2"
+                        managementIp = "127.0.0.3"
+                        username = "root"
+                        password = "password"
+
+                        totalCpu = 8
+                        totalMem = SizeUnit.GIGABYTE.toByte(10)
+                    }
+
+                    kvm {
+                        name = "kvm3"
+                        managementIp = "127.0.0.4"
+                        username = "root"
+                        password = "password"
+
+                        totalCpu = 8
+                        totalMem = SizeUnit.GIGABYTE.toByte(10)
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        service {
+                            provider = SecurityGroupConstant.SECURITY_GROUP_PROVIDER_TYPE
+                            types = [SecurityGroupConstant.SECURITY_GROUP_NETWORK_SERVICE_TYPE]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                    l3Network {
+                        name = "pubL3"
+
+                        ip {
+                            startIp = "12.16.10.10"
+                            endIp = "12.16.10.100"
+                            netmask = "255.255.255.0"
+                            gateway = "12.16.10.1"
+                        }
+                    }
+                }
+
+                attachBackupStorage("sftp")
+            }
+
+        }
+    }
 }
