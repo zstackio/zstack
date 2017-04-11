339d0e6e0952a5cc56fea2150903322b6b3f41d2
HeathHose <l96dou@outlook.com>
2017-04-11 10:24:52 +0800

    fix CloudBusProxyCase

--- test/src/test/groovy/org/zstack/test/integration/core/cloudbus/CloudBusProxyCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/cloudbus/CloudBusProxyCase.groovy
@@ -1,16 +1,11 @@
 package org.zstack.test.integration.core.cloudbus
 
-import groovy.transform.TypeChecked
-import junit.framework.Assert
 import org.zstack.core.cloudbus.CloudBus
 import org.zstack.core.cloudbus.CloudBusCallBack
 import org.zstack.header.message.MessageReply
 import org.zstack.header.message.NeedReplyMessage
 import org.zstack.test.aop.CloudBusAopProxy
-import org.zstack.test.core.cloudbus.FakeNeedReplyMessage
 import org.zstack.testlib.SubCase
-import org.zstack.utils.Utils
-import org.zstack.utils.logging.CLogger
 
 /**
  * Created by heathhose on 17-3-22.
@@ -19,7 +14,6 @@ class CloudBusProxyCase extends SubCase{
 
     CloudBus bus
     CloudBusAopProxy aop
-    boolean isSuccess = false
     List<FakeNeedReplyMessage> msgs = new ArrayList<FakeNeedReplyMessage>(2)
     FakeNeedReplyMessage msg = new FakeNeedReplyMessage()
 
@@ -39,15 +33,15 @@ class CloudBusProxyCase extends SubCase{
         bus = bean(CloudBus.class)
         aop = bean(CloudBusAopProxy.class)
 
+        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.FAIL)
         testBusSendMsgsWhenCloudBusAopProxyBehaviorFAIL()
         testBusSendCallBackMgsWhenCloudBusAopProxyBehaviorFAIL()
-        testBusCallMsgWhenCloudBusAopProxyBehaviorFAIL()
+
+        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.TIMEOUT)
         testBusSenMsgsWhenCloudBusAopProxyBehaviorTIMEOUT()
     }
 
     void testBusSendMsgsWhenCloudBusAopProxyBehaviorFAIL(){
-        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.FAIL)
-
         msg.setServiceId("A fake service id not needed")
         msgs.add(msg)
         msg = new FakeNeedReplyMessage()
@@ -57,6 +51,7 @@ class CloudBusProxyCase extends SubCase{
     }
 
     void testBusSendCallBackMgsWhenCloudBusAopProxyBehaviorFAIL(){
+        boolean isSuccess
         msg = new FakeNeedReplyMessage()
         msg.setServiceId("A fake service id not needed")
         bus.send(msg, new CloudBusCallBack(null) {
@@ -67,19 +62,15 @@ class CloudBusProxyCase extends SubCase{
                 }
             }
         })
-    }
 
-    void testBusCallMsgWhenCloudBusAopProxyBehaviorFAIL(){
         msg = new FakeNeedReplyMessage()
         msg.setServiceId("A fake service id not needed")
         MessageReply reply = bus.call(msg)
-        Assert.assertFalse(reply.isSuccess())
-        Assert.assertTrue(isSuccess)
+        assert !reply.isSuccess()
+        assert isSuccess
     }
 
     void testBusSenMsgsWhenCloudBusAopProxyBehaviorTIMEOUT(){
-        aop.addMessage(NeedReplyMessage.class, CloudBusAopProxy.Behavior.TIMEOUT)
-
         msgs.clear()
         msg = new FakeNeedReplyMessage()
         msg.setServiceId("A fake service id not needed")
@@ -91,7 +82,7 @@ class CloudBusProxyCase extends SubCase{
         msgs.add(msg)
         List<MessageReply> replies = bus.call(msgs)
         for (MessageReply r : replies) {
-            Assert.assertFalse(r.isSuccess())
+            assert !r.isSuccess()
         }
     }
 
