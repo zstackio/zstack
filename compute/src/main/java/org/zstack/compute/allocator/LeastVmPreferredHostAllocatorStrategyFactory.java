10169814d6ba94b3c779f64e33b58f81bd696807
lining <2313806311@qq.com>
2017-04-11 16:50:15 +0800

    fix bug LeastVmPreferredHostAllocatorStrategy

--- compute/src/main/java/org/zstack/compute/allocator/LeastVmPreferredHostAllocatorStrategyFactory.java
+++ compute/src/main/java/org/zstack/compute/allocator/LeastVmPreferredHostAllocatorStrategyFactory.java
@@ -1,6 +1,7 @@
 package org.zstack.compute.allocator;
 
 import org.zstack.header.allocator.HostAllocatorConstant;
+import org.zstack.header.allocator.HostAllocatorStrategy;
 import org.zstack.header.allocator.HostAllocatorStrategyType;
 
 public class LeastVmPreferredHostAllocatorStrategyFactory extends AbstractHostAllocatorStrategyFactory {
---
+++ header/src/main/java/org/zstack/header/allocator/MarshalResultFunction.java
@@ -0,0 +1,13 @@
+package org.zstack.header.allocator;
+
+import org.zstack.header.host.HostVO;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by lining on 2017/4/11.
+ */
+public interface MarshalResultFunction {
+    void marshal(List<HostVO> hosts);
+}
