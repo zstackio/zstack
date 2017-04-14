2a4104b9f007b174f6cf718131ccdae91f3164ef
aias_for_win10 <aiassing@outlook.com>
2017-04-14 21:06:00 +0800

    fix bug 2683

---
+++ test/src/test/groovy/org/zstack/test/integration/storage/primary/local/AddLocalStorageCase.groovy
@@ -0,0 +1,80 @@
+package org.zstack.test.integration.storage.primary.local
+
+import org.springframework.http.HttpEntity
+import org.zstack.compute.vm.VmGlobalConfig
+import org.zstack.core.db.DatabaseFacade
+import org.zstack.header.storage.primary.PrimaryStorageState
+import org.zstack.header.storage.primary.PrimaryStorageStateEvent
+import org.zstack.header.storage.primary.PrimaryStorageVO
+import org.zstack.header.vm.VmInstanceDeletionPolicyManager.VmInstanceDeletionPolicy
+import org.zstack.header.vm.VmInstanceState
+import org.zstack.header.vm.VmInstanceVO
+import org.zstack.kvm.KVMAgentCommands
+import org.zstack.kvm.KVMConstant
+import org.zstack.sdk.AddLocalPrimaryStorageAction
+import org.zstack.storage.primary.local.APILocalStorageMigrateVolumeMsg
+import org.zstack.test.integration.storage.Env
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.*
+import org.zstack.utils.gson.JSONObjectUtil
+
+/**
+ * Created by camile on 2017/4/
+ */
+class AddLocalStorageCase extends SubCase {
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(StorageTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env {
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                attachBackupStorage("sftp")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+            addErrorPathLSailure()
+        }
+    }
+
+    void addErrorPathLSailure() {
+        String zoneUuid = (env.specByName("zone") as ZoneSpec).inventory.uuid
+        AddLocalPrimaryStorageAction addLocalPrimaryStorageAction = new AddLocalPrimaryStorageAction()
+        addLocalPrimaryStorageAction.url = "/dev/test"
+        addLocalPrimaryStorageAction.name = "test2"
+        addLocalPrimaryStorageAction.zoneUuid = zoneUuid
+        addLocalPrimaryStorageAction.sessionId = adminSession()
+        AddLocalPrimaryStorageAction.Result res= addLocalPrimaryStorageAction.call()
+        res.error !=null
+        addLocalPrimaryStorageAction.url = "/proc/test"
+        res= addLocalPrimaryStorageAction.call()
+        res.error !=null
+        addLocalPrimaryStorageAction.url = "/sys/test"
+        res= addLocalPrimaryStorageAction.call()
+        res.error !=null
+    }
+}
---
+++ test/src/test/groovy/org/zstack/test/integration/storage/primary/smp/AddSMPStorageCase.groovy
@@ -0,0 +1,69 @@
+package org.zstack.test.integration.storage.primary.smp
+
+import org.zstack.sdk.AddLocalPrimaryStorageAction
+import org.zstack.sdk.AddSharedMountPointPrimaryStorageAction
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.testlib.ZoneSpec
+
+/**
+ * Created by camile on 2017/4/
+ */
+class AddSMPStorageCase extends SubCase {
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(StorageTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env {
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                attachBackupStorage("sftp")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+            addErrorPathLSailure()
+        }
+    }
+
+    void addErrorPathLSailure() {
+        String zoneUuid = (env.specByName("zone") as ZoneSpec).inventory.uuid
+        AddSharedMountPointPrimaryStorageAction addLocalPrimaryStorageAction = new AddSharedMountPointPrimaryStorageAction()
+        addLocalPrimaryStorageAction.url = "/dev/test"
+        addLocalPrimaryStorageAction.name = "test2"
+        addLocalPrimaryStorageAction.zoneUuid = zoneUuid
+        addLocalPrimaryStorageAction.sessionId = adminSession()
+        AddSharedMountPointPrimaryStorageAction.Result res = addLocalPrimaryStorageAction.call()
+        res.error != null
+        addLocalPrimaryStorageAction.url = "/proc/test"
+        res = addLocalPrimaryStorageAction.call()
+        res.error != null
+        addLocalPrimaryStorageAction.url = "/sys/test"
+        res = addLocalPrimaryStorageAction.call()
+        res.error != null
+    }
+}
