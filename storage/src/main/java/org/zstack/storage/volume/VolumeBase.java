7d14b0cb29504a6185f7291045b3ee8757cab6d9
J M <630911437@qq.com>
2017-04-18 18:00:35 +0800

    fix 2711 in a dirty way

--- storage/src/main/java/org/zstack/storage/volume/VolumeBase.java
+++ storage/src/main/java/org/zstack/storage/volume/VolumeBase.java
@@ -1076,12 +1076,13 @@ public class VolumeBase implements Volume {
                 sql = SQL.New("select vm " +
                         "from VmInstanceVO vm,PrimaryStorageClusterRefVO ref,PrimaryStorageEO ps " +
                         "where vm.state in (:vmStates) " +
-                        "and vm.hypervisorType not in (:hvTypes)  " +
+                        "and vm.hypervisorType <> :hvType  " +
                         "and vm.clusterUuid = ref.clusterUuid " +
                         "and ref.primaryStorageUuid = ps.uuid " +
                         "and ps.state in (:psState) " +
                         "group by vm.uuid")
-                        .param("hvTypes", VolumeFormat.valueOf(VolumeConstant.VOLUME_FORMAT_VMTX).getHypervisorTypesSupportingThisVolumeFormatInString())
+                        //TODO:  this is a dirty fix, delete it when VMWare support DataVolume
+                        .param("hvType", "ESX")
                         .param("psState",PrimaryStorageState.Enabled);
             } else {
                 DebugUtils.Assert(false, String.format("should not reach here, volume[uuid:%s]", self.getUuid()));
