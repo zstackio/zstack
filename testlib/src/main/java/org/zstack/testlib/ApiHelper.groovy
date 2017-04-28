98fc47ac9f926419a34bc1935a7ebdd542c9d00e
HeathHose <l96dou@outlook.com>
2017-04-28 17:06:27 +0800

    add the action of APILocalStorageGetVolumeMigratableHostsMsg

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -1,5 +1,6 @@
 package org.zstack.testlib
 
+import org.zstack.sdk.LocalStorageGetVolumeMigratableHostsAction
 import org.zstack.utils.gson.JSONObjectUtil
 import org.zstack.core.Platform
 
@@ -2233,6 +2234,31 @@ trait ApiHelper {
         }
     }
 
+    def localStorageGetVolumeMigratableHosts(@DelegatesTo(strategy =  Closure.OWNER_FIRST, value = org.zstack.sdk.LocalStorageGetVolumeMigratableHostsAction.class) Closure c) {
+        def a = new org.zstack.sdk.LocalStorageGetVolumeMigratableHostsAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
 
     def getEipAttachableVmNics(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetEipAttachableVmNicsAction.class) Closure c) {
         def a = new org.zstack.sdk.GetEipAttachableVmNicsAction()
