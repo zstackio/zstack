f436317a56a30afea73ec78a6650d8f239e83589
Frank.Zhang <xin.zhang@mevoco.com>
2017-05-02 19:43:08 -0400

    add webhook to notification system

--- test/src/test/groovy/org/zstack/test/integration/core/notification/NotificationCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/notification/NotificationCase.groovy
@@ -1,5 +1,8 @@
 package org.zstack.test.integration.core.notification
 
+import org.springframework.http.HttpEntity
+import org.zstack.core.notification.NotificationConstant
+import org.zstack.core.notification.NotificationGlobalConfig
 import org.zstack.core.notification.NotificationStatus
 import org.zstack.core.notification.NotificationVO
 import org.zstack.header.zone.ZoneVO
@@ -7,6 +10,7 @@ import org.zstack.sdk.NotificationInventory
 import org.zstack.sdk.ZoneInventory
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.SubCase
+import org.zstack.utils.gson.JSONObjectUtil
 
 /**
  * Created by xing5 on 2017/3/18.
@@ -76,12 +80,37 @@ class NotificationCase extends SubCase {
         assert !dbIsExists(inv.uuid, NotificationVO.class)
     }
 
+    void testNotificationWebhook() {
+        String webhook = "/notification-web-hook"
+
+        List<NotificationInventory> invs = null
+        env.simulator(webhook) { HttpEntity<String> e ->
+            invs = JSONObjectUtil.toCollection(e.getBody(), ArrayList.class, NotificationInventory.class)
+        }
+
+        NotificationGlobalConfig.WEBHOOK_URL.updateValue("http://127.0.0.1:8989$webhook")
+
+        createZone {
+            name = "test zone"
+        }
+
+        retryInSecs {
+            return {
+                assert invs != null
+                assert invs.size() == 1
+                NotificationInventory inv = invs[0]
+                assert inv.sender == NotificationConstant.API_SENDER
+            }
+        }
+    }
+
     @Override
     void test() {
         env.create {
             testQueryNotifications()
             testUpdateNotificationsStatus()
             testDeleteNotifications()
+            testNotificationWebhook()
         }
     }
 }
