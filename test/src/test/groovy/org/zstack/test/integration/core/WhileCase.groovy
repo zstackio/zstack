538bb1346c61dd993f34a9b6fcb7d2e296b2fae4
J M <630911437@qq.com>
2017-05-02 15:23:13 +0800

    fix while

---
+++ test/src/test/groovy/org/zstack/test/integration/core/WhileCase.groovy
@@ -0,0 +1,51 @@
+package org.zstack.test.integration.core
+
+import org.zstack.core.asyncbatch.While
+import org.zstack.header.core.FutureCompletion
+import org.zstack.header.core.NoErrorCompletion
+import org.zstack.testlib.SubCase
+
+/**
+ * Created by Administrator on 2017-05-02.
+ */
+class WhileCase extends SubCase{
+    @Override
+    void clean() {
+
+    }
+
+    @Override
+    void setup() {
+        INCLUDE_CORE_SERVICES = false
+    }
+
+    @Override
+    void environment() {
+
+    }
+
+    @Override
+    void test() {
+        int a = 0
+        FutureCompletion future = new FutureCompletion(null)
+
+        new While<>(new ArrayList<String>()).all(new While.Do<String>() {
+            @Override
+            void accept(String item, NoErrorCompletion completion) {
+                completion.done()
+            }
+        }).run(new NoErrorCompletion(){
+
+            @Override
+            void done() {
+                future.success()
+            }
+        })
+
+        future.await(10)
+
+        assert future.success
+
+
+    }
+}
