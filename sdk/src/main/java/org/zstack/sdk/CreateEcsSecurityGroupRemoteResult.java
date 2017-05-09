5086dc8f885f28ec363b07860732a4ff42441f2d
mingjian2049 <mingjian.deng@mevoco.com>
2017-05-09 18:58:26 +0800

    add remote apis (hybrid)

---
+++ sdk/src/main/java/org/zstack/sdk/CreateEcsSecurityGroupRemoteResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class CreateEcsSecurityGroupRemoteResult {
+    public EcsSecurityGroupInventory inventory;
+    public void setInventory(EcsSecurityGroupInventory inventory) {
+        this.inventory = inventory;
+    }
+    public EcsSecurityGroupInventory getInventory() {
+        return this.inventory;
+    }
+
+}
---
+++ sdk/src/main/java/org/zstack/sdk/CreateEcsVpcRemoteAction.java
@@ -0,0 +1,83 @@
+package org.zstack.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreateEcsVpcRemoteAction extends AbstractAction {
+
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    public static class Result {
+        public ErrorCode error;
+        public CreateEcsVpcRemoteResult value;
+
+        public Result throwExceptionIfError() {
+            if (error != null) {
+                throw new ApiException(
+                    String.format("error[code: %s, description: %s, details: %s]", error.code, error.description, error.details)
+                );
+            }
+            
+            return this;
+        }
+    }
+
+    @Param(required = false)
+    public java.lang.String resourceUuid;
+
+    @Param(required = false)
+    public java.util.List systemTags;
+
+    @Param(required = false)
+    public java.util.List userTags;
+
+    @Param(required = true)
+    public String sessionId;
+
+    public long timeout;
+    
+    public long pollingInterval;
+
+
+    private Result makeResult(ApiResult res) {
+        Result ret = new Result();
+        if (res.error != null) {
+            ret.error = res.error;
+            return ret;
+        }
+        
+        CreateEcsVpcRemoteResult value = res.getResult(CreateEcsVpcRemoteResult.class);
+        ret.value = value == null ? new CreateEcsVpcRemoteResult() : value; 
+
+        return ret;
+    }
+
+    public Result call() {
+        ApiResult res = ZSClient.call(this);
+        return makeResult(res);
+    }
+
+    public void call(final Completion<Result> completion) {
+        ZSClient.call(this, new InternalCompletion() {
+            @Override
+            public void complete(ApiResult res) {
+                completion.complete(makeResult(res));
+            }
+        });
+    }
+
+    Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    RestInfo getRestInfo() {
+        RestInfo info = new RestInfo();
+        info.httpMethod = "POST";
+        info.path = "/hybrid/aliyun/vpc";
+        info.needSession = true;
+        info.needPoll = true;
+        info.parameterName = "params";
+        return info;
+    }
+
+}
