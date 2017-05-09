763f6725ec2147d71c2ba164b3f5009d3fc8c2f6
mingjian2049 <mingjian.deng@mevoco.com>
2017-05-09 19:03:45 +0800

    update qga sdk

--- sdk/src/main/java/org/zstack/sdk/GetVmQgaResult.java
+++ sdk/src/main/java/org/zstack/sdk/GetVmQgaResult.java
@@ -1,5 +1,20 @@
 package org.zstack.sdk;
 
 public class GetVmQgaResult {
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public boolean enable;
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+    public boolean getEnable() {
+        return this.enable;
+    }
 
 }
