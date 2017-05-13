409a9be779993a3966ee1214644e9df2d6ed31cc
AlanJager <873863981@qq.com>
2017-05-13 17:51:23 +0800

    Add case to confirm local storage batch create vm  for https://github.com/zstackio/issues/issues/2604

---
+++ test/src/test/groovy/org/zstack/test/integration/kvm/vm/BatchCreateVmOnLocalStorageCase.groovy
@@ -0,0 +1,202 @@
+package org.zstack.test.integration.kvm.vm
+
+import org.zstack.core.db.Q
+import org.zstack.header.network.service.NetworkServiceType
+import org.zstack.header.vm.VmInstanceVO
+import org.zstack.header.vm.VmInstanceVO_
+import org.zstack.network.service.eip.EipConstant
+import org.zstack.network.service.flat.FlatNetworkServiceConstant
+import org.zstack.network.service.userdata.UserdataConstant
+import org.zstack.sdk.DiskOfferingInventory
+import org.zstack.sdk.ImageInventory
+import org.zstack.sdk.InstanceOfferingInventory
+import org.zstack.sdk.L3NetworkInventory
+import org.zstack.sdk.PrimaryStorageInventory
+import org.zstack.test.integration.kvm.KvmTest
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.utils.data.SizeUnit
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by AlanJager on 2017/5/13.
+ */
+class BatchCreateVmOnLocalStorageCase extends SubCase{
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(KvmTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+
+            diskOffering {
+                name = "diskOffering"
+                diskSize = SizeUnit.GIGABYTE.toByte(20)
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image1"
+                    url  = "http://zstack.org/download/test.qcow2"
+                }
+
+                image {
+                    name = "iso"
+                    url  = "http://zstack.org/download/test.iso"
+                }
+
+                image {
+                    name = "vr"
+                    url  = "http://zstack.org/download/vr.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    kvm {
+                        name = "kvm1"
+                        managementIp = "127.0.0.2"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    kvm {
+                        name = "kvm2"
+                        managementIp = "127.0.0.3"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    kvm {
+                        name = "kvm3"
+                        managementIp = "127.0.0.4"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                    availableCapacity = SizeUnit.GIGABYTE.toByte(50)
+                    totalCapacity = SizeUnit.GIGABYTE.toByte(50)
+                }
+
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        service {
+                            provider = FlatNetworkServiceConstant.FLAT_NETWORK_SERVICE_TYPE_STRING
+                            types = [NetworkServiceType.DHCP.toString(), EipConstant.EIP_NETWORK_SERVICE_TYPE, UserdataConstant.USERDATA_TYPE_STRING]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                    l3Network {
+                        name = "pubL3"
+
+                        ip {
+                            startIp = "11.168.100.10"
+                            endIp = "11.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "11.168.100.1"
+                        }
+                    }
+                }
+
+                attachBackupStorage("sftp")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+
+            testBatchCreateVm()
+        }
+    }
+
+    void testBatchCreateVm() {
+        PrimaryStorageInventory ps = env.inventoryByName("local")
+        InstanceOfferingInventory instanceOffering = env.inventoryByName("instanceOffering")
+        L3NetworkInventory l3 = env.inventoryByName("l3")
+        ImageInventory image = env.inventoryByName("iso")
+        DiskOfferingInventory diskOffering = env.inventoryByName("diskOffering")
+        assert ps.availableCapacity == SizeUnit.GIGABYTE.toByte(200)
+
+        final CountDownLatch latch = new CountDownLatch(10)
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                void run() {
+                    try {
+                        createVmInstance {
+                            name = "test-" + i
+                            instanceOfferingUuid = instanceOffering.uuid
+                            l3NetworkUuids = [l3.uuid]
+                            imageUuid = image.uuid
+                            rootDiskOfferingUuid = diskOffering.uuid
+                        }
+
+                        latch.countDown()
+                    } catch (Throwable t) {
+                    }
+                }
+            }).run()
+        }
+
+        latch.await(2, TimeUnit.MINUTES);
+        assert Q.New(VmInstanceVO.class).notNull(VmInstanceVO_.uuid).list().size() == 10
+    }
+}
