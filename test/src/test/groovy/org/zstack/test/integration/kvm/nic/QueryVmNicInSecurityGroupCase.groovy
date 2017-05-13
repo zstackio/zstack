04d90de2b5d08c5f77471f0023e7d4c76397f5a4
lining <2313806311@qq.com>
2017-05-13 23:14:36 +0800

    fix bug APIQueryVmNicInSecurityGroupMsg replyWithCount doesn't work

---
+++ test/src/test/groovy/org/zstack/test/integration/kvm/nic/QueryVmNicInSecurityGroupCase.groovy
@@ -0,0 +1,179 @@
+package org.zstack.test.integration.kvm.nic
+
+import org.zstack.header.network.service.NetworkServiceType
+import org.zstack.network.securitygroup.SecurityGroupConstant
+import org.zstack.network.service.virtualrouter.VirtualRouterConstant
+import org.zstack.sdk.*
+import org.zstack.test.integration.kvm.KvmTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.testlib.Test
+import org.zstack.testlib.util.search.SDKQueryTestValidator
+import org.zstack.utils.data.SizeUnit
+
+/**
+ * Created by lining on 2017/5/13.
+ */
+class QueryVmNicInSecurityGroupCase extends SubCase {
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(KvmTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = env{
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(8)
+                cpu = 4
+            }
+
+            diskOffering {
+                name = "diskOffering"
+                diskSize = SizeUnit.GIGABYTE.toByte(20)
+            }
+
+            sftpBackupStorage {
+                name = "sftp"
+                url = "/sftp"
+                username = "root"
+                password = "password"
+                hostname = "localhost"
+
+                image {
+                    name = "image1"
+                    url = "http://zstack.org/download/test.qcow2"
+                }
+
+                image {
+                    name = "vr"
+                    url = "http://zstack.org/download/vr.qcow2"
+                }
+            }
+
+            zone {
+                name = "zone"
+                description = "test"
+
+                cluster {
+                    name = "cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "kvm"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                    }
+
+                    attachPrimaryStorage("local")
+                    attachL2Network("l2")
+                }
+
+                localPrimaryStorage {
+                    name = "local"
+                    url = "/local_ps"
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        service {
+                            provider = VirtualRouterConstant.PROVIDER_TYPE
+                            types = [NetworkServiceType.DHCP.toString(), NetworkServiceType.DNS.toString()]
+                        }
+
+                        service {
+                            provider = SecurityGroupConstant.SECURITY_GROUP_PROVIDER_TYPE
+                            types = [SecurityGroupConstant.SECURITY_GROUP_NETWORK_SERVICE_TYPE]
+                        }
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+
+                    l3Network {
+                        name = "pubL3"
+
+                        ip {
+                            startIp = "12.16.10.10"
+                            endIp = "12.16.10.100"
+                            netmask = "255.255.255.0"
+                            gateway = "12.16.10.1"
+                        }
+                    }
+                }
+
+                virtualRouterOffering {
+                    name = "vr"
+                    memory = SizeUnit.MEGABYTE.toByte(512)
+                    cpu = 2
+                    useManagementL3Network("pubL3")
+                    usePublicL3Network("pubL3")
+                    useImage("vr")
+                }
+
+                attachBackupStorage("sftp")
+
+                securityGroup {
+                    name = "sg"
+                    attachL3Network("l3")
+
+                    rule {
+                        type = "Ingress"
+                        startPort = 100
+                        endPort = 110
+                        protocol = "TCP"
+                    }
+
+                    useVmNic("vm", "l3")
+                }
+            }
+
+            vm {
+                name = "vm"
+                useInstanceOffering("instanceOffering")
+                useImage("image1")
+                useL3Networks("l3")
+            }
+        }
+    }
+
+    @Override
+    void test() {
+        env.create {
+            runTest()
+        }
+    }
+
+    void runTest() {
+        VmInstanceInventory vm = env.inventoryByName("vm")
+        L3NetworkInventory l3NetworkInventory = env.inventoryByName("l3")
+
+        QueryVmNicInSecurityGroupAction queryVmNicInSecurityGroupAction = new QueryVmNicInSecurityGroupAction(
+                sessionId: Test.currentEnvSpec.session.uuid,
+                replyWithCount : true
+        )
+        QueryVmNicInSecurityGroupAction.Result result = queryVmNicInSecurityGroupAction.call()
+        assert 1 == result.value.total
+        assert 1 == result.value.inventories.size()
+
+        SDKQueryTestValidator.validateEQ(new QueryVmNicInSecurityGroupAction(), result.value.inventories[0])
+    }
+}
