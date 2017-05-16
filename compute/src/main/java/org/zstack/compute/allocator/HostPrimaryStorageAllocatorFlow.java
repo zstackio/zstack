183b9624b6fa549d5a35515202589028d9a27133
Camile <camilesing@gmail.com>
2017-05-16 15:23:58 +0800

    fix bug 3557

--- compute/src/main/java/org/zstack/compute/allocator/HostPrimaryStorageAllocatorFlow.java
+++ compute/src/main/java/org/zstack/compute/allocator/HostPrimaryStorageAllocatorFlow.java
@@ -98,7 +98,7 @@ public class HostPrimaryStorageAllocatorFlow extends AbstractHostAllocatorFlow {
             } else {
                 // the primary storage doesn't have the image in cache
                 // so we need to add the image size
-                cap = ratioMgr.calculatePrimaryStorageAvailableCapacityByRatio(psUuid, cap) + spec.getImage().getActualSize();
+                cap = ratioMgr.calculatePrimaryStorageAvailableCapacityByRatio(psUuid, cap) - spec.getImage().getActualSize();
             }
 
             if (cap > spec.getDiskSize()) {
