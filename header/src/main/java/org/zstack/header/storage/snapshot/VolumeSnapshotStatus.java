0b621f03183213e139a4a53d01793dcc1dd02af3
Camile <camilesing@gmail.com>
2017-05-16 16:31:54 +0800

    fix bug 538
    Conflicts:
    	conf/db/upgrade/V2.0.0__schema.sql
    Cherry pick 31fee28ae78d7f3fe884b1b9dfdbcbc6592ee4a4 from devstage

--- header/src/main/java/org/zstack/header/storage/snapshot/VolumeSnapshotStatus.java
+++ header/src/main/java/org/zstack/header/storage/snapshot/VolumeSnapshotStatus.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public enum VolumeSnapshotStatus {
     Creating,
     Ready,
+    Deleted,
     Deleting;
 
     static {
---
+++ sdk/src/main/java/org/zstack/sdk/SnapShotSpending.java
@@ -0,0 +1,13 @@
+package org.zstack.sdk;
+
+public class SnapShotSpending extends SpendingDetails {
+
+    public java.util.List<SnapShotSpendingInventory> sizeInventory;
+    public void setSizeInventory(java.util.List<SnapShotSpendingInventory> sizeInventory) {
+        this.sizeInventory = sizeInventory;
+    }
+    public java.util.List<SnapShotSpendingInventory> getSizeInventory() {
+        return this.sizeInventory;
+    }
+
+}
