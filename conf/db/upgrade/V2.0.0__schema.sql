0b0ba163d9a76526b8575091c4bd6d41e2c03868
Camile <camilesing@gmail.com>
2017-05-16 16:31:54 +0800

    fix bug 538

--- conf/db/upgrade/V2.0.0__schema.sql
+++ conf/db/upgrade/V2.0.0__schema.sql
@@ -66,3 +66,22 @@ INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "BaremetalPxeServerVO
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "BaremetalChessisVO" FROM BaremetalChessisVO t;
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "BaremetalHostCfgVO" FROM BaremetalHostCfgVO t;
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "BaremetalHostNicCfgVO" FROM BaremetalHostNicCfgVO t;
+
+-- ----------------------------
+--  Table structure for `SnapshotUsageVO`
+-- ----------------------------
+CREATE TABLE `SnapShotUsageVO` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `volumeUuid` varchar(32) NOT NULL,
+  `SnapshotUuid` varchar(32) NOT NULL,
+  `SnapshotStatus` varchar(64) NOT NULL,
+  `SnapshotName` varchar(255) NOT NULL,
+  `accountUuid` varchar(32) NOT NULL,
+  `SnapshotSize` bigint unsigned NOT NULL,
+  `dateInLong` bigint unsigned NOT NULL,
+  `inventory` text,
+  `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
