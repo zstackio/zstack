5726a4478041e67ed80d7e17e43bfeb8574834d9
lining <2313806311@qq.com>
2017-05-19 14:46:18 +0800

    update assert

--- test/src/test/groovy/org/zstack/test/integration/storage/primary/nfs/capacity/AttachNfsWhenNoConnectedHostCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/storage/primary/nfs/capacity/AttachNfsWhenNoConnectedHostCase.groovy
@@ -135,11 +135,12 @@ class AttachNfsWhenNoConnectedHostCase extends SubCase {
         GetPrimaryStorageCapacityResult capacity = getPrimaryStorageCapacity {
             primaryStorageUuids = [ps.uuid]
         }
-        assert 0 == capacity.availableCapacity
-        assert 0 == capacity.availablePhysicalCapacity
-        assert 0 == capacity.totalPhysicalCapacity
-        assert 0 == capacity.totalCapacity
-
+        retryInSecs(2){
+            assert 0 == capacity.availableCapacity
+            assert 0 == capacity.availablePhysicalCapacity
+            assert 0 == capacity.totalPhysicalCapacity
+            assert 0 == capacity.totalCapacity
+        }
 
         env.afterSimulator(KVMConstant.KVM_CONNECT_PATH) { rsp, HttpEntity<String> e ->
             rsp.success = false
