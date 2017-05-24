53f4609e2aa78901f53cda58a09940bf67dde8e8
mingjian2049 <mingjian.deng@mevoco.com>
2017-05-24 10:46:56 +0800

    fix NullPointException

--- utils/src/main/java/org/zstack/utils/StringDSL.java
+++ utils/src/main/java/org/zstack/utils/StringDSL.java
@@ -121,6 +121,6 @@ public class StringDSL {
     }
 
     public static boolean isZStackUuid(String uuid) {
-        return uuid.matches("[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}");
+        return uuid != null && uuid.matches("[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}");
     }
 }
