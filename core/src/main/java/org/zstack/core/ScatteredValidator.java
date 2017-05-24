1bcbf7bf42303ad6a341c4336c2e2ad03299a68d
J M <630911437@qq.com>
2017-05-24 20:25:51 +0800

    add ps state check extension when attach volume

--- core/src/main/java/org/zstack/core/ScatteredValidator.java
+++ core/src/main/java/org/zstack/core/ScatteredValidator.java
@@ -3,6 +3,8 @@ package org.zstack.core;
 import org.zstack.header.errorcode.OperationFailureException;
 import org.zstack.header.exception.CloudRuntimeException;
 import org.zstack.utils.DebugUtils;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -16,6 +18,16 @@ import java.util.Set;
  */
 public abstract class ScatteredValidator {
     protected static List<Method> methods;
+    private static final CLogger logger = Utils.getLogger(ScatteredValidator.class);
+
+    private static final String getClassNameForStatic() {
+        return new Object() {
+            String getClassName() {
+                String className = this.getClass().getName();
+                return className.substring(0, className.lastIndexOf('$'));
+            }
+        }.getClassName();
+    }
 
     protected static void collectValidatorMethods(Class annotationClass, Class...argTypes) {
         if (argTypes == null) {
@@ -24,6 +36,12 @@ public abstract class ScatteredValidator {
 
         methods = new ArrayList<>();
         Set<Method> ms = Platform.getReflections().getMethodsAnnotatedWith(annotationClass);
+        if (ms.isEmpty()){
+            logger.warn(String.format("no validator found in %s", getClassNameForStatic()));
+        } else {
+            logger.debug(String.format("found validator %s",ms.iterator().next().getName()));
+        }
+
         for (Method m : ms) {
             if (!Modifier.isStatic(m.getModifiers())) {
                 throw new CloudRuntimeException(String.format("@%s %s.%s must be defined as static method", annotationClass, m.getDeclaringClass(), m.getName()));
@@ -47,12 +65,13 @@ public abstract class ScatteredValidator {
 
             m.setAccessible(true);
             methods.add(m);
+            logger.debug(String.format("add method validator %s",ms.iterator().next().getName()));
         }
     }
 
     protected void invokeValidatorMethods(Object...args) {
+        logger.debug("start invoke validator");
         DebugUtils.Assert(methods != null, "call collectValidatorMethods in static block before calling any methods");
-
         for (Method m : methods) {
             try {
                 m.invoke(null, args);
