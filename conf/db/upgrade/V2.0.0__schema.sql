f148cb6a50ff434a214d66c4af320f8dad12518e
Camile <camilesing@gmail.com>
2017-05-27 13:35:15 +0800

    create 2.1.0.sql

--- conf/db/upgrade/V2.0.0__schema.sql
+++ conf/db/upgrade/V2.0.0__schema.sql
@@ -176,21 +176,3 @@ INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "VpcVpnConnectionVO" 
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "VpcVpnGatewayVO" FROM VpcVpnGatewayVO t;
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "VpcVpnIkeConfigVO" FROM VpcVpnIkeConfigVO t;
 INSERT INTO ResourceVO (uuid, resourceType) SELECT t.uuid, "VpcVpnIpSecConfigVO" FROM VpcVpnIpSecConfigVO t;
--- ----------------------------
---  Table structure for `SnapshotUsageVO`
--- ----------------------------
-CREATE TABLE `SnapShotUsageVO` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `volumeUuid` varchar(32) NOT NULL,
-  `SnapshotUuid` varchar(32) NOT NULL,
-  `SnapshotStatus` varchar(64) NOT NULL,
-  `SnapshotName` varchar(255) NOT NULL,
-  `accountUuid` varchar(32) NOT NULL,
-  `SnapshotSize` bigint unsigned NOT NULL,
-  `dateInLong` bigint unsigned NOT NULL,
-  `inventory` text,
-  `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
