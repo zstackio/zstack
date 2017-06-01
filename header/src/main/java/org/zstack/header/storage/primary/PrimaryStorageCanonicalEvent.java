2afad269d84a76e8fc8be4f1118fdc91da4fc4e5
HeathHose <l96dou@outlook.com>
2017-06-01 13:56:06 +0800

    [fix bug2284]when ps is maintain and policy is direct , the volume cannot be deleted directly

--- header/src/main/java/org/zstack/header/storage/primary/PrimaryStorageCanonicalEvent.java
+++ header/src/main/java/org/zstack/header/storage/primary/PrimaryStorageCanonicalEvent.java
@@ -8,7 +8,7 @@ import org.zstack.header.message.NeedJsonSchema;
 public class PrimaryStorageCanonicalEvent {
     public static final String PRIMARY_STORAGE_DELETED_PATH = "/primaryStorage/delete";
     public static final String PRIMARY_STORAGE_STATUS_CHANGED_PATH = "/primaryStorage/status/change";
-
+    public static final String PRIMARY_STORAGE_STATE_CHANGED_PATH = "/primaryStorage/state/change";
     @NeedJsonSchema
     public static class PrimaryStorageDeletedData {
         private String primaryStorageUuid;
@@ -60,6 +60,46 @@ public class PrimaryStorageCanonicalEvent {
 
         public void setNewStatus(String newStatus) {
             this.newStatus = newStatus;
+        }
+
+        public PrimaryStorageInventory getInventory() {
+            return inventory;
+        }
+
+        public void setInventory(PrimaryStorageInventory inventory) {
+            this.inventory = inventory;
+        }
+    }
+
+    @NeedJsonSchema
+    public static class PrimaryStorageStateChangedData{
+        private String primaryStorageUuid;
+        private PrimaryStorageState oldState;
+        private PrimaryStorageState newState;
+        private PrimaryStorageInventory inventory;
+
+        public String getPrimaryStorageUuid() {
+            return primaryStorageUuid;
+        }
+
+        public void setPrimaryStorageUuid(String primaryStorageUuid) {
+            this.primaryStorageUuid = primaryStorageUuid;
+        }
+
+        public PrimaryStorageState getOldState() {
+            return oldState;
+        }
+
+        public void setOldState(PrimaryStorageState oldState) {
+            this.oldState = oldState;
+        }
+
+        public PrimaryStorageState getNewState() {
+            return newState;
+        }
+
+        public void setNewState(PrimaryStorageState newState) {
+            this.newState = newState;
         }
 
         public PrimaryStorageInventory getInventory() {
