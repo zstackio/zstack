d867ea1bd200091ca87c7ef5978413ce115e2de4
Guo Yi <yi.guo@zstack.io>
2017-06-22 14:27:56 +0800

    Add SDK for APIPowerStatusBaremetalHostMsg

---
+++ sdk/src/main/java/org/zstack/sdk/PowerStatusBaremetalHostResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class PowerStatusBaremetalHostResult {
+    public java.lang.String status;
+    public void setStatus(java.lang.String status) {
+        this.status = status;
+    }
+    public java.lang.String getStatus() {
+        return this.status;
+    }
+
+}
