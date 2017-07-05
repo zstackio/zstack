1fc87f0a5e039d0890029c3ca0bd2b3db227d3c3
mingjian.deng <mingjian.deng@zstack.io>
2017-07-05 22:42:25 +0800

    add new api APISyncHybridEipFromRemoteMsg

---
+++ sdk/src/main/java/org/zstack/sdk/SyncHybridEipFromRemoteResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class SyncHybridEipFromRemoteResult {
+    public java.util.List<HybridEipAddressInventory> inventories;
+    public void setInventories(java.util.List<HybridEipAddressInventory> inventories) {
+        this.inventories = inventories;
+    }
+    public java.util.List<HybridEipAddressInventory> getInventories() {
+        return this.inventories;
+    }
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -13559,6 +13559,33 @@ trait ApiHelper {
     }
 
 
+    def syncHybridEipFromRemote(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SyncHybridEipFromRemoteAction.class) Closure c) {
+        def a = new org.zstack.sdk.SyncHybridEipFromRemoteAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def syncImageSize(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SyncImageSizeAction.class) Closure c) {
         def a = new org.zstack.sdk.SyncImageSizeAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
