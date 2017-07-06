f089be2160f0b6ecbcea733e34b061787a2b1d9e
Camile <camilesing@gmail.com>
2017-07-06 14:30:46 +0800

    update

---
+++ sdk/src/main/java/org/zstack/sdk/CreateEcsEipResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class CreateEcsEipResult {
+    public HybridEipAddressInventory inventory;
+    public void setInventory(HybridEipAddressInventory inventory) {
+        this.inventory = inventory;
+    }
+    public HybridEipAddressInventory getInventory() {
+        return this.inventory;
+    }
+
+}
