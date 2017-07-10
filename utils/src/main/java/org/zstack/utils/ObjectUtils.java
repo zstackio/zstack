9cb44fc83b47b00a6c64e4c072d47ef283e2c60a
J M <630911437@qq.com>
2017-07-10 16:58:31 +0800

    add premiumConfig

--- utils/src/main/java/org/zstack/utils/ObjectUtils.java
+++ utils/src/main/java/org/zstack/utils/ObjectUtils.java
@@ -1,7 +1,9 @@
 package org.zstack.utils;
 
 import org.zstack.utils.logging.CLogger;
+import org.zstack.utils.serializable.SerializableHelper;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
@@ -56,5 +58,13 @@ public class ObjectUtils {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static <T> T serializableCopy(T object) throws IOException, ClassNotFoundException {
+        if(object == null){
+            return null;
+        }
+        byte[] temp = SerializableHelper.writeObject(object);
+        return SerializableHelper.readObject(temp);
     }
 }
