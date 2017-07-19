6d70f55c37974f3311b3a7aad1bbad2dbd0826c4
mingjian.deng <mingjian.deng@zstack.io>
2017-07-19 14:32:52 +0800

    adjust API

--- sdk/src/main/java/org/zstack/sdk/AddConnectionAccessPointFromRemoteAction.java
+++ sdk/src/main/java/org/zstack/sdk/AddConnectionAccessPointFromRemoteAction.java
@@ -25,6 +25,9 @@ public class AddConnectionAccessPointFromRemoteAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String dataCenterUuid;
 
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String accessPointId;
+
     @Param(required = false)
     public java.lang.String resourceUuid;
 
