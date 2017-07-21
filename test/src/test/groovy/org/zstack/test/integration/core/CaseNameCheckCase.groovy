18253443c17c8f375ad4bb6732cf7a5285ac08a9
lining <2313806311@qq.com>
2017-07-21 14:49:43 +0800

    add CaseNameCheckCase

---
+++ test/src/test/groovy/org/zstack/test/integration/core/CaseNameCheckCase.groovy
@@ -0,0 +1,26 @@
+package org.zstack.test.integration.core
+
+import org.zstack.testlib.SubCase
+
+/**
+ * Created by lining on 2017/7/21.
+ */
+class CaseNameCheckCase extends SubCase {
+
+    @Override
+    void clean() {
+    }
+
+    @Override
+    void setup() {
+    }
+
+    @Override
+    void environment() {
+    }
+
+    @Override
+    void test() {
+        assert true
+    }
+}
