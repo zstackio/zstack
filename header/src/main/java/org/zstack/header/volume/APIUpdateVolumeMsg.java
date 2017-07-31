01194cca0a47349521a55573e33d78beb4e5f2f3
AlanJager <873863981@qq.com>
2017-07-31 14:54:02 +0800

    Add missing __notification__
    Add notification for data volume
     Resolves ZSTAC-5149 and Resolves ZSTAC-5818
    
    Add missing notification for host
     Resolves ZSTAC-5819
    
    Add missing notification for vrouter
     Resolves ZSTAC-5837

--- header/src/main/java/org/zstack/header/volume/APIUpdateVolumeMsg.java
+++ header/src/main/java/org/zstack/header/volume/APIUpdateVolumeMsg.java
@@ -2,8 +2,10 @@ package org.zstack.header.volume;
 
 import org.springframework.http.HttpMethod;
 import org.zstack.header.identity.Action;
+import org.zstack.header.message.APIEvent;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.APIParam;
+import org.zstack.header.notification.ApiNotification;
 import org.zstack.header.rest.RestRequest;
 
 /**
@@ -60,6 +62,18 @@ public class APIUpdateVolumeMsg extends APIMessage implements VolumeMessage {
         msg.setUuid(uuid());
 
         return msg;
+    }
+
+    public ApiNotification __notification__() {
+        APIMessage that = this;
+
+        return new ApiNotification() {
+            @Override
+            public void after(APIEvent evt) {
+                ntfy("Updated").resource(uuid, VolumeVO.class.getSimpleName())
+                        .messageAndEvent(that, evt).done();
+            }
+        };
     }
 
 }
