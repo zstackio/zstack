c5cab398f4f8a8968d8feea222d52be0891f8736
mingjian.deng <mingjian.deng@zstack.io>
2017-08-01 14:31:05 +0800

    use ecs type instead of instance-offering

---
+++ sdk/src/main/java/org/zstack/sdk/GetEcsInstanceTypeResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class GetEcsInstanceTypeResult {
+    public java.util.List<EcsInstanceType> types;
+    public void setTypes(java.util.List<EcsInstanceType> types) {
+        this.types = types;
+    }
+    public java.util.List<EcsInstanceType> getTypes() {
+        return this.types;
+    }
+
+}
