301f4acd7f85bd5da077024c54668638f108d63f
mingjian.deng <mingjian.deng@zstack.io>
2017-08-15 23:35:56 +0800

    fix db sql bug

--- conf/db/upgrade/V2.1.1__schema.sql
+++ conf/db/upgrade/V2.1.1__schema.sql
@@ -1,6 +1,5 @@
 ALTER TABLE IdentityZoneVO CHANGE deleted closed varchar(1) DEFAULT NULL;
 
-DELETE FROM `zstack`.`AccountResourceRefVO` where resourceType='VolumeVO' and resourceUuid not in
-(SELECT uuid FROM `zstack`.`VolumeVO`)
+DELETE FROM `zstack`.`AccountResourceRefVO` where resourceType='VolumeVO' and resourceUuid not in (SELECT uuid FROM `zstack`.`VolumeVO`);
 ALTER TABLE HybridAccountVO ADD COLUMN aliyunAccountId varchar(32) DEFAULT NULL;
 ALTER TABLE HybridAccountVO ADD COLUMN aliyunUserId varchar(32) DEFAULT NULL;
