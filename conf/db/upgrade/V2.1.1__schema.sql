6a811aa13c8a1c4c980218f9e7af17bb204f38d3
J M <630911437@qq.com>
2017-08-15 16:11:34 +0800

    Delete dirty db record Resolve ZSTAC-1334 ZSTACK-5103

--- conf/db/upgrade/V2.1.1__schema.sql
+++ conf/db/upgrade/V2.1.1__schema.sql
@@ -1,1 +1,4 @@
 ALTER TABLE IdentityZoneVO CHANGE deleted closed varchar(1) DEFAULT NULL;
+
+DELETE FROM `zstack`.`AccountResourceRefVO` where resourceType='VolumeVO' and resourceUuid not in
+(SELECT uuid FROM `zstack`.`VolumeVO`)
