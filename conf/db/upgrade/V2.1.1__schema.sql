9fccd8843a4c43f5b03063f72bb798e04044cfbe
mingjian.deng <mingjian.deng@zstack.io>
2017-08-19 16:54:12 +0800

    adjust vbr vo

--- conf/db/upgrade/V2.1.1__schema.sql
+++ conf/db/upgrade/V2.1.1__schema.sql
@@ -7,3 +7,5 @@ ALTER TABLE HybridAccountVO ADD COLUMN hybridUserName varchar(64) DEFAULT NULL;
 
 ALTER TABLE EcsInstanceVO ADD COLUMN expireDate datetime DEFAULT '2999-01-01 00:00:00';
 ALTER TABLE EcsInstanceVO ADD COLUMN chargeType varchar(16) DEFAULT 'postpaid';
+
+ALTER TABLE VirtualBorderRouterVO MODIFY COLUMN description varchar(1024) DEFAULT NULL;
