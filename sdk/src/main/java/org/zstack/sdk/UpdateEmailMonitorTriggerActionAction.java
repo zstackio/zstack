29e3c65b1944185618190250e50a44fdc1d3dd5a
lining <yaoning.li@zstack.io>
2017-08-24 00:58:32 +0800

    add api sdk

---
+++ sdk/src/main/java/org/zstack/sdk/UpdateEmailMonitorTriggerActionAction.java
@@ -0,0 +1,95 @@
+package org.zstack.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateEmailMonitorTriggerActionAction extends AbstractAction {
+
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    public static class Result {
+        public ErrorCode error;
+        public UpdateMonitorTriggerActionResult value;
+
+        public Result throwExceptionIfError() {
+            if (error != null) {
+                throw new ApiException(
+                    String.format("error[code: %s, description: %s, details: %s]", error.code, error.description, error.details)
+                );
+            }
+            
+            return this;
+        }
+    }
+
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String uuid;
+
+    @Param(required = false, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String name;
+
+    @Param(required = false, maxLength = 512, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String email;
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String mediaUuid;
+
+    @Param(required = false, maxLength = 2048, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String description;
+
+    @Param(required = false)
+    public java.util.List systemTags;
+
+    @Param(required = false)
+    public java.util.List userTags;
+
+    @Param(required = true)
+    public String sessionId;
+
+    public long timeout;
+    
+    public long pollingInterval;
+
+
+    private Result makeResult(ApiResult res) {
+        Result ret = new Result();
+        if (res.error != null) {
+            ret.error = res.error;
+            return ret;
+        }
+        
+        UpdateMonitorTriggerActionResult value = res.getResult(UpdateMonitorTriggerActionResult.class);
+        ret.value = value == null ? new UpdateMonitorTriggerActionResult() : value; 
+
+        return ret;
+    }
+
+    public Result call() {
+        ApiResult res = ZSClient.call(this);
+        return makeResult(res);
+    }
+
+    public void call(final Completion<Result> completion) {
+        ZSClient.call(this, new InternalCompletion() {
+            @Override
+            public void complete(ApiResult res) {
+                completion.complete(makeResult(res));
+            }
+        });
+    }
+
+    Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    RestInfo getRestInfo() {
+        RestInfo info = new RestInfo();
+        info.httpMethod = "PUT";
+        info.path = "/monitoring/trigger-actions/emails/{uuid}";
+        info.needSession = true;
+        info.needPoll = true;
+        info.parameterName = "updateEmailMonitorTriggerAction";
+        return info;
+    }
+
+}
