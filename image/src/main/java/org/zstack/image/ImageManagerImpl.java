7cc73c6ffa147c4c2fd696c9eea16798cfea7423
Camile <camilesing@gmail.com>
2017-08-29 17:09:54 +0800

    optimization db operation

--- image/src/main/java/org/zstack/image/ImageManagerImpl.java
+++ image/src/main/java/org/zstack/image/ImageManagerImpl.java
@@ -953,9 +953,9 @@ public class ImageManagerImpl extends AbstractService implements ImageManager, M
             ref.setBackupStorageUuid(uuid);
             ref.setStatus(ImageStatus.Downloading);
             ref.setImageUuid(ivo.getUuid());
-            ref = dbf.persistAndRefresh(ref);
             refs.add(ref);
         }
+        dbf.updateCollection(refs);
 
         Defer.guard(() -> dbf.remove(ivo));
 
