a14f269bf5579e138b37f4e48f71e30c31b4c005
AlanJager <873863981@qq.com>
2017-09-02 13:21:51 +0800

    Update sdk

---
+++ sdk/src/main/java/org/zstack/sdk/UpdateLicenseAction.java
@@ -0,0 +1,82 @@
+package org.zstack.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateLicenseAction extends AbstractAction {
+
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    public static class Result {
+        public ErrorCode error;
+        public UpdateLicenseResult value;
+
+        public Result throwExceptionIfError() {
+            if (error != null) {
+                throw new ApiException(
+                    String.format("error[code: %s, description: %s, details: %s]", error.code, error.description, error.details)
+                );
+            }
+            
+            return this;
+        }
+    }
+
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String managementNodeUuid;
+
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String license;
+
+    @Param(required = false)
+    public java.util.List systemTags;
+
+    @Param(required = false)
+    public java.util.List userTags;
+
+    @Param(required = true)
+    public String sessionId;
+
+
+    private Result makeResult(ApiResult res) {
+        Result ret = new Result();
+        if (res.error != null) {
+            ret.error = res.error;
+            return ret;
+        }
+        
+        UpdateLicenseResult value = res.getResult(UpdateLicenseResult.class);
+        ret.value = value == null ? new UpdateLicenseResult() : value; 
+
+        return ret;
+    }
+
+    public Result call() {
+        ApiResult res = ZSClient.call(this);
+        return makeResult(res);
+    }
+
+    public void call(final Completion<Result> completion) {
+        ZSClient.call(this, new InternalCompletion() {
+            @Override
+            public void complete(ApiResult res) {
+                completion.complete(makeResult(res));
+            }
+        });
+    }
+
+    Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    RestInfo getRestInfo() {
+        RestInfo info = new RestInfo();
+        info.httpMethod = "PUT";
+        info.path = "/licenses/mn/{managementNodeUuid}/actions";
+        info.needSession = true;
+        info.needPoll = false;
+        info.parameterName = "updateLicense";
+        return info;
+    }
+
+}
---
+++ sdk/src/main/java/org/zstack/sdk/UpdateLicenseResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class UpdateLicenseResult {
+    public LicenseInventory inventory;
+    public void setInventory(LicenseInventory inventory) {
+        this.inventory = inventory;
+    }
+    public LicenseInventory getInventory() {
+        return this.inventory;
+    }
+
+}
