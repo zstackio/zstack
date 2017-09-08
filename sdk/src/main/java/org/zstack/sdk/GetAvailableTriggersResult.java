073e58a32583a74d813351f9e11dec7bda391b77
Author: AlanJager <alanjager@outlook.com>
2017-09-08 15:54:52 +0800

    Generate sdk
    Resolves ZSTAC-6721
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

---
+++ sdk/src/main/java/org/zstack/sdk/GetAvailableTriggersResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class GetAvailableTriggersResult {
+    public java.util.List<SchedulerTriggerInventory> inventories;
+    public void setInventories(java.util.List<SchedulerTriggerInventory> inventories) {
+        this.inventories = inventories;
+    }
+    public java.util.List<SchedulerTriggerInventory> getInventories() {
+        return this.inventories;
+    }
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -7440,6 +7440,33 @@ trait ApiHelper {
     }
 
 
+    def getAvailableTriggers(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetAvailableTriggersAction.class) Closure c) {
+        def a = new org.zstack.sdk.GetAvailableTriggersAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getBackupStorageCapacity(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetBackupStorageCapacityAction.class) Closure c) {
         def a = new org.zstack.sdk.GetBackupStorageCapacityAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
