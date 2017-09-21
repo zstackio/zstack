5ba201588cd5e4695b9528ae82353f678d7219c9
mingjian.deng <mingjian.deng@zstack.io>
2017-09-21 17:25:35 +0800

    update sdk

--- sdk/src/main/java/org/zstack/sdk/SyncVirtualBorderRouterFromRemoteResult.java
+++ sdk/src/main/java/org/zstack/sdk/SyncVirtualBorderRouterFromRemoteResult.java
@@ -1,12 +1,12 @@
 package org.zstack.sdk;
 
 public class SyncVirtualBorderRouterFromRemoteResult {
-    public java.util.List<VirtualBorderRouterInventory> inventoies;
-    public void setInventoies(java.util.List<VirtualBorderRouterInventory> inventoies) {
-        this.inventoies = inventoies;
+    public java.util.List<VirtualBorderRouterInventory> inventories;
+    public void setInventories(java.util.List<VirtualBorderRouterInventory> inventories) {
+        this.inventories = inventories;
     }
-    public java.util.List<VirtualBorderRouterInventory> getInventoies() {
-        return this.inventoies;
+    public java.util.List<VirtualBorderRouterInventory> getInventories() {
+        return this.inventories;
     }
 
 }
