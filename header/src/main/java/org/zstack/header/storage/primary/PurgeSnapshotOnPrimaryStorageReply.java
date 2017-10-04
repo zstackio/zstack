592274060865d75c01c5d544a165fe0099e3f70c
Guo Yi <yi.guo@zstack.io>
2017-10-04 17:09:52 +0800

    Reimplement storage migration

---
+++ header/src/main/java/org/zstack/header/storage/primary/PurgeSnapshotOnPrimaryStorageReply.java
@@ -0,0 +1,9 @@
+package org.zstack.header.storage.primary;
+
+import org.zstack.header.message.MessageReply;
+
+/**
+ * Created by GuoYi on 10/12/17.
+ */
+public class PurgeSnapshotOnPrimaryStorageReply extends MessageReply {
+}
