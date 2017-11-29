a0258c82727de2f3b3d7147d8636d04476490bdb
Frank.Zhang <xin.zhang@mevoco.com>
2017-11-29 09:04:05 -0500

    fix http://dev.zstack.io/browse/ZSTAC-7627 fix ut

--- test/src/test/groovy/org/zstack/test/integration/kvm/host/ReconnectHostCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/kvm/host/ReconnectHostCase.groovy
@@ -1,17 +1,7 @@
 package org.zstack.test.integration.kvm.host
 
-import groovy.transform.Synchronized
-import org.zstack.compute.vm.VmInstanceBase
-import org.zstack.core.cloudbus.CloudBus
-import org.zstack.core.db.DatabaseFacade
-import org.zstack.core.db.Q
-import org.zstack.header.host.HostStatus
 import org.zstack.header.vm.VmInstanceState
-import org.zstack.header.vm.VmInstanceStateEvent
-import org.zstack.header.vm.VmInstanceVO
-import org.zstack.header.vm.VmInstanceVO_
-import org.zstack.header.vm.VmStateChangedOnHostMsg
-import org.zstack.header.vm.VmStateChangedOnHostReply
+import org.zstack.kvm.KVMConstant
 import org.zstack.sdk.VmInstanceInventory
 import org.zstack.test.integration.kvm.Env
 import org.zstack.test.integration.kvm.KvmTest
@@ -27,7 +17,6 @@ class ReconnectHostCase extends SubCase{
     EnvSpec env
     private final static CLogger logger = Utils.getLogger(ReconnectHostCase.class)
     static RECONNECT_TIME = 10
-    volatile int unknownVm, runVm, other
 
     @Override
     void setup() {
@@ -42,7 +31,8 @@ class ReconnectHostCase extends SubCase{
     @Override
     void test() {
         env.create {
-            testRreconnectHostVmState()
+            testReconnectHostVmState()
+            testReconnectFailureHostVmState()
         }
     }
 
@@ -51,22 +41,31 @@ class ReconnectHostCase extends SubCase{
         env.delete()
     }
 
-    void testRreconnectHostVmState(){
+    void testReconnectFailureHostVmState() {
         VmInstanceInventory vmInv = env.inventoryByName("vm") as VmInstanceInventory
-        int numberOfVm = Q.New(VmInstanceVO.class).eq(VmInstanceVO_.hostUuid, vmInv.hostUuid).count()
-        DatabaseFacade dbf = bean(DatabaseFacade.class)
 
-        unknownVm = 0
-        runVm = 0
-        other = 0
-        env.message(VmStateChangedOnHostMsg.class) { VmStateChangedOnHostMsg msg, CloudBus bus ->
-            addTimes(msg.getStateOnHost())
-            VmInstanceVO vmvo = Q.New(VmInstanceVO.class).eq(VmInstanceVO_.uuid, msg.vmInstanceUuid).find()
-            vmvo.setState(VmInstanceState.valueOf(msg.stateOnHost))
-            dbf.updateAndRefresh(vmvo)
-            def reply = new VmStateChangedOnHostReply()
-            bus.reply(msg, reply)
+        env.simulator(KVMConstant.KVM_CONNECT_PATH) {
+            throw new Exception("on purpose")
         }
+
+        expect(AssertionError.class) {
+            reconnectHost {
+                uuid = vmInv.hostUuid
+            }
+        }
+
+        retryInSecs {
+            List<VmInstanceInventory> vmInvs = queryVmInstance {
+                conditions = ["state=${VmInstanceState.Unknown}"]
+            } as List<VmInstanceInventory>
+
+            assert vmInvs.size() == 2
+        }
+    }
+
+    // Reconnect host will not change VM's state, this test confirm VMs are always Running after host reconnecting
+    void testReconnectHostVmState(){
+        VmInstanceInventory vmInv = env.inventoryByName("vm") as VmInstanceInventory
 
         for (int i = 0; i < RECONNECT_TIME; i++) {
             reconnectHost {
@@ -81,27 +80,6 @@ class ReconnectHostCase extends SubCase{
 
                 assert vmInvs.size() == 2
             }
-            assert runVm == numberOfVm * (i + 1)
-            assert unknownVm == numberOfVm * (i + 1)
-        }
-
-        assert runVm == numberOfVm * RECONNECT_TIME
-        assert unknownVm == numberOfVm * RECONNECT_TIME
-    }
-
-    @Synchronized
-    void addTimes(String status){
-        int temp
-        if(status == VmInstanceState.Running.toString()){
-            temp = runVm
-            runVm ++
-            logger.debug(String.format("runVm add one: from:%d to %d", temp, runVm))
-        }else if(status == VmInstanceState.Unknown.toString()){
-            temp = unknownVm
-            unknownVm ++
-            logger.debug(String.format("unknownVm add one: from:%d to %d", temp, unknownVm))
-        }else {
-            other ++
         }
     }
 }
