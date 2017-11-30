eee778806750fcd8e6e4d7a95761ec8409351157
GuoYi <yi.guo@zstack.io>
2017-12-01 00:08:30 +0800

    Fixes ZSTAC-8345
    resume vm info when ChangeVmImageMsg rollback

---
+++ compute/src/main/java/org/zstack/compute/vm/VmInstantiateResourceForChangeImageFlow.java
@@ -0,0 +1,99 @@
+package org.zstack.compute.vm;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.zstack.core.componentloader.PluginRegistry;
+import org.zstack.header.core.Completion;
+import org.zstack.header.core.workflow.Flow;
+import org.zstack.header.core.workflow.FlowException;
+import org.zstack.header.core.workflow.FlowRollback;
+import org.zstack.header.core.workflow.FlowTrigger;
+import org.zstack.header.errorcode.ErrorCode;
+import org.zstack.header.vm.*;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
+public class VmInstantiateResourceForChangeImageFlow implements Flow {
+    private static final CLogger logger = Utils.getLogger(VmInstantiateResourceForChangeImageFlow.class);
+
+    @Autowired
+    private PluginRegistry pluginRgty;
+
+    private static List<ChangeVmImageExtensionPoint> extensions = null;
+
+    public VmInstantiateResourceForChangeImageFlow() {
+        if (extensions == null) {
+            extensions = pluginRgty.getExtensionList(ChangeVmImageExtensionPoint.class);
+        }
+    }
+    
+    private void runExtensions(final Iterator<ChangeVmImageExtensionPoint> it, final VmInstanceSpec spec, final FlowTrigger chain) {
+        if (!it.hasNext()) {
+            chain.next();
+            return;
+        }
+
+        ChangeVmImageExtensionPoint extp = it.next();
+
+        logger.debug(String.format("run VmInstantiateResourceExtensionPoint[%s]", extp.getClass()));
+        extp.preInstantiateVmResource(spec, new Completion(chain) {
+            @Override
+            public void success() {
+                runExtensions(it, spec, chain);
+            }
+
+            @Override
+            public void fail(ErrorCode errorCode) {
+                chain.fail(errorCode);
+            }
+        });
+    }
+
+    @Override
+    public void run(FlowTrigger chain, Map data) {
+        VmInstanceSpec spec = (VmInstanceSpec) data.get(VmInstanceConstant.Params.VmInstanceSpec.toString());
+        for (ChangeVmImageExtensionPoint extp : extensions) {
+            try {
+                extp.preBeforeInstantiateVmResource(spec);
+            } catch (VmInstantiateResourceException vie) {
+                throw new FlowException(vie.getErrorCode());
+            }
+        }
+
+        runExtensions(extensions.iterator(), spec, chain);
+    }
+
+    private void rollbackExtensions(final Iterator<ChangeVmImageExtensionPoint> it, final VmInstanceSpec spec, final FlowRollback chain) {
+        if (!it.hasNext()) {
+            chain.rollback();
+            return;
+        }
+
+        ChangeVmImageExtensionPoint extp = it.next();
+        logger.debug(String.format("rollback VmInstantiateResourceExtensionPoint[%s]", extp.getClass()));
+        extp.preReleaseVmResource(spec, new Completion(chain) {
+            @Override
+            public void success() {
+                rollbackExtensions(it, spec, chain);
+            }
+
+            @Override
+            public void fail(ErrorCode errorCode) {
+                logger.warn(errorCode.toString());
+                rollbackExtensions(it, spec, chain);
+            }
+        });
+    }
+
+    @Override
+    public void rollback(FlowRollback chain, Map data) {
+        VmInstanceSpec spec = (VmInstanceSpec) data.get(VmInstanceConstant.Params.VmInstanceSpec.toString());
+        rollbackExtensions(extensions.iterator(), spec, chain);
+    }
+}
---
+++ storage/src/main/java/org/zstack/storage/volume/InstantiateVolumeForVmChangeImageExtension.java
@@ -0,0 +1,9 @@
+package org.zstack.storage.volume;
+
+import org.zstack.header.vm.ChangeVmImageExtensionPoint;
+
+/**
+ * Created by GuoYi on 12/1/17.
+ */
+public class InstantiateVolumeForVmChangeImageExtension extends InstantiateVolumeForNewCreatedVmExtension implements ChangeVmImageExtensionPoint {
+}
