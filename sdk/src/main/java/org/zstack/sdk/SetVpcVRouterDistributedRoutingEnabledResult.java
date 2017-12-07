49c5ba975c672a9505c6cf245e194b1f88882d59
Wei Wang <wei.wang@zstack.io>
2017-12-07 13:17:02 +0800

    Support zsn api

---
+++ sdk/src/main/java/org/zstack/sdk/SetVpcVRouterDistributedRoutingEnabledResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class SetVpcVRouterDistributedRoutingEnabledResult {
+    public boolean enabled;
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    public boolean getEnabled() {
+        return this.enabled;
+    }
+
+}
