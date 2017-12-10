f0c99638c747d388fce90a74b7b4dcbebadc2482
Frank.Zhang <xin.zhang@mevoco.com>
2017-12-10 01:54:38 -0500

    working on zwatch

--- plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/APIGetL3NetworkDhcpIpAddressMsg.java
+++ plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/APIGetL3NetworkDhcpIpAddressMsg.java
@@ -3,9 +3,12 @@ package org.zstack.network.service.flat;
 import org.springframework.http.HttpMethod;
 import org.zstack.header.identity.Action;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.message.APIParam;
 import org.zstack.header.message.APISyncCallMessage;
+import org.zstack.header.network.l3.L3Network;
 import org.zstack.header.network.l3.L3NetworkConstant;
 import org.zstack.header.network.l3.L3NetworkMessage;
+import org.zstack.header.network.l3.L3NetworkVO;
 import org.zstack.header.rest.RestRequest;
 
 /**
@@ -18,6 +21,7 @@ import org.zstack.header.rest.RestRequest;
         responseClass = APIGetL3NetworkDhcpIpAddressReply.class
 )
 public class APIGetL3NetworkDhcpIpAddressMsg extends APISyncCallMessage implements L3NetworkMessage {
+    @APIParam(resourceType = L3NetworkVO.class, checkAccount = true, operationTarget = true)
     private String l3NetworkUuid;
 
     @Override
