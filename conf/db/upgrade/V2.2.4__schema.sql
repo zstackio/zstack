f36bcb74102b4e8964eb0ef4dbcede02c0f15fa8
J M <630911437@qq.com>
2017-12-13 15:22:02 +0800

    Resovle ZSTAC-8407 add IpsecVO extends ResourceVO

--- conf/db/upgrade/V2.2.4__schema.sql
+++ conf/db/upgrade/V2.2.4__schema.sql
@@ -1,2 +1,4 @@
 ALTER TABLE VCenterPrimaryStorageVO ADD COLUMN datastore varchar(64) DEFAULT NULL;
 ALTER TABLE VCenterBackupStorageVO  ADD COLUMN datastore varchar(64) DEFAULT NULL;
+
+INSERT INTO ResourceVO (uuid, resourceName, resourceType) SELECT t.uuid, t.name, "IPsecConnectionVO" FROM IPsecConnectionVO t;
