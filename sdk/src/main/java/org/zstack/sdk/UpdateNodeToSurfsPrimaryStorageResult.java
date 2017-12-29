cb04dd862946d7efc0cd5c7a22bac388e84fb291
[zhp36] <[zhp36@sohu.com]>
2017-12-29 13:34:30 -0500

    add surfs java sdk api

---
+++ sdk/src/main/java/org/zstack/sdk/UpdateNodeToSurfsPrimaryStorageResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class UpdateNodeToSurfsPrimaryStorageResult {
+    public SurfsPrimaryStorageInventory inventory;
+    public void setInventory(SurfsPrimaryStorageInventory inventory) {
+        this.inventory = inventory;
+    }
+    public SurfsPrimaryStorageInventory getInventory() {
+        return this.inventory;
+    }
+
+}
