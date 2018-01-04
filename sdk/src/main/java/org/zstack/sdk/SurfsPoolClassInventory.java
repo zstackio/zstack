7b559f47de4054aed5d26be3a83f1bb40b5412a8
[zhp36] <[zhp36@sohu.com]>
2018-01-04 11:40:23 -0500

    add surfs file to zstack add surfs class config
    add surfs.xml path
    
    add surfs.xml for globalcfg
    
    add surfs.xml for serviceConfig
    
    add surfs.xml for springConfig
    
    add surfs message
    
    add surfs sdk api
    
    add surfs plugin code
    
    add surfs module
    
    add sql file for surfs

---
+++ sdk/src/main/java/org/zstack/sdk/SurfsPoolClassInventory.java
@@ -0,0 +1,85 @@
+package org.zstack.sdk;
+
+public class SurfsPoolClassInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String fsid;
+    public void setFsid(java.lang.String fsid) {
+        this.fsid = fsid;
+    }
+    public java.lang.String getFsid() {
+        return this.fsid;
+    }
+
+    public java.lang.String clsname;
+    public void setClsname(java.lang.String clsname) {
+        this.clsname = clsname;
+    }
+    public java.lang.String getClsname() {
+        return this.clsname;
+    }
+
+    public java.lang.String clsdisplayname;
+    public void setClsdisplayname(java.lang.String clsdisplayname) {
+        this.clsdisplayname = clsdisplayname;
+    }
+    public java.lang.String getClsdisplayname() {
+        return this.clsdisplayname;
+    }
+
+    public boolean isrootcls;
+    public void setIsrootcls(boolean isrootcls) {
+        this.isrootcls = isrootcls;
+    }
+    public boolean getIsrootcls() {
+        return this.isrootcls;
+    }
+
+    public boolean isactive;
+    public void setIsactive(boolean isactive) {
+        this.isactive = isactive;
+    }
+    public boolean getIsactive() {
+        return this.isactive;
+    }
+
+    public long totalCapacity;
+    public void setTotalCapacity(long totalCapacity) {
+        this.totalCapacity = totalCapacity;
+    }
+    public long getTotalCapacity() {
+        return this.totalCapacity;
+    }
+
+    public long availableCapacity;
+    public void setAvailableCapacity(long availableCapacity) {
+        this.availableCapacity = availableCapacity;
+    }
+    public long getAvailableCapacity() {
+        return this.availableCapacity;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
