f2e10b4021102308d5a8b38971508c91502a327d
[zhp36] <[zhp36@sohu.com]>
2018-01-04 11:40:23 -0500

    add surfs file to zstack add surfs class config
    add surfs.xml path
    
    add surfs.xml for globalcfg
    
    add surfs.xml for serviceConfig
    
    add surfs.xml for springConfig
    
    add surfs message
    
    add surfs sdk api
    
    add surfs plugin code
    
    add surfs module

---
+++ sdk/src/main/java/org/zstack/sdk/UpdateSurfsBackupStorageNodeAction.java
@@ -0,0 +1,106 @@
+package org.zstack.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateSurfsBackupStorageNodeAction extends AbstractAction {
+
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    private static final HashMap<String, Parameter> nonAPIParameterMap = new HashMap<>();
+
+    public static class Result {
+        public ErrorCode error;
+        public UpdateNodeToSurfsBackupStorageResult value;
+
+        public Result throwExceptionIfError() {
+            if (error != null) {
+                throw new ApiException(
+                    String.format("error[code: %s, description: %s, details: %s]", error.code, error.description, error.details)
+                );
+            }
+            
+            return this;
+        }
+    }
+
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = false, noTrim = false)
+    public java.lang.String nodeUuid;
+
+    @Param(required = false, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String hostname;
+
+    @Param(required = false, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String sshUsername;
+
+    @Param(required = false, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String sshPassword;
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, numberRange = {1L,65535L}, noTrim = false)
+    public java.lang.Integer sshPort;
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, numberRange = {1L,65535L}, noTrim = false)
+    public java.lang.Integer nodePort;
+
+    @Param(required = false)
+    public java.util.List systemTags;
+
+    @Param(required = false)
+    public java.util.List userTags;
+
+    @Param(required = true)
+    public String sessionId;
+
+    @NonAPIParam
+    public long timeout = -1;
+
+    @NonAPIParam
+    public long pollingInterval = -1;
+
+
+    private Result makeResult(ApiResult res) {
+        Result ret = new Result();
+        if (res.error != null) {
+            ret.error = res.error;
+            return ret;
+        }
+        
+        UpdateNodeToSurfsBackupStorageResult value = res.getResult(UpdateNodeToSurfsBackupStorageResult.class);
+        ret.value = value == null ? new UpdateNodeToSurfsBackupStorageResult() : value; 
+
+        return ret;
+    }
+
+    public Result call() {
+        ApiResult res = ZSClient.call(this);
+        return makeResult(res);
+    }
+
+    public void call(final Completion<Result> completion) {
+        ZSClient.call(this, new InternalCompletion() {
+            @Override
+            public void complete(ApiResult res) {
+                completion.complete(makeResult(res));
+            }
+        });
+    }
+
+    Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    Map<String, Parameter> getNonAPIParameterMap() {
+        return nonAPIParameterMap;
+    }
+
+    RestInfo getRestInfo() {
+        RestInfo info = new RestInfo();
+        info.httpMethod = "PUT";
+        info.path = "/backup-storage/surfs/nodes/{nodeUuid}/actions";
+        info.needSession = true;
+        info.needPoll = true;
+        info.parameterName = "updateSurfsBackupStorageNode";
+        return info;
+    }
+
+}
