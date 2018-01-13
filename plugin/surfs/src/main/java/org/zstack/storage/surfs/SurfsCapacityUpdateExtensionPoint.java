03feff9f09602b36e6b8fa7da4809a5812ab4c5d
shuang.he <shuang.he@zstack.io>
2018-01-13 15:57:56 +0000

    Revert "Merge branch 'addsurf@@3' into 'master'"
    This reverts merge request !2054

--- plugin/surfs/src/main/java/org/zstack/storage/surfs/SurfsCapacityUpdateExtensionPoint.java
+++
@@ -1,8 +0,0 @@
-package org.zstack.storage.surfs;
-
-/**
- * Created by zhouhaiping 2017-09-01
- */
-public interface SurfsCapacityUpdateExtensionPoint {
-    void update(String fsid,long total, long avail);
-}
