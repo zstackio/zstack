64555bde79861737ff20603cb91d756093148942
Frank.Zhang <xin.zhang@mevoco.com>
2018-01-14 17:55:57 -0500

    working on zwatch

--- core/src/main/java/org/zstack/core/externalservice/AbstractLocalExternalService.java
+++ core/src/main/java/org/zstack/core/externalservice/AbstractLocalExternalService.java
@@ -1,15 +1,27 @@
 package org.zstack.core.externalservice;
 
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.zstack.core.Platform;
+import org.zstack.core.thread.CancelablePeriodicTask;
+import org.zstack.core.thread.ThreadFacade;
+import org.zstack.core.thread.TimerTask;
+import org.zstack.utils.DebugUtils;
 import org.zstack.utils.ProcessFinder;
 import org.zstack.utils.Utils;
 import org.zstack.utils.logging.CLogger;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
 @Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
 public abstract class AbstractLocalExternalService implements LocalExternalService {
     protected static final CLogger logger = Utils.getLogger(AbstractLocalExternalService.class);
+
+    @Autowired
+    protected ThreadFacade thdf;
 
     protected abstract String[] getCommandLineKeywords();
 
@@ -31,6 +43,87 @@ public abstract class AbstractLocalExternalService implements LocalExternalServi
 
         logger.debug(String.format("[External Service] stopped %s", getName()));
     }
+
+    protected void doActionIfServicePIDNotShowUpInTwoMinutes(Runnable runnable) {
+        ActionIfServiceNotUp a = new ActionIfServiceNotUp();
+        a.action = runnable;
+        a.run();
+    }
+
+    protected class ActionIfServiceNotUp implements Runnable {
+        public Integer timeout = 120; // in secs
+        public int interval = 2; // in secs
+        public Runnable action;
+        public Callable<Boolean> upChecker = () -> getPID() != null;
+        public int successTimes = 3;
+
+        private long endTime = System.currentTimeMillis();
+
+        private void doAction() {
+            action.run();
+        }
+
+        @Override
+        public void run() {
+            DebugUtils.Assert(action != null, "action cannot be null");
+            DebugUtils.Assert(upChecker != null, "upChecker cannot be null");
+            DebugUtils.Assert(timeout != null, "timeout cannot be null");
+
+            endTime = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(timeout);
+
+            thdf.submitCancelablePeriodicTask(new CancelablePeriodicTask() {
+                private boolean checkServiceUp() throws Exception {
+                    for (int i=0; i<successTimes; i++) {
+                        if (!upChecker.call()) {
+                            return false;
+                        }
+
+                        TimeUnit.SECONDS.sleep(interval);
+                    }
+
+                    return true;
+                }
+
+                @Override
+                public boolean run() {
+                    if (System.currentTimeMillis() >= endTime) {
+                        // timeout
+                        doAction();
+                        return true;
+                    }
+
+                    try {
+                        if (checkServiceUp()) {
+                            // service up
+                            return true;
+                        }
+                    } catch (Throwable t) {
+                        logger.warn("unhandled exception", t);
+                        return false;
+                    }
+
+                    // service not up, continue
+                    return false;
+                }
+
+                @Override
+                public TimeUnit getTimeUnit() {
+                    return TimeUnit.SECONDS;
+                }
+
+                @Override
+                public long getInterval() {
+                    return interval;
+                }
+
+                @Override
+                public String getName() {
+                    return "wait-for-external-service-up";
+                }
+            });
+        }
+    }
+
 
     public void restart() {
         stop();
