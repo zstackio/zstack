dc7de5f6c0264265a1c4603be424e3cb1821eb64
GuoYi <yi.guo@zstack.io>
2018-01-16 15:08:15 +0800

    Update LongJob Doc

--- header/src/main/java/org/zstack/header/longjob/APIDeleteLongJobMsgDoc_zh_cn.groovy
+++ header/src/main/java/org/zstack/header/longjob/APIDeleteLongJobMsgDoc_zh_cn.groovy
@@ -13,8 +13,8 @@ doc {
         request {
 			url "DELETE /v1/longjobs/{uuid}"
 
+			header (Authorization: 'OAuth the-session-uuid')
 
-            header(Authorization: 'OAuth the-session-uuid')
 
             clz APIDeleteLongJobMsg.class
 
