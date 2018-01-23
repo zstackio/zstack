6e1229889ff790d8ef921b79eb329a919ef45564
[zhp36] <[zhp36@sohu.com]>
2018-01-23 13:18:07 -0500

    Add surfs file to zstack update config file for surfs
    update build setting for surfs
    
    add java api for surfs
    
    add surfs java code to zstack

---
+++ sdk/src/main/java/org/zstack/sdk/RemoveNodeFromSurfsBackupStorageResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class RemoveNodeFromSurfsBackupStorageResult {
+    public SurfsBackupStorageInventory inventory;
+    public void setInventory(SurfsBackupStorageInventory inventory) {
+        this.inventory = inventory;
+    }
+    public SurfsBackupStorageInventory getInventory() {
+        return this.inventory;
+    }
+
+}
