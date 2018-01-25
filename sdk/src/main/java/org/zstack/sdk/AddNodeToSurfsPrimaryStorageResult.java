7ff2cbdf4e5ab05b338b3cd6ba6134dc54d3ecc7
[zhp36] <[zhp36@sohu.com]>
2018-01-25 13:18:28 -0500

    Add surfs file to zstack add surfs java file to zstack
    update build pom.xml setting for surfs
    
    add conf setting xml for surfs
    
    add sdk java api for surfs

---
+++ sdk/src/main/java/org/zstack/sdk/AddNodeToSurfsPrimaryStorageResult.java
@@ -0,0 +1,12 @@
+package org.zstack.sdk;
+
+public class AddNodeToSurfsPrimaryStorageResult {
+    public SurfsPrimaryStorageInventory inventory;
+    public void setInventory(SurfsPrimaryStorageInventory inventory) {
+        this.inventory = inventory;
+    }
+    public SurfsPrimaryStorageInventory getInventory() {
+        return this.inventory;
+    }
+
+}
