984896a2c40e0849eed110ccc4edd20dcd0bc1a0
Qun Li <qun.li@zstack.io>
2018-01-30 18:09:53 +0800

    Add surfs plugin from github.
    c.f. https://github.com/zstackio/zstack/pull/1233
    
    Signed-off-by: Qun Li <qun.li@zstack.io>

---
+++ plugin/surfs/src/main/java/org/zstack/storage/surfs/backup/APIAddSurfsBackupStorageMsgDoc_zh_cn.groovy
@@ -0,0 +1,132 @@
+package org.zstack.storage.surfs.backup
+
+import org.zstack.header.storage.backup.APIAddBackupStorageEvent
+
+doc {
+    title "AddSurfsBackupStorage"
+
+    category "未知类别"
+
+    desc """在这里填写API描述"""
+
+    rest {
+        request {
+			url "POST /v1/backup-storage/surfs"
+
+
+            header(Authorization: 'OAuth the-session-uuid')
+
+            clz APIAddSurfsBackupStorageMsg.class
+
+            desc """"""
+            
+			params {
+
+				column {
+					name "nodeUrls"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "List"
+					optional false
+					since "0.6"
+					
+				}
+				column {
+					name "poolName"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "url"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional false
+					since "0.6"
+					
+				}
+				column {
+					name "name"
+					enclosedIn "params"
+					desc "资源名称"
+					location "body"
+					type "String"
+					optional false
+					since "0.6"
+					
+				}
+				column {
+					name "description"
+					enclosedIn "params"
+					desc "资源的详细描述"
+					location "body"
+					type "String"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "type"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "importImages"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "boolean"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "resourceUuid"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "systemTags"
+					enclosedIn ""
+					desc ""
+					location "body"
+					type "List"
+					optional true
+					since "0.6"
+					
+				}
+				column {
+					name "userTags"
+					enclosedIn ""
+					desc ""
+					location "body"
+					type "List"
+					optional true
+					since "0.6"
+					
+				}
+			}
+        }
+
+        response {
+            clz APIAddBackupStorageEvent.class
+        }
+    }
+}
