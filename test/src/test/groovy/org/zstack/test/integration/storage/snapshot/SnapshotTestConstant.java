792bac2881c8177d296f6c49b32e1e5ac6a1dfd3
Qun Li <qun.li@zstack.io>
2018-03-28 23:23:22 +0800

    Resolves ZSTAC-10570
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- test/src/test/groovy/org/zstack/test/integration/storage/snapshot/SnapshotTestConstant.java
+++ test/src/test/groovy/org/zstack/test/integration/storage/snapshot/SnapshotTestConstant.java
@@ -4,5 +4,5 @@ package org.zstack.test.integration.storage.snapshot;
  * Created by ads6 on 2018/1/4.
  */
 public interface SnapshotTestConstant {
-    int ROOT_SNAPSHOT_DISNTANCE = 1;
+    int ROOT_SNAPSHOT_DISNTANCE = 0;
 }
--- test/src/test/groovy/org/zstack/test/integration/storage/volume/PerVolumeSnapshotMaxNumCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/storage/volume/PerVolumeSnapshotMaxNumCase.groovy
@@ -2,6 +2,7 @@ package org.zstack.test.integration.storage.volume
 
 import org.zstack.core.db.Q
 import org.zstack.header.storage.snapshot.VolumeSnapshotTreeVO
+import org.zstack.header.storage.snapshot.VolumeSnapshotTreeVO_
 import org.zstack.header.volume.VolumeVO
 import org.zstack.sdk.ImageInventory
 import org.zstack.sdk.InstanceOfferingInventory
@@ -9,11 +10,10 @@ import org.zstack.sdk.L3NetworkInventory
 import org.zstack.sdk.VmInstanceInventory
 import org.zstack.storage.snapshot.VolumeSnapshotGlobalConfig
 import org.zstack.storage.volume.VolumeSystemTags
-import org.zstack.test.integration.storage.StorageTest
 import org.zstack.test.integration.storage.Env
+import org.zstack.test.integration.storage.StorageTest
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.SubCase
-
 /**
  * Created by MaJin on 2017-10-19.
  */
@@ -40,6 +40,7 @@ class PerVolumeSnapshotMaxNumCase extends SubCase{
             testNoSystemTagCreateSnapshot()
             testPerVolumeSystemTagCreateSnapshot()
             testAddVolumeSystemTagAfterCreateSnapshot()
+            testVolumeSnapshotTree()
         }
     }
 
@@ -117,6 +118,44 @@ class PerVolumeSnapshotMaxNumCase extends SubCase{
         createSnapshots(vm2.rootVolumeUuid, 1)
 
         assert Q.New(VolumeSnapshotTreeVO.class).count() == nowCount + 1
+    }
+
+    void testVolumeSnapshotTree() {
+        def instanceOfferingInv = env.inventoryByName("instanceOffering") as InstanceOfferingInventory
+        def imageInv = env.inventoryByName("image1") as ImageInventory
+        def l3Inv = env.inventoryByName("l3") as L3NetworkInventory
+        def vm3 = createVmInstance {
+            name = "vm3"
+            instanceOfferingUuid = instanceOfferingInv.uuid
+            imageUuid = imageInv.uuid
+            l3NetworkUuids = [l3Inv.uuid]
+        } as VmInstanceInventory
+
+        assert getVolumeTreeCount(vm3.rootVolumeUuid) == 0
+
+        createSystemTag {
+            resourceType = VolumeVO.getSimpleName()
+            resourceUuid = vm3.rootVolumeUuid
+            tag = "volumeMaxIncrementalSnapshotNum::1"
+        }
+
+        createSnapshots(vm3.rootVolumeUuid, 1)
+        assert getVolumeTreeCount(vm3.rootVolumeUuid) == 1
+
+        createSnapshots(vm3.rootVolumeUuid, 1)
+        assert getVolumeTreeCount(vm3.rootVolumeUuid) == 2
+
+        createSnapshots(vm3.rootVolumeUuid, 1)
+        assert getVolumeTreeCount(vm3.rootVolumeUuid) == 2
+
+        createSnapshots(vm3.rootVolumeUuid, 1)
+        assert getVolumeTreeCount(vm3.rootVolumeUuid) == 3
+    }
+
+    private static Long getVolumeTreeCount(String volUuid) {
+        return Q.New(VolumeSnapshotTreeVO.class)
+                .eq(VolumeSnapshotTreeVO_.volumeUuid, volUuid)
+                .count()
     }
 
     private void createSnapshots(String volUuid, int count){
