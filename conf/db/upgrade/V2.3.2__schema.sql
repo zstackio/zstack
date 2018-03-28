0d25cb4b38201a596ad892b29d8eeebcbdb6e125
Frank.Zhang <xin.zhang@mevoco.com>
2018-03-28 15:15:57 -0400

    working on rbac

---
+++ conf/db/upgrade/V2.3.2__schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE `RoleAccountRefVO` (
+    `roleUuid` VARCHAR(32) NOT NULL,
+    `accountUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`roleUuid`,`accountUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RolePolicyRefVO` (
+    `roleUuid` VARCHAR(32) NOT NULL,
+    `policyUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`roleUuid`,`policyUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RoleUserGroupRefVO` (
+    `roleUuid` VARCHAR(32) NOT NULL,
+    `groupUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`roleUuid`,`groupUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RoleUserRefVO` (
+    `roleUuid` VARCHAR(32) NOT NULL,
+    `userUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`roleUuid`,`groupUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `RoleVO` (
+    `uuid` VARCHAR(32) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`roleUuid`,`groupUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
