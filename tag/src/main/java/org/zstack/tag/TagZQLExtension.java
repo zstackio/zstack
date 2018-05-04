492a1d6cc42615dceb65dcfdff0a71437aaebfcb
Frank.Zhang <xin.zhang@mevoco.com>
2018-05-03 21:02:08 -0400

    re-write zql

--- tag/src/main/java/org/zstack/tag/TagZQLExtension.java
+++ tag/src/main/java/org/zstack/tag/TagZQLExtension.java
@@ -1,5 +1,6 @@
 package org.zstack.tag;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.db.EntityMetadata;
 import org.zstack.header.tag.SystemTagVO;
@@ -10,12 +11,32 @@ import org.zstack.header.zql.ZQLExtensionContext;
 import org.zstack.identity.AccountManager;
 import org.zstack.zql.ast.ZQLMetadata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TagZQLExtension implements RestrictByExprExtensionPoint {
     public static final String USER_TAG_NAME = "__userTag__";
     public static final String SYS_TAG_NAME = "__systemTag__";
 
     @Autowired
     private AccountManager acntMgr;
+
+    private static Map<String, String> FALSE_OP = new HashMap();
+
+    static {
+        FALSE_OP.put("!=", "=");
+        FALSE_OP.put("not in", "in");
+        FALSE_OP.put("not like", "like");
+    }
+
+    private String getConditionValue(String op, ASTNode.RestrictExpr expr) {
+        if ("in".equals(op) || "not in".equals(op)) {
+            ASTNode.ListValue lv = (ASTNode.ListValue) expr.getValue();
+            return String.format("(%s)", StringUtils.join(lv.getValues(), ","));
+        } else {
+            return ((ASTNode.PlainValue)expr.getValue()).getText();
+        }
+    }
 
     @Override
     public String restrictByExpr(ZQLExtensionContext context, ASTNode.RestrictExpr expr) {
@@ -36,12 +57,23 @@ public class TagZQLExtension implements RestrictByExprExtensionPoint {
         String primaryKey = EntityMetadata.getPrimaryKeyField(src.inventoryAnnotation.mappingVOClass()).getName();
         String tableName = expr.getField().equals(USER_TAG_NAME) ? UserTagVO.class.getSimpleName() : SystemTagVO.class.getSimpleName();
 
-        if (expr.getValue() == null) {
-            return String.format("(%s.%s IN (SELECT tagvo.resourceUuid FROM %s tagvo WHERE tagvo.resourceType = '%s' AND tagvo.tag %s))",
-                    src.simpleInventoryName(), primaryKey, tableName, resourceType.getSimpleName(), expr.getOperator());
+        String subCondition;
+        if (FALSE_OP.containsKey(expr.getOperator())) {
+            String reserveOp = FALSE_OP.get(expr.getOperator());
+            subCondition = String.format("tagvo.uuid IN (SELECT tagvo_.uuid FROM %s tagvo_ WHERE tagvo_.tag %s %s)",
+                    tableName, reserveOp, getConditionValue(reserveOp, expr));
+
+            return String.format("(%s.%s NOT IN (SELECT tagvo.resourceUuid FROM %s tagvo WHERE tagvo.resourceType = '%s' AND %s))",
+                    src.simpleInventoryName(), primaryKey, tableName, resourceType.getSimpleName(), subCondition);
         } else {
-            return String.format("(%s.%s IN (SELECT tagvo.resourceUuid FROM %s tagvo WHERE tagvo.resourceType = '%s' AND tagvo.tag %s %s))",
-                    src.simpleInventoryName(), primaryKey, tableName, resourceType.getSimpleName(), expr.getOperator(), ((ASTNode.PlainValue)expr.getValue()).getText());
+            if (expr.getValue() == null) {
+                subCondition = String.format("tagvo.tag %s", expr.getOperator());
+            } else {
+                subCondition = String.format("tagvo.tag %s %s", expr.getOperator(), getConditionValue(expr.getOperator(), expr));
+            }
+
+            return String.format("(%s.%s IN (SELECT tagvo.resourceUuid FROM %s tagvo WHERE tagvo.resourceType = '%s' AND %s))",
+                    src.simpleInventoryName(), primaryKey, tableName, resourceType.getSimpleName(), subCondition);
         }
     }
 }
