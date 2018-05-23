30ca3b8816986e1ed033056aee5f9d5773a05190
mingjian.deng <mingjian.deng@zstack.io>
2018-05-23 09:40:03 +0800

    drop redundant db search

--- header/src/main/java/org/zstack/header/message/SkipLogger.java
+++ header/src/main/java/org/zstack/header/message/SkipLogger.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Target;
 
 /**
  * Created by mingjian.deng on 2018/5/22.
- * only skip dump msg logger
+ * only skip dump msg logger, usually uses for password
  */
 @Target({java.lang.annotation.ElementType.FIELD})
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
