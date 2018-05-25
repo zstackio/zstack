e9768dfcdb425ddc8c031086f222ec5fbec7519a
mingjian.deng <mingjian.deng@zstack.io>
2018-05-25 14:46:13 +0800

    add APIQueryImageCacheVO API

---
+++ header/src/main/java/org/zstack/header/storage/primary/APIQueryImageCacheMsgDoc_zh_cn.groovy
@@ -0,0 +1,30 @@
+package org.zstack.header.storage.primary
+
+import org.zstack.header.query.APIQueryMessage
+
+doc {
+    title "QueryImageCache"
+
+    category "storage.primary"
+
+    desc """查询主存储上的镜像缓存"""
+
+    rest {
+        request {
+			url "GET /v1/primary-storage/imagecache"
+
+
+            header(Authorization: 'OAuth the-session-uuid')
+
+            clz APIQueryImageCacheMsg.class
+
+            desc """"""
+            
+			params APIQueryMessage.class
+        }
+
+        response {
+            clz APIQueryImageCacheReply.class
+        }
+    }
+}
