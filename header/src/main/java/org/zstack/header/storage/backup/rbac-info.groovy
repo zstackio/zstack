9c3f4a2127de09ce2202fefd6b107030d7bfd090
Frank.Zhang <xin.zhang@mevoco.com>
2018-05-30 18:51:31 -0400

    support cloning roles when creating IAM2 projects

--- header/src/main/java/org/zstack/header/storage/backup/rbac-info.groovy
+++ header/src/main/java/org/zstack/header/storage/backup/rbac-info.groovy
@@ -1,6 +1,7 @@
 package org.zstack.header.storage.backup
 
 import org.zstack.header.core.StaticInit
+import org.zstack.header.image.ImageVO
 
 import static org.zstack.header.identity.rbac.RBAC.rbac
 
@@ -10,12 +11,14 @@ static void init() {
         permissions {
             adminOnlyAPIs("org.zstack.header.storage.backup.**")
 
-            normalAPIs(APIQueryBackupStorageMsg.class.name)
+            normalAPIs(APIQueryBackupStorageMsg.class.name, APIExportImageFromBackupStorageMsg.class.name)
+
+            targetResources = [ImageVO.class]
         }
 
         contributeToRole {
             roleName = "other"
-            actions(APIQueryBackupStorageMsg.class.name)
+            actions(APIQueryBackupStorageMsg.class.name, APIExportImageFromBackupStorageMsg.class.name)
         }
     }
 }
