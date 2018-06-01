531e743a5beacdf111724b674074e83ae81d9204
Ruan Shixin <shixin.ruan@zstack.io>
2018-06-01 16:33:25 +0800

    move vrouter version check from mn startup phase

---
+++ plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/CheckVirtualRouterVmVersionReply.java
@@ -0,0 +1,9 @@
+package org.zstack.network.service.virtualrouter;
+
+import org.zstack.header.message.MessageReply;
+
+/**
+ * Created by shixin on 06/01/2018.
+ */
+public class CheckVirtualRouterVmVersionReply extends MessageReply {
+}
--- test/src/test/groovy/org/zstack/test/integration/core/branchcascade/BranchCascadeTest.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/branchcascade/BranchCascadeTest.groovy
@@ -17,6 +17,7 @@ class BranchCascadeTest extends Test {
         securityGroup()
         kvm()
         ceph()
+        vyos()
         include("KvmTest.xml")
     }
 
