586789fe2769198224f9f4bd02413426c759fe91
Ruan Shixin <shixin.ruan@zstack.io>
2018-06-07 19:32:26 +0800

    bootup a vr if not existed
    Fix ZSTAC-12892

--- plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/vyos/VyosCentralizedDnsBackend.java
+++ plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/vyos/VyosCentralizedDnsBackend.java
@@ -1,6 +1,10 @@
 package org.zstack.network.service.virtualrouter.vyos;
 
+import org.zstack.header.core.ReturnValueCompletion;
 import org.zstack.header.network.service.NetworkServiceProviderType;
+import org.zstack.network.service.virtualrouter.VirtualRouterGlobalProperty;
+import org.zstack.network.service.virtualrouter.VirtualRouterStruct;
+import org.zstack.network.service.virtualrouter.VirtualRouterVmInventory;
 import org.zstack.network.service.virtualrouter.dns.VirtualRouterCentralizedDnsBackend;
 
 /**
@@ -10,5 +14,14 @@ public class VyosCentralizedDnsBackend extends VirtualRouterCentralizedDnsBacken
     @Override
     public NetworkServiceProviderType getProviderType() {
         return VyosConstants.PROVIDER_TYPE;
+    }
+
+    @Override
+    protected void acquireVirtualRouterVm(VirtualRouterStruct struct, ReturnValueCompletion<VirtualRouterVmInventory> completion) {
+        struct.setApplianceVmType(VyosConstants.VYOS_VM_TYPE);
+        struct.setProviderType(VyosConstants.VYOS_ROUTER_PROVIDER_TYPE);
+        struct.setVirtualRouterOfferingSelector(new VyosOfferingSelector());
+        struct.setApplianceVmAgentPort(VirtualRouterGlobalProperty.AGENT_PORT);
+        super.acquireVirtualRouterVm(struct, completion);
     }
 }
