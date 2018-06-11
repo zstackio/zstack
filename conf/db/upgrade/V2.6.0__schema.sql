aed3f5da23500d773975caa6992f9ea5aeb7c8bf
Qun Li <qun.li@zstack.io>
2018-06-11 21:32:07 +0800

    add database records for volumebackup
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- conf/db/upgrade/V2.6.0__schema.sql
+++ conf/db/upgrade/V2.6.0__schema.sql
@@ -1,3 +1,51 @@
+ALTER TABLE `SchedulerTriggerVO` ADD COLUMN `cron` varchar(32) DEFAULT NULL COMMENT 'interval in cron format';
+
+CREATE TABLE IF NOT EXISTS `VolumeBackupVO` (
+    `uuid` VARCHAR(32) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `volumeUuid` VARCHAR(32) NOT NULL,
+    `size` bigint unsigned NOT NULL,
+    `state` varchar(64) NOT NULL,
+    `status` varchar(64) NOT NULL,
+    `metadata` text DEFAULT NULL,
+    `lastOpDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Foreign keys for table VolumeBackupVO
+ALTER TABLE VolumeBackupVO ADD CONSTRAINT fkVolumeBackupVOResourceVO FOREIGN KEY (uuid) REFERENCES ResourceVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE;
+ALTER TABLE VolumeBackupVO ADD CONSTRAINT fkVolumeBackupVOVolumeEO FOREIGN KEY (volumeUuid) REFERENCES VolumeEO (uuid) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS `VolumeBackupStorageRefVO` (
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `backupStorageUuid` varchar(32) NOT NULL,
+    `volumeBackupUuid` varchar(32) NOT NULL,
+    `status` varchar(64) NOT NULL,
+    `installPath` VARCHAR(2048) NOT NULL,
+    `lastOpDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Foreign keys for table VolumeBackupStorageRefVO
+ALTER TABLE VolumeBackupStorageRefVO ADD CONSTRAINT fkVolumeBackupStorageRefVOBackupStorageEO FOREIGN KEY (backupStorageUuid) REFERENCES BackupStorageEO (uuid) ON DELETE CASCADE;
+ALTER TABLE VolumeBackupStorageRefVO ADD CONSTRAINT fkVolumeBackupStorageRefVOVolumeBackupVO FOREIGN KEY (volumeBackupUuid) REFERENCES VolumeBackupVO (uuid) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS `VolumeBackupHistoryVO` (
+    `uuid` VARCHAR(32) NOT NULL,
+    `bitmap` VARCHAR(32) NOT NULL,
+    `lastBackupUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# Foreign keys for table VolumeBackupHistoryVO
+ALTER TABLE VolumeBackupHistoryVO ADD CONSTRAINT fkVolumeBackupHistoryVOVolumeBackupVO FOREIGN KEY (lastBackupUuid) REFERENCES VolumeBackupVO (uuid) ON DELETE CASCADE;
+ALTER TABLE VolumeBackupHistoryVO ADD CONSTRAINT fkVolumeBackupHistoryVOVolumeEO FOREIGN KEY (uuid) REFERENCES VolumeEO (uuid) ON DELETE CASCADE;
+
 SET FOREIGN_KEY_CHECKS = 0;
 
 ALTER TABLE AccountResourceRefVO ADD COLUMN `concreteResourceType` varchar(512) NOT NULL;
@@ -15,4 +63,3 @@ CREATE TABLE `TwoFactorAuthenticationSecretVO` (
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
