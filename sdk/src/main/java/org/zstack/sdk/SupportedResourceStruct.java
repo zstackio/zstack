ef1b5f7c42433075d514ba65c00a597423809c45
mingjian.deng <mingjian.deng@zstack.io>
2018-07-11 16:59:20 +0800

    supported resources

---
+++ sdk/src/main/java/org/zstack/sdk/SupportedResourceStruct.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk;
+
+
+
+public class SupportedResourceStruct  {
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String type;
+    public void setType(java.lang.String type) {
+        this.type = type;
+    }
+    public java.lang.String getType() {
+        return this.type;
+    }
+
+    public java.lang.String actionName;
+    public void setActionName(java.lang.String actionName) {
+        this.actionName = actionName;
+    }
+    public java.lang.String getActionName() {
+        return this.actionName;
+    }
+
+    public java.util.List resources;
+    public void setResources(java.util.List resources) {
+        this.resources = resources;
+    }
+    public java.util.List getResources() {
+        return this.resources;
+    }
+
+}
--- sdk/src/main/java/org/zstack/sdk/ZQLQueryReturn.java
+++ sdk/src/main/java/org/zstack/sdk/ZQLQueryReturn.java
@@ -28,4 +28,12 @@ public class ZQLQueryReturn  {
         return this.returnWith;
     }
 
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
 }
