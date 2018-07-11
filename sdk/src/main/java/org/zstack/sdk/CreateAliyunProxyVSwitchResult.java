58e193778a88870394bf216f6e7a7309c8774c47
lining <2313806311@qq.com>
2018-07-11 15:20:26 +0800

    ecs api adpater

---
+++ sdk/src/main/java/org/zstack/sdk/CreateAliyunProxyVSwitchResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+import org.zstack.sdk.AliyunProxyVSwitchInventory;
+
+public class CreateAliyunProxyVSwitchResult {
+    public AliyunProxyVSwitchInventory inventory;
+    public void setInventory(AliyunProxyVSwitchInventory inventory) {
+        this.inventory = inventory;
+    }
+    public AliyunProxyVSwitchInventory getInventory() {
+        return this.inventory;
+    }
+
+}
