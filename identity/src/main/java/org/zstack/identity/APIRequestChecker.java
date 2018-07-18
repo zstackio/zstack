2150706f1e1e92f2fb606b5ab308a0903172ccd2
AlanJager <alanjager@outlook.com>
2018-07-18 20:04:50 +0800

    Fix function parameter when checking api request header
    Resolves ZSTAC-13598
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- identity/src/main/java/org/zstack/identity/APIRequestChecker.java
+++ identity/src/main/java/org/zstack/identity/APIRequestChecker.java
@@ -5,7 +5,7 @@ import org.zstack.header.identity.rbac.RBACEntity;
 public interface APIRequestChecker {
     void check(RBACEntity msg);
 
-    default boolean bypass(RBACEntity msg) {
+    default boolean bypass(RBACEntity entity) {
         return false;
     }
 }
