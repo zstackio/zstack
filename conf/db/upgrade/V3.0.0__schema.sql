f742873d165bca81c60c60a245a3369394bf9fbe
Wei Wang <wei.wang@zstack.io>
2018-08-06 11:29:02 +0800

    Support iscsi login

--- conf/db/upgrade/V3.0.0__schema.sql
+++ conf/db/upgrade/V3.0.0__schema.sql
@@ -1,11 +1,64 @@
 ALTER TABLE `CaptchaVO` DROP COLUMN `attempts`;
 
 CREATE TABLE `LoginAttemptsVO` (
-    `uuid` VARCHAR(32) NOT NULL,
-    `targetResourceIdentity` VARCHAR(256) NOT NULL,
-    `attempts` int(10) unsigned DEFAULT 0,
-    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-    PRIMARY KEY (`uuid`),
-    UNIQUE KEY `uuid` (`uuid`)
+  `uuid` VARCHAR(32) NOT NULL,
+  `targetResourceIdentity` VARCHAR(256) NOT NULL,
+  `attempts` int(10) unsigned DEFAULT 0,
+  `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `IscsiServerVO` (
+  `uuid` VARCHAR(32) NOT NULL,
+  `ip` VARCHAR(64) NOT NULL,
+  `port` smallint unsigned DEFAULT 3260,
+  `state` VARCHAR(32) NOT NULL,
+  `chapUserName` VARCHAR(256) DEFAULT NULL,
+  `chapUserPassword` VARCHAR(256) DEFAULT NULL,
+  `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`uuid`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `IscsiServerClusterRefVO` (
+  `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+  `clusterUuid` VARCHAR(32) NOT NULL,
+  `iscsiServerUuid` VARCHAR(32) NOT NULL,
+  `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fkIscsiServerClusterRefVOIscsiServerVO` FOREIGN KEY (`iscsiServerUuid`) REFERENCES IscsiServerVO (`uuid`) ON DELETE CASCADE,
+  CONSTRAINT `fkIscsiServerClusterRefVOClusterEO` FOREIGN KEY (`clusterUuid`) REFERENCES ClusterEO (`uuid`) ON DELETE CASCADE
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `IscsiTargetVO` (
+  `uuid` VARCHAR(32) NOT NULL,
+  `iqn` VARCHAR(256) NOT NULL,
+  `state` VARCHAR(32) NOT NULL,
+  `iscsiServerUuid` VARCHAR(32) NOT NULL,
+  `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`uuid`),
+  CONSTRAINT `fkIscsiTargetVOIscsiServerVO` FOREIGN KEY (`iscsiServerUuid`) REFERENCES IscsiServerVO (`uuid`) ON DELETE CASCADE
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `IscsiLunVO` (
+  `uuid` VARCHAR(32) NOT NULL,
+  `wwid` VARCHAR(256) NOT NULL,
+  `vendor` VARCHAR(256) DEFAULT NULL,
+  `model` VARCHAR(256) DEFAULT NULL,
+  `wwn` VARCHAR(256) DEFAULT NULL,
+  `serial` VARCHAR(256) DEFAULT NULL,
+  `hctl` VARCHAR(64) DEFAULT NULL,
+  `type` VARCHAR(128) NOT NULL,
+  `path` VARCHAR(128) DEFAULT NULL,
+  `size` bigint unsigned NOT NULL,
+  `iscsiTargetUuid` VARCHAR(32) NOT NULL,
+  `multipathDeviceUuid` VARCHAR(32) DEFAULT NULL,
+  `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+  `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`uuid`),
+  CONSTRAINT `fkIscsiLunVOIscsiTargetVO` FOREIGN KEY (`iscsiTargetUuid`) REFERENCES IscsiTargetVO (`uuid`) ON DELETE CASCADE
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ sdk/src/main/java/org/zstack/sdk/IscsiServerClusterRefInventory.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk;
+
+
+
+public class IscsiServerClusterRefInventory  {
+
+    public java.lang.String iscsiServerUuid;
+    public void setIscsiServerUuid(java.lang.String iscsiServerUuid) {
+        this.iscsiServerUuid = iscsiServerUuid;
+    }
+    public java.lang.String getIscsiServerUuid() {
+        return this.iscsiServerUuid;
+    }
+
+    public java.lang.String clusterUuid;
+    public void setClusterUuid(java.lang.String clusterUuid) {
+        this.clusterUuid = clusterUuid;
+    }
+    public java.lang.String getClusterUuid() {
+        return this.clusterUuid;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
