2260d2bf98ea689ddb74d44bb4fbb0ab910d23b7
GuoYi <yi.guo@zstack.io>
2018-10-08 19:58:53 +0800

    sdk for APIBatchCreateBaremetalChassisMsg

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -2582,6 +2582,33 @@ trait ApiHelper {
     }
 
 
+    def batchCreateBaremetalChassis(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.BatchCreateBaremetalChassisAction.class) Closure c) {
+        def a = new org.zstack.sdk.BatchCreateBaremetalChassisAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def batchQuery(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.BatchQueryAction.class) Closure c) {
         def a = new org.zstack.sdk.BatchQueryAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
