9725eb2a2b6ff70e6ce1214202556f390200b70a
AlanJager <alanjager@outlook.com>
2018-10-09 10:59:27 +0800

    Add tag for backup storage data network
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- storage/src/main/java/org/zstack/storage/backup/BackupStorageSystemTags.java
+++ storage/src/main/java/org/zstack/storage/backup/BackupStorageSystemTags.java
@@ -1,6 +1,7 @@
 package org.zstack.storage.backup;
 
 import org.zstack.header.storage.backup.BackupStorageVO;
+import org.zstack.header.storage.primary.PrimaryStorageVO;
 import org.zstack.header.tag.TagDefinition;
 import org.zstack.tag.PatternedSystemTag;
 
@@ -12,4 +13,6 @@ public class BackupStorageSystemTags {
     public static String BACKUP_STORE_DEPLOY_REMOTE_TOKEN = "remote";
     public static PatternedSystemTag BACKUP_STORE_DEPLOY_REMOTE = new PatternedSystemTag(String.format("%s", BACKUP_STORE_DEPLOY_REMOTE_TOKEN), BackupStorageVO.class);
 
+    public static final String BACKUP_STORAGE_DATA_NETWORK_TOKEN = "backupStorageDataNetwork";
+    public static PatternedSystemTag BACKUP_STORAGE_DATA_NETWORK = new PatternedSystemTag(String.format("backupStorage::data::network::cidr::{%s}", BACKUP_STORAGE_DATA_NETWORK_TOKEN), BackupStorageVO.class);
 }
