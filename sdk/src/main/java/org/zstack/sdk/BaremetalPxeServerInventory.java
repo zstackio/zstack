d6df2842200f5017762952e195963ce8f25d3e8f
GuoYi <yi.guo@zstack.io>
2018-10-10 15:06:14 +0800

    External PXE Server

--- sdk/src/main/java/org/zstack/sdk/BaremetalPxeServerInventory.java
+++ sdk/src/main/java/org/zstack/sdk/BaremetalPxeServerInventory.java
@@ -12,6 +12,14 @@ public class BaremetalPxeServerInventory  {
         return this.uuid;
     }
 
+    public java.lang.String zoneUuid;
+    public void setZoneUuid(java.lang.String zoneUuid) {
+        this.zoneUuid = zoneUuid;
+    }
+    public java.lang.String getZoneUuid() {
+        return this.zoneUuid;
+    }
+
     public java.lang.String name;
     public void setName(java.lang.String name) {
         this.name = name;
@@ -28,12 +36,60 @@ public class BaremetalPxeServerInventory  {
         return this.description;
     }
 
+    public java.lang.String hostname;
+    public void setHostname(java.lang.String hostname) {
+        this.hostname = hostname;
+    }
+    public java.lang.String getHostname() {
+        return this.hostname;
+    }
+
+    public java.lang.String sshUsername;
+    public void setSshUsername(java.lang.String sshUsername) {
+        this.sshUsername = sshUsername;
+    }
+    public java.lang.String getSshUsername() {
+        return this.sshUsername;
+    }
+
+    public java.lang.String sshPassword;
+    public void setSshPassword(java.lang.String sshPassword) {
+        this.sshPassword = sshPassword;
+    }
+    public java.lang.String getSshPassword() {
+        return this.sshPassword;
+    }
+
+    public java.lang.Integer sshPort;
+    public void setSshPort(java.lang.Integer sshPort) {
+        this.sshPort = sshPort;
+    }
+    public java.lang.Integer getSshPort() {
+        return this.sshPort;
+    }
+
+    public java.lang.String storagePath;
+    public void setStoragePath(java.lang.String storagePath) {
+        this.storagePath = storagePath;
+    }
+    public java.lang.String getStoragePath() {
+        return this.storagePath;
+    }
+
     public java.lang.String dhcpInterface;
     public void setDhcpInterface(java.lang.String dhcpInterface) {
         this.dhcpInterface = dhcpInterface;
     }
     public java.lang.String getDhcpInterface() {
         return this.dhcpInterface;
+    }
+
+    public java.lang.String dhcpInterfaceAddress;
+    public void setDhcpInterfaceAddress(java.lang.String dhcpInterfaceAddress) {
+        this.dhcpInterfaceAddress = dhcpInterfaceAddress;
+    }
+    public java.lang.String getDhcpInterfaceAddress() {
+        return this.dhcpInterfaceAddress;
     }
 
     public java.lang.String dhcpRangeBegin;
@@ -60,6 +116,14 @@ public class BaremetalPxeServerInventory  {
         return this.dhcpRangeNetmask;
     }
 
+    public java.lang.String state;
+    public void setState(java.lang.String state) {
+        this.state = state;
+    }
+    public java.lang.String getState() {
+        return this.state;
+    }
+
     public java.lang.String status;
     public void setStatus(java.lang.String status) {
         this.status = status;
@@ -82,6 +146,30 @@ public class BaremetalPxeServerInventory  {
     }
     public java.sql.Timestamp getLastOpDate() {
         return this.lastOpDate;
+    }
+
+    public java.lang.Long totalCapacity;
+    public void setTotalCapacity(java.lang.Long totalCapacity) {
+        this.totalCapacity = totalCapacity;
+    }
+    public java.lang.Long getTotalCapacity() {
+        return this.totalCapacity;
+    }
+
+    public java.lang.Long availableCapacity;
+    public void setAvailableCapacity(java.lang.Long availableCapacity) {
+        this.availableCapacity = availableCapacity;
+    }
+    public java.lang.Long getAvailableCapacity() {
+        return this.availableCapacity;
+    }
+
+    public java.util.List attachedClusterUuids;
+    public void setAttachedClusterUuids(java.util.List attachedClusterUuids) {
+        this.attachedClusterUuids = attachedClusterUuids;
+    }
+    public java.util.List getAttachedClusterUuids() {
+        return this.attachedClusterUuids;
     }
 
 }
