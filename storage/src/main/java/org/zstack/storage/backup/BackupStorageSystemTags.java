b02945d5a25b6c9925bf655ed7102d5a70f617e2
AlanJager <alanjager@outlook.com>
2018-10-23 11:17:19 +0800

    Add extra ips systemg tag for backup storage
    Resolves ZSTAC-15810
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- storage/src/main/java/org/zstack/storage/backup/BackupStorageSystemTags.java
+++ storage/src/main/java/org/zstack/storage/backup/BackupStorageSystemTags.java
@@ -1,7 +1,6 @@
 package org.zstack.storage.backup;
 
 import org.zstack.header.storage.backup.BackupStorageVO;
-import org.zstack.header.storage.primary.PrimaryStorageVO;
 import org.zstack.header.tag.TagDefinition;
 import org.zstack.tag.PatternedSystemTag;
 
@@ -15,4 +14,7 @@ public class BackupStorageSystemTags {
 
     public static final String BACKUP_STORAGE_DATA_NETWORK_TOKEN = "backupStorageDataNetwork";
     public static PatternedSystemTag BACKUP_STORAGE_DATA_NETWORK = new PatternedSystemTag(String.format("backupStorage::data::network::cidr::{%s}", BACKUP_STORAGE_DATA_NETWORK_TOKEN), BackupStorageVO.class);
+
+    public static String EXTRA_IPS_TOKEN = "extraips";
+    public static PatternedSystemTag EXTRA_IPS = new PatternedSystemTag(String.format("extraips::{%s}", EXTRA_IPS_TOKEN), BackupStorageVO.class);
 }
