114c64448069a4597f36aa5fce1ce506b4b8e106
mingjian.deng <mingjian.deng@zstack.io>
2018-11-21 16:05:57 +0800

    ebs1.3

--- header/src/main/java/org/zstack/header/vm/VmAttachIsoExtensionPoint.java
+++ header/src/main/java/org/zstack/header/vm/VmAttachIsoExtensionPoint.java
@@ -1,5 +1,6 @@
 package org.zstack.header.vm;
 
+import org.zstack.header.errorcode.ErrorCode;
 import org.zstack.header.image.ImageInventory;
 
 import java.util.List;
@@ -8,6 +9,6 @@ import java.util.List;
  * Created by mingjian.deng on 2018/11/20.
  */
 public interface VmAttachIsoExtensionPoint {
-    void filtCandidateVms(String isoUuid, List<VmInstanceInventory> vms);
+    ErrorCode filtCandidateVms(String isoUuid, List<VmInstanceInventory> vms);
     void filtCandidateIsos(String vmUuid, List<ImageInventory> isos);
 }
