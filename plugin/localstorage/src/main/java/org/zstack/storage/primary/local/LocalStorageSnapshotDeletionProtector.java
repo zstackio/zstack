4a7b197ff8e3fa45ee0b543fadc538ab846ce7f6
Wei Wang <wei.wang@zstack.io>
2018-12-18 21:50:27 +0800

    Implement sharedblock snapshot protector
    Resolves ZSTAC-17354

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSnapshotDeletionProtector.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSnapshotDeletionProtector.java
@@ -1,5 +1,6 @@
 package org.zstack.storage.primary.local;
 
+import org.zstack.header.core.Completion;
 import org.zstack.header.errorcode.OperationFailureException;
 import org.zstack.header.storage.snapshot.VolumeSnapshotDeletionProtector;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;
@@ -16,11 +17,13 @@ public class LocalStorageSnapshotDeletionProtector implements VolumeSnapshotDele
     }
 
     @Override
-    public void protect(VolumeSnapshotInventory snapshot) {
+    public void protect(VolumeSnapshotInventory snapshot, Completion completion) {
         Path path = Paths.get(snapshot.getPrimaryStorageInstallPath());
         if (!path.getParent().toString().contains(snapshot.getVolumeUuid())) {
-            throw new OperationFailureException(inerr("the snapshot[name:%s, uuid:%s, path: %s] seems not belong to the volume[uuid:%s]",
+            completion.fail(inerr("the snapshot[name:%s, uuid:%s, path: %s] seems not belong to the volume[uuid:%s]",
                     snapshot.getName(), snapshot.getUuid(), snapshot.getPrimaryStorageInstallPath(), snapshot.getVolumeUuid()));
+            return;
         }
+        completion.success();
     }
 }
