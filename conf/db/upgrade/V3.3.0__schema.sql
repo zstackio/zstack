fd713e86b27cee97787b663e79f68278afb48d3d
shixin.ruan <shixin.ruan@zstack.io>
2018-12-24 14:59:13 +0800

    sync vcenter resource pool

--- conf/db/upgrade/V3.3.0__schema.sql
+++ conf/db/upgrade/V3.3.0__schema.sql
@@ -12,3 +12,38 @@ CREATE TABLE IF NOT EXISTS `ElaborationVO` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE INDEX idxElaborationVOmd5sum ON ElaborationVO (md5sum);
+
+CREATE TABLE  `zstack`.`VCenterResourcePoolVO` (
+    `uuid` varchar(32) NOT NULL COMMENT 'VCenter Resource Pool uuid',
+    `vCenterClusterUuid` varchar(32) NOT NULL COMMENT 'VCenter cluster uuid',
+    `name` varchar(256) NOT NULL COMMENT 'VCenter Resource Pool name',
+    `morVal` varchar(256) NOT NULL COMMENT 'VCenter Resource Pool management object value in vcenter',
+    `parentUuid` varchar(32) COMMENT 'Parent Resource Pool uuid or NULL',
+    `CPULimit` bigint(64),
+    `CPUOverheadLimit` bigint(64),
+    `CPUReservation` bigint(64),
+    `CPUShares` bigint(64),
+    `CPULevel` varchar(64),
+    `MemoryLimit` bigint(64),
+    `MemoryOverheadLimit` bigint(64),
+    `MemoryReservation` bigint(64),
+    `MemoryShares` bigint(64),
+    `MemoryLevel` varchar(64),
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT `fkVCenterResourcePoolVOVCenterClusterVO` FOREIGN KEY (`vCenterClusterUuid`) REFERENCES `VCenterClusterVO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `zstack`.`VCenterResourcePoolUsageVO` (
+    `uuid` varchar(32) NOT NULL COMMENT 'VCenter Resource Pool usage uuid',
+    `vCenterResourcePoolUuid` varchar(32) NOT NULL COMMENT 'VCenter Resource Pool uuid',
+    `resourceUuid` varchar(32) NOT NULL COMMENT 'VCenter Resource resource uuid',
+    `resourceType` varchar(256) NOT NULL COMMENT 'VCenter Resource resource type',
+    `resourceName` varchar(256) COMMENT 'VCenter Resource resource name',
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+     PRIMARY KEY  (`uuid`),
+     UNIQUE KEY `VCenterResourcePoolUsageVO` (`vCenterResourcePoolUuid`, `resourceUuid`) USING BTREE,
+     CONSTRAINT `fkVCenterResourcePoolUsageVOVCenterResourcePoolVO` FOREIGN KEY (`vCenterResourcePoolUuid`) REFERENCES `VCenterResourcePoolVO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -17898,6 +17898,35 @@ trait ApiHelper {
     }
 
 
+    def queryVCenterResourcePool(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.QueryVCenterResourcePoolAction.class) Closure c) {
+        def a = new org.zstack.sdk.QueryVCenterResourcePoolAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+        a.conditions = a.conditions.collect { it.toString() }
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def queryVRouterRouteEntry(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.QueryVRouterRouteEntryAction.class) Closure c) {
         def a = new org.zstack.sdk.QueryVRouterRouteEntryAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
