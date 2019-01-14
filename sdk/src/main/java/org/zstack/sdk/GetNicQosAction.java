223ba7ff46ea50a07bd6898d0bbd0177e0de853f
mingjian.deng <mingjian.deng@zstack.io>
2019-01-14 10:28:53 +0800

    support sync qos

--- sdk/src/main/java/org/zstack/sdk/GetNicQosAction.java
+++ sdk/src/main/java/org/zstack/sdk/GetNicQosAction.java
@@ -28,6 +28,9 @@ public class GetNicQosAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String uuid;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.Boolean forceSync;
+
     @Param(required = false)
     public java.util.List systemTags;
 
