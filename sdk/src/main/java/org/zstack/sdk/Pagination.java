27c57fc88331520f2d5035b2992e5fe143c7a563
kefeng.wang <hotwkf@gmail.com>
2019-03-18 16:43:56 +0800

    add API CalculateResourceSpending

---
+++ sdk/src/main/java/org/zstack/sdk/Pagination.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk;
+
+
+
+public class Pagination  {
+
+    public java.lang.Integer start;
+    public void setStart(java.lang.Integer start) {
+        this.start = start;
+    }
+    public java.lang.Integer getStart() {
+        return this.start;
+    }
+
+    public java.lang.Integer limit;
+    public void setLimit(java.lang.Integer limit) {
+        this.limit = limit;
+    }
+    public java.lang.Integer getLimit() {
+        return this.limit;
+    }
+
+    public java.lang.Integer total;
+    public void setTotal(java.lang.Integer total) {
+        this.total = total;
+    }
+    public java.lang.Integer getTotal() {
+        return this.total;
+    }
+
+}
