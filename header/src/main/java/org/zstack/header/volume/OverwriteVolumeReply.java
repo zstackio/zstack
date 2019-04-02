3979bbe8250b7d8c1cbb5972be611a90de6e44ff
J M <630911437@qq.com>
2019-04-02 19:14:51 +0800

    fix revert volume from backup Resovles ZSTAC-17533

---
+++ header/src/main/java/org/zstack/header/volume/OverwriteVolumeReply.java
@@ -0,0 +1,9 @@
+package org.zstack.header.volume;
+
+import org.zstack.header.message.MessageReply;
+
+/**
+ * Created by MaJin on 2019/4/2.
+ */
+public class OverwriteVolumeReply extends MessageReply {
+}
