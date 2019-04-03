0c356fdf04c09a48d4c2d33c5892d6cc453a1929
Wei Wang <wei.wang@zstack.io>
2019-04-03 15:06:45 +0800

    Update MiniStorage SDK

--- sdk/src/main/java/org/zstack/sdk/MiniStorageInventory.java
+++ sdk/src/main/java/org/zstack/sdk/MiniStorageInventory.java
@@ -4,14 +4,6 @@ import org.zstack.sdk.MiniStorageType;
 
 public class MiniStorageInventory extends org.zstack.sdk.PrimaryStorageInventory {
 
-    public java.util.List miniStorageHostRefs;
-    public void setMiniStorageHostRefs(java.util.List miniStorageHostRefs) {
-        this.miniStorageHostRefs = miniStorageHostRefs;
-    }
-    public java.util.List getMiniStorageHostRefs() {
-        return this.miniStorageHostRefs;
-    }
-
     public MiniStorageType miniStorageType;
     public void setMiniStorageType(MiniStorageType miniStorageType) {
         this.miniStorageType = miniStorageType;
