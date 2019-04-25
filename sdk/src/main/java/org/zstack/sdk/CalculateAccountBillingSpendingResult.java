22bcda26b3d8b458185b258ddac3593ac7dd74cc
lining <2016220101025@std.uestc.edu.cn>
2019-04-25 17:12:51 +0800

    update 3.4.1 scheam

---
+++ sdk/src/main/java/org/zstack/sdk/CalculateAccountBillingSpendingResult.java
@@ -0,0 +1,22 @@
+package org.zstack.sdk;
+
+
+
+public class CalculateAccountBillingSpendingResult {
+    public double total;
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    public double getTotal() {
+        return this.total;
+    }
+
+    public java.util.List spending;
+    public void setSpending(java.util.List spending) {
+        this.spending = spending;
+    }
+    public java.util.List getSpending() {
+        return this.spending;
+    }
+
+}
