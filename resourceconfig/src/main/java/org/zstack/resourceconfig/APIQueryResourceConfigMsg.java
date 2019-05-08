5dc0b43cc431d2b89962a8e9222921a102d208e5
J M <630911437@qq.com>
2019-05-08 14:33:08 +0800

    add miss api example

--- resourceconfig/src/main/java/org/zstack/resourceconfig/APIQueryResourceConfigMsg.java
+++ resourceconfig/src/main/java/org/zstack/resourceconfig/APIQueryResourceConfigMsg.java
@@ -5,7 +5,13 @@ import org.zstack.header.query.APIQueryMessage;
 import org.zstack.header.query.AutoQuery;
 import org.zstack.header.rest.RestRequest;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestRequest(path = "/resource-configurations", method = HttpMethod.GET, responseClass = APIQueryResourceConfigReply.class)
 @AutoQuery(replyClass = APIQueryResourceConfigReply.class, inventoryClass = ResourceConfigInventory.class)
 public class APIQueryResourceConfigMsg extends APIQueryMessage {
+    public static List<String> __example__() {
+        return Collections.singletonList("category=host");
+    }
 }
--- resourceconfig/src/main/java/org/zstack/resourceconfig/APIQueryResourceConfigReply.java
+++ resourceconfig/src/main/java/org/zstack/resourceconfig/APIQueryResourceConfigReply.java
@@ -1,8 +1,12 @@
 package org.zstack.resourceconfig;
 
+import org.zstack.core.Platform;
+import org.zstack.header.cluster.ClusterVO;
 import org.zstack.header.query.APIQueryReply;
 import org.zstack.header.rest.RestResponse;
 
+import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.List;
 
 @RestResponse(allTo = "inventories")
@@ -15,5 +19,20 @@ public class APIQueryResourceConfigReply extends APIQueryReply {
 
     public void setInventories(List<ResourceConfigInventory> inventories) {
         this.inventories = inventories;
+    }
+
+    public static APIQueryResourceConfigReply __example__() {
+        APIQueryResourceConfigReply reply = new APIQueryResourceConfigReply();
+        ResourceConfigInventory clusterConfig = new ResourceConfigInventory();
+        clusterConfig.setCategory("host");
+        clusterConfig.setName("cpu.overProvisioning.ratio");
+        clusterConfig.setResourceType(ClusterVO.class.getSimpleName());
+        clusterConfig.setResourceUuid(Platform.getUuid());
+        clusterConfig.setUuid(Platform.getUuid());
+        clusterConfig.setCreateDate(new Timestamp(org.zstack.header.message.DocUtils.date));
+        clusterConfig.setLastOpDate(new Timestamp(org.zstack.header.message.DocUtils.date));
+        clusterConfig.setValue("10");
+        reply.setInventories(Collections.singletonList(clusterConfig));
+        return reply;
     }
 }
