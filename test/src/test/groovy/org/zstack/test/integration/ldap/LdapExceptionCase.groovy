67af4a82ae59196f4209106f970726327e74a3ac
wangjie <jie.wang@zstack.io>
2019-06-11 13:10:57 +0800

    fix ZSTAC-17455

---
+++ test/src/test/groovy/org/zstack/test/integration/ldap/LdapExceptionCase.groovy
@@ -0,0 +1,127 @@
+package org.zstack.test.integration.ldap
+
+import com.unboundid.ldap.sdk.LDAPInterface
+import com.unboundid.ldap.sdk.SearchResult
+import com.unboundid.ldap.sdk.SearchScope
+import org.junit.Rule
+import org.zapodot.junit.ldap.EmbeddedLdapRule
+import org.zapodot.junit.ldap.EmbeddedLdapRuleBuilder
+import org.zstack.sdk.AddLdapServerAction
+import org.zstack.sdk.ApiResult
+import org.zstack.sdk.ZSClient
+import org.zstack.test.integration.ZStackTest
+import org.zstack.test.integration.stabilisation.StabilityTestCase
+import org.zstack.test.integration.stabilisation.TestCaseStabilityTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.SubCase
+import org.zstack.testlib.Test
+
+class LdapExceptionCase extends SubCase {
+    EnvSpec env
+
+    public static String DOMAIN_DSN = "dc=example,dc=com"
+
+    @Rule
+    public static EmbeddedLdapRule embeddedLdapRule = EmbeddedLdapRuleBuilder.newInstance().bindingToPort(1888).
+            usingDomainDsn(DOMAIN_DSN).importingLdifs("users-import.ldif").build()
+
+    @Override
+    void setup() {
+        spring {
+            kvm()
+            localStorage()
+            sftpBackupStorage()
+            include("LdapManagerImpl.xml")
+            include("captcha.xml")
+        }
+    }
+
+    @Override
+    void environment() {
+        env = Env.localStorageOneVmEnv()
+    }
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void test() {
+        env.create {
+            testAddLdap()
+        }
+    }
+
+    LDAPInterface getLdapConn(){
+        try{
+            return embeddedLdapRule.ldapConnection()
+        }catch (Exception e){
+        }
+
+        try {
+            return ZStackTest.embeddedLdapRule.ldapConnection()
+        }catch (Exception e){
+        }
+
+        try {
+            return StabilityTestCase.embeddedLdapRule.ldapConnection()
+        }catch (Exception e){
+        }
+
+        try {
+            return TestCaseStabilityTest.embeddedLdapRule.ldapConnection()
+        }catch (Exception e){
+        }
+    }
+
+    void testAddLdap() {
+        LDAPInterface ldapConnection = this.getLdapConn()
+        final SearchResult searchResult = ldapConnection.search(ZStackTest.DOMAIN_DSN, SearchScope.SUB, "(objectClass=person)")
+        assert searchResult.getEntryCount() == 3
+
+        AddLdapServerAction addLdapServerAction = new AddLdapServerAction(
+                name : "ldap0",
+                description : "test-ldap0",
+                base : ZStackTest.DOMAIN_DSN,
+                url : "ldap://localhost:1888",
+                username : "",
+                password : "",
+                encryption : "None",
+                systemTags : ["ephemeral::validationOnly"],
+                sessionId : Test.currentEnvSpec.session.uuid
+        )
+        ApiResult res = ZSClient.call(addLdapServerAction)
+        assert res.error == null
+
+        addLdapServerAction = new AddLdapServerAction(
+                name : "ldap0",
+                description : "test-ldap0",
+                base : ZStackTest.DOMAIN_DSN,
+                url : "ldap://localhost:1899",
+                username : "",
+                password : "",
+                encryption : "None",
+                systemTags : ["ephemeral::validationOnly"],
+                sessionId : Test.currentEnvSpec.session.uuid
+        )
+        res = ZSClient.call(addLdapServerAction)
+        assert res.error != null
+        assert res.error.details.contains("communication false, please checkout IP, port and Base DN")
+
+        addLdapServerAction = new AddLdapServerAction(
+                name : "ldap0",
+                description : "test-ldap0",
+                base : ZStackTest.DOMAIN_DSN,
+                url : "ldap://localhost:1888",
+                username : "",
+                password : "wrongPassword",
+                encryption : "None",
+                systemTags : ["ephemeral::validationOnly"],
+                sessionId : Test.currentEnvSpec.session.uuid
+        )
+        res = ZSClient.call(addLdapServerAction)
+        assert res.error != null
+        assert res.error.details.contains("Invalid Credentials, please checkout User DN and password")
+    }
+}
