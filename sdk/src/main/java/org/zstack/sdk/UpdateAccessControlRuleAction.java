60d4de0f67d84ab52d1ef98154b4c47c2dd6338c
Alan Jager <alanjager@outlook.com>
2019-07-04 18:22:37 +0800

    Update sdk
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- sdk/src/main/java/org/zstack/sdk/UpdateAccessControlRuleAction.java
+++ sdk/src/main/java/org/zstack/sdk/UpdateAccessControlRuleAction.java
@@ -34,6 +34,9 @@ public class UpdateAccessControlRuleAction extends AbstractAction {
     @Param(required = false, maxLength = 2048, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String description;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String rule;
+
     @Param(required = false)
     public java.util.List systemTags;
 
