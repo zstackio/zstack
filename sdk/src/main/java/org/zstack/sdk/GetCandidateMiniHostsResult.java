6e14b9556d4626089467bcababdbb900d78b58c4
Wei Wang <wei.wang@zstack.io>
2019-07-05 00:14:02 +0800

    Support scan and add host

---
+++ sdk/src/main/java/org/zstack/sdk/GetCandidateMiniHostsResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetCandidateMiniHostsResult {
+    public java.util.List hosts;
+    public void setHosts(java.util.List hosts) {
+        this.hosts = hosts;
+    }
+    public java.util.List getHosts() {
+        return this.hosts;
+    }
+
+}
---
+++ sdk/src/main/java/org/zstack/sdk/MiniNetworkConfigStruct.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk;
+
+
+
+public class MiniNetworkConfigStruct  {
+
+    public java.lang.String gw;
+    public void setGw(java.lang.String gw) {
+        this.gw = gw;
+    }
+    public java.lang.String getGw() {
+        return this.gw;
+    }
+
+    public java.lang.String ip;
+    public void setIp(java.lang.String ip) {
+        this.ip = ip;
+    }
+    public java.lang.String getIp() {
+        return this.ip;
+    }
+
+    public java.lang.String vlan;
+    public void setVlan(java.lang.String vlan) {
+        this.vlan = vlan;
+    }
+    public java.lang.String getVlan() {
+        return this.vlan;
+    }
+
+    public java.lang.String bond;
+    public void setBond(java.lang.String bond) {
+        this.bond = bond;
+    }
+    public java.lang.String getBond() {
+        return this.bond;
+    }
+
+}
