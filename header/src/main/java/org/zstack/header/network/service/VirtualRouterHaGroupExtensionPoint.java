090b60e9ae014742911a1952c9007b87ca026a7c
shixin.ruan <shixin.ruan@zstack.io>
2019-07-12 16:00:32 +0800

    fix attach unique value for ha group fix GetIpAddressCapacity error
    fix ZSTAC-21643

--- header/src/main/java/org/zstack/header/network/service/VirtualRouterHaGroupExtensionPoint.java
+++ header/src/main/java/org/zstack/header/network/service/VirtualRouterHaGroupExtensionPoint.java
@@ -30,6 +30,7 @@ public interface VirtualRouterHaGroupExtensionPoint {
     List<String> getAllVipsOnThisRouter(String vrUuid);
 
     void attachNetworkServiceToHaRouter(String type, List<String> uuids, String vrUuid);
+    void attachNetworkServiceToHaRouter(String type, List<String> uuids, String vrUuid, boolean override);
     void detachNetworkServiceFromHaRouter(String type, List<String> uuids, String vrUuid);
     List<String> getHaVrUuidsFromNetworkService(String type, String Uuid);
     List<String> getNetworkServicesFromHaVrUuid(String type, String vrUuid);
