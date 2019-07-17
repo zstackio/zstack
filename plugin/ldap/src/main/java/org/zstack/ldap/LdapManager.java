72d780a2dbf4da7dc7d505595df27a3078bdf2b9
AlanJager <alanjager@outlook.com>
2019-07-17 16:14:42 +0800

    Support mock ldapUtil
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- plugin/ldap/src/main/java/org/zstack/ldap/LdapManager.java
+++ plugin/ldap/src/main/java/org/zstack/ldap/LdapManager.java
@@ -1,12 +1,12 @@
 package org.zstack.ldap;
 
+import org.zstack.core.Platform;
 import org.zstack.header.identity.AccountConstant;
 
 /**
  * Created by miao on 16-9-6.
  */
 public interface LdapManager {
-    LdapUtil ldapUtil = new LdapUtil(AccountConstant.LOGIN_TYPE);
-
+    LdapUtil ldapUtil = Platform.New(() -> new LdapUtil(AccountConstant.LOGIN_TYPE));
     boolean isValid(String uid, String password);
 }
