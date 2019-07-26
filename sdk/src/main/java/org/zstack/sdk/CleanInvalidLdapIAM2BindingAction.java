f827012f30e96b42753ccbc8bfcd291828770121
lining <2016220101025@std.uestc.edu.cn>
2019-07-26 13:31:46 +0800

    update sdk

--- sdk/src/main/java/org/zstack/sdk/CleanInvalidLdapIAM2BindingAction.java
+++ sdk/src/main/java/org/zstack/sdk/CleanInvalidLdapIAM2BindingAction.java
@@ -40,6 +40,9 @@ public class CleanInvalidLdapIAM2BindingAction extends AbstractAction {
     @Param(required = false)
     public String accessKeySecret;
 
+    @Param(required = false)
+    public String requestIp;
+
     @NonAPIParam
     public long timeout = -1;
 
