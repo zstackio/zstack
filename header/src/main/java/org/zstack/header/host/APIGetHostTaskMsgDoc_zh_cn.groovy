18d12f11891c48420ffc9cb0c4769ca72e995f35
J M <630911437@qq.com>
2019-08-29 19:31:58 +0800

    add update group api Resolves ZSTAC-20382

---
+++ header/src/main/java/org/zstack/header/host/APIGetHostTaskMsgDoc_zh_cn.groovy
@@ -0,0 +1,61 @@
+package org.zstack.header.host
+
+import org.zstack.header.host.APIGetHostTaskReply
+
+doc {
+    title "GetHostTask"
+
+    category "host"
+
+    desc """获取物理机上的任务信息"""
+
+    rest {
+        request {
+			url "GET /v1/hosts/task-details"
+
+			header (Authorization: 'OAuth the-session-uuid')
+
+            clz APIGetHostTaskMsg.class
+
+            desc """"""
+            
+			params {
+
+				column {
+					name "hostUuids"
+					enclosedIn ""
+					desc "物理机UUIDs"
+					location "query"
+					type "List"
+					optional false
+					since "3.6.0"
+					
+				}
+				column {
+					name "systemTags"
+					enclosedIn ""
+					desc ""
+					location "query"
+					type "List"
+					optional true
+					since "3.6.0"
+					
+				}
+				column {
+					name "userTags"
+					enclosedIn ""
+					desc ""
+					location "query"
+					type "List"
+					optional true
+					since "3.6.0"
+					
+				}
+			}
+        }
+
+        response {
+            clz APIGetHostTaskReply.class
+        }
+    }
+}
