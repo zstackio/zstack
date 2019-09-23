e6ccc4adcec6e0534b18cf27e5b489283f9527a0
lining <2016220101025@std.uestc.edu.cn>
2019-09-23 17:17:36 +0800

    update BillingResourceLabelVO schema

--- conf/db/upgrade/V3.7.0__schema.sql
+++ conf/db/upgrade/V3.7.0__schema.sql
@@ -177,3 +177,5 @@ CREATE TABLE IF NOT EXISTS `zstack`.`GuestToolsVO` (
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `BillingResourceLabelVO` modify labelKey varchar(255) NOT NULL;
