12e15b62398b9d1ca3515a6667c2c1c5c66feba4
kaicai.hu <kaicai.hu@zstack.io>
2019-10-29 17:04:49 +0800

    fix:ZSTAC-23171

--- sdk/src/main/java/org/zstack/sdk/GetPrimaryStorageCandidatesForVmMigrationAction.java
+++ sdk/src/main/java/org/zstack/sdk/GetPrimaryStorageCandidatesForVmMigrationAction.java
@@ -28,6 +28,9 @@ public class GetPrimaryStorageCandidatesForVmMigrationAction extends AbstractAct
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String vmInstanceUuid;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public boolean withDataVolumes = true;
+
     @Param(required = false)
     public java.util.List systemTags;
 
