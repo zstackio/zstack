842e6ca70c10871792126b551f75d39eabfff21c
yaohua.wu <pandawuu@163.com>
2020-01-08 16:35:29 +0800

    [BugFix: ZSTACK-25363] add firewall rule description length validate

--- sdk/src/main/java/org/zstack/sdk/CreateFirewallRuleAction.java
+++ sdk/src/main/java/org/zstack/sdk/CreateFirewallRuleAction.java
@@ -67,7 +67,7 @@ public class CreateFirewallRuleAction extends AbstractAction {
     @Param(required = true, validValues = {"enable","disable"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String state;
 
-    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    @Param(required = false, maxLength = 2048, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String description;
 
     @Param(required = false)
