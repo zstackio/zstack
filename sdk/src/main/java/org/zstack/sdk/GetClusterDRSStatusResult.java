ea3bc522223b7c00d50ece7c34d04b6e128f7635
lining <2016220101025@std.uestc.edu.cn>
2020-01-13 00:48:24 +0800

    [Improvement: ZSTACK-25357]

--- sdk/src/main/java/org/zstack/sdk/GetClusterDRSStatusResult.java
+++ sdk/src/main/java/org/zstack/sdk/GetClusterDRSStatusResult.java
@@ -3,14 +3,6 @@ package org.zstack.sdk;
 
 
 public class GetClusterDRSStatusResult {
-    public java.lang.Boolean balance;
-    public void setBalance(java.lang.Boolean balance) {
-        this.balance = balance;
-    }
-    public java.lang.Boolean getBalance() {
-        return this.balance;
-    }
-
     public java.util.List hostLoadOverThreshold;
     public void setHostLoadOverThreshold(java.util.List hostLoadOverThreshold) {
         this.hostLoadOverThreshold = hostLoadOverThreshold;
---
+++ sdk/src/main/java/org/zstack/sdk/GetFactoryModeStateResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetFactoryModeStateResult {
+    public java.lang.Boolean factoryModeState;
+    public void setFactoryModeState(java.lang.Boolean factoryModeState) {
+        this.factoryModeState = factoryModeState;
+    }
+    public java.lang.Boolean getFactoryModeState() {
+        return this.factoryModeState;
+    }
+
+}
