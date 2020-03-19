c22ac8a689dfdfdcb4dd811145cb4362ee94c251
kaicai.hu <kaicai.hu@zstack.io>
2020-03-19 09:48:18 +0800

    [BugFix: ZSTACK-26113]

--- conf/db/upgrade/V3.8.3__schema.sql
+++ conf/db/upgrade/V3.8.3__schema.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `zstack`.`VipVO` add column `system` tinyint unsigned NOT NULL DEFAULT 0;
+
+UPDATE ResourceVO SET resourceType = "PrimaryStorageVO", concreteResourceType = "org.zstack.storage.primary.sharedblock.SharedBlockGroupVO"  WHERE resourceType = "SharedBlockGroupVO";
