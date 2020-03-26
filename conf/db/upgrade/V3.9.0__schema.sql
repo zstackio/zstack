a4b115270f9f79be73250b0b221bace6bfb9b966
kaicai.hu <kaicai.hu@zstack.io>
2020-03-26 11:24:46 +0800

    'system' is a keyword in higher versions of MySQL/Mariadb

--- conf/db/upgrade/V3.9.0__schema.sql
+++ conf/db/upgrade/V3.9.0__schema.sql
@@ -15,7 +15,7 @@ ALTER TABLE ImageBackupStorageRefVO ADD COLUMN exportMd5Sum VARCHAR(255) DEFAULT
 ALTER TABLE ImageBackupStorageRefVO ADD COLUMN exportUrl VARCHAR(2048) DEFAULT NULL;
 UPDATE ImageBackupStorageRefVO ibs, ImageVO i SET ibs.exportMd5Sum = i.exportMd5Sum, ibs.exportUrl = i.exportUrl WHERE ibs.imageUuid = i.uuid;
 DROP VIEW IF EXISTS `zstack`.`ImageVO`;
-CREATE VIEW `zstack`.`ImageVO` AS SELECT uuid, name, description, status, state, size, actualSize, md5Sum, platform, type, format, url, system, mediaType, createDate, lastOpDate, guestOsType FROM `zstack`.`ImageEO` WHERE deleted IS NULL;
+CREATE VIEW `zstack`.`ImageVO` AS SELECT uuid, name, description, status, state, size, actualSize, md5Sum, platform, type, format, url, `system`, mediaType, createDate, lastOpDate, guestOsType FROM `zstack`.`ImageEO` WHERE deleted IS NULL;
 ALTER TABLE ImageEO DROP exportMd5Sum, DROP exportUrl;
 
 ALTER TABLE `zstack`.`PolicyRouteRuleSetVO` ADD COLUMN type VARCHAR(64) DEFAULT "User" NOT NULL;
