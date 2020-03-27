1c906a706ca84a4966c5a07b78a62821cfca2e92
Qun Li <qun.li@zstack.io>
2020-03-28 00:04:07 +0800

    fix the beforeValidate on empty environment
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- conf/db/upgrade/beforeValidate.sql
+++ conf/db/upgrade/beforeValidate.sql
@@ -1,23 +1,24 @@
-use zstack;
-
 -- Update `schema_version' since we've changed SQL to match the expectation
 -- of the newer version MariaDB.
+--
+-- WARNING: `schema_version' table doesn't exist on a clean environment, thus
+-- after 'flyway clean' we prepared an empty table after 'flyway baseline'.
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `update_schema_checksum` $$
+DROP PROCEDURE IF EXISTS `zstack`.`update_schema_checksum` $$
 
-CREATE PROCEDURE `update_schema_checksum`()
+CREATE PROCEDURE `zstack`.`update_schema_checksum`()
 BEGIN
     IF EXISTS(SELECT table_name FROM information_schema.tables WHERE table_name = 'schema_version')
     THEN
-        update `schema_version` set `checksum`=1083194846  where `script`='V1.6__schema.sql'   and `checksum` <> 1083194846;
-        update `schema_version` set `checksum`=-1569422253 where `script`='V2.1.0__schema.sql' and `checksum` <> -1569422253;
-        update `schema_version` set `checksum`=1564279419  where `script`='V3.0.0__schema.sql' and `checksum` <> 1564279419;
-        update `schema_version` set `checksum`=565652311   where `script`='V3.7.2__schema.sql' and `checksum` <> 565652311;
+        update `zstack`.`schema_version` set `checksum`=1083194846  where `script`='V1.6__schema.sql'   and `checksum` <> 1083194846;
+        update `zstack`.`schema_version` set `checksum`=-1569422253 where `script`='V2.1.0__schema.sql' and `checksum` <> -1569422253;
+        update `zstack`.`schema_version` set `checksum`=1564279419  where `script`='V3.0.0__schema.sql' and `checksum` <> 1564279419;
+        update `zstack`.`schema_version` set `checksum`=565652311   where `script`='V3.7.2__schema.sql' and `checksum` <> 565652311;
     END IF;
 END $$
 
 DELIMITER  ;
 
-CALL update_schema_checksum();
-DROP PROCEDURE IF EXISTS update_schema_checksum;
+CALL `zstack`.update_schema_checksum();
+DROP PROCEDURE IF EXISTS `zstack`.update_schema_checksum;
