84415b72f0ce2c756f8ec6fb0cd3d2cd718e1f99
shixin.ruan <shixin.ruan@zstack.io>
2020-04-08 21:43:13 +0800

    [BugFix: ZSTACK-26287]   change vpc ha strategy to Force

--- conf/db/upgrade/V3.9.0__schema.sql
+++ conf/db/upgrade/V3.9.0__schema.sql
@@ -219,34 +219,3 @@ CREATE TABLE IF NOT EXISTS `zstack`.`LoadBalancerListenerACLRefVO` (
   CONSTRAINT `fkLoadbalancerListenerACLRefVOLoadBalancerListenerVO` FOREIGN KEY (`listenerUuid`) REFERENCES `LoadBalancerListenerVO` (`uuid`) ON DELETE RESTRICT,
   CONSTRAINT `fkLoadbalancerListenerACLRefVOAccessControlListVO` FOREIGN KEY (`aclUuid`) REFERENCES `AccessControlListVO` (`uuid`) ON DELETE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
-
-DROP PROCEDURE IF EXISTS addResourceConfigForVirtualRouter;
-DELIMITER $$
-CREATE PROCEDURE addResourceConfigForVirtualRouter()
-BEGIN
-    DECLARE virtualRouterUuid VARCHAR(32);
-    DECLARE ruuid VARCHAR(32);
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE cur CURSOR FOR SELECT uuid FROM `zstack`.`VirtualRouterVmVO`;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    OPEN cur;
-    read_loop: LOOP
-        FETCH cur INTO virtualRouterUuid;
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        SET ruuid = REPLACE(UUID(), '-', '');
-        INSERT INTO zstack.ResourceConfigVO (uuid, name, description, category, value, resourceUuid, resourceType, lastOpDate, createDate)
-        values(ruuid, "vm.ha.strategy", "vm.ha.strategy", "ha", "Force", virtualRouterUuid, "VmInstanceVO", CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-
-    END LOOP;
-    CLOSE cur;
-
-    # work around a bug of mysql : jira.mariadb.org/browse/MDEV-4602
-    SELECT CURTIME();
-END $$
-DELIMITER ;
-
-CALL addResourceConfigForVirtualRouter();
-DROP PROCEDURE IF EXISTS addResourceConfigForVirtualRouter;
