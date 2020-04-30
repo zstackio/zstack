19ea9bfe88e9813bc8c313b4d686c9f137c899a2
Alan Jager <alanjager@outlook.com>
2020-04-30 16:28:18 +0800

    Add groovy doc for APISetImageBootModeMsg
    Signed-off-by: AlanJager <alanjager@outlook.com>

---
+++ header/src/main/java/org/zstack/header/image/APISetImageBootModeEventDoc_zh_cn.groovy
@@ -0,0 +1,17 @@
+package org.zstack.header.image
+
+import org.zstack.header.errorcode.ErrorCode
+
+doc {
+
+	title "设置镜像启动模式的结果"
+
+	ref {
+		name "error"
+		path "org.zstack.header.image.APISetImageBootModeEvent.error"
+		desc "错误码，若不为null，则表示操作失败, 操作成功时该字段为null",false
+		type "ErrorCode"
+		since "3.9.0"
+		clz ErrorCode.class
+	}
+}
