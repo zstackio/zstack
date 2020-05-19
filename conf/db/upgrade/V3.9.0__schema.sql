535d077e0042c2b428ff6846706047bd2854aef8
yaoning.li <2016220101025@std.uestc.edu.cn>
2020-05-19 10:47:48 +0800

    update db schema, add sdk

--- conf/db/upgrade/V3.9.0__schema.sql
+++ conf/db/upgrade/V3.9.0__schema.sql
@@ -371,3 +371,28 @@ alter table HybridAccountVO
     add constraint fkHybridAccountVOAccountVO foreign key (accountUuid) references AccountVO (uuid);
 
 UPDATE AlarmVO SET period = 60 WHERE uuid = "65e8f1a4892231b692cc7a881581f3da" and period != 60;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`MetricDataHttpReceiverVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(256) NOT NULL,
+    `url` varchar(256) NOT NULL,
+    `state` VARCHAR(128) NOT NUll,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`MetricTemplateVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `receiverUuid` varchar(32) NOT NULL,
+    `template` varchar(4096) NOT NULL,
+    `namespace` varchar(64) NOT NULL,
+    `metricName` varchar(128) NOT NULL,
+    `labelsJsonStr` varchar(256) DEFAULT NULL,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT `fkMetricTemplateVOMetricDataHttpReceiverVO` FOREIGN KEY (`receiverUuid`) REFERENCES `zstack`.`MetricDataHttpReceiverVO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ sdk/src/main/java/org/zstack/sdk/zwatch/metricpusher/ReceiverState.java
@@ -0,0 +1,6 @@
+package org.zstack.sdk.zwatch.metricpusher;
+
+public enum ReceiverState {
+	Enabled,
+	Disabled,
+}
