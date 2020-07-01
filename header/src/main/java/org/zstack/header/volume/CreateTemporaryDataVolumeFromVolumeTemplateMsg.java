e4ca81826447aea7d1423f92778101eb91b92074
J M <630911437@qq.com>
2020-07-01 20:17:49 +0800

    [BugFix: ZSTACK-27858] add timeout for internal msg

--- header/src/main/java/org/zstack/header/volume/CreateTemporaryDataVolumeFromVolumeTemplateMsg.java
+++ header/src/main/java/org/zstack/header/volume/CreateTemporaryDataVolumeFromVolumeTemplateMsg.java
@@ -1,8 +1,14 @@
 package org.zstack.header.volume;
 
+import org.zstack.header.message.DefaultTimeout;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by MaJin on 2019/8/1.
  */
+
+@DefaultTimeout(timeunit = TimeUnit.HOURS, value = 72)
 public class CreateTemporaryDataVolumeFromVolumeTemplateMsg extends CreateDataVolumeFromVolumeTemplateMsg {
     private String originVolumeUuid;
 
