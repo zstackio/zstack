328cce460765d3120b619faa0cd1851056ec3308
shixin.ruan <shixin.ruan@zstack.io>
2020-07-07 16:12:05 +0800

    [Feature: ZSTACK-27765] vpc nic qos

---
+++ compute/src/main/java/org/zstack/compute/vm/VmNicQosStruct.java
@@ -0,0 +1,12 @@
+package org.zstack.compute.vm;
+
+public class VmNicQosStruct {
+    public String hostUuid;
+    public String vmUuid;
+    public String vmNicUuid;
+    public String internalName;
+    public Long outboundBandwidth;
+    public Long inboundBandwidth;
+    public Long outboundBandwidthUpthreshold;
+    public Long inboundBandwidthUpthreshold;
+}
