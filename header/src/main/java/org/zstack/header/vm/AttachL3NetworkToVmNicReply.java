f4bf3d8b0d99782c14f45cb85ac447f94f5c9b23
Shixin Ruan <shixin.ruan@zstack.io>
2020-08-02 09:35:57 +0800

    [BugFix: ZSTACK-25967] remove unused ipv6 code

--- header/src/main/java/org/zstack/header/vm/AttachL3NetworkToVmNicReply.java
+++
@@ -1,9 +0,0 @@
-package org.zstack.header.vm;
-
-import org.zstack.header.message.MessageReply;
-
-/**
- * Created by shixin on 10/18/2018.
- */
-public class AttachL3NetworkToVmNicReply extends MessageReply {
-}
