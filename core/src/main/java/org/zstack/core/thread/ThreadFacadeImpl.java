1c3cf676419cfe3cae5232b0123a718e9d4cc7d2
yaoning.li <2016220101025@std.uestc.edu.cn>
2020-08-04 14:51:42 +0800

    [BugFix: ZSTACK-28495] register hook for ThreadFacadeImpl#_syncpool

--- core/src/main/java/org/zstack/core/thread/ThreadFacadeImpl.java
+++ core/src/main/java/org/zstack/core/thread/ThreadFacadeImpl.java
@@ -209,7 +209,7 @@ public class ThreadFacadeImpl implements ThreadFacade, ThreadFactory, RejectedEx
     @Override
     public void unregisterHook(ThreadAroundHook hook) {
         _pool.unregisterHook(hook);
-        _pool.unregisterHook(hook);
+        _syncpool.unregisterHook(hook);
     }
 
     @Override
