228cc959db7d2ce5f7c95ec7e21cc1e066857727
kaicai.hu <kaicai.hu@zstack.io>
2020-08-14 16:58:53 +0800

    [BugFix: ZSTACK-28448]

--- test/src/test/groovy/org/zstack/test/integration/core/ElaborationReconnectHostCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/ElaborationReconnectHostCase.groovy
@@ -2,8 +2,12 @@ package org.zstack.test.integration.core
 
 import org.zstack.core.Platform
 import org.zstack.header.errorcode.ErrorCode
+import org.zstack.header.errorcode.SysErrors
+import org.zstack.header.host.HostErrors
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.SubCase
+
+import static org.zstack.core.Platform.operr
 
 class ElaborationReconnectHostCase extends SubCase {
     EnvSpec env
@@ -34,6 +38,18 @@ class ElaborationReconnectHostCase extends SubCase {
         assert err.elaboration != null
         assert err.elaboration.trim() == "错误信息: 无法重连物理机"
 
+        def err1 = Platform.operr("failed to create bridge") as ErrorCode
+        assert err1.elaboration == null
+
+        def err2 = Platform.err(HostErrors.CONNECTION_ERROR, err1, "connection error for KVM host[uuid:%s, ip:%s]", Platform.getUuid(), "127.0.0.1") as ErrorCode
+        assert err2.getElaboration() == null
+
+        def err3 =  Platform.err(HostErrors.CONNECTION_ERROR, err2, "connection error for KVM host[uuid:%s, ip:%s]", Platform.getUuid(), "127.0.0.1") as ErrorCode
+        assert err3.getElaboration() == null
+
+        def err4 = Platform.err(SysErrors.OPERATION_ERROR, err, "failed to create bridge") as ErrorCode
+        assert err4.getElaboration() != null
+        assert err4.elaboration.trim() == "错误信息: 无法重连物理机"
     }
 
 }
