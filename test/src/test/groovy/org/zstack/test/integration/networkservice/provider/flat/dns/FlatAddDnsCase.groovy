052c330345780d985fc3615a153f8335f1c1e6ab
dengsong.yang <dengsong.yang@zstack.io>
2020-08-24 16:09:12 +0800

    [BugFix: ZSTACK-28959] avoid to add same ipv6 dns to l3 network

--- test/src/test/groovy/org/zstack/test/integration/networkservice/provider/flat/dns/FlatAddDnsCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/networkservice/provider/flat/dns/FlatAddDnsCase.groovy
@@ -2,6 +2,8 @@ package org.zstack.test.integration.networkservice.provider.flat.dns
 
 import org.springframework.http.HttpEntity
 import org.zstack.network.service.flat.FlatDhcpBackend
+import org.zstack.sdk.IpRangeInventory
+import org.zstack.sdk.L2NetworkInventory
 import org.zstack.sdk.L3NetworkInventory
 import org.zstack.test.integration.networkservice.provider.NetworkServiceProviderTest
 import org.zstack.test.integration.networkservice.provider.flat.FlatNetworkServiceEnv
@@ -9,6 +11,8 @@ import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.L3NetworkSpec
 import org.zstack.testlib.SubCase
 import org.zstack.utils.gson.JSONObjectUtil
+import org.zstack.utils.network.IPv6Constants
+
 /**
  * Created by weiwang on 25/05/2017.
  */
@@ -50,6 +54,38 @@ class FlatAddDnsCase extends SubCase {
         assert cmd != null
         assert cmd.dhcp.get(0).dns.size() == 1
         assert cmd.dhcp.get(0).dns.get(0) == dns1
+
+        def l2 = env.inventoryByName("l2") as L2NetworkInventory
+
+        L3NetworkInventory l3_pub_ipv6 = createL3Network {
+            category = "Public"
+            l2NetworkUuid = l2.uuid
+            name = "ipv6"
+            ipVersion = 6
+        }
+
+        IpRangeInventory ipr1 = addIpv6Range {
+            l3NetworkUuid = l3_pub_ipv6.uuid
+            name = "test-ipv6-range"
+            startIp = "1000::10"
+            endIp = "1000::20"
+            prefixLen = 64
+            gateway = "1000::1"
+            addressMode = IPv6Constants.Stateful_DHCP
+            systemTags = [String.format("flatNetwork::DhcpServer::%s::ipUuid::null", "1000--2")]
+        }
+
+        addDnsToL3Network {
+            delegate.l3NetworkUuid = l3_pub_ipv6.getUuid()
+            delegate.dns = "1::1"
+        }
+
+        expectError {
+            addDnsToL3Network {
+                delegate.l3NetworkUuid = l3_pub_ipv6.getUuid()
+                delegate.dns = "1::0:1"
+            }
+        }
     }
 
     @Override
