3497c9e81afa907a2d2136053a4f5f524209a053
GuoYi <yi.guo@zstack.io>
2020-08-27 12:43:45 +0800

    [BugFix: ZSTACK-29062]
    check interface length when attach l2 vlan network to kvm cluster

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMApiInterceptor.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMApiInterceptor.java
@@ -1,16 +1,23 @@
 package org.zstack.kvm;
 
-import edu.emory.mathcs.backport.java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.db.SimpleQuery;
 import org.zstack.core.db.SimpleQuery.Op;
 import org.zstack.core.errorcode.ErrorFacade;
-import org.zstack.header.apimediator.GlobalApiMessageInterceptor;
 import org.zstack.header.apimediator.ApiMessageInterceptionException;
 import org.zstack.header.apimediator.ApiMessageInterceptor;
+import org.zstack.header.apimediator.GlobalApiMessageInterceptor;
+import org.zstack.header.cluster.ClusterVO;
 import org.zstack.header.message.APIMessage;
+import org.zstack.header.network.l2.APIAttachL2NetworkToClusterMsg;
+import org.zstack.header.network.l2.L2NetworkConstant;
+import org.zstack.header.network.l2.L2VlanNetworkVO;
+import org.zstack.utils.CollectionDSL;
+import org.zstack.utils.network.NetworkUtils;
+
 import java.util.List;
+
 import static org.zstack.core.Platform.argerr;
 
 /**
@@ -25,6 +32,8 @@ public class KVMApiInterceptor implements ApiMessageInterceptor, GlobalApiMessag
     public APIMessage intercept(APIMessage msg) throws ApiMessageInterceptionException {
         if (msg instanceof APIAddKVMHostMsg) {
             validate((APIAddKVMHostMsg) msg);
+        } else if (msg instanceof APIAttachL2NetworkToClusterMsg) {
+            validate((APIAttachL2NetworkToClusterMsg) msg);
         }
         return msg;
     }
@@ -38,9 +47,28 @@ public class KVMApiInterceptor implements ApiMessageInterceptor, GlobalApiMessag
         }
     }
 
+    // if attach l2 vlan network to kvm cluster, then check its interface length
+    private void validate(APIAttachL2NetworkToClusterMsg msg) {
+        ClusterVO cls = dbf.findByUuid(msg.getClusterUuid(), ClusterVO.class);
+        if (cls != null && !cls.getHypervisorType().equals(KVMConstant.KVM_HYPERVISOR_TYPE)) {
+            return;
+        }
+
+        L2VlanNetworkVO l2 = dbf.findByUuid(msg.getL2NetworkUuid(), L2VlanNetworkVO.class);
+        if (l2 == null) {
+            return;
+        }
+
+        if (NetworkUtils.generateVlanDeviceName(l2.getPhysicalInterface(), l2.getVlan()).length()
+                > L2NetworkConstant.LINUX_IF_NAME_MAX_SIZE) {
+            throw new ApiMessageInterceptionException(argerr("cannot create vlan-device on %s because it's too long"
+                    , l2.getPhysicalInterface()));
+        }
+    }
+
     @Override
     public List<Class> getMessageClassToIntercept() {
-        return Collections.emptyList();
+        return CollectionDSL.list(APIAttachL2NetworkToClusterMsg.class);
     }
 
     @Override
