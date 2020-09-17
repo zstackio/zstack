a36e108ee16c2c745912b27430e25d649cf73d08
qiuyu.zhang <qiuyu.zhang@zstack.io>
2020-09-17 14:51:32 +0800

    [Feature: ZSTACK-29381] iam2 modify

--- conf/db/upgrade/V4.0.0__schema.sql
+++ conf/db/upgrade/V4.0.0__schema.sql
@@ -175,3 +175,65 @@ CALL checkVirtualhostsExist();
 DROP PROCEDURE IF EXISTS checkVirtualhostsExist;
 
 ALTER TABLE `zstack`.`VpcRouterVmVO` ADD COLUMN `generalVersion` varchar(32) DEFAULT NULL;
+
+INSERT INTO `zstack`.`IAM2OrganizationVO` (uuid, name, state, type, srcType, createDate, lastOpDate, rootOrganizationUuid)
+              values ('6e3d19dab98348d8bd67657378843f82', 'default', 'Enabled', 'Default', 'ZStack', NOW(), NOW(), '6e3d19dab98348d8bd67657378843f82');
+
+DELIMITER $$
+CREATE PROCEDURE insertDefaultIAM2Organization()
+BEGIN
+    DECLARE virtualIDUuid VARCHAR(32);
+    DECLARE organizationUuid VARCHAR(32);
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT uuid, '6e3d19dab98348d8bd67657378843f82' FROM zstack.IAM2VirtualIDVO where type = 'ZStack' and uuid not in (SELECT virtualIDUuid FROM zstack.IAM2VirtualIDOrganizationRefVO);
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO virtualIDUuid, organizationUuid;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        INSERT INTO `zstack`.IAM2VirtualIDOrganizationRefVO (virtualIDUuid, organizationUuid) VALUES (virtualIDUuid, organizationUuid);
+
+    END LOOP;
+    CLOSE cur;
+    SELECT CURTIME();
+END $$
+DELIMITER ;
+
+CALL insertDefaultIAM2Organization();
+DROP PROCEDURE IF EXISTS insertDefaultIAM2Organization;
+
+
+CREATE TABLE `IAM2ProjectVirtualIDGroupRefVO` (
+    `groupUuid` VARCHAR(32) NOT NULL,
+    `projectUuid` VARCHAR(32) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`groupUuid`,`projectUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER $$
+CREATE PROCEDURE insertIAM2ProjectVirtualIDGroupRef()
+BEGIN
+    DECLARE groupUuid VARCHAR(32);
+    DECLARE projectUuid VARCHAR(32);
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT uuid, projectUuid  FROM zstack.IAM2VirtualIDGroupVO;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO groupUuid, projectUuid;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        INSERT INTO `zstack`.IAM2ProjectVirtualIDGroupRefVO (groupUuid, projectUuid) VALUES (groupUuid, projectUuid);
+
+    END LOOP;
+    CLOSE cur;
+    SELECT CURTIME();
+END $$
+DELIMITER ;
+
+CALL insertIAM2ProjectVirtualIDGroupRef();
+DROP PROCEDURE IF EXISTS insertIAM2ProjectVirtualIDGroupRef;
