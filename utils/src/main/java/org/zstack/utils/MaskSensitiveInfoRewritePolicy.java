627ea0e0ed93ace50c76f2e6b901a1b925f167f7
J M <630911437@qq.com>
2020-09-23 15:38:19 +0800

    encrypt log

--- utils/src/main/java/org/zstack/utils/MaskSensitiveInfoRewritePolicy.java
+++ utils/src/main/java/org/zstack/utils/MaskSensitiveInfoRewritePolicy.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.message.ObjectMessage;
 import org.apache.logging.log4j.message.ReusableObjectMessage;
 import org.apache.logging.log4j.message.ReusableSimpleMessage;
 import org.apache.logging.log4j.message.SimpleMessage;
+import org.apache.logging.log4j.util.Strings;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -23,31 +24,52 @@ import java.util.Set;
 public final class MaskSensitiveInfoRewritePolicy implements RewritePolicy {
     static Set<String> loggerNames = Collections.synchronizedSet(new HashSet<>());
 
+    private static EncryptRSAUtil rsa = null;
+
+    public static void initKey(String key) {
+        rsa = Strings.isEmpty(key) ? null : new EncryptRSAUtil(key);
+    }
+
     @Override
     public LogEvent rewrite(final LogEvent event) {
-        if (!loggerNames.contains(event.getLoggerName())) {
+        if (rsa == null && !loggerNames.contains(event.getLoggerName())) {
             return event;
         }
 
         if (event.getMessage() instanceof ObjectMessage) {
             Log4jLogEvent.Builder builder = new Log4jLogEvent.Builder(event);
-            builder.setMessage(new SimpleMessage(Utils.maskSensitiveInfo(event.getMessage().getFormattedMessage())));
+            builder.setMessage(new SimpleMessage(encryptLog(event.getMessage().getFormattedMessage(), event.getLoggerName())));
             return builder.build();
         } else if (event.getMessage() instanceof SimpleMessage) {
             Log4jLogEvent.Builder builder = new Log4jLogEvent.Builder(event);
-            builder.setMessage(new SimpleMessage(Utils.maskSensitiveInfo(event.getMessage().getFormattedMessage())));
+            builder.setMessage(new SimpleMessage(encryptLog(event.getMessage().getFormattedMessage(), event.getLoggerName())));
             return builder.build();
         } else if (event.getMessage() instanceof ReusableObjectMessage) {
             ReusableObjectMessage msg = (ReusableObjectMessage) event.getMessage();
-            msg.set(Utils.maskSensitiveInfo(msg.getFormattedMessage()));
+            msg.set(encryptLog(msg.getFormattedMessage(), event.getLoggerName()));
             return event;
         } else if (event.getMessage() instanceof ReusableSimpleMessage) {
             ReusableSimpleMessage msg = (ReusableSimpleMessage) event.getMessage();
-            msg.set(Utils.maskSensitiveInfo(msg.getFormattedMessage()));
+            msg.set(encryptLog(msg.getFormattedMessage(), event.getLoggerName()));
             return event;
+        } else if (rsa != null) {
+            Log4jLogEvent.Builder builder = new Log4jLogEvent.Builder(event);
+            builder.setMessage(new SimpleMessage(encryptLog(event.getMessage().getFormattedMessage(), event.getLoggerName())));
+            return builder.build();
         } else {
             return event;
         }
+    }
+
+    private String encryptLog(String raw, String logName) {
+        if (loggerNames.contains(logName)) {
+            raw = Utils.maskSensitiveInfo(raw);
+        }
+
+        if (rsa != null) {
+            raw = rsa.encrypt(raw);
+        }
+        return raw;
     }
 
     @PluginFactory
