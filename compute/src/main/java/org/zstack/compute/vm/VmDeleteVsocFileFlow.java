4e883057274efdd20d856829773dbd7b6ec91ae7
jintao.chen <jintao.chen@zstack.io>
2020-09-24 14:54:29 +0800

    change VmDeleteVsocFileFlow to no rollbackflow

--- compute/src/main/java/org/zstack/compute/vm/VmDeleteVsocFileFlow.java
+++ compute/src/main/java/org/zstack/compute/vm/VmDeleteVsocFileFlow.java
@@ -10,6 +10,7 @@ import org.zstack.core.componentloader.PluginRegistry;
 import org.zstack.header.core.workflow.Flow;
 import org.zstack.header.core.workflow.FlowRollback;
 import org.zstack.header.core.workflow.FlowTrigger;
+import org.zstack.header.core.workflow.NoRollbackFlow;
 import org.zstack.header.host.HostConstant;
 import org.zstack.header.message.MessageReply;
 import org.zstack.header.vm.*;
@@ -22,7 +23,7 @@ import java.util.Map;
 import static org.zstack.core.CoreGlobalProperty.PLATFORM_ID;
 
 @Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
-public class VmDeleteVsocFileFlow implements Flow {
+public class VmDeleteVsocFileFlow extends NoRollbackFlow {
     private static final CLogger logger = Utils.getLogger(VmCreateOnHypervisorFlow.class);
 
     @Autowired
@@ -53,10 +54,5 @@ public class VmDeleteVsocFileFlow implements Flow {
                 }
             }
         });
-    }
-
-    @Override
-    public void rollback(final FlowRollback chain, Map data) {
-        chain.rollback();
     }
 }
