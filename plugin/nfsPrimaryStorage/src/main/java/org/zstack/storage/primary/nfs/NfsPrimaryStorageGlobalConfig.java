9701d3a510a50101a44a33270631c1affcd5bc8e
kaicai.hu <kaicai.hu@zstack.io>
2020-09-25 16:15:09 +0800

    [BugFix: ZSTACK-29521]

--- plugin/nfsPrimaryStorage/src/main/java/org/zstack/storage/primary/nfs/NfsPrimaryStorageGlobalConfig.java
+++ plugin/nfsPrimaryStorage/src/main/java/org/zstack/storage/primary/nfs/NfsPrimaryStorageGlobalConfig.java
@@ -3,6 +3,8 @@ package org.zstack.storage.primary.nfs;
 import org.zstack.core.config.GlobalConfig;
 import org.zstack.core.config.GlobalConfigDefinition;
 import org.zstack.core.config.GlobalConfigValidation;
+import org.zstack.header.storage.primary.PrimaryStorageVO;
+import org.zstack.resourceconfig.BindResourceConfig;
 
 /**
  */
@@ -15,4 +17,8 @@ public class NfsPrimaryStorageGlobalConfig {
 
     @GlobalConfigValidation
     public static GlobalConfig GC_INTERVAL = new GlobalConfig(CATEGORY, "deletion.gcInterval");
+
+    @GlobalConfigValidation(validValues = {"none", "metadata", "falloc", "full"})
+    @BindResourceConfig({PrimaryStorageVO.class})
+    public static GlobalConfig QCOW2_ALLOCATION = new GlobalConfig(CATEGORY, "qcow2.allocation");
 }
--- plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/SMPPrimaryStorageGlobalConfig.java
+++ plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/SMPPrimaryStorageGlobalConfig.java
@@ -3,6 +3,8 @@ package org.zstack.storage.primary.smp;
 import org.zstack.core.config.GlobalConfig;
 import org.zstack.core.config.GlobalConfigDefinition;
 import org.zstack.core.config.GlobalConfigValidation;
+import org.zstack.header.storage.primary.PrimaryStorageVO;
+import org.zstack.resourceconfig.BindResourceConfig;
 
 /**
  * Created by AlanJager on 2017/3/14.
@@ -13,4 +15,8 @@ public class SMPPrimaryStorageGlobalConfig {
 
     @GlobalConfigValidation
     public static GlobalConfig GC_INTERVAL = new GlobalConfig(CATEGORY, "deletion.gcInterval");
+
+    @GlobalConfigValidation(validValues = {"none", "metadata", "falloc", "full"})
+    @BindResourceConfig({PrimaryStorageVO.class})
+    public static GlobalConfig QCOW2_ALLOCATION = new GlobalConfig(CATEGORY, "qcow2.allocation");
 }
