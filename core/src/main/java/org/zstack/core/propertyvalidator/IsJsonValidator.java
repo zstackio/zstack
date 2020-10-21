f7dcb9a37c6859aff6b9d0d3e72601de626b60c5
lianghy <hanyu.liang@zstack.io>
2020-10-21 13:06:43 +0800

    [Improvement: ZSTACK-27718]
    [Improvement: ZSTACK-27718]
    
    [Improvement: ZSTACK-27718]
    
    [Improvement: ZSTACK-27718]

---
+++ core/src/main/java/org/zstack/core/propertyvalidator/IsJsonValidator.java
@@ -0,0 +1,27 @@
+package org.zstack.core.propertyvalidator;
+
+import java.util.ArrayList;
+
+import org.zstack.header.storage.backup.PrimaryStoragePriorityGetter;
+import org.zstack.utils.gson.JSONObjectUtil;
+
+/**
+ *  * Created by LiangHanYu on 2020/10/24 12:53
+ *   */
+
+@ValidateTarget(target = IsJson.class)
+public class IsJsonValidator implements GlobalPropertyValidator {
+    @Override
+    public boolean validate(String name, String value, Object rule) throws GlobalPropertyValidatorExecption {
+    //when value is null, isJSONStringParsable will return true
+    if (value == null || value.isEmpty()) {
+            return true;
+        }
+        try {
+            JSONObjectUtil.toCollection(value, ArrayList.class, PrimaryStoragePriorityGetter.PriorityMap.class);
+        } catch (RuntimeException e) {
+            throw new GlobalPropertyValidatorExecption(String.format("invalid json value %s for %s", value, name));// json not valid
+        }
+        return true;
+    }
+}
