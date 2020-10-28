bb106293e3f9f46f4fc8b800155c3116660059f3
qiuyu.zhang <qiuyu.zhang@zstack.io>
2020-10-28 15:14:33 +0800

    [Improvement: ZSTACK-29662] OAuth no login

---
+++ sdk/src/main/java/org/zstack/sdk/AuthClientDetailsInventory.java
@@ -0,0 +1,87 @@
+package org.zstack.sdk;
+
+
+
+public class AuthClientDetailsInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String clientId;
+    public void setClientId(java.lang.String clientId) {
+        this.clientId = clientId;
+    }
+    public java.lang.String getClientId() {
+        return this.clientId;
+    }
+
+    public java.lang.String clientSecret;
+    public void setClientSecret(java.lang.String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    public java.lang.String getClientSecret() {
+        return this.clientSecret;
+    }
+
+    public java.lang.String clientName;
+    public void setClientName(java.lang.String clientName) {
+        this.clientName = clientName;
+    }
+    public java.lang.String getClientName() {
+        return this.clientName;
+    }
+
+    public java.lang.String redirectUri;
+    public void setRedirectUri(java.lang.String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+    public java.lang.String getRedirectUri() {
+        return this.redirectUri;
+    }
+
+    public java.lang.String authorizeUri;
+    public void setAuthorizeUri(java.lang.String authorizeUri) {
+        this.authorizeUri = authorizeUri;
+    }
+    public java.lang.String getAuthorizeUri() {
+        return this.authorizeUri;
+    }
+
+    public java.lang.String accessTokenUri;
+    public void setAccessTokenUri(java.lang.String accessTokenUri) {
+        this.accessTokenUri = accessTokenUri;
+    }
+    public java.lang.String getAccessTokenUri() {
+        return this.accessTokenUri;
+    }
+
+    public java.util.List scopes;
+    public void setScopes(java.util.List scopes) {
+        this.scopes = scopes;
+    }
+    public java.util.List getScopes() {
+        return this.scopes;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
