d122c527196012ae194cdee6c58f4a4b771aefbb
shixin.ruan <shixin.ruan@zstack.io>
2020-10-30 14:58:34 +0800

    [BugFix: ZSTACK-xxx]  add l3 type to slb group network

--- sdk/src/main/java/org/zstack/sdk/SlbGroupL3NetworkRefInventory.java
+++ sdk/src/main/java/org/zstack/sdk/SlbGroupL3NetworkRefInventory.java
@@ -20,6 +20,22 @@ public class SlbGroupL3NetworkRefInventory  {
         return this.l3NetworkUuid;
     }
 
+    public java.lang.String l3NetworkCategory;
+    public void setL3NetworkCategory(java.lang.String l3NetworkCategory) {
+        this.l3NetworkCategory = l3NetworkCategory;
+    }
+    public java.lang.String getL3NetworkCategory() {
+        return this.l3NetworkCategory;
+    }
+
+    public java.lang.String l3NetworkType;
+    public void setL3NetworkType(java.lang.String l3NetworkType) {
+        this.l3NetworkType = l3NetworkType;
+    }
+    public java.lang.String getL3NetworkType() {
+        return this.l3NetworkType;
+    }
+
     public java.lang.String type;
     public void setType(java.lang.String type) {
         this.type = type;
