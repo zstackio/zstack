69d737f91d30d4e31af704f02b6d40675655dae7
ting.su <ting.su@zstack.io>
2020-11-11 11:59:54 +0800

    [BugFix:ZSTAC-29323]:Project zstack

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/backup/CephBackupStoragePrimaryStorageExtension.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/backup/CephBackupStoragePrimaryStorageExtension.java
@@ -1,18 +1,21 @@
 package org.zstack.storage.ceph.backup;
 
 import org.zstack.core.db.SQL;
+import org.zstack.header.allocator.BackupStorageAllocatorFilterExtensionPoint;
 import org.zstack.header.core.Completion;
 import org.zstack.header.image.ImageInventory;
 import org.zstack.header.storage.backup.BackupStorageInventory;
 import org.zstack.header.storage.backup.BackupStoragePrimaryStorageExtensionPoint;
 import org.zstack.header.storage.primary.PrimaryStorageInventory;
+import org.zstack.storage.ceph.CephConstants;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Created by mingjian.deng on 2017/10/31.
  */
-public class CephBackupStoragePrimaryStorageExtension implements BackupStoragePrimaryStorageExtensionPoint {
+public class CephBackupStoragePrimaryStorageExtension implements BackupStoragePrimaryStorageExtensionPoint, BackupStorageAllocatorFilterExtensionPoint {
     @Override
     public String getPrimaryStoragePriorityMap(BackupStorageInventory bs, ImageInventory image) {
         if (!bs.getType().equals("Ceph")) {
@@ -31,5 +34,18 @@ public class CephBackupStoragePrimaryStorageExtension implements BackupStoragePr
     @Override
     public void cleanupPrimaryCacheForBS(PrimaryStorageInventory ps, String hostUuid, Completion completion) {
         completion.success();
+    }
+
+    @Override
+    public List<BackupStorageInventory> filterBackupStorageCandidatesByPS(List<BackupStorageInventory> candidates, String psUuid) {
+        List<String> bsUuids = getBackupStorageSupportedPS(psUuid);
+        if (bsUuids.isEmpty()) {
+            return candidates;
+        }
+
+        candidates.removeIf(c -> c.getType().equals(CephConstants.CEPH_BACKUP_STORAGE_TYPE) && !bsUuids.contains(c.getUuid()));
+
+        return candidates;
+
     }
 }
