31f7299430cdd312b99fb92aaed3574cc4374e35
anquan.wu <anquan.wu@zstack.io>
2020-11-12 20:38:57 +0800

    [Other: ZSTACK-xxx] update APIHelper

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -4256,6 +4256,33 @@ abstract class ApiHelper {
     }
 
 
+    def changeLoadBalancerBackendServer(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.ChangeLoadBalancerBackendServerAction.class) Closure c) {
+        def a = new org.zstack.sdk.ChangeLoadBalancerBackendServerAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def changeLoadBalancerListener(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.ChangeLoadBalancerListenerAction.class) Closure c) {
         def a = new org.zstack.sdk.ChangeLoadBalancerListenerAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
