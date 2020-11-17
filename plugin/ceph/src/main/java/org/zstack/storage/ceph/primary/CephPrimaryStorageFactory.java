a372a6c3678d0f726e40be78cd4075b9a49b0017
Zhang, Wenhao <wenhao.zhang@zstack.io>
2020-11-17 11:01:20 +0800

    [BugFix: ZSTAC-31612]

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageFactory.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageFactory.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonSyntaxException;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.zstack.compute.host.HostSystemTags;
 import org.zstack.compute.vm.VmCapabilities;
 import org.zstack.compute.vm.VmCapabilitiesExtensionPoint;
 import org.zstack.configuration.DiskOfferingSystemTags;
@@ -1201,10 +1200,7 @@ public class CephPrimaryStorageFactory implements PrimaryStorageFactory, CephCap
         bus.makeTargetServiceIdByResourceUuid(msg, PrimaryStorageConstant.SERVICE_ID, msg.getPrimaryStorageUuid());
 
         List<String> avoidHostUuids = new ArrayList<>();
-        if (spec.getAvoidHostUuids() != null) {
-            avoidHostUuids.addAll(spec.getAvoidHostUuids());
-        }
-        if (spec.getSoftAvoidHostUuids() != null ) {
+        if (spec.getSoftAvoidHostUuids() != null) {
             avoidHostUuids.addAll(spec.getSoftAvoidHostUuids());
         }
 
@@ -1231,7 +1227,7 @@ public class CephPrimaryStorageFactory implements PrimaryStorageFactory, CephCap
                 }
 
                 GetVolumeWatchersReply rly = (GetVolumeWatchersReply)reply;
-                List watchers = rly.getWatchers();
+                List<String> watchers = rly.getWatchers();
                 if (watchers == null || watchers.isEmpty()) {
                     completion.success();
                     return;
