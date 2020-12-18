af990b001f522ce1ed31e7f0a27ab951fa9c2a6f
qiuyu.zhang <qiuyu.zhang@zstack.io>
2020-12-18 13:24:43 +0800

    [BugFix: ZSTACK-34798] update IAM2VirtualIDRoleRefVO date

--- conf/db/upgrade/V4.0.0__schema.sql
+++ conf/db/upgrade/V4.0.0__schema.sql
@@ -195,7 +195,7 @@ BEGIN
 
         SET targetProjectUuid = SUBSTRING_INDEX(projectOperatorTag, '::', -1);
         SELECT `accountUuid` into targetAccountUuid FROM `IAM2ProjectAccountRefVO` WHERE `projectUuid` = targetProjectUuid LIMIT 1;
-
+        delete from IAM2VirtualIDRoleRefVO where virtualIDUuid = iam2VirtualIDUuid and roleUuid = 'f2f474c60e7340c0a1d44080d5bde3a9';
         INSERT INTO IAM2VirtualIDRoleRefVO (`virtualIDUuid`, `roleUuid`, `targetAccountUuid`, createDate, lastOpDate) VALUES (iam2VirtualIDUuid, 'f2f474c60e7340c0a1d44080d5bde3a9', targetAccountUuid, NOW(), NOW());
     END LOOP;
     CLOSE cur;
@@ -224,7 +224,7 @@ BEGIN
 
         SET targetProjectUuid = SUBSTRING_INDEX(projectAdminTag, '::', -1);
         SELECT `accountUuid` into targetAccountUuid FROM `IAM2ProjectAccountRefVO` WHERE `projectUuid` = targetProjectUuid LIMIT 1;
-
+        delete from IAM2VirtualIDRoleRefVO where virtualIDUuid = iam2VirtualIDUuid and roleUuid = 'f2f474c60e7340c0a1d44080d5bde3a9';
         INSERT INTO IAM2VirtualIDRoleRefVO (`virtualIDUuid`, `roleUuid`, `targetAccountUuid`, createDate, lastOpDate) VALUES (iam2VirtualIDUuid, 'f2f474c60e7340c0a1d44080d5bde3a9', targetAccountUuid, NOW(), NOW());
     END LOOP;
     CLOSE cur;
