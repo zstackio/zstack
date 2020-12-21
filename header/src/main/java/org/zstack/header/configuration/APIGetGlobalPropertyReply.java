f95025344bc9017158bda7956a5ee7e471ff52c5
GuoYi <yi.guo@zstack.io>
2020-12-21 20:17:06 +0800

    recover APIGetGlobalPropertyMsg

--- header/src/main/java/org/zstack/header/configuration/APIGetGlobalPropertyReply.java
+++ header/src/main/java/org/zstack/header/configuration/APIGetGlobalPropertyReply.java
@@ -1,6 +1,7 @@
 package org.zstack.header.configuration;
 
 import org.zstack.header.message.APIReply;
+import org.zstack.header.rest.RestResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Created by frank on 7/23/2015.
  */
+@RestResponse(allTo = "properties")
 public class APIGetGlobalPropertyReply extends APIReply {
     private List<String> properties;
 
@@ -24,10 +26,7 @@ public class APIGetGlobalPropertyReply extends APIReply {
         List<String> list = new ArrayList<>();
         list.add("tapResourcesForBilling: false");
         list.add("unitTestOn: false");
-
         reply.setProperties(list);
-
         return reply;
     }
-
 }
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -16001,6 +16001,33 @@ abstract class ApiHelper {
     }
 
 
+    def getGlobalProperty(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetGlobalPropertyAction.class) Closure c) {
+        def a = new org.zstack.sdk.GetGlobalPropertyAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getHostAllocatorStrategies(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetHostAllocatorStrategiesAction.class) Closure c) {
         def a = new org.zstack.sdk.GetHostAllocatorStrategiesAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
