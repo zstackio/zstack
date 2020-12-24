8dac045a5dfa3b2610a119256ba680f40a599177
GuoYi <yi.guo@zstack.io>
2020-12-24 18:36:58 +0800

    Revert "recover APIGetGlobalPropertyMsg"
    This reverts commit f95025344bc9017158bda7956a5ee7e471ff52c5.

--- header/src/main/java/org/zstack/header/configuration/APIGetGlobalPropertyReply.java
+++ header/src/main/java/org/zstack/header/configuration/APIGetGlobalPropertyReply.java
@@ -1,7 +1,6 @@
 package org.zstack.header.configuration;
 
 import org.zstack.header.message.APIReply;
-import org.zstack.header.rest.RestResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.List;
 /**
  * Created by frank on 7/23/2015.
  */
-@RestResponse(allTo = "properties")
 public class APIGetGlobalPropertyReply extends APIReply {
     private List<String> properties;
 
@@ -26,7 +24,10 @@ public class APIGetGlobalPropertyReply extends APIReply {
         List<String> list = new ArrayList<>();
         list.add("tapResourcesForBilling: false");
         list.add("unitTestOn: false");
+
         reply.setProperties(list);
+
         return reply;
     }
+
 }
