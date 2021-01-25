926a1877eeb985dd48850c517ba331c0fb3ec495
wuaq <wuaq@certusnet.com.cn>
2021-01-25 12:31:03 +0800

    [BugFix: ZSTACK-27537] sdk APICreatePolicyRouteRuleSetMsg name: maxlength to 255

--- sdk/src/main/java/org/zstack/sdk/CreatePolicyRouteRuleSetAction.java
+++ sdk/src/main/java/org/zstack/sdk/CreatePolicyRouteRuleSetAction.java
@@ -25,7 +25,7 @@ public class CreatePolicyRouteRuleSetAction extends AbstractAction {
         }
     }
 
-    @Param(required = true, maxLength = 16, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    @Param(required = true, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String name;
 
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
