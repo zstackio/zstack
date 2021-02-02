a686e5de7dd1a5315fde26a9abb52fb19a70b1bf
jintao.chen <jintao.chen@zstack.io>
2021-02-02 13:44:57 +0800

    change sscardid::123456789

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
@@ -2990,7 +2990,7 @@ public class KVMAgentCommands {
     public static class VsocMigrateCommand extends AgentCommand {
         public String vmUuid;
         public String socId;
-        public String type;
+        public int type;
         public String platformId;
     }
 
