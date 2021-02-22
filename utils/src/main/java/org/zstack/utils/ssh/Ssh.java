b443793da37dd6e3925a8ccd2108614d9762a023
mingjian.deng <mingjian.deng@zstack.io>
2021-02-22 14:19:16 +0800

    [ssh npe]

--- utils/src/main/java/org/zstack/utils/ssh/Ssh.java
+++ utils/src/main/java/org/zstack/utils/ssh/Ssh.java
@@ -409,7 +409,9 @@ public class Ssh {
             script.cleanup();
         }
 
-        session.disconnect();
+        if (session != null) {
+            session.disconnect();
+        }
     }
 
     public SshResult run() {
