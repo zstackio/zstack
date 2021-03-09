ca6497c222d88660a53f4e4d39a18e6c0b230065
dengsong.yang <dengsong.yang@zstack.io>
2021-03-09 16:24:43 +0800

    [BugFix:ZSTACK-37963]

--- network/src/main/java/org/zstack/network/l3/L3BasicNetwork.java
+++ network/src/main/java/org/zstack/network/l3/L3BasicNetwork.java
@@ -238,22 +238,39 @@ public class L3BasicNetwork implements L3Network {
     }
 
     private void handle(AllocateIpMsg msg) {
-        IpAllocatorType strategyType = getIpAllocatorType(msg);
-        IpAllocatorStrategy ias = l3NwMgr.getIpAllocatorStrategy(strategyType);
-        AllocateIpReply reply = new AllocateIpReply();
-        UsedIpInventory ip = ias.allocateIp(msg);
-        if (ip == null) {
-            String reason = msg.getRequiredIp() == null ?
-                    String.format("no ip is available in this l3Network[name:%s, uuid:%s]", self.getName(), self.getUuid()) :
-                    String.format("IP[%s] is not available", msg.getRequiredIp());
-            reply.setError(err(L3Errors.ALLOCATE_IP_ERROR,
-                    "IP allocator strategy[%s] failed, because %s", strategyType, reason));
-        } else {
-            logger.debug(String.format("Ip allocator strategy[%s] successfully allocates an ip[%s]", strategyType, printer.print(ip)));
-            reply.setIpInventory(ip);
-        }
+        thdf.chainSubmit(new ChainTask(msg) {
 
-        bus.reply(msg, reply);
+            @Override
+            public String getSyncSignature() {
+                return getName();
+            }
+
+            @Override
+            public void run(SyncTaskChain chain) {
+                IpAllocatorType strategyType = getIpAllocatorType(msg);
+                IpAllocatorStrategy ias = l3NwMgr.getIpAllocatorStrategy(strategyType);
+                AllocateIpReply reply = new AllocateIpReply();
+                UsedIpInventory ip = ias.allocateIp(msg);
+                if (ip == null) {
+                    String reason = msg.getRequiredIp() == null ?
+                            String.format("no ip is available in this l3Network[name:%s, uuid:%s]", self.getName(), self.getUuid()) :
+                            String.format("IP[%s] is not available", msg.getRequiredIp());
+                    reply.setError(err(L3Errors.ALLOCATE_IP_ERROR,
+                            "IP allocator strategy[%s] failed, because %s", strategyType, reason));
+                } else {
+                    logger.debug(String.format("Ip allocator strategy[%s] successfully allocates an ip[%s]", strategyType, printer.print(ip)));
+                    reply.setIpInventory(ip);
+                }
+
+                bus.reply(msg, reply);
+                chain.next();
+            }
+
+            @Override
+            public String getName() {
+                return String.format("allocate-ip-from-l3-%s", msg.getL3NetworkUuid());
+            }
+        });
     }
 
     private void handleApiMessage(APIMessage msg) {
