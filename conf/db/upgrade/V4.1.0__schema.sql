911f3865f575b0baba3f2c51f281aabf7eb1902e
Zhang, Wenhao <wenhao.zhang@zstack.io>
2021-03-12 10:57:08 +0800

    [BugFix: ZSTAC-35527]

---
+++ conf/db/upgrade/V4.1.0__schema.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE `zstack`.`PciDeviceVO` ADD `chooser` varchar(32) default 'None';
+
+-- TODO
+
+-- DROP TABLE IF EXISTS `zstack`.`VmInstancePciSpecDeviceRefVO`;
--- sdk/src/main/java/org/zstack/sdk/PciDeviceStatus.java
+++ sdk/src/main/java/org/zstack/sdk/PciDeviceStatus.java
@@ -4,4 +4,5 @@ public enum PciDeviceStatus {
 	Active,
 	Attached,
 	System,
+	Reserve,
 }
