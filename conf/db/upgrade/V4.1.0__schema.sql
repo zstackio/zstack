0c26d301f9b33adb39ea5b05b8e937355390ed13
Zhang, Wenhao <wenhao.zhang@zstack.io>
2021-03-16 15:37:32 +0800

    [Feature: ZSTAC-38924]
    1. Add table LicenseHistoryVO in database
    2. Add get license records API

--- conf/db/upgrade/V4.1.0__schema.sql
+++ conf/db/upgrade/V4.1.0__schema.sql
@@ -68,3 +68,23 @@ alter table `ConsoleProxyAgentVO` add `consoleProxyPort` int NOT NULL;
 
 alter table GarbageCollectorVO add index idxName (`name`(255));
 alter table GarbageCollectorVO add index idxStatus (`status`);
+
+CREATE TABLE IF NOT EXISTS `zstack`.`LicenseHistoryVO`
+(
+    `id`          bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `uuid`        varchar(32) NOT NULL,
+    `cpuNum`      int(10) NOT NULL,
+    `hostNum`     int(10) NOT NULL,
+    `vmNum`       int(10) NOT NULL,
+    `expiredDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `issuedDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `uploadDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `licenseType` varchar(32) NOT NULL,
+    `userName`    varchar(32) NOT NULL,
+    `prodInfo`    varchar(32) DEFAULT NULL,
+    `createDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `lastOpDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE INDEX idxLicenseHistoryVOUploadDate ON LicenseHistoryVO (uploadDate);
