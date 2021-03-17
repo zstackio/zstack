355a93f21de093a20fc5fe844e70765706803a57
qiuyu.zhang <qiuyu.zhang@zstack.io>
2021-03-17 18:29:50 +0800

    [BugFix: ZSTACK-39226] mv schema

--- conf/db/upgrade/V3.10.4__schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `zstack`.`AutoScalingRuleSchedulerJobTriggerVO` (
-    `uuid` VARCHAR(32) NOT NULL UNIQUE,
-    `schedulerJobUuid` VARCHAR(32) NOT NULL UNIQUE,
-    PRIMARY KEY (`uuid`),
-    CONSTRAINT `fkAutoScalingRuleSchedulerJobTriggerVO` FOREIGN KEY (`schedulerJobUuid`) REFERENCES `SchedulerJobVO` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
