48dff0fdf9d896b62dcaa32a024dc7d76177291e
AlanJager <alanjager@outlook.com>
2021-03-25 11:15:04 +0800

    [Feature: ZSTACK-39121]
    Add APISetVmClockTrackMsg to set vm clock track
    
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- sdk/src/main/java/org/zstack/sdk/DeleteVxlanL2NetworkAction.java
+++ sdk/src/main/java/org/zstack/sdk/DeleteVxlanL2NetworkAction.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.zstack.sdk.*;
 
-public class CreateSchedulerJobAction extends AbstractAction {
+public class DeleteVxlanL2NetworkAction extends AbstractAction {
 
     private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
 
@@ -12,7 +12,7 @@ public class CreateSchedulerJobAction extends AbstractAction {
 
     public static class Result {
         public ErrorCode error;
-        public org.zstack.sdk.CreateSchedulerJobResult value;
+        public org.zstack.sdk.DeleteL2NetworkResult value;
 
         public Result throwExceptionIfError() {
             if (error != null) {
@@ -25,26 +25,11 @@ public class CreateSchedulerJobAction extends AbstractAction {
         }
     }
 
-    @Param(required = true, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.lang.String name;
-
-    @Param(required = false, maxLength = 2048, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.lang.String description;
-
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.lang.String targetResourceUuid;
-
-    @Param(required = true, validValues = {"startVm","stopVm","rebootVm","volumeSnapshot","volumeBackup","rootVolumeBackup","vmBackup","databaseBackup","localRaidSelfTest","runAutoScalingGroup"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.lang.String type;
-
-    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.util.Map parameters;
+    public java.lang.String uuid;
 
     @Param(required = false)
-    public java.lang.String resourceUuid;
-
-    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public java.util.List tagUuids;
+    public java.lang.String deleteMode = "Permissive";
 
     @Param(required = false)
     public java.util.List systemTags;
@@ -78,8 +63,8 @@ public class CreateSchedulerJobAction extends AbstractAction {
             return ret;
         }
         
-        org.zstack.sdk.CreateSchedulerJobResult value = res.getResult(org.zstack.sdk.CreateSchedulerJobResult.class);
-        ret.value = value == null ? new org.zstack.sdk.CreateSchedulerJobResult() : value; 
+        org.zstack.sdk.DeleteL2NetworkResult value = res.getResult(org.zstack.sdk.DeleteL2NetworkResult.class);
+        ret.value = value == null ? new org.zstack.sdk.DeleteL2NetworkResult() : value; 
 
         return ret;
     }
@@ -108,11 +93,11 @@ public class CreateSchedulerJobAction extends AbstractAction {
 
     protected RestInfo getRestInfo() {
         RestInfo info = new RestInfo();
-        info.httpMethod = "POST";
-        info.path = "/scheduler/jobs";
+        info.httpMethod = "DELETE";
+        info.path = "/l2-networks/vxlan/{uuid}";
         info.needSession = true;
         info.needPoll = true;
-        info.parameterName = "params";
+        info.parameterName = "";
         return info;
     }
 
