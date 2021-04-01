21802376a4d78f3ea23ea8e97cd088b08484e2a1
haibiao.xiao <haibiao.xiao@zstack.io>
2021-04-01 15:23:39 +0800

    add vSwitchType to L2NetworkVO
    Conflicts:
    	conf/db/upgrade/V4.1.0__schema.sql

---
+++ header/src/main/java/org/zstack/header/network/l2/APIGetVSwitchTypesReply.java
@@ -0,0 +1,44 @@
+package org.zstack.header.network.l2;
+
+import org.zstack.header.message.APIReply;
+import org.zstack.header.rest.RestResponse;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @apiResult api reply for message :ref:`APIGetVSwitchTypesMsg`
+ * @category l2network
+ * @example {
+ * "org.zstack.header.network.l2.APIGetVSwitchTypesReply": {
+ * "vSwitchTypes": [
+ * "LinuxBridge",
+ * "OVSDPDK"
+ * ],
+ * "success": true
+ * }
+ * }
+ * @since 4.1.0
+ */
+
+@RestResponse(fieldsTo = {"types=vSwitchTypes"})
+public class APIGetVSwitchTypesReply extends APIReply {
+    /**
+     * @desc a list of supported vSwitch types
+     */
+    private List<String> vSwitchTypes;
+
+    public List<String> getVSwitchTypes() {
+        return vSwitchTypes;
+    }
+
+    public void setVSwitchTypes(List<String> vSwitchTypes) {
+        this.vSwitchTypes = vSwitchTypes;
+    }
+
+    public static APIGetVSwitchTypesReply __example__() {
+        APIGetVSwitchTypesReply reply = new APIGetVSwitchTypesReply();
+        reply.setVSwitchTypes(Arrays.asList("LinuxBridge", "OVSDPDK"));
+        return reply;
+    }
+}
