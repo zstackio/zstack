604a346a509554eb2a19f50e15fafc2984964955
mingjian.deng <mingjian.deng@zstack.io>
2021-04-15 10:16:48 +0800

    [BugFix: ZSTACK-39958] drop redundant api

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -17486,33 +17486,6 @@ abstract class ApiHelper {
     }
 
 
-    def getMissedElaboration(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetMissedElaborationAction.class) Closure c) {
-        def a = new org.zstack.sdk.GetMissedElaborationAction()
-        a.sessionId = Test.currentEnvSpec?.session?.uuid
-        c.resolveStrategy = Closure.OWNER_FIRST
-        c.delegate = a
-        c()
-        
-
-        if (System.getProperty("apipath") != null) {
-            if (a.apiId == null) {
-                a.apiId = Platform.uuid
-            }
-    
-            def tracker = new ApiPathTracker(a.apiId)
-            def out = errorOut(a.call())
-            def path = tracker.getApiPath()
-            if (!path.isEmpty()) {
-                Test.apiPaths[a.class.name] = path.join(" --->\n")
-            }
-        
-            return out
-        } else {
-            return errorOut(a.call())
-        }
-    }
-
-
     def getMonitorItem(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetMonitorItemAction.class) Closure c) {
         def a = new org.zstack.sdk.GetMonitorItemAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
