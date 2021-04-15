8fc8fbe884837422fac38dea68711c3e9f0f554b
haibiao.xiao <haibiao.xiao@zstack.io>
2021-04-15 17:13:03 +0800

    trigger next while vm has no vDPA nics during migration

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
@@ -1696,6 +1696,12 @@ public class KVMHost extends HostBase implements Host {
                 .eq(VmNicVO_.type, "vDPA")
                 .list();
         List<NicTO> nicTos = VmNicInventory.valueOf(nics).stream().map(this::completeNicInfo).collect(Collectors.toList());
+        List<NicTO> vDPANics = new ArrayList<NicTO>();
+        for (NicTO nicTo : nicTos) {
+            if (nicTo.getType().equals("vDPA")) {
+                vDPANics.add(nicTo);
+            }
+        }
 
         FlowChain chain = FlowChainBuilder.newShareFlowChain();
         chain.setName(String.format("migrate-vm-%s-on-kvm-host-%s", vmUuid, self.getUuid()));
@@ -1707,7 +1713,7 @@ public class KVMHost extends HostBase implements Host {
 
                     @Override
                     public void run(final FlowTrigger trigger, Map data) {
-                        if (nicTos.isEmpty()) {
+                        if (vDPANics.isEmpty()) {
                             trigger.next();
                         }
                         GenerateVdpaCmd cmd = new GenerateVdpaCmd();
@@ -1873,7 +1879,7 @@ public class KVMHost extends HostBase implements Host {
 
                     @Override
                     public void run(final FlowTrigger trigger, Map data) {
-                        if (nicTos.isEmpty()) {
+                        if (vDPANics.isEmpty()) {
                             trigger.next();
                         }
                         DeleteVdpaCmd cmd = new DeleteVdpaCmd();
