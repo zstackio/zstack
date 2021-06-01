d8c78afb6624de099f761bd174d2c85d0f24509b
Wang,Jing <jing.wang@zstack.io>
2021-06-01 10:59:41 +0800

    [Feature: BareMetal2 Billing]

--- conf/db/upgrade/V4.1.3__schema.sql
+++ conf/db/upgrade/V4.1.3__schema.sql
@@ -124,3 +124,54 @@ ALTER TABLE `zstack`.`LoadBalancerListenerACLRefVO` ADD CONSTRAINT fkLoadBalance
 ALTER TABLE `zstack`.`LoadBalancerListenerACLRefVO` DROP FOREIGN KEY fkLoadbalancerListenerACLRefVOAccessControlListVO;
 ALTER TABLE `zstack`.`LoadBalancerListenerACLRefVO` ADD CONSTRAINT fkLoadbalancerListenerACLRefVOAccessControlListVO FOREIGN KEY (aclUuid) REFERENCES `zstack`.`AccessControlListVO` (uuid) ON DELETE CASCADE;
 
+CREATE TABLE IF NOT EXISTS `zstack`.`BareMetal2VmUsageVO`
+(
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `bareMetal2ChassisOfferingUuid` varchar(32) NOT NULL,
+    `vmUuid` varchar(32) NOT NULL,
+    `vmName` varchar(255) DEFAULT NULL,
+    `state` varchar(64) NOT NULL,
+    `accountUuid` varchar(32) NOT NULL,
+    `dateInLong` bigint(20) unsigned NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`),
+    KEY `idxBareMetal2VmUsageVOaccountUuid` (`accountUuid`,`dateInLong`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `zstack`.`PriceBareMetal2ChassisOfferingRefVO`
+(
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `priceUuid` varchar(32) NOT NULL,
+    `bareMetal2ChassisOfferingUuid` varchar(32) NOT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fkPriceBareMetal2ChassisOfferingRefVOPriceVO` FOREIGN KEY (`priceUuid`) REFERENCES `zstack`.`PriceVO` (`uuid`) ON DELETE CASCADE,
+    CONSTRAINT `fkPriceBareMetal2ChassisOfferingRefVOBareMetal2ChassisOfferingVO` FOREIGN KEY (`bareMetal2ChassisOfferingUuid`) REFERENCES `zstack`.`BareMetal2ChassisOfferingVO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`BareMetalUsageHistoryVO`
+(
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `bareMetal2ChassisOfferingUuid` varchar(32) NOT NULL,
+    `vmUuid` varchar(32) NOT NULL,
+    `vmName` varchar(255) DEFAULT NULL,
+    `state` varchar(64) NOT NULL,
+    `accountUuid` varchar(32) NOT NULL,
+    `dateInLong` bigint(20) unsigned NOT NULL,
+    `resourcePriceUserConfig` varchar(1024) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`id`),
+    KEY `idxBareMetal2VmUsageVOaccountUuid` (`accountUuid`,`dateInLong`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `BareMetalBillingVO`
+(
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `bareMetal2ChassisOfferingUUid` varchar(32) NOT NULL,
+    `bareMetal2ChassisOfferingName` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
