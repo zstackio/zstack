21953a9826f229fb686b9dc4e8365b8413d174e0
zaifeng.wang <zaifeng.wang@zstack.io>
2021-06-03 15:00:06 +0800

    bk2

--- conf/db/upgrade/V4.1.2.1__schema.sql
+++ conf/db/upgrade/V4.1.2.1__schema.sql
@@ -6,3 +6,16 @@ CREATE TABLE  `zstack`.`CdpBackupStorageVO` (
     `sshPort` int unsigned NOT NULL,
     PRIMARY KEY  (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `zstack`.`CdpPolicyEO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(255) DEFAULT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `retentionTimePerDay` int unsigned NOT NULL,
+    `incrementalPointPerMinute` int unsigned NOT NULL,
+    `recoveryPointPerSecond` int unsigned NOT NULL,
+    `deleted` varchar(255) DEFAULT NULL,
+    PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE VIEW `zstack`.`CdpPolicyVO` AS SELECT uuid, name, description, retentionTimePerDay, incrementalPointPerMinute, recoveryPointPerSecond FROM `zstack`.`CdpPolicyEO` WHERE deleted IS NULL;
