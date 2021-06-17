6e6b0b7b0c45bde7a2666ca2c597c72ca8f0a2af
Wang,Jing <jing.wang@zstack.io>
2021-06-18 05:54:50 +0800

    [Feature: BareMetal2 Billing] generate new sdk

---
+++ sdk/src/main/java/org/zstack/sdk/BareMetalBillingInventory.java
@@ -0,0 +1,15 @@
+package org.zstack.sdk;
+
+
+
+public class BareMetalBillingInventory extends org.zstack.sdk.BillingInventory {
+
+    public java.lang.String bareMetal2ChassisOfferingUUid;
+    public void setBareMetal2ChassisOfferingUUid(java.lang.String bareMetal2ChassisOfferingUUid) {
+        this.bareMetal2ChassisOfferingUUid = bareMetal2ChassisOfferingUUid;
+    }
+    public java.lang.String getBareMetal2ChassisOfferingUUid() {
+        return this.bareMetal2ChassisOfferingUUid;
+    }
+
+}
