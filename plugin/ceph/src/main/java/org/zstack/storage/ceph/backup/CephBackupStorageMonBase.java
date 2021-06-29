48ef11f7b51fddf6ef990614140bb388ea73adc8
J M <630911437@qq.com>
2021-06-29 13:47:16 +0800

    [Feature: ZSTACK-40437] support ceph backup storage image export

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/backup/CephBackupStorageMonBase.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/backup/CephBackupStorageMonBase.java
@@ -54,6 +54,7 @@ public class CephBackupStorageMonBase extends CephMonBase {
 
     public static final String ECHO_PATH = "/ceph/backupstorage/echo";
     public static final String PING_PATH = "/ceph/backupstorage/ping";
+    public static final String EXPORT = "/ceph/export";
 
     public static class AgentCmd {
         public String monUuid;
