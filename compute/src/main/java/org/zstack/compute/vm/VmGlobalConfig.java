b5b6d292a8beb3b23ff02b837abba5ece4ac4f02
qiuyu.zhang <qiuyu.zhang@zstack.io>
2021-07-15 17:52:41 +0800

    [BugFix: ZSTACK-40399] add vendor_id

--- compute/src/main/java/org/zstack/compute/vm/VmGlobalConfig.java
+++ compute/src/main/java/org/zstack/compute/vm/VmGlobalConfig.java
@@ -1,6 +1,7 @@
 package org.zstack.compute.vm;
 
 import org.zstack.core.config.GlobalConfig;
+import org.zstack.core.config.GlobalConfigDef;
 import org.zstack.core.config.GlobalConfigDefinition;
 import org.zstack.core.config.GlobalConfigValidation;
 import org.zstack.header.cluster.ClusterVO;
@@ -71,4 +72,8 @@ public class VmGlobalConfig {
 
     @GlobalConfigValidation(numberGreaterThan = 1)
     public static GlobalConfig UNKNOWN_GC_INTERVAL = new GlobalConfig(CATEGORY, "set.unknown.gc.interval");
+
+    @GlobalConfigDef(defaultValue = "Microsoft Hv", type = String.class, description = "set vendor_id")
+    @BindResourceConfig(value = {VmInstanceVO.class, ClusterVO.class})
+    public static GlobalConfig VENDOR_ID = new GlobalConfig(CATEGORY, "vendorId");
 }
