ec3b5fa7bc0dd60998742744f179cbc7c0d602cd
jintao.chen <jintao.chen@zstack.io>
2021-08-03 11:16:25 +0800

    [BugFix: ZSTACK-41883] set repo when updatedependency

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
@@ -3965,6 +3965,7 @@ public class KVMHost extends HostBase implements Host {
 
                             UpdateDependencyCmd cmd = new UpdateDependencyCmd();
                             cmd.hostUuid = self.getUuid();
+                            cmd.zstackRepo = AnsibleGlobalProperty.ZSTACK_REPO;
 
                             if (info.isNewAdded()) {
                                 cmd.enableExpRepo = true;
