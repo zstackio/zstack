446129b677ce8e1c1f834450a1c03826db0dd41c
qiuyu <qiuyu.zhang@zstack.io>
2021-08-16 18:25:25 +0800

    [BugFix: ZSTACK-41766] Project maintenance scheduled task

--- sdk/src/main/java/org/zstack/sdk/CreateSchedulerJobAction.java
+++ sdk/src/main/java/org/zstack/sdk/CreateSchedulerJobAction.java
@@ -2,6 +2,7 @@ package org.zstack.sdk;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.zstack.sdk.*;
 
 public class CreateSchedulerJobAction extends AbstractAction {
 
@@ -33,7 +34,7 @@ public class CreateSchedulerJobAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String targetResourceUuid;
 
-    @Param(required = true, validValues = {"startVm","stopVm","rebootVm","volumeSnapshot","volumeBackup","rootVolumeBackup","vmBackup","databaseBackup","localRaidSelfTest","runAutoScalingGroup"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    @Param(required = true, validValues = {"startVm","stopVm","rebootVm","volumeSnapshot","volumeBackup","rootVolumeBackup","vmBackup","databaseBackup","localRaidSelfTest","runAutoScalingGroup","cancelIAM2ProjectLoginExpired","addIAM2ProjectLoginExpired"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String type;
 
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
