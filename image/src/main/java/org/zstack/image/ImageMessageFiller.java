ab825bb2096b9c2a1968ae4a870ad03e2c31249a
ws <1007947985@qqcom>
2021-08-18 10:31:24 +0800

    [BugFix:ZSATC-41148]
    fix bug 'add DataVolumeTemplate fail on vcenter'

--- image/src/main/java/org/zstack/image/ImageMessageFiller.java
+++ image/src/main/java/org/zstack/image/ImageMessageFiller.java
@@ -17,13 +17,12 @@ import org.zstack.header.volume.VolumeVO_;
  */
 public class ImageMessageFiller {
     public static void fillDefault(AddImageMessage msg) {
-        //TODO(weiw): in some cases, platform is needed by default, ZSTAC-41141
-        if (msg.getPlatform() == null) {
-            msg.setPlatform(ImagePlatform.Linux.toString());
-        }
-
         if (ImageConstant.ImageMediaType.DataVolumeTemplate.toString().equals(msg.getMediaType())) {
             return;
+        }
+
+        if (msg.getPlatform() == null) {
+            msg.setPlatform(ImagePlatform.Linux.toString());
         }
 
         if (msg.getPlatform().equals(ImagePlatform.Paravirtualization.toString())) {
