7c2c2005ef57079424af535c8dcfa17dd8f7a5d1
Ning,GuoHui <guohui.ning@zstack.io>
2021-08-18 18:07:34 +0800

    [Test: ZSTACK]
    Create root volume template from root volume long job delete after canceled case

--- test/src/test/groovy/org/zstack/test/integration/longjob/CreateRootVolumeTemplateLongJobCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/longjob/CreateRootVolumeTemplateLongJobCase.groovy
@@ -1,13 +1,17 @@
 package org.zstack.test.integration.longjob
 
 import com.google.gson.Gson
+import org.springframework.http.HttpEntity
 import org.zstack.core.Platform
+import org.zstack.core.agent.AgentConstant
 import org.zstack.core.db.Q
 import org.zstack.header.image.APICreateRootVolumeTemplateFromRootVolumeMsg
 import org.zstack.header.image.ImagePlatform
 import org.zstack.header.image.ImageVO
+import org.zstack.header.image.ImageVO_
 import org.zstack.header.longjob.LongJobVO
 import org.zstack.header.longjob.LongJobVO_
+import org.zstack.kvm.KVMAgentCommands
 import org.zstack.sdk.AccountInventory
 import org.zstack.sdk.AccountResourceRefInventory
 import org.zstack.sdk.BackupStorageInventory
@@ -21,6 +25,7 @@ import org.zstack.test.integration.ZStackTest
 import org.zstack.test.integration.storage.Env
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.SubCase
+import org.zstack.utils.gson.JSONObjectUtil
 
 /**
  * Created by camile on 2/6/18.
@@ -54,6 +59,63 @@ class CreateRootVolumeTemplateLongJobCase extends SubCase {
             testCreateRootVolumeTemplate()
             testCreateRootVolumeTemplateAppointResourceUuid()
             testCreateRootVolumeOnNormalAccountResource()
+            testDeleteAfterCanceled()
+        }
+    }
+
+    void testDeleteAfterCanceled(){
+        APICreateRootVolumeTemplateFromRootVolumeMsg msg = new APICreateRootVolumeTemplateFromRootVolumeMsg()
+        msg.name = "root-volume"
+        msg.rootVolumeUuid = vm.rootVolumeUuid
+        msg.platform = ImagePlatform.Linux.toString()
+        msg.backupStorageUuids = Collections.singletonList(bs.uuid)
+
+        boolean canceled = false
+        String hostUuid
+
+        env.afterSimulator(LocalStorageKvmBackend.CREATE_TEMPLATE_FROM_VOLUME){ rsp, HttpEntity<String> e ->
+            def cmd = JSONObjectUtil.toObject(e.body, KVMAgentCommands.AgentCommand.class)
+            hostUuid = e.getHeaders().getFirst(org.zstack.header.Constants.AGENT_HTTP_HEADER_RESOURCE_UUID)
+            while (!canceled){
+                sleep(500)
+            }
+
+            rsp.error = "job canceled"
+            rsp.success = false
+            return rsp
+        }
+
+        LongJobInventory jobInv
+        env.afterSimulator(AgentConstant.CANCEL_JOB){ rsp, HttpEntity<String> e ->
+            def cmd = JSONObjectUtil.toObject(e.body,KVMAgentCommands.CancelCmd.class)
+            def canceledHostUuid = e.getHeaders().getFirst(org.zstack.header.Constants.AGENT_HTTP_HEADER_RESOURCE_UUID)
+
+            assert cmd.cancellationApiId == jobInv.apiId
+            assert canceledHostUuid == hostUuid
+            canceled = true
+            return rsp
+        }
+
+        jobInv = submitLongJob {
+            jobName = msg.getClass().getSimpleName()
+            jobData = gson.toJson(msg)
+        } as LongJobInventory
+
+        while (hostUuid == null){
+            sleep(500)
+        }
+
+        assert jobInv.jobName == msg.getClass().getSimpleName()
+        assert jobInv.state.toString() == org.zstack.sdk.LongJobState.Running.toString()
+
+        cancelLongJob {
+            uuid = jobInv.uuid
+        }
+
+        retryInSecs {
+            LongJobVO job = dbFindByUuid(jobInv.uuid,LongJobVO.class)
+            assert job.state.toString() == org.zstack.sdk.LongJobState.Canceled.toString()
+            assert !Q.New(ImageVO.class).eq(ImageVO_.uuid, msg.resourceUuid).isExists()
         }
     }
 
