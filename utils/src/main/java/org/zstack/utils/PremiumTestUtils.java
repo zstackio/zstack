d2acd370d1dcb76225a659dba68c24f60e4696dc
gantao <tao.gan@zstack.io>
2021-09-02 09:37:00 +0800

    [BugFix: ZSTACK-41151] Simulator synchronous mirroring
    i# [BugFix: ZSTACK-XXX] 修复bug
    
    i# [BugFix: ZSTACK-XXX] 修复bug

---
+++ utils/src/main/java/org/zstack/utils/PremiumTestUtils.java
@@ -0,0 +1,29 @@
+package org.zstack.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PremiumTestUtils {
+    public String getSHA1(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.update(str.getBytes("UTF8"));
+        byte s[] = md.digest();
+        String result = "";
+        for (int i = 0; i < s.length; i++) {
+            result += Integer.toHexString((0x000000ff & s[i]) | 0xffffff00).substring(6);
+        }
+        return result;
+    }
+
+    public String getSHA256(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(str.getBytes("UTF8"));
+        byte s[] = md.digest();
+        String result = "";
+        for (int i = 0; i < s.length; i++) {
+            result += Integer.toHexString((0x000000ff & s[i]) | 0xffffff00).substring(6);
+        }
+        return result;
+    }
+}
