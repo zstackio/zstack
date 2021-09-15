7ff9a8e88e2f1d02da783b175db283b02e0e384d
tao.gan <tao.gan@zstack.io>
2021-09-15 19:06:45 +0800

    [BugFix: ZSTACK-4210] Upgrade to 4.3.0，deduplicate repeat Volume GC

---
+++ storage/src/main/java/org/zstack/storage/volume/DeleteVolumeGcExtension.java
@@ -0,0 +1,77 @@
+package org.zstack.storage.volume;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.transaction.annotation.Transactional;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.Q;
+import org.zstack.core.db.SQL;
+import org.zstack.core.gc.GCStatus;
+import org.zstack.core.gc.GarbageCollectorVO;
+import org.zstack.core.gc.GarbageCollectorVO_;
+import org.zstack.core.thread.ThreadFacade;
+import org.zstack.header.Component;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+import java.util.*;
+
+import static org.zstack.storage.volume.VolumeGlobalProperty.DELETE_VOLUME_GC;
+
+
+@Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
+public class DeleteVolumeGcExtension implements Component {
+    protected static final CLogger logger = Utils.getLogger(DeleteVolumeGcExtension.class);
+
+    @Autowired
+    private DatabaseFacade dbf;
+
+    @Autowired
+    private ThreadFacade thdf;
+
+    @Override
+    public boolean start() {
+        if (DELETE_VOLUME_GC){
+
+        }
+        deleteVolumeGC();
+        return true;
+    }
+
+    @Override
+    public boolean stop() {
+        return true;
+    }
+
+    private String getContextVolumeUuid(GarbageCollectorVO vo) {
+        String context = vo.getContext();
+        JsonParser jp = new JsonParser();
+        JsonObject jo = jp.parse(context).getAsJsonObject();
+        return jo.get("volume").getAsJsonObject().get("uuid").getAsString();
+    }
+
+    @Transactional
+    public boolean deleteVolumeGC() {
+        long count = Q.New(GarbageCollectorVO.class)
+                .eq(GarbageCollectorVO_.status, GCStatus.Idle)
+                .count();
+
+        HashSet<String> volumeUuids = new HashSet<>();
+        SQL.New("select vo from GarbageCollectorVO vo where vo.status = :status and vo.name like ('gc-ceph%' " +
+                        "or 'gc-shared-block%' or 'gc-nfs%' or 'gc-smp%' or 'gc-delete-volume%' or 'gc-mini-storage%' " +
+                        "or 'gc-aliyun-ebs%' or 'gc-aliyun-nas%')")
+                .param("status", GCStatus.Idle)
+                .limit(1000).paginate(count, (List<GarbageCollectorVO> vos) -> vos.forEach(vo -> {
+                    String volUuid = getContextVolumeUuid(vo);
+                    if (volumeUuids.contains(volUuid)) {
+                        dbf.getEntityManager().remove(vo);
+                    } else {
+                        volumeUuids.add(volUuid);
+                    }
+                }));
+        return true;
+    }
+}
---
+++ test/src/test/groovy/org/zstack/test/integration/storage/volume/VolumeGcCase.groovy
@@ -0,0 +1,156 @@
+package org.zstack.test.integration.storage.volume
+
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import org.zstack.core.db.DatabaseFacade
+import org.zstack.core.db.Q
+import org.zstack.core.db.SQL
+import org.zstack.core.gc.GCStatus
+import org.zstack.core.gc.GarbageCollectorVO
+import org.zstack.core.gc.GarbageCollectorVO_
+import org.zstack.header.exception.CloudRuntimeException
+import org.zstack.header.volume.VolumeDeletionPolicyManager
+import org.zstack.sdk.DiskOfferingInventory
+import org.zstack.sdk.PrimaryStorageInventory
+import org.zstack.sdk.VolumeInventory
+import org.zstack.storage.ceph.CephGlobalConfig
+import org.zstack.storage.ceph.primary.CephDeleteVolumeGC
+import org.zstack.storage.ceph.primary.CephPrimaryStorageBase
+import org.zstack.storage.volume.DeleteVolumeOnPrimaryStorageGC
+import org.zstack.storage.volume.VolumeGlobalConfig
+import org.zstack.test.integration.storage.CephEnv
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.DiskOfferingSpec
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.HttpError
+import org.zstack.testlib.PrimaryStorageSpec
+import org.zstack.testlib.SubCase
+import org.zstack.storage.volume.DeleteVolumeGcExtension
+import org.zstack.utils.BeanUtils
+
+import java.util.concurrent.TimeUnit
+
+class VolumeGcCase extends SubCase {
+    EnvSpec env
+    DatabaseFacade dbf
+    PrimaryStorageInventory ceph
+    DiskOfferingInventory diskOffering
+    boolean deleteFail = false
+    boolean called = false
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(StorageTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = CephEnv.CephStorageOneVmEnv()
+    }
+
+    @Override
+    void test() {
+        env.create {
+            dbf = bean(DatabaseFacade.class)
+
+            ceph = (env.specByName("ceph-pri") as PrimaryStorageSpec).inventory
+            diskOffering = (env.specByName("diskOffering") as DiskOfferingSpec).inventory
+
+            // set a very long time so the GC won't run, we use API to trigger it
+            CephGlobalConfig.GC_INTERVAL.updateValue(TimeUnit.DAYS.toSeconds(1))
+            VolumeGlobalConfig.VOLUME_DELETION_POLICY.updateValue(VolumeDeletionPolicyManager.VolumeDeletionPolicy.Direct.toString())
+
+            prepareEnv()
+            testDeleteVolumeGcExtension()
+        }
+    }
+
+    void prepareEnv() {
+        env.afterSimulator(CephPrimaryStorageBase.DELETE_PATH) { rsp ->
+            called = true
+
+            if (deleteFail) {
+                throw new HttpError(403, "on purpose")
+            }
+
+            return rsp
+        }
+    }
+
+    void testDeleteVolumeGcExtension() {
+        VolumeInventory vol1 = createDataVolume {
+            name = "data1"
+            diskOfferingUuid = diskOffering.uuid
+            primaryStorageUuid = ceph.uuid
+        }
+
+        VolumeInventory vol2 = createDataVolume {
+            name = "data2"
+            diskOfferingUuid = diskOffering.uuid
+            primaryStorageUuid = ceph.uuid
+        }
+
+        VolumeInventory vol3 = createDataVolume {
+            name = "data3"
+            diskOfferingUuid = diskOffering.uuid
+            primaryStorageUuid = ceph.uuid
+        }
+
+        deleteFail = true
+
+        deleteDataVolume {
+            uuid = vol1.uuid
+        }
+
+        deleteDataVolume {
+            uuid = vol2.uuid
+        }
+
+        deleteDataVolume {
+            uuid = vol3.uuid
+        }
+
+        List<GarbageCollectorVO> cephVo = Q.New(GarbageCollectorVO.class).list()
+        List<GarbageCollectorVO> vos = new ArrayList()
+        cephVo.each { it ->
+            for (int i = 100000; i < 100009; i++) {
+                GarbageCollectorVO vo = new GarbageCollectorVO()
+                vo.uuid = String.format(getContextVolumeUuid(it).substring(0, 26) + i)
+                vo.status = it.status
+                vo.context = it.context
+                vo.name = it.name
+                vo.managementNodeUuid = it.managementNodeUuid
+                vo.runnerClass = it.runnerClass
+                vo.lastOpDate = it.lastOpDate
+                vo.createDate = it.createDate
+                vo.type = it.type
+                vo.resourceName = it.resourceName
+                vo.resourceType = it.resourceType
+                vo.concreteResourceType = it.concreteResourceType
+                vos.add(vo)
+            }
+        }
+        dbf.persistCollection(vos)
+
+        def t1 = new Date()
+        new DeleteVolumeGcExtension().deleteVolumeGC()
+        def t2 = new Date()
+
+        assert Q.New(GarbageCollectorVO.class)
+                .eq(GarbageCollectorVO_.runnerClass, CephDeleteVolumeGC.getName())
+                .eq(GarbageCollectorVO_.status, GCStatus.Idle)
+                .count() == 3
+    }
+
+    String getContextVolumeUuid(GarbageCollectorVO vo) {
+        String context = vo.getContext()
+        JsonParser jp = new JsonParser();
+        JsonObject jo = jp.parse(context).getAsJsonObject();
+        return jo.get("volume").getAsJsonObject().get("uuid").getAsString()
+    }
+}
