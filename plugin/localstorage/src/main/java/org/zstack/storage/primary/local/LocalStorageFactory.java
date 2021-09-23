6bab39b61db3c644fec607f5a7eb362c6d41ba69
tao.gan <tao.gan@zstack.io>
2021-09-23 09:44:36 +0800

    将mevovo中flow删除

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageFactory.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageFactory.java
@@ -30,6 +30,8 @@ import org.zstack.header.host.*;
 import org.zstack.header.image.ImageConstant;
 import org.zstack.header.image.ImageInventory;
 import org.zstack.header.image.ImagePlatform;
+import org.zstack.header.message.AbstractBeforeDeliveryMessageInterceptor;
+import org.zstack.header.message.Message;
 import org.zstack.header.message.MessageReply;
 import org.zstack.header.query.AddExpandedQueryExtensionPoint;
 import org.zstack.header.query.ExpandedQueryAliasStruct;
@@ -310,6 +312,27 @@ public class LocalStorageFactory implements PrimaryStorageFactory, Component,
                 backupStorageMediatorMap.put(key, m);
             }
         }
+
+        bus.installBeforeDeliveryMessageInterceptor(new AbstractBeforeDeliveryMessageInterceptor() {
+            @Override
+            public void beforeDeliveryMessage(Message msg) {
+                if (msg instanceof ResizeVolumeOnHypervisorReply){
+                    VolumeInventory volume = ((ResizeVolumeOnHypervisorReply) msg).getVolume();
+                    PrimaryStorageVO primaryStorageVO = dbf.findByUuid(volume.getPrimaryStorageUuid(), PrimaryStorageVO.class);
+                    LocalStorageResourceRefVO localStorageResourceRefVO = dbf.findByUuid(volume.getUuid(), LocalStorageResourceRefVO.class);
+                    String hostUuid = localStorageResourceRefVO.getHostUuid();
+                    Long size = volume.getSize();
+
+                    final boolean isLocalPS = LocalStorageConstants.LOCAL_STORAGE_TYPE.equals(primaryStorageVO.getType());
+                    if (isLocalPS) {
+                        SQL.New(LocalStorageResourceRefVO.class)
+                                .condAnd(LocalStorageResourceRefVO_.resourceUuid, SimpleQuery.Op.EQ, volume.getUuid())
+                                .condAnd(LocalStorageResourceRefVO_.hostUuid, SimpleQuery.Op.EQ, hostUuid)
+                                .set(LocalStorageResourceRefVO_.size, size).update();
+                    }
+                }
+            }
+        }, ResizeVolumeOnHypervisorReply.class);
 
         return true;
     }
