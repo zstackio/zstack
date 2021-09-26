fc1f34baf75332d97bd2417ee738bcb5a1cac55e
tao.gan <tao.gan@zstack.io>
2021-09-26 10:23:29 +0800

    修改bus，拦截子类，不拦截父类

--- header/src/main/java/org/zstack/header/host/ResizeVolumeOnHypervisorReply.java
+++ header/src/main/java/org/zstack/header/host/ResizeVolumeOnHypervisorReply.java
@@ -1,13 +1,12 @@
 package org.zstack.header.host;
 
 import org.zstack.header.message.MessageReply;
-import org.zstack.header.message.NeedReplyMessage;
 import org.zstack.header.volume.VolumeInventory;
 
 /**
  * Created by kayo on 2018/4/2.
  */
-public class ResizeVolumeOnHypervisorReply extends NeedReplyMessage {
+public class ResizeVolumeOnHypervisorReply extends MessageReply {
     VolumeInventory volume;
 
     public VolumeInventory getVolume() {
