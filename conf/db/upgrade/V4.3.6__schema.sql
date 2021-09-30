fd7e21c4e75eae8f12f235d2534f61915201a686
haibiao.xiao <haibiao.xiao@zstack.io>
2021-09-30 11:43:03 +0800

    [BugFix: ZSTAC-42654] support different bonding type and add dpdk bonding

--- conf/db/upgrade/V4.3.6__schema.sql
+++ conf/db/upgrade/V4.3.6__schema.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `zstack`.`LicenseHistoryVO` ADD COLUMN `hash` char(32) DEFAULT 'unknown';
 DROP INDEX idxLicenseHistoryVOUploadDate ON LicenseHistoryVO;
 CREATE INDEX idxLicenseHistoryVOHash ON LicenseHistoryVO (hash);
+
+ALTER TABLE `zstack`.`HostNetworkBondingVO` ADD COLUMN `type` char(32) DEFAULT 'unknown';
