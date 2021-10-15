4ad73afeefd4edffc4330da2f8803b48987a0680
Qun Li <qun.il@zstack.io>
2021-10-15 09:52:05 +0800

    update sdk
    Signed-off-by: Qun Li <qun.il@zstack.io>

---
+++ sdk/src/main/java/org/zstack/sdk/UKeyStatus.java
@@ -0,0 +1,7 @@
+package org.zstack.sdk;
+
+public enum UKeyStatus {
+	Ready,
+	Missing,
+	Fault,
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -17486,6 +17486,33 @@ abstract class ApiHelper {
     }
 
 
+    def getLicenseUKeyStatus(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetLicenseUKeyStatusAction.class) Closure c) {
+        def a = new org.zstack.sdk.GetLicenseUKeyStatusAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getLoadBalancerListenerACLEntries(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetLoadBalancerListenerACLEntriesAction.class) Closure c) {
         def a = new org.zstack.sdk.GetLoadBalancerListenerACLEntriesAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
