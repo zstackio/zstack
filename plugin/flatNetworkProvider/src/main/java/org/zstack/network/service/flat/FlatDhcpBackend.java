34df8b17a9b6ef936c76b1acc36b30009f8705e6
xingxing.cui <xingxing.cui@zstack.io>
2021-10-28 12:46:40 +0800

    [BugFix: ZSTAC-43264]

--- plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/FlatDhcpBackend.java
+++ plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/FlatDhcpBackend.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.zstack.compute.vm.StaticIpOperator;
 import org.zstack.compute.vm.VmSystemTags;
+import org.zstack.core.Platform;
 import org.zstack.core.asyncbatch.While;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.cloudbus.CloudBusCallBack;
@@ -26,13 +27,14 @@ import org.zstack.header.core.workflow.NoRollbackFlow;
 import org.zstack.header.errorcode.ErrorCode;
 import org.zstack.header.errorcode.ErrorCodeList;
 import org.zstack.header.errorcode.OperationFailureException;
+import org.zstack.header.exception.CloudRuntimeException;
 import org.zstack.header.host.HostConstant;
 import org.zstack.header.host.HostErrors;
-import org.zstack.header.host.HostVO;
-import org.zstack.header.host.HostVO_;
+import org.zstack.header.host.HypervisorType;
 import org.zstack.header.message.APIMessage;
 import org.zstack.header.message.Message;
 import org.zstack.header.message.MessageReply;
+import org.zstack.header.network.l2.*;
 import org.zstack.header.network.l3.*;
 import org.zstack.header.network.service.*;
 import org.zstack.header.vm.*;
@@ -40,7 +42,10 @@ import org.zstack.header.vm.VmAbnormalLifeCycleStruct.VmAbnormalLifeCycleOperati
 import org.zstack.identity.AccountManager;
 import org.zstack.kvm.*;
 import org.zstack.kvm.KvmCommandSender.SteppingSendCallback;
-import org.zstack.network.service.*;
+import org.zstack.network.service.DhcpExtension;
+import org.zstack.network.service.NetworkProviderFinder;
+import org.zstack.network.service.NetworkServiceManager;
+import org.zstack.network.service.NetworkServiceProviderLookup;
 import org.zstack.network.service.flat.IpStatisticConstants.VmType;
 import org.zstack.network.service.vip.VipVO;
 import org.zstack.tag.SystemTagCreator;
@@ -63,8 +68,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static org.zstack.core.Platform.argerr;
-import static org.zstack.core.Platform.operr;
+import static org.zstack.core.Platform.*;
 import static org.zstack.network.service.flat.IpStatisticConstants.ResourceType;
 import static org.zstack.network.service.flat.IpStatisticConstants.SortBy;
 import static org.zstack.utils.CollectionDSL.*;
@@ -91,6 +95,8 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
     private DhcpExtension dhcpExtension;
     @Autowired
     private NetworkServiceManager nwServiceMgr;
+
+    private Map<String, L3NetworkGetIpStatisticExtensionPoint> getIpStatisticExts = new HashMap<>();
 
     public static final String APPLY_DHCP_PATH = "/flatnetworkprovider/dhcp/apply";
     public static final String BATCH_APPLY_DHCP_PATH = "/flatnetworkprovider/dhcp/batchApply";
@@ -350,6 +356,11 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
                     resourceTypes.add(ResourceType.VPC_VROUTER);
                 }
             }
+
+            L3NetworkGetIpStatisticExtensionPoint exp = getExtensionPointFactory(element.getVmInstanceType());
+            if (exp != null) {
+                element.setVmInstanceUuid(exp.getParentUuid(element.getVmInstanceUuid()));
+            }
         }
 
         return ipStatistics;
@@ -509,13 +520,11 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
             IpStatisticData element = new IpStatisticData();
             ipStatistics.add(element);
             element.setIp((String) result[0]);
-
             String uuid = (String) result[1];
             element.setVmInstanceUuid(uuid);
             if (StringUtils.isNotEmpty(uuid)) {
                 vmUuids.add(element.getVmInstanceUuid());
             }
-
             element.setVmInstanceName((String) result[2]);
 
             String type = (String) result[3];
@@ -536,6 +545,8 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
         Map<String, VmInstanceVO> vmvos = vms.stream()
                 .collect(Collectors.toMap(VmInstanceVO::getUuid, inv -> inv));
         Map<String, List<String>> vmToDefaultIpMap = new HashMap<>();
+        Map<String, Tuple> vrInfos = getApplianceVmInfo(vmUuids);
+
         for (IpStatisticData element : ipStatistics) {
             VmInstanceVO vmvo = vmvos.get(element.getVmInstanceUuid());
             if (vmvo == null) {
@@ -563,6 +574,18 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
             } else {
                 element.setVmDefaultIp(vmToDefaultIpMap.get(vmvo.getUuid()));
             }
+
+            if (element.getVmInstanceUuid() != null) {
+                Tuple vrInfo = vrInfos.get(element.getVmInstanceUuid());
+                if (vrInfo != null) {
+                    String vmInstanceType = vrInfo.get(1, String.class);
+                    L3NetworkGetIpStatisticExtensionPoint exp = getExtensionPointFactory(vmInstanceType);
+                    if (exp != null) {
+                        element.setVmInstanceUuid(exp.getParentUuid(element.getVmInstanceUuid()));
+                    }
+                }
+            }
+
         }
 
         return ipStatistics;
@@ -804,7 +827,14 @@ public class FlatDhcpBackend extends AbstractService implements NetworkServiceDh
 
     @Override
     public boolean start() {
+        for (L3NetworkGetIpStatisticExtensionPoint ext : pluginRgty.getExtensionList(L3NetworkGetIpStatisticExtensionPoint.class)) {
+            getIpStatisticExts.put(ext.getApplianceVmInstanceType(), ext);
+        }
         return true;
+    }
+
+    private L3NetworkGetIpStatisticExtensionPoint getExtensionPointFactory(String type) {
+        return getIpStatisticExts.get(type);
     }
 
     @Override
