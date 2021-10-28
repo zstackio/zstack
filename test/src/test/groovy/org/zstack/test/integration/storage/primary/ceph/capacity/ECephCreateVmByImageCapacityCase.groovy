c16b302326c3e2ef4688e4f0838d99e3f771058c
tao.gan <tao.gan@zstack.io>
2021-10-28 14:59:26 +0800

    i

--- test/src/test/groovy/org/zstack/test/integration/storage/primary/ceph/capacity/ECephCreateVmByImageCapacityCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/storage/primary/ceph/capacity/ECephCreateVmByImageCapacityCase.groovy
@@ -11,6 +11,7 @@ import org.zstack.test.integration.storage.StorageTest
 import org.zstack.testlib.CephPrimaryStorageSpec
 import org.zstack.testlib.EnvSpec
 import org.zstack.testlib.SubCase
+import org.zstack.testlib.Test
 import org.zstack.utils.data.SizeUnit
 import org.zstack.utils.gson.JSONObjectUtil
 
@@ -32,7 +33,103 @@ class ECephCreateVmByImageCapacityCase extends SubCase {
 
     @Override
     void environment() {
-        env = CephEnv.CephStorageOneVmEnv()
+        env = Test.makeEnv {
+            instanceOffering {
+                name = "instanceOffering"
+                memory = SizeUnit.GIGABYTE.toByte(1)
+                cpu = 1
+            }
+            diskOffering {
+                name = "diskOffering"
+                diskSize = SizeUnit.GIGABYTE.toByte(10)
+            }
+            zone{
+                name = "zone"
+                cluster {
+                    name = "test-cluster"
+                    hypervisorType = "KVM"
+
+                    kvm {
+                        name = "ceph-mon"
+                        managementIp = "localhost"
+                        username = "root"
+                        password = "password"
+                        usedMem = 1000
+                        totalCpu = 10
+                    }
+                    kvm {
+                        name = "host"
+                        username = "root"
+                        password = "password"
+                        usedMem = 1000
+                        totalCpu = 10
+                    }
+
+                    attachPrimaryStorage("ceph-pri")
+                    attachL2Network("l2")
+                }
+
+                l2NoVlanNetwork {
+                    name = "l2"
+                    physicalInterface = "eth0"
+
+                    l3Network {
+                        name = "l3"
+
+                        ip {
+                            startIp = "192.168.100.10"
+                            endIp = "192.168.100.100"
+                            netmask = "255.255.255.0"
+                            gateway = "192.168.100.1"
+                        }
+                    }
+                }
+
+                cephPrimaryStorage {
+                    name="ceph-pri"
+                    description="Test"
+                    totalCapacity = SizeUnit.GIGABYTE.toByte(100)
+                    availableCapacity= SizeUnit.GIGABYTE.toByte(100)
+                    url="ceph://pri"
+                    fsid="7ff218d9-f525-435f-8a40-3618d1772a64"
+                    monUrls=["root:password@localhost/?monPort=7777"]
+                }
+
+
+                attachBackupStorage("ceph-bk")
+            }
+
+            cephBackupStorage {
+                name="ceph-bk"
+                description="Test"
+                totalCapacity = SizeUnit.GIGABYTE.toByte(100)
+                availableCapacity= SizeUnit.GIGABYTE.toByte(100)
+                url = "/bk"
+                fsid ="7ff218d9-f525-435f-8a40-3618d1772a64"
+                monUrls = ["root:password@localhost/?monPort=7777"]
+
+                image {
+                    name = "test-iso"
+                    mediaType = ImageConstant.ImageMediaType.ISO.toString()
+                    url  = "http://zstack.org/download/test.iso"
+                }
+                image {
+                    name = "image"
+                    url  = "http://zstack.org/download/image.qcow2"
+                }
+            }
+
+            vm {
+                name = "test-vm"
+                useCluster("test-cluster")
+                useHost("host")
+                useL3Networks("l3")
+                useInstanceOffering("instanceOffering")
+                useRootDiskOffering("diskOffering")
+                useImage("image")
+
+            }
+        }
     }
 
     @Override
@@ -53,7 +150,6 @@ class ECephCreateVmByImageCapacityCase extends SubCase {
         def download_image_path_invoked = false
         def image_virtual_size = SizeUnit.GIGABYTE.toByte(10)//10G
         def image_physical_size = SizeUnit.GIGABYTE.toByte(1)//1G
-
         env.simulator(CephBackupStorageBase.DOWNLOAD_IMAGE_PATH) {
             def rsp = new CephBackupStorageBase.DownloadRsp()
             rsp.size = image_virtual_size
@@ -61,20 +157,17 @@ class ECephCreateVmByImageCapacityCase extends SubCase {
             download_image_path_invoked = true
             return rsp
         }
-
         ImageInventory sizedImage = addImage {
             name = "sized-image"
             url = "http://my-site/foo.qcow2"
             backupStorageUuids = [bs.uuid]
             format = ImageConstant.QCOW2_FORMAT_STRING
         }
-
         assert download_image_path_invoked
 
         GetPrimaryStorageCapacityResult beforeCapacityResult = getPrimaryStorageCapacity {
             primaryStorageUuids = [ps.uuid]
         }
-
         VmInstanceInventory vm = createVmInstance {
             name = "crt-vm"
             instanceOfferingUuid = instanceOffering.uuid
@@ -82,15 +175,19 @@ class ECephCreateVmByImageCapacityCase extends SubCase {
             l3NetworkUuids = [l3.uuid]
             rootDiskOfferingUuid = diskOffering.uuid
         } as VmInstanceInventory
+        GetPrimaryStorageCapacityResult capacityResult = getPrimaryStorageCapacity {
+            primaryStorageUuids = [ps.uuid]
+        }
+        assert beforeCapacityResult.availableCapacity == capacityResult.availableCapacity + image_virtual_size + image_physical_size
 
         def volume1 = createDataVolume {
             name = "dataVolume1"
-            diskOfferingUuid = diskOfferingInventory.uuid
+            diskOfferingUuid = diskOffering.uuid
             primaryStorageUuid = ps.uuid
         } as VolumeInventory
         def volume2 = createDataVolume {
             name = "dataVolume2"
-            diskOfferingUuid = diskOfferingInventory.uuid
+            diskOfferingUuid = diskOffering.uuid
             primaryStorageUuid = ps.uuid
         } as VolumeInventory
         attachDataVolumeToVm {
@@ -102,10 +199,12 @@ class ECephCreateVmByImageCapacityCase extends SubCase {
             vmInstanceUuid = vm.uuid
         }
 
-        GetPrimaryStorageCapacityResult capacityResult = getPrimaryStorageCapacity {
-            primaryStorageUuids = [ps.uuid]
+        cloneVmInstance {
+            names = ["test"]
+            vmInstanceUuid = vm.uuid
+            full = true
         }
-        assert beforeCapacityResult.availableCapacity == capacityResult.availableCapacity + image_virtual_size + image_physical_size
+        assert 1 == 1
 
         env.simulator(CephPrimaryStorageBase.INIT_PATH) { HttpEntity<String> e, EnvSpec spec ->
             def cmd = JSONObjectUtil.toObject(e.body, CephPrimaryStorageBase.InitCmd.class)
