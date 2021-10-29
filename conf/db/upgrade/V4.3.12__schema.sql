a10ae20733628339abc2f76f5e2a98a432b3bbfe
Zhang, Wenhao <wenhao.zhang@zstack.io>
2021-10-29 18:41:21 +0800

    <fix>[crypto]: add cryptography security compliance module
    Add Cryptography Security UKey Authentication in
    Cryptography Security Compliance Module.
    
    Resolves: ZSTAC-43163
    
    Change-Id: I777578786f797675716179727378797267706674

--- conf/db/upgrade/V4.3.12__schema.sql
+++ conf/db/upgrade/V4.3.12__schema.sql
@@ -84,3 +84,29 @@ ALTER TABLE `BareMetal2ChassisVO` ADD COLUMN `provisionType` varchar(32) NOT NUL
 ALTER TABLE `BareMetal2InstanceVO` ADD COLUMN `provisionType` varchar(32) NOT NULL DEFAULT 'Remote';
 ALTER TABLE `BareMetal2ChassisOfferingVO` ADD COLUMN `provisionType` varchar(32) NOT NULL DEFAULT 'Remote';
 ALTER TABLE `BareMetal2ChassisDiskVO` ADD COLUMN `wwn` varchar(128) DEFAULT NULL;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`CCSCertificateVO` (
+    `uuid`        varchar(32) NOT NULL UNIQUE,
+    `algorithm`   varchar(10) NOT NULL DEFAULT 'SM2',
+    `format`      char(3) NOT NULL DEFAULT 'CER',
+    `issuerDN`    varchar(64) NOT NULL,
+    `subjectDN`   varchar(64) NOT NULL,
+    `serNumber`   bigint unsigned NOT NULL,
+    `effectiveTime`  bigint unsigned NOT NULL DEFAULT 0,
+    `expirationTime` bigint unsigned NOT NULL DEFAULT 0,
+    `createDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `lastOpDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`uuid`),
+    UNIQUE KEY `subjectDNAndSerNumber` (`subjectDN`, `serNumber`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`CCSCertificateUserRefVO` (
+    `id`          bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `userUuid`    char(32) NOT NULL,
+    `certificateUuid` char(32) NOT NULL,
+    `state`       varchar(10) NOT NULL DEFAULT 'Disabled',
+    `createDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `lastOpDate`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fkCCSCertificateUserRefVOCertificateUuid` FOREIGN KEY (`certificateUuid`) REFERENCES `zstack`.`CCSCertificateVO` (`uuid`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
