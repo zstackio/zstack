f1a392dd94df36f2203b2777a4d9a76b516f7c51
hanyu.liang <hanyu.liang@zstack.io>
2021-11-04 15:44:46 +0800

    [Feature: ZSTACK-43164]

--- conf/db/upgrade/V4.3.12__schema.sql
+++ conf/db/upgrade/V4.3.12__schema.sql
@@ -110,3 +110,59 @@ CREATE TABLE IF NOT EXISTS `zstack`.`CCSCertificateUserRefVO` (
     PRIMARY KEY (`id`),
     CONSTRAINT `fkCCSCertificateUserRefVOCertificateUuid` FOREIGN KEY (`certificateUuid`) REFERENCES `zstack`.`CCSCertificateVO` (`uuid`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`SecretResourcePoolVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `zoneUuid` varchar(32) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `type` varchar(255) NOT NULL,
+    `state` varchar(32) NOT NULL,
+    `model` varchar(32) NOT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY  (`uuid`),
+    INDEX `idxSecretResourcePoolVOUuid` (`uuid`),
+    CONSTRAINT fkSecretResourcePoolVOZoneEO FOREIGN KEY (zoneUuid) REFERENCES ZoneEO (uuid) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`SecurityMachineVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `zoneUuid` varchar(32) NOT NULL,
+    `secretResourcePoolUuid` varchar(32) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `state` varchar(32) NOT NULL,
+    `status` varchar(32) NOT NULL,
+    `model` varchar(32) NOT NULL,
+    `type` varchar(32) NOT NULL,
+    `managementIp` varchar(255) NOT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY  (`uuid`),
+    INDEX `idxSecurityMachineVOUuid` (`uuid`),
+    INDEX `idxSecurityMachineVOSecretResourcePoolUuid` (`secretResourcePoolUuid`),
+    CONSTRAINT fkSecurityMachineVOSecretResourcePoolVO FOREIGN KEY (secretResourcePoolUuid) REFERENCES SecretResourcePoolVO (uuid) ON DELETE RESTRICT,
+    CONSTRAINT fkSecurityMachineVOZoneEO FOREIGN KEY (zoneUuid) REFERENCES ZoneEO (uuid) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`InfoSecSecretResourcePoolVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `connectionMode` int unsigned NOT NULL,
+    `autoCheck` boolean NOT NULL DEFAULT TRUE,
+    `connectTimeOut` bigint unsigned NOT NULL,
+    `interval` bigint unsigned NOT NULL,
+    `activatedToken` varchar(32) NOT NULL,
+    `protectToken` varchar(32) NOT NULL,
+    `hmacToken` varchar(32) NOT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT fkInfoSecSecretResourcePoolVOSecretResourcePoolVO FOREIGN KEY (uuid) REFERENCES SecretResourcePoolVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`InfoSecSecurityMachineVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `port` int unsigned NOT NULL,
+    `password` varchar(255) NOT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT fkInfoSecSecurityMachineVOSecurityMachineVO FOREIGN KEY (uuid) REFERENCES SecurityMachineVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
