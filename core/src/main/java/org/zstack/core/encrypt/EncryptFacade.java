386b2046c0aae98bde830d02082929aede48b41a
hanyu.liang <hanyu.liang@zstack.io>
2021-11-04 15:44:46 +0800

    <feature>[core]: modify encrypt driver with crypto secret machine
    1. add infoSec crypto ext-libs;
    2. move crypto sql to V4.3.18;
    3. modify encrypt driver with crypto secret machine;
    
    Resolves: ZSTAC-43163
    Resolves: ZSTAC-43164
    
    Change-Id: I686a776267706769717a76666163636472777579

--- core/src/main/java/org/zstack/core/encrypt/EncryptFacade.java
+++ core/src/main/java/org/zstack/core/encrypt/EncryptFacade.java
@@ -1,5 +1,7 @@
 package org.zstack.core.encrypt;
 
+import java.security.cert.X509Certificate;
+
 /**
  * Created by kayo on 2018/9/7.
  */
@@ -7,4 +9,21 @@ public interface EncryptFacade {
     String encrypt(String decryptString);
 
     String decrypt(String encryptString);
+    /**
+     * Decrypt cipher text to plain text, with certificate info.
+     *
+     * @return Not null.
+     * If decrypt process raise an Exception, describe it in {@link EncryptFacadeResult#error}.
+     */
+    EncryptFacadeResult<String> attachedVerify(String cipherText);
+
+    /**
+     * Parse the CCS certificate cipher text and return the CCS certificate information.
+     * @return CCS certificate information, not null
+     */
+    EncryptFacadeResult<X509Certificate> parseCertificate(String certificateText);
+
+    EncryptFacadeResult<String> encrypt(String data, EncryptType algType);
+
+    EncryptFacadeResult<String> decrypt(String data, EncryptType algType);
 }
