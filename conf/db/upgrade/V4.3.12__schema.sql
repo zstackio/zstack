5eaff0c1cff9f0f3c5ea83b4f7c76db6b9ef3b74
shixin.ruan <shixin.ruan@zstack.io>
2021-11-30 11:52:57 +0800

    <fix>[conf]: change foreign key of SlbVmInstanceVO
    DBImpact
    
    Resolves: ZSTAC-43597
    
    Change-Id: I6478796c676d7677617869756f6172706b7a6673

--- conf/db/upgrade/V4.3.12__schema.sql
+++ conf/db/upgrade/V4.3.12__schema.sql
@@ -1,1 +1,7 @@
 ALTER TABLE `zstack`.`ConsoleProxyVO` ADD COLUMN `version` varchar(32) DEFAULT NULL;
+
+ALTER TABLE `zstack`.`SlbVmInstanceVO` DROP FOREIGN KEY `fkSlbVmInstanceVOSlbGroupVO`;
+ALTER TABLE `zstack`.`SlbVmInstanceVO` DROP KEY `fkSlbVmInstanceVOSlbGroupVO`;
+ALTER TABLE `zstack`.`SlbVmInstanceVO` MODIFY COLUMN `slbGroupUuid` varchar(32) DEFAULT NULL;
+ALTER TABLE `zstack`.`SlbVmInstanceVO` ADD CONSTRAINT `fkSlbVmInstanceVOVmInstanceEO` FOREIGN KEY (`uuid`) REFERENCES `VmInstanceEO` (`uuid`) ON DELETE CASCADE;
+ALTER TABLE `zstack`.`SlbVmInstanceVO` ADD CONSTRAINT `fkSlbVmInstanceVOSlbGroupVO` FOREIGN KEY (`slbGroupUuid`) REFERENCES `SlbGroupVO` (`uuid`) ON DELETE SET NULL;
