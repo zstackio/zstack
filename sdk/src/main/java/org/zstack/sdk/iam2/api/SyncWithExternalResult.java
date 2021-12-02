9cae9d4f78b75a074a47ed610deaddf0f625c15c
lei.liu@zstack.io <lei.liu@zstack.io>
2021-12-02 19:24:59 +0800

    <feature>[iam2]: Support sync with RMS
    Change-Id: I756f6c756c7977626e65617a7766736b6c697566

---
+++ sdk/src/main/java/org/zstack/sdk/iam2/api/SyncWithExternalResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk.iam2.api;
+
+
+
+public class SyncWithExternalResult {
+    public java.lang.String response;
+    public void setResponse(java.lang.String response) {
+        this.response = response;
+    }
+    public java.lang.String getResponse() {
+        return this.response;
+    }
+
+}
