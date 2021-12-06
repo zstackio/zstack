72a4cef5145dbcb82d61d6dba44aef3e9d7b949d
mingmin.wen <mingmin.wen@zstack.io>
2021-12-06 16:02:21 +0800

    <feature>[sdk]: add vnuma productization sdk
    1.add vnuma productization sdk.
    
    Resolves: ZSTAC-42888
    
    Change-Id: I756374676c666b6b6c6d686e7777646a64676b76

---
+++ sdk/src/main/java/org/zstack/sdk/GetVmEmulatorPinningResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetVmEmulatorPinningResult {
+    public java.lang.String emulatorPinning;
+    public void setEmulatorPinning(java.lang.String emulatorPinning) {
+        this.emulatorPinning = emulatorPinning;
+    }
+    public java.lang.String getEmulatorPinning() {
+        return this.emulatorPinning;
+    }
+
+}
