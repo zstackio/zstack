94695ecd936cfd796ab3327a234ba859bc2267a9
mingmin.wen <mingmin.wen@zstack.io>
2021-12-06 16:02:21 +0800

    <feature>[sdk]: add vnuma productization sdk
    1.add vnuma productization sdk.
    
    Resolves: ZSTAC-42888
    
    Change-Id: I756374676c666b6b6c6d686e7777646a64676b76

---
+++ sdk/src/main/java/org/zstack/sdk/GetVmvNUMATopologyResult.java
@@ -0,0 +1,38 @@
+package org.zstack.sdk;
+
+
+
+public class GetVmvNUMATopologyResult {
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String hostUuid;
+    public void setHostUuid(java.lang.String hostUuid) {
+        this.hostUuid = hostUuid;
+    }
+    public java.lang.String getHostUuid() {
+        return this.hostUuid;
+    }
+
+    public java.util.List topology;
+    public void setTopology(java.util.List topology) {
+        this.topology = topology;
+    }
+    public java.util.List getTopology() {
+        return this.topology;
+    }
+
+}
