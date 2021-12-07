a0f97fe870c492c070346b0c9d84bb975969bbe9
hanyu.liang <hanyu.liang@zstack.io>
2021-12-07 15:46:33 +0800

    <fix>[crypto]: modify the location of the identity authentication interface implementation
    delete the original identity authentication interface and move the
    implementation to the place where it is used
    delete the two interfaces and implementations of authentication in
    EncryptFacade
    
    Resolves: ZSTAC-43675
    Change-Id:cd9398e9e024039abe3bb61f4b8f54ec32d05ed8

--- core/src/main/java/org/zstack/core/encrypt/EncryptDriver.java
+++ core/src/main/java/org/zstack/core/encrypt/EncryptDriver.java
@@ -1,26 +1,11 @@
 package org.zstack.core.encrypt;
 
-import java.security.cert.X509Certificate;
-
 public interface EncryptDriver {
     EncryptDriverType getDriverType();
 
     String encrypt(String data);
 
     String decrypt(String data);
-    /**
-     * Decrypt cipher text to plain text, with certificate info.
-     *
-     * @return Not null.
-     * If decrypt process raise an Exception, describe it in {@link EncryptFacadeResult#error}.
-     */
-    EncryptFacadeResult<String> attachedVerify(String cipherText);
-
-    /**
-     * Parse the CCS certificate cipher text and return the CCS certificate information.
-     * @return CCS certificate information, not null
-     */
-    EncryptFacadeResult<X509Certificate> parseCertificate(String certificateText);
 
     EncryptFacadeResult<String> encrypt(String data, String algType);
 
