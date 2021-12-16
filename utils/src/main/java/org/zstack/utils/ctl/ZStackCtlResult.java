411af576bfc810ad0832722aae781ddc274e448e
qiuyu.zhang <qiuyu.zhang@zstack.io>
2021-12-16 18:29:56 +0800

    <fix>[crypto]: encryption AuditsVO reduced synchronization Encrypting 1000 data at a time is too large to reduce the concurrency
    Resolves/Related: ZSTAC-44221

---
+++ utils/src/main/java/org/zstack/utils/ctl/ZStackCtlResult.java
@@ -0,0 +1,22 @@
+package org.zstack.utils.ctl;
+
+public class ZStackCtlResult {
+    private boolean success = true;
+    private String error;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
