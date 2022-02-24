e1c2f35e6a7305c7033d751ecfb2a5e33ba2b99e
Qun Li <qun.il@zstack.io>
2022-02-24 14:28:18 +0800

    re-generate API helpers
    Signed-off-by: Qun Li <qun.il@zstack.io>

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -9062,6 +9062,33 @@ abstract class ApiHelper {
     }
 
 
+    def createVmFromCdpBackup(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.CreateVmFromCdpBackupAction.class) Closure c) {
+        def a = new org.zstack.sdk.CreateVmFromCdpBackupAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def createVmInstance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.CreateVmInstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.CreateVmInstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
