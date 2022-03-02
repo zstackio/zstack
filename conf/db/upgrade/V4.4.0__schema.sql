745a2542bddd102b3c4ceec77b2e686f8b06c832
hanyu.liang <hanyu.liang@zstack.io>
2022-03-02 10:40:14 +0800

    <feature>[iam2]: add project association table
    create IAM2ProjectResourceRefVO table
    
    Resolves:ZSTACK-45277
    
    Change-Id: I7466796123416345717378706566756168727463
    
    Conflicts:
    	conf/db/upgrade/V4.4.0__schema.sql

--- conf/db/upgrade/V4.4.0__schema.sql
+++ conf/db/upgrade/V4.4.0__schema.sql
@@ -125,4 +125,13 @@ ALTER TABLE `zstack`.`VmInstanceNumaNodeVO` ADD CONSTRAINT `VmInstanceNumaNodeVO
 ALTER TABLE `zstack`.`HostAllocatedCpuVO` DROP FOREIGN KEY `HostAllocatedCpuVO_HostEO_uuid_fk`;
 ALTER TABLE `zstack`.`HostAllocatedCpuVO` ADD CONSTRAINT `HostAllocatedCpuVO_HostEO_uuid_fk` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE ;
 
+CREATE TABLE  `zstack`.`IAM2ProjectResourceRefVO` (
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `resourceUuid` varchar(32) NOT NULL,
+    `resourceType` varchar(128) NOT NULL,
+    `projectUuid` varchar(32) NOT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
