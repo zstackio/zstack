305b0f615c5d4a5b35faab21f56e37c5a75b9251
Qi Le <qi.le@zstack.io>
2022-03-03 18:25:07 +0800

    <feature>[image]: parse ovf file sdk
    parse ovf
    
    Resolves: ZSTAC-45171
    
    Change-Id: I6b706563717371716a74657567766d72686c7378

---
+++ sdk/src/main/java/org/zstack/sdk/OvfNetworkInfo.java
@@ -0,0 +1,15 @@
+package org.zstack.sdk;
+
+
+
+public class OvfNetworkInfo  {
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -21509,6 +21509,33 @@ abstract class ApiHelper {
     }
 
 
+    def parseOvf(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.ParseOvfAction.class) Closure c) {
+        def a = new org.zstack.sdk.ParseOvfAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def pauseVmInstance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.PauseVmInstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.PauseVmInstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
