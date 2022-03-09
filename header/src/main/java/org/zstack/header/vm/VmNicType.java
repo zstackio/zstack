72dc5007ce9bec940f858e20eb2223ca2cad2b45
xingxing.cui <xingxing.cui@zstack.io>
2022-03-09 14:16:02 +0800

    <fix> [compute] vmNicType function overloading
    Change-Id: I6e6b63216a99582262566a34746679656c706461

--- header/src/main/java/org/zstack/header/vm/VmNicType.java
+++ header/src/main/java/org/zstack/header/vm/VmNicType.java
@@ -1,16 +1,35 @@
 package org.zstack.header.vm;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import org.zstack.header.network.l2.VSwitchType;
+
+import java.util.*;
 
 public class VmNicType {
     private static Map<String, VmNicType> types = Collections.synchronizedMap(new HashMap<>());
+    private static Map<String, String> vSwitchAndSriovTypes = Collections.synchronizedMap(new HashMap<>());
+
     private final String typeName;
 
     public VmNicType(String typeName) {
         this.typeName = typeName;
         types.put(typeName, this);
+    }
+
+    public VmNicType(String typeName, String vSwitchType) {
+        this.typeName = typeName;
+        types.put(typeName, this);
+        vSwitchAndSriovTypes.put(vSwitchType, typeName);
+        vSwitchAndSriovTypes.put(vSwitchType + "sriov", typeName);
+    }
+
+    public VmNicType(String typeName, String vSwitchType, Boolean enableSriov) {
+        this.typeName = typeName;
+        types.put(typeName, this);
+        vSwitchAndSriovTypes.put(vSwitchType, typeName);
+        if (enableSriov) {
+            vSwitchType += "sriov";
+            vSwitchAndSriovTypes.put(vSwitchType, typeName);
+        }
     }
 
     public static VmNicType valueOf(String typeName) {
@@ -19,6 +38,19 @@ public class VmNicType {
             throw new IllegalArgumentException("VmNicType type: " + typeName + " was not registered by any VmNicFactory");
         }
         return type;
+    }
+
+    public static VmNicType valueOf(VSwitchType vSwitchType) {
+        String typeName = vSwitchAndSriovTypes.get(vSwitchType.toString());
+        return valueOf(typeName);
+    }
+
+    public static VmNicType valueOf(VSwitchType vSwitchType, Boolean enableSriov) {
+        String typeName = vSwitchAndSriovTypes.get(vSwitchType.toString());
+        if (enableSriov) {
+            typeName = vSwitchAndSriovTypes.get(vSwitchType + "sriov");
+        }
+        return valueOf(typeName);
     }
 
     @Override
