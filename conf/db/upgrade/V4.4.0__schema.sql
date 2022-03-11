535f8db2f005941e4c427b3f12b560b9b72866d3
boce.wang <boce.wang@zstack.io>
2022-03-11 16:20:15 +0800

    <feature>[vpc]: ipsec sql update for strongswan upgrade

--- conf/db/upgrade/V4.4.0__schema.sql
+++ conf/db/upgrade/V4.4.0__schema.sql
@@ -115,3 +115,13 @@ DELIMITER ;
 
 call deleteVmLevelCpuModeIfAlreadySetClusterLevelCpuMode();
 DROP PROCEDURE IF EXISTS deleteVmLevelCpuModeIfAlreadySetClusterLevelCpuMode;
+
+
+
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeVersion` varchar(16) NOT NULL DEFAULT 'ikev1';
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `idType` varchar(16) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `remoteId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `localId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeLifeTime` int(10) DEFAULT 0;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `lifeTime` int(10) DEFAULT 0;
+
