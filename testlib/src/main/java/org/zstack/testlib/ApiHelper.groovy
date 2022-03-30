29fc4ec553b6da8a66c53c9271588f7ea702ef91
hanyu.liang <hanyu.liang@zstack.io>
2022-03-30 16:22:25 +0800

    <improvement>[crypto]: add sdk
    add APISecurityMachineDetectSyncMsg sdk
    
    Resolves: ZSTAC-45072
    
    Change-Id: I726e45072f797663686e6d786a6f66746f65646b

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -30597,6 +30597,33 @@ abstract class ApiHelper {
     }
 
 
+    def securityMachineDetectSync(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SecurityMachineDetectSyncAction.class) Closure c) {
+        def a = new org.zstack.sdk.SecurityMachineDetectSyncAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def securityMachineEncrypt(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SecurityMachineEncryptAction.class) Closure c) {
         def a = new org.zstack.sdk.SecurityMachineEncryptAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
