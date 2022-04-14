715120e4e28dff9048f49e7fb011f5855d236ea7
Qun Li <qun.li@zstack.io>
2022-04-14 15:19:28 +0800

    re-generate API helpers
    Signed-off-by: Qun Li <qun.li@zstack.io>

---
+++ sdk/src/main/java/org/zstack/sdk/DescribeVmInstanceRecoveryPointResult.java
@@ -0,0 +1,30 @@
+package org.zstack.sdk;
+
+
+
+public class DescribeVmInstanceRecoveryPointResult {
+    public java.util.Map realSizes;
+    public void setRealSizes(java.util.Map realSizes) {
+        this.realSizes = realSizes;
+    }
+    public java.util.Map getRealSizes() {
+        return this.realSizes;
+    }
+
+    public java.util.Map virtualSizes;
+    public void setVirtualSizes(java.util.Map virtualSizes) {
+        this.virtualSizes = virtualSizes;
+    }
+    public java.util.Map getVirtualSizes() {
+        return this.virtualSizes;
+    }
+
+    public java.lang.String timestamp;
+    public void setTimestamp(java.lang.String timestamp) {
+        this.timestamp = timestamp;
+    }
+    public java.lang.String getTimestamp() {
+        return this.timestamp;
+    }
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -14273,6 +14273,33 @@ abstract class ApiHelper {
     }
 
 
+    def describeVmInstanceRecoveryPoint(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DescribeVmInstanceRecoveryPointAction.class) Closure c) {
+        def a = new org.zstack.sdk.DescribeVmInstanceRecoveryPointAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def destroyBaremetalInstance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DestroyBaremetalInstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.DestroyBaremetalInstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
