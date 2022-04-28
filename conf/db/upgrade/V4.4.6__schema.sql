894c26eb938060bd36497952d7b218e8fac38ea4
tao.gan <tao.gan@zstack.io>
2022-04-28 18:35:05 +0800

    <fix>[conf]: <description
    DBImpact
    
    Resolves: ZSTAC-44777
    
    Change-Id: I63796d6e6a636d6869647a686271796672657378

--- conf/db/upgrade/V4.4.6__schema.sql
+++ conf/db/upgrade/V4.4.6__schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `zstack`.`SharedBlockCapacityVO` (
-    `uuid` varchar(32) NOT NULL UNIQUE COMMENT 'shared block uuid',
-    `totalCapacity` bigint unsigned NOT NULL COMMENT 'total capacity of lun in bytes',
-    `availableCapacity` bigint unsigned NOT NULL COMMENT 'available capacity of lun in bytes',
-    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
-    `createDate` timestamp,
-    PRIMARY KEY  (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--CREATE TABLE IF NOT EXISTS `zstack`.`SharedBlockCapacityVO` (
+--    `uuid` varchar(32) NOT NULL UNIQUE COMMENT 'shared block uuid',
+--    `totalCapacity` bigint unsigned NOT NULL COMMENT 'total capacity of lun in bytes',
+--    `availableCapacity` bigint unsigned NOT NULL COMMENT 'available capacity of lun in bytes',
+--    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+--    `createDate` timestamp,
+--    PRIMARY KEY  (`uuid`)
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8;
