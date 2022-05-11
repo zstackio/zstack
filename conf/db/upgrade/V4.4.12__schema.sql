f628db674a10ecd94ba1adfc9bea549b522dd331
xingxing.cui <xingxing.cui@zstack.io>
2022-05-11 17:44:26 +0800

    <feature>[vpc] vpc support selecting storage path
    support selecting storage path when create vpc
    support designate host, ceph pool and provision strategy
    support getPrimaryStorageCandidatesForVmMigrationMsg for vpc
    
    Resolved: ZSTAC-46656
    
    Change-Id: I6e6b61116a99511262566a34746229656c111412

--- conf/db/upgrade/V4.4.12__schema.sql
+++ conf/db/upgrade/V4.4.12__schema.sql
@@ -22,3 +22,6 @@ DELIMITER ;
 
 CALL Alter_SCSI_Table();
 DROP PROCEDURE Alter_SCSI_Table;
+
+UPDATE `zstack`.`VmInstanceVO` t1, `zstack`.`HostVO` t2 set t1.`architecture` = t2.`architecture` where t1.`type` = 'ApplianceVm' and t1.`hostUuid` = t2.`uuid`;
+UPDATE `zstack`.`VmInstanceVO` t1, `zstack`.`HostVO` t2 set t1.`architecture` = t2.`architecture` where t1.`type` = 'ApplianceVm' and t1.`architecture` IS NULL and t1.`lastHostUuid` = t2.`uuid`;
