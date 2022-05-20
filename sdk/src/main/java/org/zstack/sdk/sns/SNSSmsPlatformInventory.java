8746281010e9ae2b4ae80dcad82c3e3fef672d98
Wang,Qilin <qilin.wang@zstack.io>
2022-05-20 18:07:03 +0800

    [Feature:ZSTAC-45195] Dizhenju sms

---
+++ sdk/src/main/java/org/zstack/sdk/sns/SNSSmsPlatformInventory.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk.sns;
+
+
+
+public class SNSSmsPlatformInventory extends org.zstack.sdk.sns.SNSApplicationPlatformInventory {
+
+    public java.lang.String serverIp;
+    public void setServerIp(java.lang.String serverIp) {
+        this.serverIp = serverIp;
+    }
+    public java.lang.String getServerIp() {
+        return this.serverIp;
+    }
+
+    public java.lang.String srcID;
+    public void setSrcID(java.lang.String srcID) {
+        this.srcID = srcID;
+    }
+    public java.lang.String getSrcID() {
+        return this.srcID;
+    }
+
+    public java.lang.String downlinkChannelNumber;
+    public void setDownlinkChannelNumber(java.lang.String downlinkChannelNumber) {
+        this.downlinkChannelNumber = downlinkChannelNumber;
+    }
+    public java.lang.String getDownlinkChannelNumber() {
+        return this.downlinkChannelNumber;
+    }
+
+    public java.lang.String username;
+    public void setUsername(java.lang.String username) {
+        this.username = username;
+    }
+    public java.lang.String getUsername() {
+        return this.username;
+    }
+
+}
