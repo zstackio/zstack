8b2781e9d72e2777472db92fd9798d0b99e73971
boce.wang <boce.wang@zstack.io>
2022-05-20 15:54:30 +0800

    <feature>[vpc]: ipsec sql update for strongswan upgrade
    # Possible types: fix/feature/test/refactor/chore
    # Possible tags: APIImpact/GlobalConfigImpact/GlobalPropertyImpact/DBImpact/ZQLImpact
    # Possible jira-footers: Resolves/Related
    # Please describe the commit as detailed as possible!
    # 1. Why is this change necessary?
    # 2. How does it address the problem?
    # 3. Are there any side effects?
    vpc ipsec update for strongswan new version .
    
    Resolves: ZSTAC-45099
    
    Change-Id: I726977787064736a61746269776d6a626e736128
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch 3.10.32
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #       modified:   vpc/src/main/java/org/zstack/vpc/VpcApiInterceptor.java
    #

---
+++ conf/db/upgrade/v4.4.12__schema.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeVersion` varchar(16) NOT NULL DEFAULT 'ikev1';
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `idType` varchar(16) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `remoteId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `localId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeLifeTime` int(10) DEFAULT 0;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `lifeTime` int(10) DEFAULT 0;
