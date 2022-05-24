3bac366d674c14262c36712ca7c4b624f6ee331f
boce.wang <boce.wang@zstack.io>
2022-05-24 15:04:43 +0800

    sql return 4.4.6 for test ,this commit will undo when feature merge

--- conf/db/upgrade/V4.4.6__schema.sql
+++ conf/db/upgrade/V4.4.6__schema.sql
@@ -15,3 +15,10 @@ CREATE TABLE IF NOT EXISTS `zstack`.`ImagePackageVO` (
     CONSTRAINT `fkImagePackageVOVmInstanceEO` FOREIGN KEY (`vmUuid`) REFERENCES VmInstanceEO(`uuid`) ON DELETE SET NULL,
     CONSTRAINT `fkImagePackageVOBackupStorageEO` FOREIGN KEY (`backupStorageUuid`) REFERENCES BackupStorageEO(`uuid`) ON DELETE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8
+
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeVersion` varchar(16) NOT NULL DEFAULT 'ikev1';
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `idType` varchar(16) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `remoteId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `localId` varchar(128) DEFAULT NULL;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `ikeLifeTime` int(10) DEFAULT 0;
+ALTER TABLE `zstack`.`IPsecConnectionVO` ADD COLUMN `lifeTime` int(10) DEFAULT 0;
