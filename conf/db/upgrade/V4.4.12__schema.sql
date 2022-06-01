90da74a0243e786173d90b7c44f4ed408f655cee
yingzhe.hu <yinghe.hu@zstack.io>
2022-06-01 22:27:17 +0800

    <fix>[sdk]: update CreateCdpTask sdk
    Resolves: ZSTAC-45973
    
    Change-Id: I69697a6e61676277876354f174677a77656f6763

--- conf/db/upgrade/V4.4.12__schema.sql
+++ conf/db/upgrade/V4.4.12__schema.sql
@@ -4,3 +4,6 @@ ALTER TABLE `zstack`.`CdpPolicyEO` ADD COLUMN `fullBackupInterval` int unsigned 
 
 DROP VIEW IF EXISTS `zstack`.`CdpPolicyVO`;
 CREATE VIEW `zstack`.`CdpPolicyVO` AS SELECT uuid, name, description, retentionTimePerDay, dailyRPSinceDay, expireTime, recoveryPointPerSecond, fullBackupInterval, state, lastOpDate, createDate FROM `zstack`.`CdpPolicyEO` WHERE deleted IS NULL;
+
+ALTER TABLE `zstack`.`CdpTaskVO` ADD COLUMN `maxLatency` bigint(20) unsigned DEFAULT 0;
+ALTER TABLE `zstack`.`CdpTaskVO` ADD COLUMN `currentLatency` bigint(20) unsigned DEFAULT 0;
