99ae568c20e2dad505bd62863694e3dc8ddc6c5e
Lei Liu <lei.liu@zstack.io>
2022-06-20 14:27:06 +0800

    <fix>[conf]: rms iam2 update sdk
    DBImpact
    
    Change-Id: I7a7a666762626c626b64616e7063717969786b75

--- conf/db/upgrade/V4.4.9.1__schema.sql
+++ conf/db/upgrade/V4.4.9.1__schema.sql
@@ -31,3 +31,30 @@ CREATE TABLE IF NOT EXISTS `zstack`.`IAM2VirtualIDResourceRefVO` (
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`IAM2RMSOrganizationRefVO` (
+     `uuid` varchar(32) NOT NULL UNIQUE,
+     `rmsOrganizationNo` varchar(255) NOT NULL,
+     `organizationUuid` varchar(32),
+     `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+     `createDate` timestamp,
+     INDEX rmsOrganizationRefexOrgNo (rmsOrganizationNo),
+     INDEX rmsOrganizationRefOrguuid (organizationUuid),
+     PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zstack`.`IAM2RMSOrganizationRefVO` ADD CONSTRAINT fkRMSOrgRefVOOrganizationVO FOREIGN KEY (organizationUuid) REFERENCES IAM2OrganizationVO (uuid) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`IAM2RMSUserRefVO` (
+     `uuid` varchar(32) NOT NULL UNIQUE,
+     `name` varchar(255) NOT NULL,
+     `virtualIDUuid` varchar(32),
+     `rmsOrgNo` varchar(255) NOT NULL,
+     `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+     `createDate` timestamp,
+     INDEX rmsVirtualidRefexvirtualID (virtualIDUuid),
+     INDEX rmsName (name),
+     PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zstack`.`IAM2RMSUserRefVO` ADD CONSTRAINT fkRMSOrgRefVOVirtualIDVO FOREIGN KEY (virtualIDUuid) REFERENCES IAM2VirtualIDVO (uuid) ON DELETE CASCADE;
