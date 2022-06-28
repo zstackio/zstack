ff5e74c2ced23eb6ecf15ad9514274b2dd3a30b2
Wang,Qilin <qilin.wang@zstack.io>
2022-06-28 15:45:51 +0800

    <feature>[conf]:add support zsha2 for dizhenju sms
    Add sms config of the two nodes to sms platform,
    MN decides which config to use through its own IP
    
    DBImpact
    
    Change-Id: I6d70657a727966737778626272766a726a77707a
    Resolves: ZSTAC-47622
    (cherry picked from commit b35583ebb243b9b581880d586f9a39ca3049eb19)

---
+++ sdk/src/main/java/org/zstack/sdk/sns/platform/dizhenjusms/ResetSNSDizhenjuSmsClientResult.java
@@ -0,0 +1,7 @@
+package org.zstack.sdk.sns.platform.dizhenjusms;
+
+
+
+public class ResetSNSDizhenjuSmsClientResult {
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -41289,6 +41289,33 @@ abstract class ApiHelper {
     }
 
 
+    def resetSNSDizhenjuSmsClient(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.sns.platform.dizhenjusms.ResetSNSDizhenjuSmsClientAction.class) Closure c) {
+        def a = new org.zstack.sdk.sns.platform.dizhenjusms.ResetSNSDizhenjuSmsClientAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def addEmailAddressToSNSEmailEndpoint(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.sns.platform.email.AddEmailAddressToSNSEmailEndpointAction.class) Closure c) {
         def a = new org.zstack.sdk.sns.platform.email.AddEmailAddressToSNSEmailEndpointAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
