5e89588f39eb097445ef55d8644188d88efaa6f8
hanyu.liang <hanyu.liang@zstack.io>
2022-06-29 13:51:33 +0800

    <fix>[snapshotGroup]: fix the inconsistency of default vmCdRom after restoring memory snapshot
    rewrite the logic of restoring cdRom: delete all cdRoms and recreate them in order.
    
    VmInstanceDeviceAddressGroupVO adds new field vmInstanceUuid
    
    Related: ZSTAC-47670
    
    Change-Id:a665b7476701442390bf9bd93c6aaf49

--- sdk/src/main/java/org/zstack/sdk/VmInstanceDeviceAddressGroupInventory.java
+++ sdk/src/main/java/org/zstack/sdk/VmInstanceDeviceAddressGroupInventory.java
@@ -20,6 +20,14 @@ public class VmInstanceDeviceAddressGroupInventory  {
         return this.resourceUuid;
     }
 
+    public java.lang.String vmInstanceUuid;
+    public void setVmInstanceUuid(java.lang.String vmInstanceUuid) {
+        this.vmInstanceUuid = vmInstanceUuid;
+    }
+    public java.lang.String getVmInstanceUuid() {
+        return this.vmInstanceUuid;
+    }
+
     public java.sql.Timestamp createDate;
     public void setCreateDate(java.sql.Timestamp createDate) {
         this.createDate = createDate;
