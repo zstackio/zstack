5e6e3125934f78eb4e63a2ef7dac398a086a4662
Lei Liu <lei.liu@zstack.io>
2022-07-04 15:04:20 +0800

    <feature>[sdk]: add RMS plugin sdk
    Change-Id: I6c6578687179686e6a6763706d7875636f677961

---
+++ sdk/src/main/java/org/zstack/sdk/GenerateChallengeResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GenerateChallengeResult {
+    public java.lang.String challenge;
+    public void setChallenge(java.lang.String challenge) {
+        this.challenge = challenge;
+    }
+    public java.lang.String getChallenge() {
+        return this.challenge;
+    }
+
+}
