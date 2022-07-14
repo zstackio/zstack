d64c0096b6b3b363d9b4a470986f364dfe1bde84
Xingwei Yu <xingwei.yu@zstack.io>
2022-07-14 10:21:23 +0800

    <feature>[sdk]: add sdk for get IAM2VirtualID roles
    Resolves: ZSTAC-46436
    
    Change-Id: I6e6374666b6e6569677867617775726476646e6e

---
+++ sdk/src/main/java/org/zstack/sdk/iam2/api/GetRolesForIAM2VirtualIDResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk.iam2.api;
+
+
+
+public class GetRolesForIAM2VirtualIDResult {
+    public java.util.List inventories;
+    public void setInventories(java.util.List inventories) {
+        this.inventories = inventories;
+    }
+    public java.util.List getInventories() {
+        return this.inventories;
+    }
+
+}
