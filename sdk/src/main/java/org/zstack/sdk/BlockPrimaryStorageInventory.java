3a8055d9becd22014b24212a7e0826c45acfd806
Lei Liu <lei.liu@zstack.io>
2022-08-02 17:17:27 +0800

    <feature>[sdk]: Update block storage sdk
    1. AddBlockPrimaryStorageAction
    2. BlockPrimaryStorageInventory
    3. GetBlockPrimaryStorageMetadataAction
    4. QueryBlockPrimaryStorageResult
    
    Resolves: ZSTAC-48664
    
    Change-Id: I706d7a7a75746b71657967746b707a6367626c63

---
+++ sdk/src/main/java/org/zstack/sdk/BlockPrimaryStorageInventory.java
@@ -0,0 +1,55 @@
+package org.zstack.sdk;
+
+
+
+public class BlockPrimaryStorageInventory extends org.zstack.sdk.PrimaryStorageInventory {
+
+    public java.lang.String vendorName;
+    public void setVendorName(java.lang.String vendorName) {
+        this.vendorName = vendorName;
+    }
+    public java.lang.String getVendorName() {
+        return this.vendorName;
+    }
+
+    public java.lang.String metadata;
+    public void setMetadata(java.lang.String metadata) {
+        this.metadata = metadata;
+    }
+    public java.lang.String getMetadata() {
+        return this.metadata;
+    }
+
+    public java.lang.String encryptGatewayIp;
+    public void setEncryptGatewayIp(java.lang.String encryptGatewayIp) {
+        this.encryptGatewayIp = encryptGatewayIp;
+    }
+    public java.lang.String getEncryptGatewayIp() {
+        return this.encryptGatewayIp;
+    }
+
+    public java.lang.Integer encryptGatewayPort;
+    public void setEncryptGatewayPort(java.lang.Integer encryptGatewayPort) {
+        this.encryptGatewayPort = encryptGatewayPort;
+    }
+    public java.lang.Integer getEncryptGatewayPort() {
+        return this.encryptGatewayPort;
+    }
+
+    public java.lang.String encryptGatewayUsername;
+    public void setEncryptGatewayUsername(java.lang.String encryptGatewayUsername) {
+        this.encryptGatewayUsername = encryptGatewayUsername;
+    }
+    public java.lang.String getEncryptGatewayUsername() {
+        return this.encryptGatewayUsername;
+    }
+
+    public java.lang.String encryptGatewayPassword;
+    public void setEncryptGatewayPassword(java.lang.String encryptGatewayPassword) {
+        this.encryptGatewayPassword = encryptGatewayPassword;
+    }
+    public java.lang.String getEncryptGatewayPassword() {
+        return this.encryptGatewayPassword;
+    }
+
+}
