dcca97f4e98f0722cadf3169128edf3127c28855
Lei Liu <lei.liu@zstack.io>
2022-08-02 16:59:16 +0800

    <feature>[feature]: Support Block Storage
    DBImpact
    
    Resolves: ZSTAC-48664
    
    Change-Id: I796968797168696b796a69767872686173797a68

---
+++ conf/db/upgrade/V4.3.25.1__schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS `zstack`.`BlockPrimaryStorageVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `vendorName` varchar(256) NOt NULL,
+    `metadata` text DEFAULT NULL,
+    `encryptGatewayIp` varchar(64) DEFAULT NULL,
+    `encryptGatewayPort` smallint unsigned DEFAULT 8443,
+    `encryptGatewayUsername` varchar(256) DEFAULT NULL,
+    `encryptGatewayPassword` varchar(256) DEFAULT NULL,
+    PRIMARY KEY (`uuid`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`BlockScsiLunVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `target` varchar(256) DEFAULT NULL,
+    `name` VARCHAR(256) DEFAULT NULL,
+    `id` smallint unsigned NOT NULL,
+    `wwn` VARCHAR(256) DEFAULT NULL,
+    `type` VARCHAR(128) NOT NULL,
+    `size` bigint unsigned NOT NULL,
+    `lunMapId` smallint unsigned DEFAULT 0,
+    `lunInitSnapshotID` bigint unsigned DEFAULT 0,
+    `usedSize` bigint(20) unsigned DEFAULT 0,
+    `encryptedId` smallint unsigned DEFAULT 0,
+    `encryptedWwn` varchar(256) DEFAULT NULL,
+    `lunType` varchar(256) NOT NULL,
+    `volumeUuid` varchar(32) DEFAULT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`),
+    CONSTRAINT `fkScsiLunVOVolumeVO` FOREIGN KEY (`volumeUuid`) REFERENCES `zstack`.`VolumeEO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`HostInitiatorRefVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `hostUuid` varchar(32) NOT NULL,
+    `initiatorName` varchar(256) NOT NULL,
+    `metadata` text DEFAULT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`),
+    CONSTRAINT `fkHostInitiatorRefVOHostVo` FOREIGN KEY (`hostUuid`) REFERENCES `zstack`.`HostEO` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
