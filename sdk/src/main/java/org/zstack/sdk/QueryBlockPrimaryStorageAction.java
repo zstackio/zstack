188b1f0c6519fdd100b160931b29279b83e024c7
Lei Liu <lei.liu@zstack.io>
2022-08-02 17:17:27 +0800

    <feature>[sdk]: Update block storage sdk
    1. AddBlockPrimaryStorageAction
    2. BlockPrimaryStorageInventory
    3. GetBlockPrimaryStorageMetadataAction
    4. QueryBlockPrimaryStorageResult
    
    Resolves: ZSTAC-48664
    
    Change-Id: I706d7a7a75746b71657967746b707a6367626c63

---
+++ sdk/src/main/java/org/zstack/sdk/QueryBlockPrimaryStorageAction.java
@@ -0,0 +1,75 @@
+package org.zstack.sdk;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.zstack.sdk.*;
+
+public class QueryBlockPrimaryStorageAction extends QueryAction {
+
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    private static final HashMap<String, Parameter> nonAPIParameterMap = new HashMap<>();
+
+    public static class Result {
+        public ErrorCode error;
+        public org.zstack.sdk.QueryBlockPrimaryStorageResult value;
+
+        public Result throwExceptionIfError() {
+            if (error != null) {
+                throw new ApiException(
+                    String.format("error[code: %s, description: %s, details: %s]", error.code, error.description, error.details)
+                );
+            }
+            
+            return this;
+        }
+    }
+
+
+
+    private Result makeResult(ApiResult res) {
+        Result ret = new Result();
+        if (res.error != null) {
+            ret.error = res.error;
+            return ret;
+        }
+        
+        org.zstack.sdk.QueryBlockPrimaryStorageResult value = res.getResult(org.zstack.sdk.QueryBlockPrimaryStorageResult.class);
+        ret.value = value == null ? new org.zstack.sdk.QueryBlockPrimaryStorageResult() : value; 
+
+        return ret;
+    }
+
+    public Result call() {
+        ApiResult res = ZSClient.call(this);
+        return makeResult(res);
+    }
+
+    public void call(final Completion<Result> completion) {
+        ZSClient.call(this, new InternalCompletion() {
+            @Override
+            public void complete(ApiResult res) {
+                completion.complete(makeResult(res));
+            }
+        });
+    }
+
+    protected Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    protected Map<String, Parameter> getNonAPIParameterMap() {
+        return nonAPIParameterMap;
+    }
+
+    protected RestInfo getRestInfo() {
+        RestInfo info = new RestInfo();
+        info.httpMethod = "GET";
+        info.path = "/primary-storage/block";
+        info.needSession = true;
+        info.needPoll = false;
+        info.parameterName = "";
+        return info;
+    }
+
+}
