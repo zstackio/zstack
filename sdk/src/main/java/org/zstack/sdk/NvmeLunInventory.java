ba3ddc87583701b9a06148bb749f371a3c8e6e32
J M <jin.ma@zstack.io>
2022-08-11 18:14:02 +0800

    <feature>[conf]: support scan NVMe-oF
    support NVMe-oF
    
    DBImpact
    
    Resolves: ZSTAC-47329
    
    Change-Id: I7471636d637962776c706b797270697172777179

---
+++ sdk/src/main/java/org/zstack/sdk/NvmeLunInventory.java
@@ -0,0 +1,15 @@
+package org.zstack.sdk;
+
+
+
+public class NvmeLunInventory extends org.zstack.sdk.ScsiLunInventory {
+
+    public java.lang.String nvmeTargetUuid;
+    public void setNvmeTargetUuid(java.lang.String nvmeTargetUuid) {
+        this.nvmeTargetUuid = nvmeTargetUuid;
+    }
+    public java.lang.String getNvmeTargetUuid() {
+        return this.nvmeTargetUuid;
+    }
+
+}
