ad266b51fdabd3283f6a2a3ef85e235b364de200
jialong.dong <jialong.dong@zstack.io>
2022-08-16 18:03:38 +0800

    <fix>[tag]: add clonetag to tagUtils
    Resolves: ZSTAC-48954
    
    Change-Id: I797771646377796266746e70696c646f647a6f65

--- tag/src/main/java/org/zstack/tag/SystemTagUtils.java
+++ tag/src/main/java/org/zstack/tag/SystemTagUtils.java
@@ -1,8 +1,15 @@
 package org.zstack.tag;
 
+import org.zstack.header.tag.SystemTagInventory;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static org.zstack.utils.CollectionDSL.e;
+import static org.zstack.utils.CollectionDSL.map;
 
 /**
  * Created by lining on 2017/6/22.
@@ -59,5 +66,45 @@ public class SystemTagUtils {
         }
 
         return systemTags.stream().filter(tag::isMatch).collect(Collectors.toList());
+    }
+
+    public static void cloneTag(PatternedSystemTag tag, String tagToken, String srcUuid, String destUuid){
+        if (tagToken == null) {
+            return;
+        }
+        List<SystemTagInventory> srcTags = tag.getTagInventories(srcUuid);
+
+        if (srcTags == null || srcTags.isEmpty()) {
+            return;
+        }
+
+        for(SystemTagInventory srcTag: srcTags) {
+            SystemTagCreator creator = tag.newSystemTagCreator(destUuid);
+            creator.setTagByTokens(map(e(tagToken, tag.getTokenByResourceUuid(srcUuid, tagToken))));
+            creator.inherent = srcTag.isInherent();
+            creator.ignoreIfExisting = true;
+            creator.create();
+        }
+    }
+
+    public static void cloneTag(PatternedSystemTag tag, String srcUuid, String destUuid, String... tokens){
+        List<SystemTagInventory> srcTags = tag.getTagInventories(srcUuid);
+
+        if (srcTags == null || srcTags.isEmpty()) {
+            return;
+        }
+
+        Map<String, String> tokenMap = new HashMap<>();
+        for (String token : tokens) {
+            tokenMap.put(token, tag.getTokenByResourceUuid(srcUuid, token));
+        }
+
+        for(SystemTagInventory srcTag: srcTags) {
+            SystemTagCreator creator = tag.newSystemTagCreator(destUuid);
+            creator.setTagByTokens(tokenMap);
+            creator.inherent = srcTag.isInherent();
+            creator.ignoreIfExisting = true;
+            creator.create();
+        }
     }
 }
