484d2c93672f40a1501611102c468494cdf42a9d
ZqyDaoDao <249449726@qq.com>
2022-08-24 19:00:27 +0800

    <feature>[identity]: add oauth2
    Resolves: ZSTAC-48789
    
    Change-Id: I7664656a6c64727979766f68636964767a677676

---
+++ conf/db/upgrade/V4.4.27.2__schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS `zstack`.`SSOClientVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(255) DEFAULT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `clientType` varchar(255) NOT NULL,
+    `loginType` varchar(255) NOT NULL,
+    `loginMNUrl` varchar(255) DEFAULT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`OAuth2ClientVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `clientId` varchar(255) NOT NULL,
+    `clientSecret` varchar(255),
+    `authorizationUrl` varchar(255),
+    `grantType` varchar(64) NOT NULL,
+    `tokenUrl` varchar(255) NOT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT `fkOAuth2ClientVOSSOClientVO` FOREIGN KEY (`uuid`) REFERENCES `SSOClientVO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`CasClientVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `casServerLoginUrl` varchar(255) NOT NULL,
+    `casServerUrlPrefix` varchar(255) NOT NULL,
+    `serverName` varchar(255) NOT NULL,
+    `state` varchar(128) NOT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT `fkCasClientVOSSOClientVO` FOREIGN KEY (`uuid`) REFERENCES `SSOClientVO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`ThirdClientAccountRefVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `clientUuid` varchar(255) NOT NULL,
+    `resourceUuid`  varchar(255) NOT NULL,
+    `resourceType` varchar(255) NOT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`SSORedirectTemplateVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(255) DEFAULT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `clientUuid` varchar(255) NOT NULL,
+    `redirectTemplate`  varchar(2048) NOT NULL,
+    `lastOpDate` timestamp ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT `fkSSORedirectTemplateClientVO` FOREIGN KEY (`clientUuid`) REFERENCES `SSOClientVO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ sdk/src/main/java/org/zstack/sdk/RedirectUrlTemplate.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk;
+
+
+
+public class RedirectUrlTemplate  {
+
+    public java.lang.String urlTemplate;
+    public void setUrlTemplate(java.lang.String urlTemplate) {
+        this.urlTemplate = urlTemplate;
+    }
+    public java.lang.String getUrlTemplate() {
+        return this.urlTemplate;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String description;
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+    public java.lang.String getDescription() {
+        return this.description;
+    }
+
+}
