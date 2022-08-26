dce41e2a4c4b739b5c47c827be2629dd7cfff30a
xingxing.cui <xingxing.cui@zstack.io>
2022-08-26 09:51:53 +0800

    revert lldp

--- conf/db/upgrade/V4.5.0__schema.sql
+++ conf/db/upgrade/V4.5.0__schema.sql
@@ -23,22 +23,3 @@ CREATE TABLE IF NOT EXISTS `zstack`.`VxlanClusterMappingVO` (
     CONSTRAINT `fkVxlanClusterMappingVOVxlanNetworkVO` FOREIGN KEY (`vxlanUuid`) REFERENCES `VxlanNetworkVO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE,
     CONSTRAINT `fkVxlanClusterMappingVOClusterEO` FOREIGN KEY (`clusterUuid`) REFERENCES `ClusterEO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-CREATE TABLE IF NOT EXISTS `zstack`.`PortLldpInfoVO` (
-    `uuid` varchar(32) NOT NULL UNIQUE,
-    `l2NetworkUuid` varchar(32) NOT NULL,
-    `interfaceName` varchar(32) NOT NULL,
-    `interfaceType` varchar(32),
-    `bondIfName` varchar(32),
-    `portName` varchar(32),
-    `systemName` varchar(32),
-    `chassisMac` varchar(32),
-    `aggregated` boolean NOT NULL DEFAULT FALSE,
-    `aggregatedPortID` int(10),
-    `createDate`   timestamp    NOT NULL DEFAULT '0000-00-00 00:00:00',
-    `lastOpDate`   timestamp    NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY  (`uuid`),
-    CONSTRAINT `fkPortLldpInfoVOL2NetworkEO` FOREIGN KEY (`l2NetworkUuid`) REFERENCES  `L2NetworkEO` (`uuid`) ON UPDATE RESTRICT ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
