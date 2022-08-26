493dbc3ca7dc48e60dbcae17d79177e8e1e1d329
Xingwei Yu <xingwei.yu@zstack.io>
2022-08-26 15:56:12 +0800

    <fix>[conf]: add organizationUuid column to IAM2TicketFlowCollectionVO
    Resolves: ZSTAC-46436
    
    Change-Id: I666d7a696c69796c6576796f74777964786c7761

--- sdk/src/main/java/org/zstack/sdk/ticket/iam2/entity/IAM2TicketFlowCollectionInventory.java
+++ sdk/src/main/java/org/zstack/sdk/ticket/iam2/entity/IAM2TicketFlowCollectionInventory.java
@@ -4,6 +4,14 @@ package org.zstack.sdk.ticket.iam2.entity;
 
 public class IAM2TicketFlowCollectionInventory extends org.zstack.sdk.ticket.entity.TicketFlowCollectionInventory {
 
+    public java.lang.String organizationUuid;
+    public void setOrganizationUuid(java.lang.String organizationUuid) {
+        this.organizationUuid = organizationUuid;
+    }
+    public java.lang.String getOrganizationUuid() {
+        return this.organizationUuid;
+    }
+
     public java.lang.String projectUuid;
     public void setProjectUuid(java.lang.String projectUuid) {
         this.projectUuid = projectUuid;
