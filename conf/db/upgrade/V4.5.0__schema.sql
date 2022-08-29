e917aae2fd03c3d0a09961dc325d9f741de3c540
Wang,Qilin <qilin.wang@zstack.io>
2022-08-29 16:29:58 +0800

    <feature>[conf]:  add API to register license
     add API to register license using licenseRequestCode
    
    DBImpact
    
    Resolves: ZSTAC-48821
    
    Change-Id: I6a6a7867756765657171627870776b6776677362

--- conf/db/upgrade/V4.5.0__schema.sql
+++ conf/db/upgrade/V4.5.0__schema.sql
@@ -1,2 +1,9 @@
 ALTER TABLE `zstack`.`TicketStatusHistoryVO` ADD COLUMN `flowName` VARCHAR(255) DEFAULT NULL;
 ALTER TABLE `zstack`.`ArchiveTicketStatusHistoryVO` ADD COLUMN `flowName` VARCHAR(255) DEFAULT NULL;
+CREATE TABLE IF NOT EXISTS `zstack`.`RegisterLicenseApplicationVO` (
+    `appId` VARCHAR(32) NOT NULL UNIQUE,
+    `licenseRequestCode` text NOT NULL,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`appId`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
