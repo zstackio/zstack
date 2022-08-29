f6967b9b5f868670d73ca31a1d698582099e19d3
boce.wang <boce.wang@zstack.io>
2022-08-29 13:20:34 +0800

    update APIGetVirtualRouterSoftwareVersionMsg support list reply and needUpdate param

---
+++ plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/VirtualRouterSoftwareVersionInventory.java
@@ -0,0 +1,65 @@
+package org.zstack.network.service.virtualrouter;
+
+import org.zstack.header.search.Inventory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Inventory(mappingVOClass = VirtualRouterSoftwareVersionVO.class)
+public class VirtualRouterSoftwareVersionInventory {
+    private String uuid;
+    private String softwareName;
+    private String currentVersion;
+    private String latestVersion;
+
+    public static VirtualRouterSoftwareVersionInventory valueOf(VirtualRouterSoftwareVersionVO vo){
+        VirtualRouterSoftwareVersionInventory inv = new VirtualRouterSoftwareVersionInventory();
+        inv.setUuid(vo.getUuid());
+        inv.setSoftwareName(vo.getSoftwareName());
+        inv.setCurrentVersion(vo.getCurrentVersion());
+        inv.setLatestVersion(vo.getLatestVersion());
+        return inv;
+    }
+
+    public static List<VirtualRouterSoftwareVersionInventory> valueOf(Collection<VirtualRouterSoftwareVersionVO> vos){
+        List<VirtualRouterSoftwareVersionInventory> invs = new ArrayList<>(vos.size());
+        for (VirtualRouterSoftwareVersionVO vo : vos) {
+            invs.add(VirtualRouterSoftwareVersionInventory.valueOf(vo));
+        }
+        return invs;
+    }
+
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getSoftwareName() {
+        return softwareName;
+    }
+
+    public void setSoftwareName(String softwareName) {
+        this.softwareName = softwareName;
+    }
+
+    public String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    public void setCurrentVersion(String currentVersion) {
+        this.currentVersion = currentVersion;
+    }
+
+    public String getLatestVersion() {
+        return latestVersion;
+    }
+
+    public void setLatestVersion(String latestVersion) {
+        this.latestVersion = latestVersion;
+    }
+}
