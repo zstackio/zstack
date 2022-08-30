6233b7d0fe3445131e8f91b8a41bc8d59ee9b4a3
hanyu.liang <hanyu.liang@zstack.io>
2022-08-30 16:00:52 +0800

    <fix>[core]: store encrypt required field and class
    supplement encryption logic for new fields
    
    Resolved: ZSTAC-27012
    
    Change-Id: I6864927012bb50f1e42f4f47a79bf38be122b9b3
    
    Conflicts:
    	conf/db/upgrade/V4.4.24__schema.sql
    	conf/persistence.xml

--- header/src/main/java/org/zstack/header/core/encrypt/ENCRYPT.java
+++ header/src/main/java/org/zstack/header/core/encrypt/ENCRYPT.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Target;
 /**
  * Created by mingjian.deng on 16/11/1.
  */
+@Deprecated
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ENCRYPT {
