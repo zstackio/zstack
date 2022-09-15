051d9c2d1c2feed2b46af8d12979b6c942dfd705
qiuyu <qiuyu.zhang@zstack.io>
2022-09-15 11:27:37 +0800

    <fix>[sso-plugin]: add api params
    Resolves: ZSTAC-49759
    
    Change-Id: I6c6c736c626669767172716a667072696c6e6c71

--- sdk/src/main/java/org/zstack/sdk/CreateOAuthClientAction.java
+++ sdk/src/main/java/org/zstack/sdk/CreateOAuthClientAction.java
@@ -52,6 +52,9 @@ public class CreateOAuthClientAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String urlTemplate;
 
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String clientType;
+
     @Param(required = false)
     public java.lang.String resourceUuid;
 
