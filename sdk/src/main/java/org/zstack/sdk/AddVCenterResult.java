179ad956b594626662fd5872a213f4da410d27a9
Wang,Qilin <qilin.wang@zstack.io>
2022-10-11 18:01:47 +0800

    <fix>[sdk]: add info of skip sync vms to APISyncVCenterEvent and APIAddVCenterEvent
    Resolves: ZSTAC-43049
    
    Change-Id: I6c637964657479706d68716b6f7473736a717569
    (cherry picked from commit 18b3b53f98b09d1e705d75257ab93c7c61a8810a)

--- sdk/src/main/java/org/zstack/sdk/AddVCenterResult.java
+++ sdk/src/main/java/org/zstack/sdk/AddVCenterResult.java
@@ -1,6 +1,7 @@
 package org.zstack.sdk;
 
 import org.zstack.sdk.VCenterInventory;
+import org.zstack.sdk.SkippedResources;
 
 public class AddVCenterResult {
     public VCenterInventory inventory;
@@ -9,6 +10,14 @@ public class AddVCenterResult {
     }
     public VCenterInventory getInventory() {
         return this.inventory;
+    }
+
+    public SkippedResources skippedResources;
+    public void setSkippedResources(SkippedResources skippedResources) {
+        this.skippedResources = skippedResources;
+    }
+    public SkippedResources getSkippedResources() {
+        return this.skippedResources;
     }
 
 }
