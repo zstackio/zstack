17f326849a285d8158f98b809c02c9a2cace1c9a
Zhang Wenhao <wenhao.zhang@zstack.io>
2022-10-27 17:43:26 +0800

    <improvement>[sdk]: add debug option in GetVmGuestToolsInfoAction
    Resolves: ZSTAC-47374
    
    Change-Id: I7873736e717862676979626f6f69636e7877746d
    (cherry picked from commit 28432763dfefce4d5ba38de86f1d908748bcd6bc)

--- sdk/src/main/java/org/zstack/sdk/GetVmGuestToolsInfoAction.java
+++ sdk/src/main/java/org/zstack/sdk/GetVmGuestToolsInfoAction.java
@@ -28,6 +28,9 @@ public class GetVmGuestToolsInfoAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String uuid;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.util.Set debug;
+
     @Param(required = false)
     public java.util.List systemTags;
 
