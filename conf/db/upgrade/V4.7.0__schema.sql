f6eab1bf01baf1843f248b187dcf75ffe083142c
hanyu.liang <hanyu.liang@zstack.io>
2022-11-09 21:36:29 +0800

    <feature>[crypto]: Update the identity authentication interface
    update sdk
    
    Resolves: ZSTAC-50493
    
    Change-Id: I66766d696b6e6c4885049366616964656774736c

--- conf/db/upgrade/V4.7.0__schema.sql
+++ conf/db/upgrade/V4.7.0__schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS `zstack`.`HaiTaiSecretResourcePoolVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `managementIp` varchar(32) NOT NULL,
+    `port` int unsigned NOT NULL,
+    `realm` varchar(32) NOT NULL,
+    PRIMARY KEY  (`uuid`),
+    CONSTRAINT fkHaiTaiSecretResourcePoolVOSecretResourcePoolVO FOREIGN KEY (uuid) REFERENCES SecretResourcePoolVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `zstack`.`FileIntegrityVerificationVO` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `path` varchar(256) NOT NULL,
