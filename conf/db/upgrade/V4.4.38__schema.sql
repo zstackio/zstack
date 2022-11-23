f81b5771686d1d21fea0cd2aa808efd94b6ceea7
xiaoyingtao <2421815391@qq.com>
2022-11-23 10:47:08 +0800

    <fix>[conf]: modify schema name in case of influence master branch
    DBImpact
    
    Resolves: ZSTAC-1111
    
    Change-Id: I64796f6362786f776f6974737a796e636b74706c

--- conf/db/upgrade/V4.4.38__schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `AgentVersionVO` (
-                          `uuid` varchar(32) NOT NULL PRIMARY KEY,
-                          `agentType` varchar(255) DEFAULT NULL,
-                          `currentVersion` varchar(255) DEFAULT NULL,
-                          `exceptVersion` varchar(255) DEFAULT NULL,
-                          `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-                          `createOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
