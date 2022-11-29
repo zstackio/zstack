4a4666e864dc964ecbdb75a3ffc91bcd591a4dcf
Zhang Wenhao <wenhao.zhang@zstack.io>
2022-11-29 17:12:03 +0800

    <fix>[sdk]: add GetSignatureServerEncryptPublicKeyAction
    Related: ZSTAC-51240
    
    Change-Id: I757a6c63646e7568627266687367617479627568

---
+++ sdk/src/main/java/org/zstack/sdk/GetSignatureServerEncryptPublicKeyResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetSignatureServerEncryptPublicKeyResult {
+    public java.lang.String publicKey;
+    public void setPublicKey(java.lang.String publicKey) {
+        this.publicKey = publicKey;
+    }
+    public java.lang.String getPublicKey() {
+        return this.publicKey;
+    }
+
+}
