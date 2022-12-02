af8161b7caf46c1eb3e4543b58bde975c82d4cbe
Zhang Wenhao <wenhao.zhang@zstack.io>
2022-12-02 17:30:26 +0800

    <chore>[core]: update i18n and ExceptionSafeAspect
    ExceptionSafeAspect support put Exception to ErrorableValue
    
    Related: ZSTAC-51240
    
    Change-Id: I787776756f786e73716b7368766a766c63696b66

--- conf/db/upgrade/V4.5.1.1__schema.sql
+++ conf/db/upgrade/V4.5.1.1__schema.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `zstack`.`InfoSecSecretResourcePoolVO` ADD COLUMN `encryptPublicKey` varchar(128) DEFAULT NULL;
-ALTER TABLE `zstack`.`InfoSecSecretResourcePoolVO` ADD COLUMN `encryptSubjectDN` text DEFAULT NULL;
+ALTER TABLE `zstack`.`InfoSecSecretResourcePoolVO` ADD COLUMN `encryptPublicKey` text DEFAULT NULL;
+ALTER TABLE `zstack`.`InfoSecSecretResourcePoolVO` ADD COLUMN `encryptSubjectDN` varchar(128) DEFAULT NULL;
