2a8de4a8c6672aece8bfdfa29b47ba0b70072ae8
shixin.ruan <shixin.ruan@zstack.io>
2022-12-08 15:44:44 +0800

    <fix>[sugonSdnController]: 修复tf类型网络相关bug by fuwei from sugon
    Resolves: ZSTAC-12345
    
    Change-Id: I6a756b6565666b6379746f62616e70756c706467

--- plugin/sugonSdnController/src/main/java/org/zstack/sugonSdnController/network/VmDetachNicExtensionPointImpl.java
+++ plugin/sugonSdnController/src/main/java/org/zstack/sugonSdnController/network/VmDetachNicExtensionPointImpl.java
@@ -3,26 +3,21 @@ package org.zstack.sugonSdnController.network;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.zstack.compute.vm.CustomNicOperator;
-import org.zstack.compute.vm.MacOperator;
-import org.zstack.compute.vm.VmSystemTags;
-import org.zstack.header.network.l3.L3NetworkInventory;
-import org.zstack.header.vm.VmFailToAttachL3NetworkExtensionPoint;
-import org.zstack.header.vm.VmInstanceInventory;
-import org.zstack.sugonSdnController.controller.SugonSdnControllerConstant;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.header.errorcode.ErrorCode;
+import org.zstack.header.network.l3.L3NetworkInventory;
 import org.zstack.header.network.l3.L3NetworkVO;
 import org.zstack.header.vm.VmDetachNicExtensionPoint;
+import org.zstack.header.vm.VmFailToAttachL3NetworkExtensionPoint;
+import org.zstack.header.vm.VmInstanceInventory;
 import org.zstack.header.vm.VmNicInventory;
 import org.zstack.identity.AccountManager;
+import org.zstack.sugonSdnController.controller.SugonSdnControllerConstant;
 import org.zstack.sugonSdnController.controller.neutronClient.TfPortClient;
 import org.zstack.sugonSdnController.controller.neutronClient.TfPortResponse;
 import org.zstack.utils.StringDSL;
 import org.zstack.utils.Utils;
 import org.zstack.utils.logging.CLogger;
-
-import static org.zstack.utils.CollectionDSL.e;
-import static org.zstack.utils.CollectionDSL.map;
 
 public class VmDetachNicExtensionPointImpl implements VmDetachNicExtensionPoint, VmFailToAttachL3NetworkExtensionPoint {
     private static final CLogger logger = Utils.getLogger(VmDetachNicExtensionPointImpl.class);
@@ -37,14 +32,19 @@ public class VmDetachNicExtensionPointImpl implements VmDetachNicExtensionPoint,
 
     @Override
     public void beforeDetachNic(VmNicInventory nic) {
+        L3NetworkVO l3nw = dbf.findByUuid(nic.getL3NetworkUuid(), L3NetworkVO.class);
+        if (SugonSdnControllerConstant.L3_TF_NETWORK_TYPE.equals(l3nw.getType())) {
+            // bug fix: delete nic tags before detachL3Network, but won't delete nic tags when destroy vm
+            CustomNicOperator nicOperator = new CustomNicOperator(nic.getVmInstanceUuid(), l3nw.getUuid());
+            nicOperator.deleteNicTags();
+        }
     }
 
     @Override
     public void afterDetachNic(VmNicInventory nic) {
         L3NetworkVO l3nw = dbf.findByUuid(nic.getL3NetworkUuid(), L3NetworkVO.class);
         if (SugonSdnControllerConstant.L3_TF_NETWORK_TYPE.equals(l3nw.getType())) {
-            CustomNicOperator nicOperator = new CustomNicOperator();
-            nicOperator.deleteNicTags(nic.getVmInstanceUuid(), l3nw.getUuid());
+            // bug fix: won't delete nic related tags, because recovery flow will use them
             deleteTfPort(nic.getUuid(), nic.getL3NetworkUuid());
         }
     }
@@ -56,9 +56,9 @@ public class VmDetachNicExtensionPointImpl implements VmDetachNicExtensionPoint,
     @Override
     public void vmFailToAttachL3Network(VmInstanceInventory vm, L3NetworkInventory l3, ErrorCode error) {
         if (SugonSdnControllerConstant.L3_TF_NETWORK_TYPE.equals(l3.getType())) {
-            CustomNicOperator nicOperator = new CustomNicOperator();
-            String customNicId = nicOperator.getCustomNicId(vm.getUuid(), l3.getUuid());
-            nicOperator.deleteNicTags(vm.getUuid(), l3.getUuid());
+            CustomNicOperator nicOperator = new CustomNicOperator(vm.getUuid(), l3.getUuid());
+            String customNicId = nicOperator.getCustomNicId();
+            nicOperator.deleteNicTags();
             deleteTfPort(customNicId, l3.getUuid());
         }
     }
