df08ac4b34bcf7ca715b54acb5de9c0f3d9ae441
qiuyu <qiuyu.zhang@zstack.io>
2022-12-25 00:36:58 +0800

    <feature>[mevoco]: add query vm scheduling state
        APIImpact
    
        Resolves: ZSTAC-52435
    
        Change-Id: I67626a6b726169846979757767686f7a69786478

---
+++ sdk/src/main/java/org/zstack/sdk/GetVmsSchedulingStateFromSchedulingRuleResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetVmsSchedulingStateFromSchedulingRuleResult {
+    public java.util.Map ruleMapState;
+    public void setRuleMapState(java.util.Map ruleMapState) {
+        this.ruleMapState = ruleMapState;
+    }
+    public java.util.Map getRuleMapState() {
+        return this.ruleMapState;
+    }
+
+}
