84592300a96c81a9b576f9b4b4305e65ca0f1225
hanyu.liang <hanyu.liang@zstack.io>
2022-12-27 16:28:36 +0800

    <fix>[conf]: LongJobVO add index idxLongJobVOtargetResourceUuid
    Resolves: ZSTAC-50584
    
    Change-Id: I71725058416979677a6b74646a786c7065696b62

--- conf/db/upgrade/V4.4.46__schema.sql
+++ conf/db/upgrade/V4.4.46__schema.sql
@@ -1,3 +1,16 @@
+DROP PROCEDURE IF EXISTS addLongJobVOIndex;
+
+DELIMITER $$
+CREATE PROCEDURE addLongJobVOIndex()
+BEGIN
+    IF NOT EXISTS (SELECT * FROM information_schema.statistics WHERE table_schema = 'zstack' AND table_name = "LongJobVO" AND index_name = "idxLongJobVOtargetResourceUuid") THEN
+        CREATE INDEX idxLongJobVOtargetResourceUuid ON LongJobVO (targetResourceUuid);
+    END IF;
+END $$
+DELIMITER ;
+
+CALL addLongJobVOIndex();
+
 DELIMITER $$
 CREATE PROCEDURE splitHybridLicenseRecords()
 BEGIN
