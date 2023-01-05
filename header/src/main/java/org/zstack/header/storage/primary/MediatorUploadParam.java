ef3985bc8f8ebb1ecbd65e9fe38162c20cd60fac
J M <jin.ma@zstack.io>
2023-01-05 13:31:18 +0800

    <improvement>[imagestore]: upload concurrency config
    support to config concurrency when upload single image to backup storage.
    
    Resolves: ZSTAC-52119
    
    Change-Id: I72716c7762766375726f777567616c766c657462

---
+++ header/src/main/java/org/zstack/header/storage/primary/MediatorUploadParam.java
@@ -0,0 +1,10 @@
+package org.zstack.header.storage.primary;
+
+import org.zstack.header.image.ImageInventory;
+
+public class MediatorUploadParam implements MediatorParam {
+    public ImageInventory image;
+    public String primaryStorageUuid;
+    public String primaryStorageInstallPath;
+    public String backupStorageInstallPath;
+}
