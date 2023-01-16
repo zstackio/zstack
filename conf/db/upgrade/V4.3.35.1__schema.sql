09b3db30300668b1192a775b693546f3e448578f
Lei Liu <lei.liu@zstack.io>
2023-01-16 18:19:15 +0800

    <fix>[conf]: update host initiator ref vo
    DBImpact
    
    Resolves: ZSTAC-52887
    
    Change-Id: I6b6a766d666c68736a70616d6566626a636d646d

--- conf/db/upgrade/V4.3.35.1__schema.sql
+++ conf/db/upgrade/V4.3.35.1__schema.sql
@@ -1,2 +1,7 @@
 UPDATE `zstack`.`GlobalConfigVO` SET value="enable", defaultValue="enable" WHERE category="storageDevice" AND name="enable.multipath" AND value="true";
 UPDATE `zstack`.`GlobalConfigVO` SET value="ignore", defaultValue="enable" WHERE category="storageDevice" AND name="enable.multipath" AND value="false";
+ALTER TABLE `zstack`.`HostInitiatorRefVO` DROP FOREIGN KEY `fkHostInitiatorRefVOHostVo`;
+ALTER TABLE `zstack`.`HostInitiatorRefVO` DROP INDEX hostUuid;
+ALTER TABLE `zstack`.`HostInitiatorRefVO` ADD CONSTRAINT fkHostInitiatorRefVOHostVO FOREIGN KEY (hostUuid) REFERENCES `zstack`.`HostEO` (uuid) ON DELETE CASCADE;
+ALTER TABLE `zstack`.`HostInitiatorRefVO` ADD COLUMN `primaryStorageUuid` varchar (32) DEFAULT NULL;
+ALTER TABLE `zstack`.`HostInitiatorRefVO` ADD CONSTRAINT fkHostInitiatorRefVOPrimaryStorageVO FOREIGN KEY (primaryStorageUuid) REFERENCES `zstack`.`PrimaryStorageEO` (uuid) ON DELETE CASCADE;
