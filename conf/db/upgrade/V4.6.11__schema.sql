1f9364fc3d2dc656fe954f85936894887cdc19b3
hanyu.liang <hanyu.liang@zstack.io>
2023-02-10 14:41:18 +0800

    <fix>[crypto]: Modify the type of the serNumber field to varchar
    Resolves: ZSTAC-52999
    
    Change-Id: I667652999b6e6c4885049366616961251012336c

--- conf/db/upgrade/V4.6.11__schema.sql
+++ conf/db/upgrade/V4.6.11__schema.sql
@@ -1,3 +1,35 @@
+ALTER TABLE `zstack`.`L2NetworkEO` MODIFY `vSwitchType` varchar(32) NOT NULL DEFAULT 'LinuxBridge' AFTER `type`;
+ALTER TABLE `zstack`.`L2NetworkEO` ADD COLUMN `virtualNetworkId` int unsigned NOT NULL DEFAULT 0 AFTER `vSwitchType`;
+UPDATE `zstack`.`L2NetworkEO` l2 INNER JOIN `zstack`.`L2VlanNetworkVO` vlan ON l2.uuid = vlan.uuid SET l2.virtualNetworkId = vlan.vlan;
+UPDATE `zstack`.`L2NetworkEO` l2 INNER JOIN `zstack`.`VxlanNetworkVO` vxlan ON l2.uuid = vxlan.uuid SET l2.virtualNetworkId = vxlan.vni;
+DROP VIEW IF EXISTS `zstack`.L2NetworkVO;
+CREATE VIEW `zstack`.`L2NetworkVO` AS SELECT uuid, name, description, type, vSwitchType, virtualNetworkId, zoneUuid, physicalInterface, createDate, lastOpDate FROM `zstack`.`L2NetworkEO` WHERE deleted IS NULL;
+
+DELIMITER $$
+CREATE PROCEDURE addColumnsToSNSTextTemplateVO()
+    BEGIN
+        IF NOT EXISTS( SELECT 1
+                       FROM INFORMATION_SCHEMA.COLUMNS
+                       WHERE table_name = 'SNSTextTemplateVO'
+                             AND table_schema = 'zstack'
+                             AND column_name = 'subject') THEN
+
+           ALTER TABLE `zstack`.`SNSTextTemplateVO` ADD COLUMN `subject` VARCHAR(2048);
+        END IF;
+        IF NOT EXISTS( SELECT 1
+                       FROM INFORMATION_SCHEMA.COLUMNS
+                       WHERE table_name = 'SNSTextTemplateVO'
+                             AND table_schema = 'zstack'
+                             AND column_name = 'recoverySubject') THEN
+
+           ALTER TABLE `zstack`.`SNSTextTemplateVO` ADD COLUMN `recoverySubject` VARCHAR(2048);
+        END IF;
+    END $$
+DELIMITER ;
+
+call addColumnsToSNSTextTemplateVO();
+DROP PROCEDURE IF EXISTS addColumnsToSNSTextTemplateVO;
+
 CREATE TABLE IF NOT EXISTS `zstack`.`FlkSecSecretResourcePoolVO` (
     `uuid` varchar(32) NOT NULL UNIQUE,
     `encryptResult` varchar(64) DEFAULT NULL,
@@ -17,4 +49,8 @@ CREATE TABLE IF NOT EXISTS `zstack`.`FlkSecSecurityMachineVO` (
 
 ALTER TABLE `zstack`.`CCSCertificateVO` MODIFY COLUMN issuerDN varchar(255) NOT NULL;
 ALTER TABLE `zstack`.`CCSCertificateVO` MODIFY COLUMN subjectDN varchar(255) NOT NULL;
-ALTER TABLE `zstack`.`CCSCertificateVO` MODIFY COLUMN serNumber bigint(64) unsigned NOT NULL;
+ALTER TABLE `zstack`.`CCSCertificateVO` MODIFY COLUMN serNumber varchar(128) NOT NULL;
+
+ALTER TABLE `zstack`.`ESXHostVO` ADD COLUMN `esxiVersion` varchar(32);
+
+ALTER TABLE `zstack`.`CephOsdGroupVO` MODIFY COLUMN `osds` text NOT NULL;
