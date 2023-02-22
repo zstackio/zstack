fe326fef48ce3813720f81a10fad120b710317b1
AlanJager <ye.zou@zstack.io>
2023-02-22 13:43:28 +0800

    <fix>[utils]: verify cache and remove it if needed
    elaboration errors use a key-value cache and the ErrorCode's
     fmt is used as the key. For some general error fmt in code,
    like "operation error, because %s", if the elaboration caches
    the fmt into the cache, we need to check if the elaboration is
     matched with the error, remove invalid cache after verification.
    
    note: currently, we do not adding any other mechanism to
     support the argument-based elaboration search, because
    we believe that only errors with the same cause can occur
     over a period of time.
    
    Resolves: ZSTAC-46850
    
    Change-Id: I63636364676b7074767568636c7574696f797568
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- test/src/test/groovy/org/zstack/test/integration/core/RegexElaborationCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/RegexElaborationCase.groovy
@@ -41,6 +41,7 @@ class RegexElaborationCase extends SubCase {
         testElaboration6()
         testElaboration7()
         testElaboration8()
+        testElaboration7()
     }
 
     void testElaboration1() {
@@ -99,9 +100,23 @@ class RegexElaborationCase extends SubCase {
 
         ErrorCode result = err(PrimaryStorageErrors.ATTACH_ERROR, errorCodes, errorCodes.getDetails())
 
+        assert result.elaboration.trim().equals("错误信息: .*can not find vg .* and create vg with forceWipw=.*")
+        assert result.messages.message_cn.trim().equals("无法将主机上的共享块主存储加载到集群，因为存在原有数据，请勾选清理块设备并重试。")
+        assert result.messages.message_en.trim().equals("Could not attach shared block storage to cluster, because device is not empty. Please select the checkbox \"Clear LUN\" and try again.")
+
+        errCode1 = Platform.operr("operation error, because:.*can not find vg .* and create vg with forceWipw=.*") as ErrorCode
+        errCode2 = Platform.operr("operation error, because:.*can not find vg .* and create vg with forceWipw=.*") as ErrorCode
+
+        causes.clear()
+        causes.add(errCode1)
+        causes.add(errCode2)
+        errorCodes.setCauses(causes)
+
+        result = err(PrimaryStorageErrors.ATTACH_ERROR, errorCodes, errorCodes.getDetails())
         assert result.elaboration.trim().equals("错误信息: 无法将主机上的共享块主存储加载到集群，因为存在原有数据，请勾选清理块设备并重试。")
         assert result.messages.message_cn.trim().equals("无法将主机上的共享块主存储加载到集群，因为存在原有数据，请勾选清理块设备并重试。")
         assert result.messages.message_en.trim().equals("Could not attach shared block storage to cluster, because device is not empty. Please select the checkbox \"Clear LUN\" and try again.")
+
     }
 
     /**
