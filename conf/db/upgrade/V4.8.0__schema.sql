2502cd92ef3709799e992211c65b1968b85bec62
Lei Liu <lei.liu@zstack.io>
2023-02-23 14:46:40 +0800

    <fix>[conf]: Seperate encryption gateway plugin from block storage
    DBImpact
    
    Change-Id: I7475746567667a6168746c646175676e727a776f

---
+++ conf/db/upgrade/V4.8.0__schema.sql
@@ -0,0 +1,84 @@
+ALTER TABLE `zstack`.`BlockPrimaryStorageVO` DROP `encryptGatewayIp`;
+ALTER TABLE `zstack`.`BlockPrimaryStorageVO` DROP `encryptGatewayPort`;
+ALTER TABLE `zstack`.`BlockPrimaryStorageVO` DROP `encryptGatewayUsername`;
+ALTER TABLE `zstack`.`BlockPrimaryStorageVO` DROP `encryptGatewayPassword`;
+ALTER TABLE `zstack`.`BlockScsiLunVO` DROP `type`;
+ALTER TABLE `zstack`.`BlockScsiLunVO` MODIFY COLUMN `id` int unsigned default 0;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`BlockPrimaryStorageHostRefVO` (
+    `id` BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `initiatorName` varchar(256) DEFAULT NULL,
+    `metadata` text DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fkBlockPrimaryStorageHostRefVOPrimaryStorageHostRefVO` FOREIGN KEY (`id`) REFERENCES `zstack`.`PrimaryStorageHostRefVO` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER $$
+CREATE PROCEDURE checkAllBlockHostInPrimaryHostRef()
+    BEGIN
+        DECLARE hostUuid VARCHAR(32);
+        DECLARE primaryStorageUuid VARCHAR(32);
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE cur CURSOR FOR SELECT hiref.hostUuid, hiref.primaryStorageUuid FROM HostInitiatorRefVO hiref;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO hostUuid, primaryStorageUuid;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            IF (select count(*) from PrimaryStorageHostRefVO pshref where pshref.hostUuid = hostUuid and pshref.primaryStorageUuid = primaryStorageUuid) = 0 THEN
+                BEGIN
+                    INSERT INTO zstack.PrimaryStorageHostRefVO (`primaryStorageUuid`, `hostUuid`, `status`, `lastOpDate`, `createDate`) values (primaryStorageUuid, hostUuid, 'Disconnected', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+                END;
+            END IF;
+        END LOOP;
+        CLOSE cur;
+        SELECT CURTIME();
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE migrateBlockPrimaryHostRef()
+    BEGIN
+        DECLARE initiatorName VARCHAR(256);
+        DECLARE psId BIGINT(20);
+        DECLARE metadata text;
+        DECLARE psUuid VARCHAR(32);
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE cur CURSOR FOR SELECT  hostInitiatorRef.initiatorName, hostInitiatorRef.metadata, primaryStorageHostRef.id
+             FROM zstack.HostInitiatorRefVO hostInitiatorRef, zstack.PrimaryStorageHostRefVO primaryStorageHostRef
+             where hostInitiatorRef.hostUuid = primaryStorageHostRef.hostUuid and hostInitiatorRef.primaryStorageUuid = primaryStorageHostRef.primaryStorageUuid;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done =TRUE;
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO initiatorName, metadata, psId;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            IF (select count(*) from BlockPrimaryStorageHostRefVO bpshref where id = psId) = 0 THEN
+                BEGIN
+                    INSERT INTO zstack.BlockPrimaryStorageHostRefVO(id, initiatorName, metadata, lastOpDate, createDate) values(psId, initiatorName, metadata, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+                END;
+            END IF;
+        END LOOP;
+        CLOSE cur;
+        SELECT CURTIME();
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE checkHostInitiatorRefVO()
+    BEGIN
+        IF (SELECT count(*) FROM information_schema.columns WHERE table_name = 'HostInitiatorRefVO' AND column_name = 'primaryStorageUuid') != 0 THEN
+            call checkAllBlockHostInPrimaryHostRef();
+            call migrateBlockPrimaryHostRef();
+        END IF;
+    END $$
+DELIMITER ;
+call checkHostInitiatorRefVO();
+DROP PROCEDURE IF EXISTS migrateBlockPrimaryHostRef;
+DROP PROCEDURE IF EXISTS checkAllBlockHostInPrimaryHostRef;
+DROP PROCEDURE IF EXISTS checkHostInitiatorRefVO;
+DROP TABLE HostInitiatorRefVO;
