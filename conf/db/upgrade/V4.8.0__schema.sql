4d45792ae1cd1fe2394e5e9a040751de2e468481
Lei Liu <lei.liu@zstack.io>
2023-02-23 14:46:40 +0800

    <fix>[conf]: Seperate encryption gateway plugin from block storage
    DBImpact
    
    Change-Id: I7475746567667a6168746c646175676e727a776f

--- conf/db/upgrade/V4.8.0__schema.sql
+++ conf/db/upgrade/V4.8.0__schema.sql
@@ -73,3 +73,120 @@ update EventSubscriptionVO set name = 'Host Physical Disk Insert Triggered' wher
 update EventSubscriptionVO set name = 'Host Physical Disk Status Abnormal' where uuid = '4b04f06e4ba24231ad67bd6f06093ba2';
 update EventSubscriptionVO set name = 'Host Physical Cpu Status Abnormal' where uuid = '8186fbbeab1d449b93e7be78d6045c7f';
 update EventSubscriptionVO set name = 'Host Physical Fan Status Abnormal' where uuid = '37e5bdfa2eaf49538931113ddaecf927';
+
+CREATE TABLE IF NOT EXISTS `zstack`.`BlockPrimaryStorageHostRefVO` (
+    `id` BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    `initiatorName` varchar(256) DEFAULT NULL,
+    `metadata` text DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `fkBlockPrimaryStorageHostRefVOPrimaryStorageHostRefVO` FOREIGN KEY (`id`) REFERENCES `zstack`.`PrimaryStorageHostRefVO` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CALL DROP_COLUMN('BlockPrimaryStorageVO', 'encryptGatewayIp');
+CALL DROP_COLUMN('BlockPrimaryStorageVO', 'encryptGatewayPort');
+CALL DROP_COLUMN('BlockPrimaryStorageVO', 'encryptGatewayUsername');
+CALL DROP_COLUMN('BlockPrimaryStorageVO', 'encryptGatewayPassword');
+CALL DROP_COLUMN('BlockScsiLunVO', 'type');
+
+DROP PROCEDURE IF EXISTS `AlterBlockScsiLunTable`;
+DELIMITER $$
+CREATE PROCEDURE AlterBlockScsiLunTable()
+    BEGIN
+        IF EXISTS( SELECT NULL
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE table_name = 'BlockScsiLunVO'
+                            AND table_schema = 'zstack'
+                            AND column_name = 'id'
+                            AND column_type like 'smallint%') THEN
+            ALTER TABLE `zstack`.`BlockScsiLunVO` MODIFY COLUMN `id` int unsigned default 0;
+        END IF;
+        IF EXISTS( SELECT NULL
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE table_name = 'BlockScsiLunVO'
+                            AND table_schema = 'zstack'
+                            AND column_name = 'lunType'
+                            AND is_nullable = 'YES') THEN
+            ALTER TABLE `zstack`.`BlockScsiLunVO` MODIFY COLUMN lunType varchar(256) DEFAULT NULL;
+        END IF;
+        IF EXISTS( SELECT NULL
+                    FROM INFORMATION_SCHEMA.COLUMNS
+                    WHERE table_name = 'BlockScsiLunVO'
+                            AND table_schema = 'zstack'
+                            AND column_name = 'lunMapId'
+                            AND column_type like 'smallint%') THEN
+            ALTER TABLE `zstack`.`BlockScsiLunVO` MODIFY COLUMN `lunMapId` int unsigned default 0;
+        END IF;
+    END $$
+DELIMITER ;
+call AlterBlockScsiLunTable;
+DROP PROCEDURE IF EXISTS `AlterBlockScsiLunTable`;
+
+DELIMITER $$
+CREATE PROCEDURE checkAllBlockHostInPrimaryHostRef()
+    BEGIN
+        DECLARE hostUuid VARCHAR(32);
+        DECLARE primaryStorageUuid VARCHAR(32);
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE cur CURSOR FOR SELECT hiref.hostUuid, hiref.primaryStorageUuid FROM HostInitiatorRefVO hiref;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO hostUuid, primaryStorageUuid;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            IF (select count(*) from PrimaryStorageHostRefVO pshref where pshref.hostUuid = hostUuid and pshref.primaryStorageUuid = primaryStorageUuid) = 0 THEN
+                BEGIN
+                    INSERT INTO zstack.PrimaryStorageHostRefVO (`primaryStorageUuid`, `hostUuid`, `status`, `lastOpDate`, `createDate`) values (primaryStorageUuid, hostUuid, 'Disconnected', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+                END;
+            END IF;
+        END LOOP;
+        CLOSE cur;
+        SELECT CURTIME();
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE migrateBlockPrimaryHostRef()
+    BEGIN
+        DECLARE initiatorName VARCHAR(256);
+        DECLARE psId BIGINT(20);
+        DECLARE metadata text;
+        DECLARE psUuid VARCHAR(32);
+        DECLARE done INT DEFAULT FALSE;
+        DECLARE cur CURSOR FOR SELECT  hostInitiatorRef.initiatorName, hostInitiatorRef.metadata, primaryStorageHostRef.id
+             FROM zstack.HostInitiatorRefVO hostInitiatorRef, zstack.PrimaryStorageHostRefVO primaryStorageHostRef
+             where hostInitiatorRef.hostUuid = primaryStorageHostRef.hostUuid and hostInitiatorRef.primaryStorageUuid = primaryStorageHostRef.primaryStorageUuid;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET done =TRUE;
+        OPEN cur;
+        read_loop: LOOP
+            FETCH cur INTO initiatorName, metadata, psId;
+            IF done THEN
+                LEAVE read_loop;
+            END IF;
+            IF (select count(*) from BlockPrimaryStorageHostRefVO bpshref where id = psId) = 0 THEN
+                BEGIN
+                    INSERT INTO zstack.BlockPrimaryStorageHostRefVO(id, initiatorName, metadata, lastOpDate, createDate) values(psId, initiatorName, metadata, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+                END;
+            END IF;
+        END LOOP;
+        CLOSE cur;
+        SELECT CURTIME();
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE checkHostInitiatorRefVO()
+    BEGIN
+        IF (SELECT count(*) FROM information_schema.columns WHERE table_name = 'HostInitiatorRefVO' AND column_name = 'primaryStorageUuid') != 0 THEN
+            call checkAllBlockHostInPrimaryHostRef();
+            call migrateBlockPrimaryHostRef();
+        END IF;
+    END $$
+DELIMITER ;
+call checkHostInitiatorRefVO();
+DROP PROCEDURE IF EXISTS migrateBlockPrimaryHostRef;
+DROP PROCEDURE IF EXISTS checkAllBlockHostInPrimaryHostRef;
+DROP PROCEDURE IF EXISTS checkHostInitiatorRefVO;
+DROP TABLE IF EXISTS HostInitiatorRefVO;
