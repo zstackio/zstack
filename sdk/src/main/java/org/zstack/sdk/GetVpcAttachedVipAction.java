675f1f5ab14d426364a917724e52e3b9b3d52091
yingzhe.hu <yinghe.hu@zstack.io>
2023-02-28 18:44:38 +0800

    <fix>[externalmanagement]: update sdk
    Resolves: ZSTAC-48871
    
    Change-Id: I7268736476716e6365797171636f616771727173

--- sdk/src/main/java/org/zstack/sdk/GetVpcAttachedVipAction.java
+++ sdk/src/main/java/org/zstack/sdk/GetVpcAttachedVipAction.java
@@ -35,6 +35,9 @@ public class GetVpcAttachedVipAction extends AbstractAction {
     public java.lang.Integer start = 0;
 
     @Param(required = false)
+    public java.lang.String order = "asc";
+
+    @Param(required = false)
     public java.util.List systemTags;
 
     @Param(required = false)
