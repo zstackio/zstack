1bc68393a8d6bf6a13e6df3d9e4499132bee9288
ZqyDaoDao <qiuyu.zhang@zstack.io>
2023-03-14 18:15:00 +0800

    <fix>[encrypt]:encryption goes globally configured uniformly
    Resolves: ZSTAC-53998
    
    Change-Id: I79716678686b766f6268666c73696d62796d696b

--- core/src/main/java/org/zstack/core/encrypt/EncryptFacadeImpl.java
+++ core/src/main/java/org/zstack/core/encrypt/EncryptFacadeImpl.java
@@ -238,16 +238,12 @@ public class EncryptFacadeImpl implements EncryptFacade, Component {
         EncryptGlobalConfig.ENABLE_PASSWORD_ENCRYPT.installLocalBeforeUpdateExtension(new GlobalConfigBeforeUpdateExtensionPoint() {
             @Override
             public void beforeUpdateExtensionPoint(GlobalConfig oldConfig, String newValue) {
-                //encryption cannot be changed
-                if (PasswordEncryptType.SecurityResourceEncryption.toString().equals(oldConfig.value())) {
-                    throw new OperationFailureException(operr("SecurityResourceEncryption cannot be change"));
-                }
-
                 // avoid encrypt twice need to do synchronized encrypted(decrypted)
                 // e.g. use javax.persistence.Query set password to update encrypt password
                 // when PasswordConverter check this value is true the password will be encrypt
                 // again before persist, so this beforeUpdateExtension is necessary
-                if (PasswordEncryptType.LocalEncryption.toString().equals(newValue)) {
+                if (PasswordEncryptType.LocalEncryption.toString().equals(newValue)
+                        || PasswordEncryptType.SecurityResourceEncryption.toString().equals(newValue)) {
                     encryptAllPassword();
                 }
             }
