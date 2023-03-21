131fe700f439405c669f84e3807e983565533316
mingmin.wen <mingmin.wen@zstack.io>
2023-03-21 11:07:36 +0800

    <fix>[db]: modify table field type of NUMA node cpus
    DBImpact
    
    1. not enough length for saving host NUMA node cpus string by using
    field type varchar(512) when a NUMA node has more than 150 cpus;
    2. modify table HostNumaNodeVO field nodeCPUs type to text, the previous
    is varchar(512);
    3. modify table VmInstanceNumaNodeVO field vNodeCPUs type to text, the
    previous is varchar(512);
    
    Resolves: ZSTAC-53996
    
    Change-Id: I72786c75646a616c7065736d676467787a726670

--- conf/db/upgrade/V4.6.21__schema.sql
+++ conf/db/upgrade/V4.6.21__schema.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `zstack`.`SNSTopicVO` ADD COLUMN `locale` varchar(32);
+
+ALTER TABLE `zstack`.`HostNumaNodeVO` MODIFY COLUMN `nodeCPUs` TEXT NOT NULL;
+ALTER TABLE `zstack`.`VmInstanceNumaNodeVO` MODIFY COLUMN `vNodeCPUs` TEXT NOT NULL;
