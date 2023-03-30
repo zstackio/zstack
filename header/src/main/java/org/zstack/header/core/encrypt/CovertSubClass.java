f5c49fa38d3cc315a69c8d794d7874a10d4b8561
qiuyu.zhang <qiuyu.zhang@zstack.io>
2023-03-30 11:57:22 +0800

    <fix>[encrypt]: decrypt all data before recovery
    DBImpact
    
    Resolves: ZSTAC-53998
    
    Change-Id: I6e65747677766f67756e6e7a797a687772726975

--- header/src/main/java/org/zstack/header/core/encrypt/CovertSubClass.java
+++ header/src/main/java/org/zstack/header/core/encrypt/CovertSubClass.java
@@ -13,6 +13,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CovertSubClass {
     String classSimpleName();
-    String columnName();
-    String columnValue();
+    String columnName() default "";
+    String columnValue() default "";
 }
