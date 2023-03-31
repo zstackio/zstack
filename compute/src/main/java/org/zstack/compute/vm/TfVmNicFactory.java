8b872753f18104935a1f680e7495b344a942ff7f
shixin.ruan <shixin.ruan@zstack.io>
2023-03-31 15:16:40 +0800

    <fix>[sugonSdnController]: commited by fuwei from sugon
    1.调整vrouter-agent delete api调用时机
    原有逻辑：
    虚机关机和删除不调用vrouter-agent delete port api，
    彻底删除时才调用，导致回收站中的虚机恢复时，
    有概率在vrouter agent中产生ip冲突
    现在改为：
    虚机关机和删除均调用vrouter-agent delete port api
    2.tf port垃圾回收功能增加限制：
    每个周期最多删除10个tf port
    3.zstack创建子账户时，对应创建的tf project的name
    改为uuid，以防多个zstack对接一个tf用户名冲突
    
    Resolves/Related: ZSTAC-58727
    
    Change-Id: I62756a6d6e6a79706376656463687974666b6869
    (cherry picked from commit 93a299e7c1a558418607ebf9ee000a7be06602fc)

--- compute/src/main/java/org/zstack/compute/vm/TfVmNicFactory.java
+++ compute/src/main/java/org/zstack/compute/vm/TfVmNicFactory.java
@@ -1,16 +1,9 @@
 package org.zstack.compute.vm;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.zstack.core.asyncbatch.While;
 import org.zstack.core.cloudbus.CloudBus;
-import org.zstack.core.cloudbus.CloudBusCallBack;
 import org.zstack.core.db.DatabaseFacade;
-import org.zstack.header.core.WhileDoneCompletion;
 import org.zstack.header.core.workflow.FlowException;
-import org.zstack.header.errorcode.ErrorCodeList;
-import org.zstack.header.host.DetachNicFromVmOnHypervisorMsg;
-import org.zstack.header.host.HostConstant;
-import org.zstack.header.message.MessageReply;
 import org.zstack.header.network.l3.UsedIpInventory;
 import org.zstack.header.network.l3.UsedIpVO;
 import org.zstack.header.vm.*;
@@ -24,7 +17,7 @@ import java.util.List;
 import static org.zstack.core.Platform.err;
 
 
-public class TfVmNicFactory extends VmNicFactory implements VmJustBeforeDeleteFromDbExtensionPoint {
+public class TfVmNicFactory extends VmNicFactory {
     private static final CLogger logger = Utils.getLogger(TfVmNicFactory.class);
     private static final VmNicType type = new VmNicType(VmInstanceConstant.TF_VIRTUAL_NIC_TYPE);
     @Autowired
@@ -60,36 +53,5 @@ public class TfVmNicFactory extends VmNicFactory implements VmJustBeforeDeleteFr
         vnic = dbf.reload(vnic);
         spec.getDestNics().add(VmNicInventory.valueOf(vnic));
         return vnic;
-    }
-
-    @Override
-    public void vmJustBeforeDeleteFromDb(VmInstanceInventory inv) {
-        new While<>(inv.getVmNics()).each((nic, wcompl) -> {
-            DetachNicFromVmOnHypervisorMsg msg = new DetachNicFromVmOnHypervisorMsg();
-            //The host uuid is null, because teh vm is deleted
-            msg.setHostUuid(inv.getLastHostUuid());
-            msg.setVmInstanceUuid(inv.getUuid());
-            msg.setNic(nic);
-            bus.makeTargetServiceIdByResourceUuid(msg, HostConstant.SERVICE_ID, msg.getHostUuid());
-            bus.send(msg, new CloudBusCallBack(wcompl) {
-                @Override
-                public void run(MessageReply reply) {
-                    if (!reply.isSuccess()) {
-                        wcompl.addError(reply.getError());
-                        return;
-                    }
-                    wcompl.done();
-                }
-            });
-        }).run(new WhileDoneCompletion(null) {
-            @Override
-            public void done(ErrorCodeList errorCodeList) {
-                if (errorCodeList.getCauses().isEmpty()) {
-                    logger.info("Notify vrouter success before vm expunged.");
-                } else {
-                    logger.error("Notify vrouter fail before vm expunged, error code: " + errorCodeList.getCauses().get(0));
-                }
-            }
-        });
     }
 }
