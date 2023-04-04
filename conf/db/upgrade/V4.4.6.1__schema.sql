10f1073ffbbdaf4e2d24ddf202a6679367ab83b5
Wang,Qilin <qilin.wang@zstack.io>
2023-04-04 10:25:37 +0800

    <fix>[conf]: migrate schema.sql
    migrate schema.sql
    
    DBImpact
    
    Resolves: ZSTAC-
    
    Change-Id: I6b6b6a70746f656e67647376777563726d666277

--- conf/db/upgrade/V4.4.6.1__schema.sql
+++ conf/db/upgrade/V4.4.6.1__schema.sql
@@ -1,11 +1,16 @@
 CREATE TABLE IF NOT EXISTS `zstack`.`SNSSmsPlatformVO`
 (
     `uuid` varchar(32) NOT NULL UNIQUE,
-    `username` varchar(32) NOT NULL,
-    `password` varchar(32) NOT NULL,
+    `node1Ip` varchar(32) NOT NULL,
+    `node2Ip` varchar(32) NOT NULL,
+    `username1` varchar(32) NOT NULL,
+    `username2` varchar(32) NOT NULL,
+    `password1` varchar(32) NOT NULL,
+    `password2` varchar(32) NOT NULL,
     `serverIp` varchar(32) NOT NULL,
     `srcID` varchar(32) NOT NULL,
-    `downlinkChannelNumber` varchar(32) NOT NULL,
+    `downlinkChannelNumber1` varchar(32) NOT NULL,
+    `downlinkChannelNumber2` varchar(32) NOT NULL,
     PRIMARY KEY  (`uuid`),
     CONSTRAINT fkSNSSmsPlatformVOSNSApplicationPlatformVO FOREIGN KEY (uuid) REFERENCES SNSApplicationPlatformVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
