e241279c1965eda06171dfba5462f8e89769c065
hanyu.liang <hanyu.liang@zstack.io>
2023-04-18 15:15:47 +0800

    <fix>[encrypt]: Fix encrypted field cephAO subclass omission
    Resolves: ZSTAC-54828
    
    Change-Id: I6e65741237766f67756e6e7a797a687772726975

--- header/src/main/java/org/zstack/header/core/encrypt/CovertSubClass.java
+++ header/src/main/java/org/zstack/header/core/encrypt/CovertSubClass.java
@@ -13,6 +13,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CovertSubClass {
     String classSimpleName();
-    String columnName() default "";
-    String columnValue() default "";
+    String columnName();
+    String columnValue();
 }
