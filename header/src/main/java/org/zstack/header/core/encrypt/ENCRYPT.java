357c2c9c843d912e0359a7490e69a2a041c870ce
hanyu.liang <hanyu.liang@zstack.io>
2023-04-19 11:31:49 +0800

    Revert "Merge branch 'fix-54828@@2' into '4.6.31'"
    This reverts merge request !3854

--- header/src/main/java/org/zstack/header/core/encrypt/ENCRYPT.java
+++ header/src/main/java/org/zstack/header/core/encrypt/ENCRYPT.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 /**
  * Created by mingjian.deng on 16/11/1.
  */
-@Deprecated
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ENCRYPT {
