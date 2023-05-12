9b9c526ea8c1986b9b0003d66410cc7e616de4a6
Zhang Wenhao <wenhao.zhang@zstack.io>
2023-05-12 16:11:22 +0800

    <fix>[protal]: modify ApiMessageInterceptor position
    Use the position of ApiMessageInterceptor
    as the only basis for sorting.
    
    Related: ZSTAC-51240
    Related: ZSTAC-55369
    
    Change-Id: I6c76636a746473777678616a6d647577706a786f

--- header/src/main/java/org/zstack/header/apimediator/GlobalApiMessageInterceptor.java
+++ header/src/main/java/org/zstack/header/apimediator/GlobalApiMessageInterceptor.java
@@ -4,24 +4,16 @@ import java.util.List;
 
 public interface GlobalApiMessageInterceptor extends ApiMessageInterceptor {
     enum InterceptorPosition {
-        FIRST(-5000),
-        SYSTEM(-3000),
-        FRONT(-1000),
-        END(5000);
-
-        public final int priority;
-        InterceptorPosition(int priority) {
-            this.priority = priority;
-        }
+        // In execution order
+        SYSTEM, FRONT, DEFAULT, END
     }
 
     List<Class> getMessageClassToIntercept();
 
+    /**
+     * Sort by position, then {@link #getPriority()}
+     */
     default InterceptorPosition getPosition() {
         return InterceptorPosition.FRONT;
-    }
-
-    default int getPriority() {
-        return getPosition().priority;
     }
 }
