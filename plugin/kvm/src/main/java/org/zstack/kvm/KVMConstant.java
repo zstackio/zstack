1bb4828065aac82726408b40dcfb28ea779f75ab
AlanJager <ye.zou@zstack.io>
2023-05-15 17:50:21 +0800

    <fix>[vm]: add vm no-state event and state
    allow stop/reboot and console for no state vm
    
    Resolves: ZSTAC-56635
    
    Change-Id: I69787578697770636666726b6a75696e6d67736e
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMConstant.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMConstant.java
@@ -145,6 +145,8 @@ public interface KVMConstant {
                 return VmInstanceState.Paused;
             } else if (this == Crashed) {
                 return VmInstanceState.Crashed;
+            } else if (this == NoState) {
+                return VmInstanceState.NoState;
             } else {
                 return VmInstanceState.Unknown;
             }
