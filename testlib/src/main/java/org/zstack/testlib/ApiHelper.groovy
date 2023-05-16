351f51b76a88b9c7d14e90d7def1ad635790594b
shenjin <jin.shen@zstack.io>
2023-05-16 14:46:06 +0800

    <improvement>[zwatch]: add change event subscription state api
    update sdk and apihelper
    
    Resolves: ZSTAC-55461
    
    Change-Id: I63766874646a786c6672737469777266786b6673

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -44764,6 +44764,33 @@ abstract class ApiHelper {
     }
 
 
+    def changeEventSubscriptionState(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zwatch.alarm.ChangeEventSubscriptionStateAction.class) Closure c) {
+        def a = new org.zstack.sdk.zwatch.alarm.ChangeEventSubscriptionStateAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def createAlarm(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.zwatch.alarm.CreateAlarmAction.class) Closure c) {
         def a = new org.zstack.sdk.zwatch.alarm.CreateAlarmAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
