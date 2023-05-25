83cc242e66eff4e819684283ddc5ba262daffeb2
xinhao.huang <xinhao.huang@zstack.io>
2023-05-25 13:34:05 +0800

    <feature>[sdk]: chrony time server manager
    Resolves: ZHCI-1805
    
    Change-Id: I7a657175796279656965736869636b7961787765

---
+++ sdk/src/main/java/org/zstack/sdk/GetChronyServersResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetChronyServersResult {
+    public java.util.List servers;
+    public void setServers(java.util.List servers) {
+        this.servers = servers;
+    }
+    public java.util.List getServers() {
+        return this.servers;
+    }
+
+}
