b5a9eec8b865d4e5fb7c22c9d6eeab181920cbd4
AlanJager <ye.zou@zstack.io>
2023-05-31 14:16:25 +0800

    <fix>[utils]: handle return code 255 of sshpass
    During host connection, ZStack attempts to access the host using the
     username and password via sshpass to test if the management node can
     access the host and if the host can callback to the management node.
     However, private keys are not used for compatibility reasons, as both
     new and previously added hosts use the same code to perform the check.
    
    According to sshpass's man page, a return code of 5 indicates an invalid
     or incorrect password, while all other errors return 255 to avoid
     man-in-the-middle attacks. Therefore, when encountering a return code
     of 255, it should be reported as an SSH failure rather than a failure
     to perform callback access. Otherwise, this could mislead the
     user during host connection.
    
    Resolves: ZSTAC-55909
    
    Change-Id: I62736c7779636375636e67726769697a6d636577

--- utils/src/main/java/org/zstack/utils/ssh/SshShell.java
+++ utils/src/main/java/org/zstack/utils/ssh/SshShell.java
@@ -63,7 +63,8 @@ public class SshShell {
             sret.setStdout(ret.getStdout());
             if (sret.getReturnCode() == 255 && privateKeyFile != null) {
                 sret.setSshFailure(true);
-            } else if (sret.getReturnCode() == 5 && privateKeyFile == null) {
+            } else if ((sret.getReturnCode() == 5 || sret.getReturnCode() == 255)
+                    && privateKeyFile == null) {
                 sret.setSshFailure(true);
             }
             return sret;
