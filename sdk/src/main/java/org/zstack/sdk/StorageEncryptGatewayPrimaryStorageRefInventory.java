43f37e5b67ee135a4281c65ce1cc87c0a93224aa
Xingwei Yu <xingwei.yu@zstack.io>
2023-06-07 14:59:04 +0800

    <fix>[conf]: Add attach and detach storage encrypt gateway to ps api
    - add sdk, apihelper and doc
    - add StorageEncryptGatewayPrimaryStorageRefVO
    
    Resolves: ZSTAC-54733
    
    Change-Id: I6b6f6567626d726c61637462737475646c686479

---
+++ sdk/src/main/java/org/zstack/sdk/StorageEncryptGatewayPrimaryStorageRefInventory.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk;
+
+
+
+public class StorageEncryptGatewayPrimaryStorageRefInventory  {
+
+    public java.lang.String storageEncryptGatewayUuid;
+    public void setStorageEncryptGatewayUuid(java.lang.String storageEncryptGatewayUuid) {
+        this.storageEncryptGatewayUuid = storageEncryptGatewayUuid;
+    }
+    public java.lang.String getStorageEncryptGatewayUuid() {
+        return this.storageEncryptGatewayUuid;
+    }
+
+    public java.lang.String primaryStorageUuid;
+    public void setPrimaryStorageUuid(java.lang.String primaryStorageUuid) {
+        this.primaryStorageUuid = primaryStorageUuid;
+    }
+    public java.lang.String getPrimaryStorageUuid() {
+        return this.primaryStorageUuid;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
