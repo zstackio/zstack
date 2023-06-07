7236333e157f233ffaa8f54f104ebd166e0ffacd
Xingwei Yu <xingwei.yu@zstack.io>
2023-06-07 14:59:04 +0800

    <fix>[conf]: Add attach and detach storage encrypt gateway to ps api
    - add sdk, apihelper and doc
    - add StorageEncryptGatewayPrimaryStorageRefVO
    
    Resolves: ZSTAC-54733
    
    Change-Id: I6b6f6567626d726c61637462737475646c686479

---
+++ sdk/src/main/java/org/zstack/sdk/DetachStorageEncryptGatewayFromPrimaryStorageResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+import org.zstack.sdk.StorageEncryptGatewayPrimaryStorageRefInventory;
+
+public class DetachStorageEncryptGatewayFromPrimaryStorageResult {
+    public StorageEncryptGatewayPrimaryStorageRefInventory inventory;
+    public void setInventory(StorageEncryptGatewayPrimaryStorageRefInventory inventory) {
+        this.inventory = inventory;
+    }
+    public StorageEncryptGatewayPrimaryStorageRefInventory getInventory() {
+        return this.inventory;
+    }
+
+}
