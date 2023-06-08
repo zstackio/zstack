d3ce2f4d076d818e3e843570646c50736b06974f
dengsong.yang <dengsong.yang@zstack.io>
2023-06-08 17:49:38 +0800

    fix code

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHostPreConnectExtensionPoint.java
+++
@@ -1,7 +0,0 @@
-package org.zstack.kvm;
-
-import org.zstack.header.core.workflow.Flow;
-
-public interface KVMHostPreConnectExtensionPoint {
-    Flow createKvmHostPreConnectingFlow(KVMHostConnectedContext context);
-}
