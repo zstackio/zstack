fdbc741e82d506b4708a3cd57b7d5a4d44fbe292
AlanJager <ye.zou@zstack.io>
2023-06-20 11:20:21 +0800

    <fix>[compute]: fix host numa node update after reconnect
    Use Objects.equals() instead of "=="
    
    Add log trace for numa node update
    
    Resolves: ZSTAC-55952
    
    Change-Id: I656c64686161727a6b797a767665726a76686d65
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- compute/src/main/java/org/zstack/compute/host/PostHostExtensionPointForNuma.java
+++ compute/src/main/java/org/zstack/compute/host/PostHostExtensionPointForNuma.java
@@ -9,15 +9,23 @@ import org.zstack.core.db.SQL;
 import org.zstack.header.core.workflow.Flow;
 import org.zstack.header.core.workflow.FlowTrigger;
 import org.zstack.header.core.workflow.NoRollbackFlow;
-import org.zstack.header.host.*;
+import org.zstack.header.host.GetHostNumaTopologyMsg;
+import org.zstack.header.host.GetHostNumaTopologyReply;
+import org.zstack.header.host.HostConstant;
+import org.zstack.header.host.HostInventory;
+import org.zstack.header.host.HostNUMANode;
+import org.zstack.header.host.HostNumaNodeVO;
+import org.zstack.header.host.HostNumaNodeVO_;
 import org.zstack.header.message.MessageReply;
 import org.zstack.utils.Utils;
+import org.zstack.utils.gson.JSONObjectUtil;
 import org.zstack.utils.logging.CLogger;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class PostHostExtensionPointForNuma implements PostHostConnectExtensionPoint {
     @Autowired
@@ -51,6 +59,7 @@ public class PostHostExtensionPointForNuma implements PostHostConnectExtensionPo
                                 return;
                             }
 
+                            // {"0":{"distance":["10"],"cpus":["0","1","2","3"],"free":16717463552,"size":21474275328}
                             List<HostNumaNodeVO> oldHostNumaNodes = Q.New(HostNumaNodeVO.class)
                                     .eq(HostNumaNodeVO_.hostUuid, host.getUuid())
                                     .list();
@@ -68,19 +77,40 @@ public class PostHostExtensionPointForNuma implements PostHostConnectExtensionPo
 
                             oldHostNumaNodes.stream()
                                     .filter(oldNumaNode -> newHostNumaNodes.stream()
-                                            .noneMatch(newNumaNode -> newNumaNode.getNodeCPUs().equals(oldNumaNode.getNodeCPUs())
-                                                    && newNumaNode.getNodeDistance().equals(oldNumaNode.getNodeDistance())
-                                                    && newNumaNode.getNodeMemSize() == oldNumaNode.getNodeMemSize()
-                                                    && newNumaNode.getNodeID() == oldNumaNode.getNodeID()))
-                                    .forEach(oldNumaNode -> dbf.removeByPrimaryKey(oldNumaNode.getId(), HostNumaNodeVO.class));
+                                            .noneMatch(newNumaNode -> {
+                                                logger.trace(String.format("oldNumaNode.getNodeCPUs(): %s, newNumaNode.getNodeCPUs(): %s, equals: %s", oldNumaNode.getNodeCPUs(), newNumaNode.getNodeCPUs(), newNumaNode.getNodeCPUs().equals(oldNumaNode.getNodeCPUs())));
+                                                logger.trace(String.format("oldNumaNode.getNodeDistance(): %s, newNumaNode.getNodeDistance(): %s, equals: %s", oldNumaNode.getNodeDistance(), newNumaNode.getNodeDistance(), newNumaNode.getNodeDistance().equals(oldNumaNode.getNodeDistance())));
+                                                logger.trace(String.format("oldNumaNode.getNodeMemSize(): %s, newNumaNode.getNodeMemSize(): %s, equals: %s", oldNumaNode.getNodeMemSize(), newNumaNode.getNodeMemSize(), Objects.equals(newNumaNode.getNodeMemSize(), oldNumaNode.getNodeMemSize())));
+                                                logger.trace(String.format("oldNumaNode.getNodeID(): %s, newNumaNode.getNodeID(): %s, equals: %s", oldNumaNode.getNodeID(), newNumaNode.getNodeID(), Objects.equals(newNumaNode.getNodeID(), oldNumaNode.getNodeID())));
+
+                                                return newNumaNode.getNodeCPUs().equals(oldNumaNode.getNodeCPUs())
+                                                        && newNumaNode.getNodeDistance().equals(oldNumaNode.getNodeDistance())
+                                                        && Objects.equals(newNumaNode.getNodeMemSize(), oldNumaNode.getNodeMemSize())
+                                                        && Objects.equals(newNumaNode.getNodeID(), oldNumaNode.getNodeID());
+                                            }))
+                                    .forEach(oldNumaNode -> {
+                                        logger.trace("remove old numa node: " + JSONObjectUtil.toJsonString(oldNumaNode));
+
+                                        dbf.removeByPrimaryKey(oldNumaNode.getId(), HostNumaNodeVO.class);
+                                    });
 
                             newHostNumaNodes.stream()
                                     .filter(newNumaNode -> oldHostNumaNodes.stream()
-                                            .noneMatch(oldNumaNode -> oldNumaNode.getNodeCPUs().equals(newNumaNode.getNodeCPUs())
-                                                    && oldNumaNode.getNodeDistance().equals(newNumaNode.getNodeDistance())
-                                                    && oldNumaNode.getNodeMemSize() == newNumaNode.getNodeMemSize()
-                                                    && oldNumaNode.getNodeID() == newNumaNode.getNodeID()))
-                                    .forEach(newNumaNode -> dbf.persist(newNumaNode));
+                                            .noneMatch(oldNumaNode -> {
+                                                logger.trace(String.format("oldNumaNode.getNodeCPUs(): %s, newNumaNode.getNodeCPUs(): %s, equals: %s", oldNumaNode.getNodeCPUs(), newNumaNode.getNodeCPUs(), newNumaNode.getNodeCPUs().equals(oldNumaNode.getNodeCPUs())));
+                                                logger.trace(String.format("oldNumaNode.getNodeDistance(): %s, newNumaNode.getNodeDistance(): %s, equals: %s", oldNumaNode.getNodeDistance(), newNumaNode.getNodeDistance(), newNumaNode.getNodeDistance().equals(oldNumaNode.getNodeDistance())));
+                                                logger.trace(String.format("oldNumaNode.getNodeMemSize(): %s, newNumaNode.getNodeMemSize(): %s, equals: %s", oldNumaNode.getNodeMemSize(), newNumaNode.getNodeMemSize(), Objects.equals(newNumaNode.getNodeMemSize(), oldNumaNode.getNodeMemSize())));
+                                                logger.trace(String.format("oldNumaNode.getNodeID(): %s, newNumaNode.getNodeID(): %s, equals: %s", oldNumaNode.getNodeID(), newNumaNode.getNodeID(), Objects.equals(newNumaNode.getNodeID(), oldNumaNode.getNodeID())));
+
+                                                return oldNumaNode.getNodeCPUs().equals(newNumaNode.getNodeCPUs())
+                                                        && oldNumaNode.getNodeDistance().equals(newNumaNode.getNodeDistance())
+                                                        && Objects.equals(oldNumaNode.getNodeMemSize(), newNumaNode.getNodeMemSize())
+                                                        && Objects.equals(oldNumaNode.getNodeID(), newNumaNode.getNodeID());
+                                            }))
+                                    .forEach(newNumaNode -> {
+                                        logger.trace("persist newNumaNode: " + JSONObjectUtil.toJsonString(newNumaNode));
+                                        dbf.persist(newNumaNode);
+                                    });
                             
                             logger.info(String.format("Update Host[%s] NUMA Topology Successfully!", host.getUuid()));
                             trigger.next();
