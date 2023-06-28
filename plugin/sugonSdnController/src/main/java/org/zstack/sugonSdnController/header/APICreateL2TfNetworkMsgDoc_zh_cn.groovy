49a7079898dc7a16795d3ef66508db41a9ffe3fc
shixin.ruan <shixin.ruan@zstack.io>
2023-06-28 15:03:33 +0800

    <test>[testlib]: fix sdk error
    Resolves: ZSTAC-59629
    
    Change-Id: I71696a68726c70787362707a7773726b72707a61
    (cherry picked from commit ed972da6d304e3d99ddd5378ec584ce6242b746e)

--- plugin/sugonSdnController/src/main/java/org/zstack/sugonSdnController/header/APICreateL2TfNetworkMsgDoc_zh_cn.groovy
+++ plugin/sugonSdnController/src/main/java/org/zstack/sugonSdnController/header/APICreateL2TfNetworkMsgDoc_zh_cn.groovy
@@ -3,11 +3,11 @@ package org.zstack.sugonSdnController.header
 import org.zstack.header.network.l2.APICreateL2NetworkEvent
 
 doc {
-    title "CreateL2TfNetworkMsg"
+    title "CreateL2TfNetwork"
 
     category "network.l2"
 
-    desc """创建Tf二层网络"""
+    desc """在这里填写API描述"""
 
     rest {
         request {
@@ -20,6 +20,25 @@ doc {
             desc """"""
             
 			params {
+
+				column {
+					name "ipPrefix"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional true
+					since "4.8.0"
+				}
+				column {
+					name "ipPrefixLength"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "Integer"
+					optional true
+					since "4.8.0"
+				}
 				column {
 					name "name"
 					enclosedIn "params"
@@ -27,8 +46,7 @@ doc {
 					location "body"
 					type "String"
 					optional false
-					since "4.3"
-					
+					since "4.8.0"
 				}
 				column {
 					name "description"
@@ -37,8 +55,7 @@ doc {
 					location "body"
 					type "String"
 					optional true
-					since "4.3"
-					
+					since "4.8.0"
 				}
 				column {
 					name "zoneUuid"
@@ -47,28 +64,71 @@ doc {
 					location "body"
 					type "String"
 					optional false
-					since "4.3"
-					
+					since "4.8.0"
 				}
 				column {
-					name "ipPrefix"
+					name "physicalInterface"
 					enclosedIn "params"
-					desc "ip prefix"
+					desc ""
 					location "body"
 					type "String"
 					optional false
-					since "4.3"
-
+					since "4.8.0"
 				}
 				column {
-					name "ipPrefixLength"
+					name "type"
 					enclosedIn "params"
-					desc "ip prefix length"
+					desc ""
 					location "body"
-					type "Integer"
-					optional false
-					since "4.3"
-
+					type "String"
+					optional true
+					since "4.8.0"
+				}
+				column {
+					name "vSwitchType"
+					enclosedIn "params"
+					desc ""
+					location "body"
+					type "String"
+					optional true
+					since "4.8.0"
+					values ("LinuxBridge","OvsDpdk","MacVlan")
+				}
+				column {
+					name "resourceUuid"
+					enclosedIn "params"
+					desc "资源UUID"
+					location "body"
+					type "String"
+					optional true
+					since "4.8.0"
+				}
+				column {
+					name "tagUuids"
+					enclosedIn "params"
+					desc "标签UUID列表"
+					location "body"
+					type "List"
+					optional true
+					since "4.8.0"
+				}
+				column {
+					name "systemTags"
+					enclosedIn ""
+					desc "系统标签"
+					location "body"
+					type "List"
+					optional true
+					since "4.8.0"
+				}
+				column {
+					name "userTags"
+					enclosedIn ""
+					desc "用户标签"
+					location "body"
+					type "List"
+					optional true
+					since "4.8.0"
 				}
 			}
         }
