6f9a77eab3a3e191a48e01a373d8e312344d26c7
shan.wu <shan.wu@zstack.io>
2023-07-04 10:37:25 +0800

    <bug>[sblk]: fix vg being re created
    fix vg being re created
    
    http://jira.zstack.io/browse/ZSTAC-57025
    
    Resolves/Related: ZSTAC-57025
    
    Change-Id: 180d7b4804474940baf57fb02c047eb3

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -13490,6 +13490,33 @@ abstract class ApiHelper {
     }
 
 
+    def detachScsiLunFromHost(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DetachScsiLunFromHostAction.class) Closure c) {
+        def a = new org.zstack.sdk.DetachScsiLunFromHostAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def detachScsiLunFromVmInstance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.DetachScsiLunFromVmInstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.DetachScsiLunFromVmInstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
