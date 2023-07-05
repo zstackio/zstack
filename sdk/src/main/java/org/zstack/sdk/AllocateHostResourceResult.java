26522bb72134f057367c91fef754f3bd755ad002
mingmin.wen <mingmin.wen@zstack.io>
2023-07-05 18:20:29 +0800

    <feature>[sdk]: add new api AllocateHostResource
    1. add sdk files of API AllocateHostResource;
    2. update ApiHelper.groovy by new API AllocateHostResource;
    
    Resolves: ZSTAC-44660
    
    Change-Id: I6c6e6f6961757765756276646a79626d6369626a

---
+++ sdk/src/main/java/org/zstack/sdk/AllocateHostResourceResult.java
@@ -0,0 +1,30 @@
+package org.zstack.sdk;
+
+
+
+public class AllocateHostResourceResult {
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.util.List vCPUPin;
+    public void setVCPUPin(java.util.List vCPUPin) {
+        this.vCPUPin = vCPUPin;
+    }
+    public java.util.List getVCPUPin() {
+        return this.vCPUPin;
+    }
+
+}
