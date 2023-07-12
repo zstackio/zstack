11fd846526afc17d19b2dae19bea1a38b15d2b23
Zhang Wenhao <wenhao.zhang@zstack.io>
2023-07-12 17:22:02 +0800

    <fix>[utils]: move ansible call with private key methods to SshShell
    Ssh will reply failure with private key file of more than '600'
    permission, so set file posix permission before execute ssh call.
    
    Related: ZSTAC-57253
    
    Change-Id: I6771746a6c77676b68647a6e746e707972737063

--- utils/src/main/java/org/zstack/utils/path/PathUtil.java
+++ utils/src/main/java/org/zstack/utils/path/PathUtil.java
@@ -277,12 +277,22 @@ public class PathUtil {
         }
     }
 
+    /**
+     * @see #setFilePosixPermissions(File, String)
+     */
     public static void setFilePosixPermissions(String path, String perms) {
+        setFilePosixPermissions(new File(path), perms);
+    }
+
+    /**
+     * @param perms permission like: "rwxr-xr-x" "rw-------"
+     */
+    public static void setFilePosixPermissions(File file, String perms) {
         try {
             Set<PosixFilePermission> s = PosixFilePermissions.fromString(perms);
-            Files.setPosixFilePermissions(new File(path).toPath(), s);
+            Files.setPosixFilePermissions(file.toPath(), s);
         } catch (IOException ex) {
-            logger.warn(String.format("set %s permission to %s: %s", path, perms, ex.getMessage()));
+            logger.warn(String.format("set %s permission to %s: %s", file, perms, ex.getMessage()));
         }
     }
 
@@ -303,11 +313,19 @@ public class PathUtil {
     }
 
     public static void writeFile(String fpath, String content) throws IOException {
-        writeFile(fpath, content.getBytes(StandardCharsets.UTF_8));
+        writeFile(new File(fpath), content.getBytes(StandardCharsets.UTF_8));
     }
 
     public static void writeFile(String fpath, byte[] data) throws IOException {
-        try (FileOutputStream outputStream = new FileOutputStream(new File(fpath))) {
+        writeFile(new File(fpath), data);
+    }
+
+    public static void writeFile(File file, String content) throws IOException {
+        writeFile(file, content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static void writeFile(File file, byte[] data) throws IOException {
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
             outputStream.write(data);
             outputStream.flush();
         }
