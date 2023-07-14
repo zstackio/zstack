8087cb585b1810dba648f0befe8ad229577af009
qiuyu.zhang <qiuyu.zhang@zstack.io>
2023-07-14 15:03:39 +0800

    <feature>[conf]:add userinfourl to oauth2 authentication
    DBImpact
    
    Resolves: ZSTAC-56933
    
    Change-Id: I787369696b796e646f71687272716164726f7268

---
+++ conf/db/upgrade/V4.7.21__schema.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `userinfoUrl` varchar(256) DEFAULT NULL;
+ALTER TABLE `zstack`.`SSOClientVO` ADD COLUMN `redirectUrl` varchar(256) DEFAULT NULL;
+ALTER TABLE `zstack`.`OAuth2TokenVO` MODIFY COLUMN `accessToken` text DEFAULT NULL;
+ALTER TABLE `zstack`.`OAuth2TokenVO` MODIFY COLUMN `idToken` text DEFAULT NULL;
+ALTER TABLE `zstack`.`OAuth2TokenVO` MODIFY COLUMN `refreshToken` text DEFAULT NULL;
