bad8d28020b638525c42ba4bb9bb4762fc660ad3
shenjin <jin.shen@zstack.io>
2023-07-20 13:26:48 +0800

    <fix>[ceph]: remove not allow ceph mixed restriction
    remove not allow third-party ceph mixed with
    other primary storage restriction
    
    Resolves: ZSTAC-57931
    
    Change-Id: I6e76767966666171656f6e69697675626c796577
    (cherry picked from commit 8bc4ebb4db1e20e77f52ff8fec0cc601efccf0cb)

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -36850,6 +36850,32 @@ abstract class ApiHelper {
         }
     }
 
+    def updateBlockVolume(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.UpdateBlockVolumeAction.class) Closure c) {
+        def a = new org.zstack.sdk.UpdateBlockVolumeAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
 
     def updateBareMetal2Instance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.UpdateBareMetal2InstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.UpdateBareMetal2InstanceAction()
