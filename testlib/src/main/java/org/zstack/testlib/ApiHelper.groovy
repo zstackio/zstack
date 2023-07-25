a9189f47417ac246a6936f9a578e9aed3d800c94
yingzhe.hu <yinghe.hu@zstack.io>
2023-07-25 03:23:57 +0000

    <fix>[cdp]: add APIMergeDataOnBackupStorageMsg sdk
    Resolves: ZSTAC-56997
    
    Change-Id: I6776119863766c6a666f676973647077637a6e6a
    
    
    (cherry picked from commit 26196056ed47640050c7aad7ca6b358ea3951a10)

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -24479,6 +24479,33 @@ abstract class ApiHelper {
     }
 
 
+    def mergeDataOnBackupStorage(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.MergeDataOnBackupStorageAction.class) Closure c) {
+        def a = new org.zstack.sdk.MergeDataOnBackupStorageAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def migrateVm(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.MigrateVmAction.class) Closure c) {
         def a = new org.zstack.sdk.MigrateVmAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
