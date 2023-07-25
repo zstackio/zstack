9aef3308a68f3c5545982602a101f36e2c9a8663
AlanJager <ye.zou@zstack.io>
2023-07-25 11:49:35 +0800

    <feature>[sdk]: update sdk support reservedMemorySize
    Resolves: ZSTAC-56872
    
    Change-Id: I6b646271786c687470656870776a6c697a736d6c

--- sdk/src/main/java/org/zstack/sdk/CreateVmInstanceAction.java
+++ sdk/src/main/java/org/zstack/sdk/CreateVmInstanceAction.java
@@ -37,6 +37,9 @@ public class CreateVmInstanceAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.Long memorySize;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, numberRange = {0L,9223372036854775807L}, noTrim = false)
+    public java.lang.Long reservedMemorySize;
+
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String imageUuid;
 
