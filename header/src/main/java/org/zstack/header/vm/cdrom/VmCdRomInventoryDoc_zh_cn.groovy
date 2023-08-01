d1e39ab67a904d04f06567493f47cbc64f339591
shenjin <jin.shen@zstack.io>
2023-08-01 21:16:16 +0800

    <doc>[vm]: update doc
    Resolves: ZSTAC-56238
    
    Change-Id: I79776c7370736f797a706c776c6a70796b63636e

--- header/src/main/java/org/zstack/header/vm/cdrom/VmCdRomInventoryDoc_zh_cn.groovy
+++ header/src/main/java/org/zstack/header/vm/cdrom/VmCdRomInventoryDoc_zh_cn.groovy
@@ -2,7 +2,6 @@ package org.zstack.header.vm.cdrom
 
 import java.lang.Integer
 import java.sql.Timestamp
-import java.sql.Timestamp
 
 doc {
 
@@ -12,54 +11,54 @@ doc {
 		name "uuid"
 		desc "资源的UUID，唯一标示该资源"
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "vmInstanceUuid"
 		desc "云主机UUID"
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "deviceId"
 		desc ""
 		type "Integer"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "isoUuid"
 		desc ""
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "isoInstallPath"
 		desc ""
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "name"
 		desc "资源名称"
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "description"
 		desc "资源的详细描述"
 		type "String"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "createDate"
 		desc "创建时间"
 		type "Timestamp"
-		since "3.3"
+		since "4.7.13"
 	}
 	field {
 		name "lastOpDate"
 		desc "最后一次修改时间"
 		type "Timestamp"
-		since "3.3"
+		since "4.7.13"
 	}
 }
--- header/src/main/java/org/zstack/header/volume/VolumeInventoryDoc_zh_cn.groovy
+++ header/src/main/java/org/zstack/header/volume/VolumeInventoryDoc_zh_cn.groovy
@@ -1,12 +1,9 @@
 package org.zstack.header.volume
 
 import java.lang.Long
-import java.lang.Long
 import java.lang.Integer
 import java.sql.Timestamp
-import java.sql.Timestamp
 import java.lang.Boolean
-import java.sql.Timestamp
 
 doc {
 
@@ -16,126 +13,126 @@ doc {
 		name "uuid"
 		desc "资源的UUID，唯一标示该资源"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "name"
 		desc "资源名称"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "description"
 		desc "资源的详细描述"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "primaryStorageUuid"
 		desc "主存储UUID"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "vmInstanceUuid"
 		desc "云主机UUID"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "diskOfferingUuid"
 		desc "云盘规格UUID"
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "rootImageUuid"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "installPath"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "type"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "format"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "size"
 		desc ""
 		type "Long"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "actualSize"
 		desc ""
 		type "Long"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "deviceId"
 		desc ""
 		type "Integer"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "state"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "status"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "createDate"
 		desc "创建时间"
 		type "Timestamp"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "lastOpDate"
 		desc "最后一次修改时间"
 		type "Timestamp"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "isShareable"
 		desc ""
 		type "Boolean"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "volumeQos"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "lastDetachDate"
 		desc ""
 		type "Timestamp"
-		since "0.6"
+		since "4.7.13"
 	}
 	field {
 		name "lastVmInstanceUuid"
 		desc ""
 		type "String"
-		since "0.6"
+		since "4.7.13"
 	}
 }
