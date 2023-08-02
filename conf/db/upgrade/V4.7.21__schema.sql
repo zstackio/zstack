9adc90e110c72ea57e6bd06f8e83f629adc6e4dc
shenjin <jin.shen@zstack.io>
2023-08-02 16:10:33 +0800

    <fix>[conf]: add migrateJsonLabelToXmlHookVO sql
    add migrateJsonLabelToXmlHookVO sql
    
    Resolves: ZSTAC-56878
    
    Change-Id: I6e736c746675656c6c6f6b716962756779707368

--- conf/db/upgrade/V4.7.21__schema.sql
+++ conf/db/upgrade/V4.7.21__schema.sql
@@ -23,3 +23,47 @@ CREATE TABLE IF NOT EXISTS `zstack`.`XmlHookVmInstanceRefVO` (
     CONSTRAINT `fkXmlHookVmInstanceRefVO` FOREIGN KEY (`xmlHookUuid`) REFERENCES `XmlHookVO` (`uuid`) ON DELETE CASCADE,
     CONSTRAINT `fkXmlHookVmInstanceRefVO1` FOREIGN KEY (`vmInstanceUuid`) REFERENCES `ResourceVO` (`uuid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP PROCEDURE IF EXISTS migrateJsonLabelToXmlHookVO;
+DELIMITER $$
+CREATE PROCEDURE migrateJsonLabelToXmlHookVO()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE hookUuid VARCHAR(32);
+    DECLARE vmUuid VARCHAR(32);
+    DECLARE hookValue TEXT;
+    DECLARE cur CURSOR FOR SELECT DISTINCT REPLACE(labelKey,'user-defined-xml-hook-script-',''),labelValue FROM zstack.JsonLabelVO WHERE labelKey like 'user-defined-xml-hook-script-%%';
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO vmUuid, hookValue;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        IF NOT EXISTS(SELECT * from XmlHookVO where hookScript = hookValue) THEN
+            SET hookUuid = (REPLACE(UUID(), '-', ''));
+
+            INSERT zstack.ResourceVO(uuid, resourceName, resourceType, concreteResourceType)
+            VALUES (hookUuid, '自定义hook', 'XmlHookVO', 'org.zstack.header.tag.XmlHookVO');
+
+            INSERT zstack.XmlHookVO (uuid, name, description, type, hookScript, lastOpDate, createDate)
+            VALUES(hookUuid, concat('自定义hook-',NOW()), '自定义hook', 'Customization', hookValue, NOW(), NOW());
+
+            INSERT zstack.XmlHookVmInstanceRefVO(xmlHookUuid, vmInstanceUuid, lastOpDate, createDate)
+            VALUES (hookUuid, vmUuid, NOW(), NOW());
+
+        ELSEIF NOT EXISTS(SELECT * from XmlHookVmInstanceRefVO where vmInstanceUuid = vmUuid) THEN
+            SET hookUuid = (select uuid from XmlHookVO where hookScript = hookValue);
+            INSERT zstack.XmlHookVmInstanceRefVO(xmlHookUuid, vmInstanceUuid, lastOpDate, createDate)
+            VALUES (hookUuid, vmUuid, NOW(), NOW());
+        END IF;
+        END LOOP;
+        CLOSE cur;
+
+        SELECT CURTIME();
+    END $$;
+DELIMITER ;
+
+call migrateJsonLabelToXmlHookVO();
+DROP PROCEDURE IF EXISTS migrateJsonLabelToXmlHookVO;
