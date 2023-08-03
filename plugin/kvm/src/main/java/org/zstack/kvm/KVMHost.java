d3d5c0cbe83762a2ed0a64d1a510b6bf8920032d
kaicai.hu <kaicai.hu@zstack.io>
2023-08-03 15:28:04 +0800

    <fix>[kvm]: add drop rule for host port: 7069
    Resolves: ZHCI-2089
    
    Change-Id: I64786577796b6a6f786c6961736d676a626c6364

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMHost.java
@@ -4383,6 +4383,9 @@ public class KVMHost extends HostBase implements Host {
                                         "/var/lib/zstack/kvm/kvmagent-iptables",
                                         KVMConstant.IPTABLES_COMMENTS,
                                         KVMGlobalConfig.KVMAGENT_ALLOW_PORTS_LIST.value(String.class)));
+                                
+                                builder.append(String.format("; ipset create ZS-PROMETHEUS-ALLOW hash:net -exist; ipset flush ZS-PROMETHEUS-ALLOW; ipset add ZS-PROMETHEUS-ALLOW 127.0.0.1; ipset add ZS-PROMETHEUS-ALLOW %s; ipset add ZS-PROMETHEUS-ALLOW %s; iptables -t mangle -I INPUT -m set ! --match-set ZS-PROMETHEUS-ALLOW src -p tcp -m comment --comment %s -m tcp --dport 7069 -j DROP",
+                                        getSelf().getManagementIp(), Platform.getManagementServerIp(), KVMConstant.IPTABLES_COMMENTS));
                             }
 
                             try {
