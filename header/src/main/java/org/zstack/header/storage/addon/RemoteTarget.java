5eb808faa1d6c3653ad6062b9a0b911e71f3a9e3
Qun Li <qun.li@zstack.io>
2023-08-11 11:22:07 +0800

    <feature>[storage]: introduce RemoteTarget etc.
    Resolves: ZSTAC-1234
    
    Change-Id: I66636b6c786a7375626663646a676e6b6d66746e

---
+++ header/src/main/java/org/zstack/header/storage/addon/RemoteTarget.java
@@ -0,0 +1,12 @@
+package org.zstack.header.storage.addon;
+
+public interface RemoteTarget {
+    // unique identifier - how to identify image/volume.
+    String getInstallPath();
+
+    // URI to access the image/volume, e.g.:
+    //  - sftp://host/path/to/file
+    //  - nbd://host/export-name
+    //  - rbd://pool/image
+    String getResourceURI();
+}
