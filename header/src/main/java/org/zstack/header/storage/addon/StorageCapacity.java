0d3886ff0db08f1c1dd504b56b377950c65b4776
Qun Li <qun.li@zstack.io>
2023-08-11 11:22:07 +0800

    <feature>[storage]: introduce RemoteTarget etc.
    Resolves: SUG-622
    
    Change-Id: I66636b6c786a7375626663646a676e6b6d66746e

---
+++ header/src/main/java/org/zstack/header/storage/addon/StorageCapacity.java
@@ -0,0 +1,22 @@
+package org.zstack.header.storage.addon;
+
+public class StorageCapacity {
+    private long totalCapacity;
+    private long availableCapacity;
+
+    public long getTotalCapacity() {
+        return totalCapacity;
+    }
+
+    public void setTotalCapacity(long totalCapacity) {
+        this.totalCapacity = totalCapacity;
+    }
+
+    public long getAvailableCapacity() {
+        return availableCapacity;
+    }
+
+    public void setAvailableCapacity(long availableCapacity) {
+        this.availableCapacity = availableCapacity;
+    }
+}
