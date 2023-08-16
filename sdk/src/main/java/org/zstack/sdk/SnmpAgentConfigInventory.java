e418dec5df06dee4a3600f7d7425354ba3f68652
Wang,Jing <jing.wang@zstack.io>
2023-08-16 18:59:03 +0800

    <fix>[sdk]: update sdk
    Resolves: ZSTAC-58616
    
    Change-Id: I73617062656a726b66736b6a6a7a6f636267627a

--- sdk/src/main/java/org/zstack/sdk/SnmpAgentConfigInventory.java
+++ sdk/src/main/java/org/zstack/sdk/SnmpAgentConfigInventory.java
@@ -84,4 +84,20 @@ public class SnmpAgentConfigInventory  {
         return this.securityLevel;
     }
 
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
 }
