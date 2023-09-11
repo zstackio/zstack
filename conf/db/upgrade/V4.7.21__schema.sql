5ae14f20a829c702b7aa9b960b0ac5b8a3718714
pengchao.liu <pengchao.liu@zstack.io>
2023-09-11 22:53:50 +0800

    <fix>[securityGroup]: fix add vmNic to security group rule error
    Resolves: ZSTAC-59488
    
    Change-Id: I7274726f66786f6571706e636b65756876716176

--- conf/db/upgrade/V4.7.21__schema.sql
+++ conf/db/upgrade/V4.7.21__schema.sql
@@ -24,30 +24,6 @@ CREATE TABLE `NvmeServerClusterRefVO` (
 ALTER TABLE `zstack`.`NvmeTargetVO` ADD COLUMN `nvmeServerUuid` varchar(32);
 ALTER TABLE `zstack`.`NvmeTargetVO` ADD CONSTRAINT `fkNvmeTargetVONvmeServerVO` FOREIGN KEY (nvmeServerUuid) REFERENCES NvmeServerVO(uuid) ON DELETE SET NULL;
 
-ALTER TABLE `zstack`.`VmNicSecurityGroupRefVO`
-ADD COLUMN `priority` INT DEFAULT -1 AFTER `securityGroupUuid`;
-
-ALTER TABLE `zstack`.`SecurityGroupRuleVO`
-ADD COLUMN `srcPortRange` varchar(255) DEFAULT NULL AFTER `protocol`,
-ADD COLUMN `dstPortRange` varchar(255) DEFAULT NULL AFTER `protocol`,
-ADD COLUMN `srcIpRange` varchar(1024) DEFAULT NULL AFTER `protocol`,
-ADD COLUMN `dstIpRange` varchar(1024) DEFAULT NULL AFTER `protocol`,
-ADD COLUMN `description` varchar(255) DEFAULT NULL AFTER `protocol`,
-ADD COLUMN `action` varchar(32) NOT NULL DEFAULT 'ACCEPT' AFTER `protocol`,
-ADD COLUMN `priority` INT DEFAULT -1 AFTER `protocol`;
-
-
-CREATE TABLE IF NOT EXISTS `zstack`.`VmNicSecurityPolicyVO` (
-    `uuid` varchar(32) NOT NULL UNIQUE,
-    `vmNicUuid` varchar(32) NOT NULL,
-    `ingressPolicy` varchar(32) NOT NULL DEFAULT 'DENY',
-    `egressPolicy` varchar(32) NOT NULL DEFAULT 'ALLOW',
-    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY  (`uuid`),
-    CONSTRAINT `fkVmNicSecurityPolicyVOVmNicVO` FOREIGN KEY (vmNicUuid) REFERENCES VmNicVO (uuid) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `userinfoUrl` varchar(256) DEFAULT NULL;
 ALTER TABLE `zstack`.`SSOClientVO` ADD COLUMN `redirectUrl` varchar(256) DEFAULT NULL;
 ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `logoutUrl` varchar(256) DEFAULT NULL;
