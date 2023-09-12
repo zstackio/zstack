8b9a02ef18efd403609af597f6099b993911402a
pengchao.liu <pengchao.liu@zstack.io>
2023-09-12 13:39:15 +0800

    <fix>[securityGroup]: validate rule api reply code
    Resolves: ZSTAC-59546
    
    Change-Id: I736b626a667a626773726e65646b726d71766a69

--- plugin/securityGroup/src/main/java/org/zstack/network/securitygroup/SecurityGroupManagerImpl.java
+++ plugin/securityGroup/src/main/java/org/zstack/network/securitygroup/SecurityGroupManagerImpl.java
@@ -731,6 +731,7 @@ public class SecurityGroupManagerImpl extends AbstractService implements Securit
     private void handle(APIValidateSecurityGroupRuleMsg msg) {
         APIValidateSecurityGroupRuleReply reply = new APIValidateSecurityGroupRuleReply();
         reply.setAvailable(true);
+        reply.setCode(SecurityGroupErrors.RULE_CHECK_OK.toString());
         bus.reply(msg, reply);
     }
 
