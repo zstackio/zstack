b5cdb5072e6b7fe6c876434e1b22ed09e88ffee4
Xingwei Yu <xingwei.yu@zstack.io>
2023-09-12 16:59:55 +0800

    <fix>[sdk]: Update sdk for remove user roles
    Resolves: ZSTAC-51438
    
    Change-Id: I6b656d7a676464726e616d78716267787570746f

--- sdk/src/main/java/org/zstack/sdk/iam2/api/RemoveRolesFromIAM2VirtualIDAction.java
+++ sdk/src/main/java/org/zstack/sdk/iam2/api/RemoveRolesFromIAM2VirtualIDAction.java
@@ -34,6 +34,9 @@ public class RemoveRolesFromIAM2VirtualIDAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String projectUuid;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String organizationUuid;
+
     @Param(required = false)
     public java.util.List systemTags;
 
