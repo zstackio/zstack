bce348d712b6eadb4bd894a429d2a4e7bff22152
yu.sun <yu.sun@zstack.io>
2023-09-21 12:01:13 +0800

    <fix>[kvm]: Modify the mode selection for OS that support hygon
    If select hygon_customized for a OS that supports hygon,
    automatically changes to none.
    
    Resolves: ZSV-2396
    
    Change-Id: I72766873656e6f676b6474776b6f617775797765

--- plugin/kvm/src/main/java/org/zstack/kvm/HygonTagKvmStartVmExtension.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/HygonTagKvmStartVmExtension.java
@@ -1,26 +1,21 @@
 package org.zstack.kvm;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.stringtemplate.v4.ST;
 import org.zstack.compute.host.HostSystemTags;
-import org.zstack.core.db.Q;
 import org.zstack.header.errorcode.ErrorCode;
 import org.zstack.header.vm.VmInstanceSpec;
 import org.zstack.header.vm.VmInstanceVO;
 import org.zstack.resourceconfig.ResourceConfig;
 import org.zstack.resourceconfig.ResourceConfigFacade;
-import org.zstack.resourceconfig.ResourceConfigVO;
-import org.zstack.resourceconfig.ResourceConfigVO_;
-import org.zstack.utils.Utils;
-import org.zstack.utils.logging.CLogger;
+import org.zstack.resourceconfig.ResourceConfigInventory;
+
+import java.util.List;
 
 import static org.zstack.kvm.KVMHostFactory.allGuestOsCharacter;
 
 public class HygonTagKvmStartVmExtension implements KVMStartVmExtensionPoint {
     @Autowired
     ResourceConfigFacade rcf;
-
-    private static final CLogger logger = Utils.getLogger(KVMHost.class);
 
     @Override
     public void beforeStartVmOnKvm(KVMHostInventory host, VmInstanceSpec spec, KVMAgentCommands.StartVmCmd cmd) {
@@ -32,29 +27,41 @@ public class HygonTagKvmStartVmExtension implements KVMStartVmExtensionPoint {
             return;
         }
 
-        //hygonTag host and UserVm
-        String vmCpuMode = Q.New(ResourceConfigVO.class)
-                .select(ResourceConfigVO_.value)
-                .eq(ResourceConfigVO_.name, KVMGlobalConfig.NESTED_VIRTUALIZATION.getName())
-                .eq(ResourceConfigVO_.resourceUuid, spec.getVmInventory().getUuid())
-                .eq(ResourceConfigVO_.resourceType, VmInstanceVO.class.getSimpleName())
-                .findValue();
+        // vm_level cpu mode
+        String vmLevelCpuMode = null;
+        ResourceConfig rc = rcf.getResourceConfig(KVMGlobalConfig.NESTED_VIRTUALIZATION.getIdentity());
+        List<ResourceConfigInventory> configs = rc.getEffectiveResourceConfigs(spec.getVmInventory().getUuid());
+        if (!configs.isEmpty()) {
+            if (VmInstanceVO.class.getSimpleName().equals(configs.get(0).getResourceType())){
+                vmLevelCpuMode = configs.get(0).getValue();
+            }
+        }
 
-        if (vmCpuMode == null) {
-            NullVmCpuModeSetVmCpuMode(cmd, spec);
-        } else if (vmCpuMode.equals(KVMConstant.CPU_MODE_NONE)) {
-            NoneVmCpuModeSetVmCpuMode(cmd, spec);
-        } else if (vmCpuMode.equals(KVMConstant.CPU_MODE_HYGON_CUSTOMIZED)) {
-            HygonCustomizedVmCpuModeSetVmCpuMode(cmd);
+        if (vmLevelCpuMode == null) {
+            nullVmCpuModeSetVmCpuMode(cmd, spec);
+        } else if (vmLevelCpuMode.equals(KVMConstant.CPU_MODE_NONE)) {
+            noneVmCpuModeSetVmCpuMode(cmd, spec);
+        } else if (vmLevelCpuMode.equals(KVMConstant.CPU_MODE_HYGON_CUSTOMIZED)) {
+            hygonCustomizedVmCpuModeSetVmCpuMode(cmd, spec);
         }
     }
 
-    private void HygonCustomizedVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd) {
+    private void hygonCustomizedVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd, VmInstanceSpec spec) {
+        //special vm which not need add hygonTag
+        if (guestOsNotNeedHygonTag(spec)) {
+            cmd.setNestedVirtualization(KVMConstant.CPU_MODE_NONE);
+            cmd.setVmCpuModel(null);
+            //upgrade vm.cpuMode ResourceConfig
+            ResourceConfig rc = rcf.getResourceConfig(KVMGlobalConfig.NESTED_VIRTUALIZATION.getIdentity());
+            rc.updateValue(spec.getVmInventory().getUuid(), KVMConstant.CPU_MODE_NONE);
+            return;
+        }
+
         cmd.setNestedVirtualization(KVMConstant.CPU_MODE_HOST_PASSTHROUGH);
         cmd.setVmCpuModel(KVMConstant.CPU_MODE_HYGON_CUSTOMIZED);
     }
 
-    private boolean GuestOsNotNeedHygonTag(VmInstanceSpec spec) {
+    private boolean guestOsNotNeedHygonTag(VmInstanceSpec spec) {
         String vmArchPlatformRelease = String.format("%s_%s_%s", spec.getVmInventory().getArchitecture(), spec.getVmInventory().getPlatform(), spec.getVmInventory().getGuestOsType());
         if (allGuestOsCharacter.containsKey(vmArchPlatformRelease)) {
             if (allGuestOsCharacter.get(vmArchPlatformRelease).getHygonTag() != null && !allGuestOsCharacter.get(vmArchPlatformRelease).getHygonTag()) {
@@ -64,9 +71,9 @@ public class HygonTagKvmStartVmExtension implements KVMStartVmExtensionPoint {
         return false;
     }
 
-    private void NoneVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd, VmInstanceSpec spec) {
+    private void noneVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd, VmInstanceSpec spec) {
         //special vm which not need add hygonTag
-        if (GuestOsNotNeedHygonTag(spec)) {
+        if (guestOsNotNeedHygonTag(spec)) {
             return;
         }
 
@@ -78,14 +85,15 @@ public class HygonTagKvmStartVmExtension implements KVMStartVmExtensionPoint {
         rc.updateValue(spec.getVmInventory().getUuid(), KVMConstant.CPU_MODE_HYGON_CUSTOMIZED);
     }
 
-    private void NullVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd, VmInstanceSpec spec) {
+    private void nullVmCpuModeSetVmCpuMode(KVMAgentCommands.StartVmCmd cmd, VmInstanceSpec spec) {
         //special vm which not need add hygonTag
-        if (GuestOsNotNeedHygonTag(spec)) {
+        if (guestOsNotNeedHygonTag(spec)) {
             //if vm.cpuMode cluster level is hygon_customized, change mode to none
             //not change vm.cpuMode resourceConfig in cluster level
-            String NotNeedAddHygonTagVmCpuMode = rcf.getResourceConfigValue(KVMGlobalConfig.NESTED_VIRTUALIZATION, spec.getVmInventory().getUuid(), String.class);
-            if (NotNeedAddHygonTagVmCpuMode.equals(KVMConstant.CPU_MODE_HYGON_CUSTOMIZED)) {
-                cmd.setVmCpuModel(KVMConstant.CPU_MODE_NONE);
+            String notNeedAddHygonTagVmCpuMode = rcf.getResourceConfigValue(KVMGlobalConfig.NESTED_VIRTUALIZATION, spec.getVmInventory().getClusterUuid(), String.class);
+            if (notNeedAddHygonTagVmCpuMode.equals(KVMConstant.CPU_MODE_HYGON_CUSTOMIZED)) {
+                cmd.setNestedVirtualization(KVMConstant.CPU_MODE_NONE);
+                cmd.setVmCpuModel(null);
             }
             return;
         }
