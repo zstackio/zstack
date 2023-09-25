ded8b9fb8c28020cd76e126955973fa575465bf5
shixin.ruan <shixin.ruan@zstack.io>
2023-09-25 15:14:43 +0800

    <feature>[compute]: add host l2 allocator
    Resolves: ZSV-2644
    
    Change-Id: I6f747877736672697a7a70756e636864756a7a74

--- compute/src/main/java/org/zstack/compute/allocator/AttachedL2NetworkAllocatorFlow.java
+++ compute/src/main/java/org/zstack/compute/allocator/AttachedL2NetworkAllocatorFlow.java
@@ -8,12 +8,18 @@ import org.zstack.core.CoreGlobalProperty;
 import org.zstack.core.Platform;
 import org.zstack.core.componentloader.PluginRegistry;
 import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.Q;
 import org.zstack.header.allocator.AbstractHostAllocatorFlow;
 import org.zstack.header.exception.CloudRuntimeException;
 import org.zstack.header.host.HostVO;
+import org.zstack.header.host.HostVO_;
+import org.zstack.header.network.l2.L2NetworkAttachStatus;
 import org.zstack.header.network.l2.L2NetworkClusterRefVO;
+import org.zstack.header.network.l2.L2NetworkHostRefVO;
+import org.zstack.header.network.l2.L2NetworkHostRefVO_;
 import org.zstack.header.network.l3.L3NetworkInventory;
 
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -71,30 +77,39 @@ public class AttachedL2NetworkAllocatorFlow extends AbstractHostAllocatorFlow {
             return new ArrayList<>();
         }
 
+        List<String> retHostUuids;
         if (hostUuids.isEmpty()) {
-            sql = "select h from HostVO h where h.clusterUuid in (:cuuids)";
-            TypedQuery<HostVO> hq = dbf.getEntityManager().createQuery(sql, HostVO.class);
-            hq.setParameter("cuuids", clusterUuids);
-
-            if (usePagination()) {
-                hq.setFirstResult(paginationInfo.getOffset());
-                hq.setMaxResults(paginationInfo.getLimit());
-            }
-
-            return hq.getResultList();
+            retHostUuids = Q.New(HostVO.class).select(HostVO_.uuid)
+                    .in(HostVO_.clusterUuid, clusterUuids).listValues();
         } else {
-            sql = "select h from HostVO h where h.clusterUuid in (:cuuids) and h.uuid in (:huuids)";
-            TypedQuery<HostVO> hq = dbf.getEntityManager().createQuery(sql, HostVO.class);
-            hq.setParameter("cuuids", clusterUuids);
-            hq.setParameter("huuids", hostUuids);
-
-            if (usePagination()) {
-                hq.setFirstResult(paginationInfo.getOffset());
-                hq.setMaxResults(paginationInfo.getLimit());
-            }
-
-            return hq.getResultList();
+            retHostUuids = Q.New(HostVO.class).select(HostVO_.uuid)
+                    .in(HostVO_.clusterUuid, clusterUuids)
+                    .in(HostVO_.uuid, hostUuids).listValues();
         }
+
+        if (retHostUuids.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        /* in normal case, there is no L2NetworkHostRefVO  */
+        List<String> excludeHostUuids = Q.New(L2NetworkHostRefVO.class).in(L2NetworkHostRefVO_.l2NetworkUuid, l2uuids)
+                .notEq(L2NetworkHostRefVO_.attachStatus, L2NetworkAttachStatus.Attached)
+                .select(L2NetworkHostRefVO_.hostUuid).listValues();
+        retHostUuids.removeAll(excludeHostUuids);
+        if (retHostUuids.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        sql = "select h from HostVO h where h.uuid in (:huuids)";
+        TypedQuery<HostVO> hq = dbf.getEntityManager().createQuery(sql, HostVO.class);
+        hq.setParameter("huuids", retHostUuids);
+
+        if (usePagination()) {
+            hq.setFirstResult(paginationInfo.getOffset());
+            hq.setMaxResults(paginationInfo.getLimit());
+        }
+
+        return hq.getResultList();
     }
 
     @Override
