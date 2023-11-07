792606ecd369526b997dcbbb2de23cb261245114
yaohua.wu <yaohua.wu@zstack.io>
2023-11-07 17:34:13 +0800

    <fix>[storage]: support image check md5sum
    DBImpact
    
    Resolves: ZSTAC-60191
    
    Change-Id: I6b686c6a706c6f6869677772786e70736e717268

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -4343,7 +4343,34 @@ abstract class ApiHelper {
         c.resolveStrategy = Closure.OWNER_FIRST
         c.delegate = a
         c()
-        
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def calculateImageHashOnBackupStorage(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.CalculateImageHashOnBackupStorageAction.class) Closure c) {
+        def a = new org.zstack.sdk.CalculateImageHashOnBackupStorageAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+
 
         if (System.getProperty("apipath") != null) {
             if (a.apiId == null) {
