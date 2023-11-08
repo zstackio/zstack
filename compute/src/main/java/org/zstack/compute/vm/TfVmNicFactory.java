306f2718622355673dfdfe51ec4b3539afa71b40
fuwei <fuwei1@sugon.com>
2023-11-08 17:41:34 +0800

    <fix>[sugon_sdn]fix create vm with tf l3 network failed
    Related: ZSTAC-59629
    
    Change-Id: I696a1665627a6469616b7973626b74796a697266

--- compute/src/main/java/org/zstack/compute/vm/TfVmNicFactory.java
+++ compute/src/main/java/org/zstack/compute/vm/TfVmNicFactory.java
@@ -4,15 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.header.core.workflow.FlowException;
-import org.zstack.header.network.l3.UsedIpInventory;
-import org.zstack.header.network.l3.UsedIpVO;
 import org.zstack.header.vm.*;
 import org.zstack.identity.Account;
 import org.zstack.utils.Utils;
 import org.zstack.utils.logging.CLogger;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.zstack.core.Platform.err;
 
@@ -30,7 +26,7 @@ public class TfVmNicFactory extends VmNicFactory {
         return type;
     }
 
-    public VmNicVO createVmNic(VmNicInventory nic, VmInstanceSpec spec, List<UsedIpInventory> ips) {
+    public VmNicVO createVmNic(VmNicInventory nic, VmInstanceSpec spec) {
         String acntUuid = Account.getAccountUuidOfResource(spec.getVmInventory().getUuid());
 
         VmNicVO vnic = VmInstanceNicFactory.createVmNic(nic);
@@ -41,17 +37,9 @@ public class TfVmNicFactory extends VmNicFactory {
             throw new FlowException(err(VmErrors.ALLOCATE_MAC_ERROR, "unable to find an available mac address after re-try 5 times, too many collisions"));
         }
 
-        List<UsedIpVO> ipVOS = new ArrayList<>();
-        for (UsedIpInventory ip : ips) {
-            /* update usedIpVo */
-            UsedIpVO ipVO = dbf.findByUuid(ip.getUuid(), UsedIpVO.class);
-            ipVO.setVmNicUuid(vnic.getUuid());
-            ipVOS.add(ipVO);
-        }
-        dbf.updateCollection(ipVOS);
-
         vnic = dbf.reload(vnic);
         spec.getDestNics().add(VmNicInventory.valueOf(vnic));
+        logger.debug(String.format("Create TFVNIC [%s] success.", vnic.getUuid()));
         return vnic;
     }
 }
