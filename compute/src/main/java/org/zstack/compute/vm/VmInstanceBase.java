bad0606656f779d5a65b684348c2fc226ace1b0b
AlanJager <ye.zou@zstack.io>
2023-11-16 14:24:55 +0800

    <fix>[compute]: set vm hostUuid to original hostUuid after rebooted
    Resolves: ZSTAC-59159
    
    Change-Id: I646a656f667679626d766b6c6865637176657165
    (cherry picked from commit 203715898ede887fac3ae9174e88f688bb603816)

--- compute/src/main/java/org/zstack/compute/vm/VmInstanceBase.java
+++ compute/src/main/java/org/zstack/compute/vm/VmInstanceBase.java
@@ -7629,7 +7629,8 @@ public class VmInstanceBase extends AbstractVmInstance {
                 provisionAfterRebootVm(spec, new NoErrorCompletion(completion) {
                     @Override
                     public void done() {
-                        self = changeVmStateInDb(VmInstanceStateEvent.running);
+                        self = changeVmStateInDb(VmInstanceStateEvent.running,
+                                () -> self.setHostUuid(originalCopy.getHostUuid()));
                         VmInstanceInventory inv = VmInstanceInventory.valueOf(self);
                         extEmitter.afterRebootVm(inv);
                         new StaticIpOperator().deleteIpChange(self.getUuid());
