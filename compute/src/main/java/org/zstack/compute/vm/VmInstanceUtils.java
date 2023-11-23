1984f90a1e68a646a135fc1d39ef2caf42d3a21d
AlanJager <ye.zou@zstack.io>
2023-11-23 12:03:06 +0800

    <fix>[compute]: update VmInstanceUtils to setup new fields
    1. support create vm without image
    2. refactor VmInstanceUtils adjust the order of code pieces
    3. remove unused method in VmInstanceUtils
    
    Resolves: ZSTAC-61497
    
    Change-Id: I6464766c64766a6a78666d646a747a6b6a64766e
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- compute/src/main/java/org/zstack/compute/vm/VmInstanceUtils.java
+++ compute/src/main/java/org/zstack/compute/vm/VmInstanceUtils.java
@@ -2,8 +2,11 @@ package org.zstack.compute.vm;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.zstack.core.Platform;
+import org.zstack.core.db.Q;
 import org.zstack.header.configuration.InstanceOfferingInventory;
 import org.zstack.header.errorcode.OperationFailureException;
+import org.zstack.header.image.ImageVO;
+import org.zstack.header.image.ImageVO_;
 import org.zstack.header.vm.APIChangeInstanceOfferingMsg;
 import org.zstack.header.vm.APICreateVmInstanceMsg;
 import org.zstack.header.vm.CreateVmInstanceMsg;
@@ -22,7 +25,30 @@ import static java.util.Objects.requireNonNull;
 public class VmInstanceUtils {
     public static CreateVmInstanceMsg fromAPICreateVmInstanceMsg(APICreateVmInstanceMsg msg) {
         CreateVmInstanceMsg cmsg = NewVmInstanceMsgBuilder.fromAPINewVmInstanceMsg(msg);
+
+        boolean useVirtioSettingOfImage = msg.getVirtio() == null;
+
         cmsg.setImageUuid(msg.getImageUuid());
+        // create vm without image is supported
+        if (msg.getImageUuid() != null) {
+            ImageVO image = Q.New(ImageVO.class).eq(ImageVO_.uuid, msg.getImageUuid()).find();
+            cmsg.setPlatform(msg.getPlatform() == null ? image.getPlatform().toString() : msg.getPlatform());
+            cmsg.setGuestOsType(msg.getGuestOsType() == null ? image.getGuestOsType() : msg.getGuestOsType());
+            cmsg.setArchitecture(msg.getArchitecture() == null ? image.getArchitecture() : msg.getArchitecture());
+
+            if (useVirtioSettingOfImage) {
+                cmsg.setVirtio(image.getVirtio());
+            }
+        } else {
+            cmsg.setPlatform(msg.getPlatform());
+            cmsg.setArchitecture(msg.getArchitecture());
+            cmsg.setGuestOsType(msg.getGuestOsType());
+        }
+
+        if (!useVirtioSettingOfImage) {
+            cmsg.setVirtio(msg.isVirtio());
+        }
+
         cmsg.setRootDiskOfferingUuid(msg.getRootDiskOfferingUuid());
         if (msg.getRootDiskSize() != null) {
             cmsg.setRootDiskSize(msg.getRootDiskSize());
@@ -33,22 +59,15 @@ public class VmInstanceUtils {
         cmsg.setDataVolumeSystemTags(msg.getDataVolumeSystemTags());
         cmsg.setPrimaryStorageUuidForRootVolume(msg.getPrimaryStorageUuidForRootVolume());
         cmsg.setDataVolumeSystemTagsOnIndex(msg.getDataVolumeSystemTagsOnIndex());
-        cmsg.setSshKeyPairUuids(msg.getSshKeyPairUuids());
-        cmsg.setPlatform(msg.getPlatform());
-        cmsg.setGuestOsType(msg.getGuestOsType());
-        cmsg.setArchitecture(msg.getArchitecture());
+
+        cmsg.setDiskAOs(msg.getDiskAOs());
+
         if (CollectionUtils.isNotEmpty(msg.getDataDiskOfferingUuids()) || CollectionUtils.isNotEmpty(msg.getDataDiskSizes())) {
             cmsg.setPrimaryStorageUuidForDataVolume(getPSUuidForDataVolume(msg.getSystemTags()));
         }
-        cmsg.setPlatform(msg.getPlatform());
-        cmsg.setGuestOsType(msg.getGuestOsType());
-        cmsg.setArchitecture(msg.getArchitecture());
-        if (msg.getVirtio() == null) {
-            cmsg.setVirtio(false);
-        } else {
-            cmsg.setVirtio(msg.isVirtio());
-        }
-        cmsg.setDiskAOs(msg.getDiskAOs());
+
+        cmsg.setSshKeyPairUuids(msg.getSshKeyPairUuids());
+
         return cmsg;
     }
 
@@ -116,23 +135,5 @@ public class VmInstanceUtils {
         }
 
         return spec;
-    }
-
-    private static void setVmInstanceInfoFromRootDiskAO(CreateVmInstanceMsg cmsg, APICreateVmInstanceMsg msg) {
-        if (CollectionUtils.isEmpty(msg.getDiskAOs())) {
-            return;
-        }
-        APICreateVmInstanceMsg.DiskAO rootdiskAO = msg.getDiskAOs().stream()
-                .filter(APICreateVmInstanceMsg.DiskAO::isBoot).findFirst().orElse(null);
-        if (rootdiskAO == null) {
-            return;
-        }
-        cmsg.setPlatform(rootdiskAO.getPlatform());
-        cmsg.setGuestOsType(rootdiskAO.getGuestOsType());
-        cmsg.setArchitecture(rootdiskAO.getArchitecture());
-        if (CollectionUtils.isNotEmpty(rootdiskAO.getSystemTags())
-                && rootdiskAO.getSystemTags().contains(VmSystemTags.VIRTIO.getTagFormat())) {
-            cmsg.setVirtio(true);
-        }
     }
 }
