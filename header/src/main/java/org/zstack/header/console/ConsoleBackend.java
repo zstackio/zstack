833bcdc47eb80b10ed3a3da41e8a68654d813c02
shanshan.ning <shanshan.ning@zstack.io>
2023-11-24 11:47:01 +0800

    <feature>[console]: deactivate console proxy when rebooting vm
    when rebooting, the token file should be deleted to avoid connecting to
    the wrong vm console because the old target port being occupied by
    another vm.
    
    Related: ZSTAC-60409
    
    Change-Id: I6e7667726b66666963776365736c65686c707369
    (cherry picked from commit 178739894616a26c21780915e692897f95fa95db)

--- header/src/main/java/org/zstack/header/console/ConsoleBackend.java
+++ header/src/main/java/org/zstack/header/console/ConsoleBackend.java
@@ -27,4 +27,6 @@ public interface ConsoleBackend {
     String returnServiceIdForConsoleAgentMsg(ConsoleProxyAgentMessage msg, String agentUuid);
 
     void handleMessage(Message msg);
+
+    void deactivateConsoleProxy(VmInstanceInventory vmInventory, Completion completion);
 }
