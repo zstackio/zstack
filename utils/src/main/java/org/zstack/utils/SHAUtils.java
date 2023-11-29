3df07d8250b7279553bc3875c9fdc6e72d7e1b56
tao.gan <tao.gan@zstack.io>
2023-11-29 16:55:55 +0800

    <feature>[ansible]: add SshFilesMd5Checker
    1 Cache inodes and hashes of all architectur bin packages in mn memory
    2 If the bin package hash value of the target physical machine is
    not in the in-memory hash list, redeploy the bin package.
    
    Resolves: ZSTAC-59604
    
    Change-Id: I75676474797078746d757a7564716d736f6b7267

--- utils/src/main/java/org/zstack/utils/SHAUtils.java
+++ utils/src/main/java/org/zstack/utils/SHAUtils.java
@@ -1,7 +1,10 @@
 package org.zstack.utils;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,6 +20,19 @@ public class SHAUtils {
             BigInteger bigInteger = new BigInteger(1, md.digest());
             return String.format("%0128x", bigInteger);
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getDigestOfFile(String filePath, String algorithm, int precision) {
+        try {
+            byte[] fileData = Files.readAllBytes(Paths.get(filePath));
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            md.reset();
+            md.update(fileData);
+            BigInteger bigInteger = new BigInteger(1, md.digest());
+            return String.format("%" + precision + "x", bigInteger);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
