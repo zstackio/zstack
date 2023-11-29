c36bdc79029786fdf2d382b38fe64b9f9a418b18
littleya <me@littleya.com>
2023-11-29 09:36:28 +0800

    <fix>[localstorage]: fix failed clone vm when the vm in pause state
    During vm clone, the vm will crash if call query-blockstats before the
    qcow2 disk format. Therefore, create the qcow2 disk before do
    takesnapshot on local storage and nfs storage.
    
    Resolves: ZSTAC-59110
    
    Change-Id: I757067666a7a746b696c68626f79727a6a73796e

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageHypervisorBackend.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageHypervisorBackend.java
@@ -9,6 +9,7 @@ import org.zstack.header.storage.primary.*;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;
 import org.zstack.header.volume.BatchSyncVolumeSizeOnPrimaryStorageMsg;
 import org.zstack.header.volume.BatchSyncVolumeSizeOnPrimaryStorageReply;
+import org.zstack.header.volume.VolumeInfo;
 import org.zstack.header.volume.VolumeInventory;
 import org.zstack.storage.primary.EstimateVolumeTemplateSizeOnPrimaryStorageMsg;
 import org.zstack.storage.primary.EstimateVolumeTemplateSizeOnPrimaryStorageReply;
@@ -108,6 +109,7 @@ public abstract class LocalStorageHypervisorBackend extends LocalStorageBase {
 
     abstract void deleteBits(String path, String hostUuid, Completion completion);
 
+    abstract void createEmptyVolume(VolumeInventory volume, String hostUuid, ReturnValueCompletion<VolumeInfo> completion);
 
     abstract void checkHostAttachedPSMountPath(String hostUuid, Completion completion);
 
