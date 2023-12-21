bba7d3ca252c2a153f4c8e5b6beea25371d22bf8
shenjin <jin.shen@zstack.io>
2023-12-21 11:13:33 +0800

    <fix>[config]: update globalConfigOptions number range
    update globalConfigOptions number range,add options field name,
    contains number equal scenario
    
    Resolves: ZSV-4045
    
    Change-Id: I756562666169776b6563646d6b61617467726a63

--- sdk/src/main/java/org/zstack/sdk/GlobalConfigOptions.java
+++ sdk/src/main/java/org/zstack/sdk/GlobalConfigOptions.java
@@ -28,4 +28,20 @@ public class GlobalConfigOptions  {
         return this.numberLessThan;
     }
 
+    public java.lang.Long numberGreaterThanOrEqual;
+    public void setNumberGreaterThanOrEqual(java.lang.Long numberGreaterThanOrEqual) {
+        this.numberGreaterThanOrEqual = numberGreaterThanOrEqual;
+    }
+    public java.lang.Long getNumberGreaterThanOrEqual() {
+        return this.numberGreaterThanOrEqual;
+    }
+
+    public java.lang.Long numberLessThanOrEqual;
+    public void setNumberLessThanOrEqual(java.lang.Long numberLessThanOrEqual) {
+        this.numberLessThanOrEqual = numberLessThanOrEqual;
+    }
+    public java.lang.Long getNumberLessThanOrEqual() {
+        return this.numberLessThanOrEqual;
+    }
+
 }
