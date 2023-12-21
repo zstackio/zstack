af6df186411713db32ecee8a2306d84ec3c603ee
J M <jin.ma@zstack.io>
2023-12-21 19:44:16 +0800

    <fix>[compute]: support migrate with iso
    Resolves: SUG-622
    
    Change-Id: I62716c7876716c79796a7676616b6f64666c6164

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
@@ -3816,6 +3816,7 @@ public class KVMAgentCommands {
     }
 
     public static class LoginIscsiTargetCmd extends AgentCommand implements Serializable {
+        private String url;
         private String hostname;
         private int port;
         private String target;
@@ -3861,6 +3862,14 @@ public class KVMAgentCommands {
 
         public void setTarget(String target) {
             this.target = target;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
         }
     }
 
