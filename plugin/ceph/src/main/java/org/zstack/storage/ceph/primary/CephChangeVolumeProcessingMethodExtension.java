7246587d2be7381220e56e6cb255faaff19c2383
shan.wu <shan.wu@zstack.io>
2024-01-04 19:08:01 +0800

    <fix>[link-clone]: change volumeSnapshotRef after overwritevolume if need
    change VolumeSnapshotRefVO after volume installpath changed if need
    
    Resolves: ZSTAC-62524
    
    Change-Id: I6f657861636e6f77756468696878636579787173

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephChangeVolumeProcessingMethodExtension.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephChangeVolumeProcessingMethodExtension.java
@@ -6,19 +6,19 @@ import org.zstack.header.storage.primary.PrimaryStorageVO_;
 import org.zstack.header.storage.snapshot.VolumeSnapshotVO;
 import org.zstack.header.storage.snapshot.VolumeSnapshotVO_;
 import org.zstack.header.volume.VolumeDeletionPolicyManager;
-import org.zstack.header.volume.VolumeVO;
+import org.zstack.header.volume.VolumeInventory;
 import org.zstack.storage.ceph.CephConstants;
 import org.zstack.storage.volume.ChangeVolumeProcessingMethodExtensionPoint;
 
 public class CephChangeVolumeProcessingMethodExtension implements ChangeVolumeProcessingMethodExtensionPoint {
     @Override
-    public VolumeDeletionPolicyManager.VolumeDeletionPolicy getTransientVolumeDeletionPolicy(VolumeVO vo) {
-        String psType = Q.New(PrimaryStorageVO.class).eq(PrimaryStorageVO_.uuid, vo.getPrimaryStorageUuid()).select(PrimaryStorageVO_.type).findValue();
+    public VolumeDeletionPolicyManager.VolumeDeletionPolicy getTransientVolumeDeletionPolicy(VolumeInventory transientVolume) {
+        String psType = Q.New(PrimaryStorageVO.class).eq(PrimaryStorageVO_.uuid, transientVolume.getPrimaryStorageUuid()).select(PrimaryStorageVO_.type).findValue();
         if (!psType.equals(CephConstants.CEPH_PRIMARY_STORAGE_TYPE)) {
             return null;
         }
-        boolean hasSnapshots = Q.New(VolumeSnapshotVO.class).eq(VolumeSnapshotVO_.primaryStorageUuid, vo.getPrimaryStorageUuid())
-                .like(VolumeSnapshotVO_.primaryStorageInstallPath, String.format("%s@%%", vo.getInstallPath())).isExists();
+        boolean hasSnapshots = Q.New(VolumeSnapshotVO.class).eq(VolumeSnapshotVO_.primaryStorageUuid, transientVolume.getPrimaryStorageUuid())
+                .like(VolumeSnapshotVO_.primaryStorageInstallPath, String.format("%s@%%", transientVolume.getInstallPath())).isExists();
         if (!hasSnapshots) {
             return VolumeDeletionPolicyManager.VolumeDeletionPolicy.Direct;
         }
