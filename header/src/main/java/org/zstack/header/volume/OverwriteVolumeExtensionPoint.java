0462850ce30eebd692c05a37ed7516b3330854df
shan.wu <shan.wu@zstack.io>
2024-01-04 19:08:01 +0800

    <fix>[link-clone]: change volumeSnapshotRef after overwritevolume if need
    change VolumeSnapshotRefVO after volume installpath changed if need
    
    Resolves: ZSTAC-62524
    
    Change-Id:5A06BEE3B7034065BBFC9A140AD352AB
    
    
    (cherry picked from commit c25b813847b29fce9e2cf652f7ec84cbb95b87c9)

--- header/src/main/java/org/zstack/header/volume/OverwriteVolumeExtensionPoint.java
+++ header/src/main/java/org/zstack/header/volume/OverwriteVolumeExtensionPoint.java
@@ -1,8 +1,9 @@
 package org.zstack.header.volume;
-
+import org.zstack.header.volume.VolumeDeletionPolicyManager.VolumeDeletionPolicy;
 /**
  * Created by MaJin on 2019/4/2.
  */
 public interface OverwriteVolumeExtensionPoint {
+    void innerOverwriteVolume(VolumeInventory originVolume, VolumeInventory transientVolume, VolumeDeletionPolicy originVolumeDeletionPolicy);
     void afterOverwriteVolume(VolumeInventory volume, VolumeInventory transientVolume);
 }
