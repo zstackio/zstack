63b0e2de7af7de06cd97b2d273cd5be3e1d58183
Zhang, Wenhao <wenhao.zhang@zstack.io>
2024-01-19 07:43:46 +0000

    <fix>[conf]: append zsv 4.8.0.2 schema
    Related: ZSV-4408
    
    DBImpact
    
    Change-Id: I6e7366766968677172626874737266706f636d78

--- conf/db/upgrade/V4.8.0.2__schema.sql
+++ conf/db/upgrade/V4.8.0.2__schema.sql
@@ -1,5 +1,10 @@
+-- in version zsv_4.1.6
+
 ALTER TABLE AuditsVO ADD COLUMN `resourceName` varchar(255) DEFAULT NULL;
 
 ALTER TABLE `zstack`.`HostNetworkInterfaceVO` ADD COLUMN `deviceName` VARCHAR(64) DEFAULT NULL AFTER `deviceId`;
 ALTER TABLE `zstack`.`HostNetworkInterfaceVO` ADD COLUMN `vendorName` VARCHAR(64) DEFAULT NULL AFTER `deviceName`;
 ALTER TABLE `zstack`.`HostNetworkInterfaceVO` ADD COLUMN `subvendorName` VARCHAR(64) DEFAULT NULL AFTER `subdeviceId`;
+
+-- from issue: (feature) ZSV-4408
+ALTER TABLE `zstack`.`VmSchedHistoryVO` ADD COLUMN `reason` text DEFAULT NULL;
