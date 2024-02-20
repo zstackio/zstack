4abfd32d426c4e7ebeb628817bc4d55f14334be1
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-02-20 11:59:54 +0800

    <fix>[conf]: add AccountResourceRefVO for usb device resource
    Related: ZSV-4726
    
    DBImpact
    
    Change-Id: I64746f6368796c6c656d6e6e616c6161766a6f73

--- conf/db/upgrade/V4.8.0.3__schema.sql
+++ conf/db/upgrade/V4.8.0.3__schema.sql
@@ -2,3 +2,7 @@
 -- Feature: USB device support sharing | ZSV-4726
 delete from ResourceVO where resourceType = 'UsbDeviceVO' and uuid not in (select uuid from UsbDeviceVO);
 
+insert into AccountResourceRefVO (`accountUuid`,`ownerAccountUuid`,`resourceUuid`,`resourceType`,`permission`,`isShared`,`lastOpDate`,`createDate`,`concreteResourceType`)
+select '36c27e8ff05c4780bf6d2fa65700f22e', '36c27e8ff05c4780bf6d2fa65700f22e', uuid, resourceType, 2, false, NOW(), NOW(), 'org.zstack.usbDevice.UsbDeviceVO'
+    from ResourceVO where resourceType = 'UsbDeviceVO';
+
