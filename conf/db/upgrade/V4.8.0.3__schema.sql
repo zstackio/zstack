c1093c0108ba7d9a139a93a82403c8c9b3e62d73
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-02-20 10:43:17 +0800

    <fix>[conf]: clean dirty usb device ResourceVO
    Resolves: ZSV-4737
    Related: ZSV-4726
    
    DBImpact
    
    Change-Id: I6c756775616d6467617374707a6c757162716371

--- conf/db/upgrade/V4.8.0.3__schema.sql
+++ conf/db/upgrade/V4.8.0.3__schema.sql
@@ -1,2 +1,4 @@
 -- in version zsv_4.2.0
+-- Feature: USB device support sharing | ZSV-4726
+delete from ResourceVO where resourceType = 'UsbDeviceVO' and uuid not in (select uuid from UsbDeviceVO);
 
