ad2f0498fef35cd24e40329f21f1f5b45197f42f
AlanJager <ye.zou@zstack.io>
2024-02-28 18:29:05 +0800

    <fix>[kvm]: refactor KvmHostGetExtraPackagesExtensionPoint
    1. rename KvmHostGetExtraPackagesExtensionPoint to KvmHostAgentDeploymentExtensionPoint
    2. rename getExtraPackages to appendExtraPackages
    3. support trace KVMHostDeployArguments during extension point
    4. add modifyDeploymentArguments to KvmHostGetExtraPackagesExtensionPoint
    
    Resolves: ZSTAC-59969
    
    Change-Id: I6373677a627572616c6c67706262716767626170

--- plugin/kvm/src/main/java/org/zstack/kvm/KvmHostAgentDeploymentExtensionPoint.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KvmHostAgentDeploymentExtensionPoint.java
@@ -2,6 +2,10 @@ package org.zstack.kvm;
 
 import org.zstack.header.host.HostInventory;
 
-public interface KvmHostGetExtraPackagesExtensionPoint {
-    String getExtraPackages(HostInventory host);
+import java.util.List;
+
+public interface KvmHostAgentDeploymentExtensionPoint {
+    List<String> appendExtraPackages(HostInventory host);
+
+    void modifyDeploymentArguments(HostInventory host, KVMHostDeployArguments args);
 }
