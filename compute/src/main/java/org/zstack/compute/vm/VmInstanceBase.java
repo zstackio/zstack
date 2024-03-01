181e5c67070bf83fa4e0764d6f957f2ce2a13fac
AlanJager <ye.zou@zstack.io>
2024-03-01 16:57:38 +0800

    <fix>[compute]: delete vm device address if BIOS mode changed
    Resolves: ZSTAC-59291
    
    Change-Id: I7a756a626867657168616d677975717a61676f75
    Signed-off-by: AlanJager <ye.zou@zstack.io>
    (cherry picked from commit babed020dc5138a6a7da435c8e5bb3abf638685b)

--- compute/src/main/java/org/zstack/compute/vm/VmInstanceBase.java
+++ compute/src/main/java/org/zstack/compute/vm/VmInstanceBase.java
@@ -3608,6 +3608,8 @@ public class VmInstanceBase extends AbstractVmInstance {
     }
 
     private void handle(APISetVmBootModeMsg msg) {
+        final boolean[] bootModeChanged = {false};
+
         FlowChain chain = new SimpleFlowChain();
         chain.then(new Flow() {
             String __name__ = "set-vm-boot-mode";
@@ -3624,6 +3626,9 @@ public class VmInstanceBase extends AbstractVmInstance {
                 creator.create();
 
                 originLevel = msg.getBootMode();
+
+                bootModeChanged[0] = true;
+
                 trigger.next();
             }
 
@@ -3660,6 +3665,10 @@ public class VmInstanceBase extends AbstractVmInstance {
             public void handle(Map data) {
                 APISetVmBootModeEvent evt = new APISetVmBootModeEvent(msg.getId());
                 bus.publish(evt);
+
+                if (bootModeChanged[0]) {
+                    vidm.deleteAllDeviceAddressesByVm(self.getUuid());
+                }
             }
         }).start();
     }
