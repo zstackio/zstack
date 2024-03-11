9a155fd9d1af4a95cd297cddcc3c838156199b34
shenjin <jin.shen@zstack.io>
2024-03-11 14:56:52 +0800

    <feature>[expon]: expon block volume snapshot operation
    snapshot create, update, delete, resize
    
    Resolves: ZSTAC-63739
    
    Change-Id: I7a616b6679746a74746c7a7661666879626e7778

---
+++ plugin/expon/src/main/java/org/zstack/expon/sdk/volume/UpdateVolumeSnapshotRequest.java
@@ -0,0 +1,57 @@
+package org.zstack.expon.sdk.volume;
+
+import org.springframework.http.HttpMethod;
+import org.zstack.expon.sdk.ExponRequest;
+import org.zstack.expon.sdk.ExponRestRequest;
+import org.zstack.expon.sdk.Param;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ExponRestRequest(
+        path = "/block/snaps/{id}",
+        method = HttpMethod.PUT,
+        responseClass = UpdateVolumeSnapshotResponse.class,
+        sync = false
+)
+public class UpdateVolumeSnapshotRequest extends ExponRequest {
+    private static final HashMap<String, Parameter> parameterMap = new HashMap<>();
+
+    @Param
+    private String id;
+
+    @Param
+    private String name;
+
+    @Param
+    private String description;
+
+    @Override
+    public Map<String, Parameter> getParameterMap() {
+        return parameterMap;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
