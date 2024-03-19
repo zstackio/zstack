4ac99213bd5608a4bb32056ec59425b6ef7f128d
AlanJager <ye.zou@zstack.io>
2024-03-19 17:03:39 +0800

    <fix>[allocator]: introduce get reserved hosts capacity
    Resolves: ZSTAC-60658
    
    Change-Id: I6d636e61766574696d6b73717a6461676c636d6c
    (cherry picked from commit 6969987f54f38c0ef6e0f07a1158981f2ffbb287)

--- header/src/main/java/org/zstack/header/allocator/HostReservedCapacityExtensionPoint.java
+++ header/src/main/java/org/zstack/header/allocator/HostReservedCapacityExtensionPoint.java
@@ -1,9 +1,14 @@
 package org.zstack.header.allocator;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  */
 public interface HostReservedCapacityExtensionPoint {
     String getHypervisorTypeForHostReserveCapacityExtension();
 
     ReservedHostCapacity getReservedHostCapacity(String hostUuid);
+
+    Map<String, ReservedHostCapacity> getReservedHostsCapacity(List<String> hostUuids);
 }
