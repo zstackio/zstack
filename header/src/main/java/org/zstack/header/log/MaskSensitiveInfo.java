beb957f2918ed7035edcd645a3d217c049e51293
J M <jin.ma@zstack.io>
2024-04-02 15:16:42 +0800

    <fix>[storage]: add primary storage pswd mask
    it is for default mask config.
    
    Resolves: ZSTAC-64649
    
    Change-Id: I66706c63727a626e67676c6b7276736668616c6d

--- header/src/main/java/org/zstack/header/log/MaskSensitiveInfo.java
+++ header/src/main/java/org/zstack/header/log/MaskSensitiveInfo.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * mask sensitive on REST even if CoreGlobalProperty.MASK_SENSITIVE_INFO is set to false
+ * see also {@link NoLogging}
+ */
+
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MaskSensitiveInfo {
