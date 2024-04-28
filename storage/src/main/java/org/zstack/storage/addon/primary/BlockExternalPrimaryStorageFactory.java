554383b8c03c798dd4defb27e8223ae7368e4f2d
shenjin <jin.shen@zstack.io>
2024-04-28 18:40:59 +0800

    <fix>[storage]: refactor get externalPs on BareMetalGateway
    refactor get external primary storage
     on BareMetalGateway
    
    Resolves: ZSTAC-65313
    
    Change-Id: I6665637a68657a6865746e66707768647a6e7073
    (cherry picked from commit 176343ccc47349de04b589a59afef8b55685754e)

--- storage/src/main/java/org/zstack/storage/addon/primary/BlockExternalPrimaryStorageFactory.java
+++ storage/src/main/java/org/zstack/storage/addon/primary/BlockExternalPrimaryStorageFactory.java
@@ -1,9 +1,19 @@
 package org.zstack.storage.addon.primary;
 
+import org.zstack.header.host.HostInventory;
+import org.zstack.header.storage.addon.primary.BaseVolumeInfo;
 import org.zstack.header.storage.primary.PrimaryStorageVO;
+
+import java.util.Map;
 
 public interface BlockExternalPrimaryStorageFactory {
     String getType();
 
     BlockExternalPrimaryStorageBackend getBlockExternalPrimaryStorageBackend(PrimaryStorageVO vo);
+
+    void activeIscsiVolume(String clientIqn, BaseVolumeInfo vol, boolean shareable);
+
+    Map<String, String> getVolumeIscsiInfo(String volInstallPath, HostInventory host);
+
+    String getVolumeLunId(String volInstallPath);
 }
