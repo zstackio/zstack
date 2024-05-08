7d0c7ee9748051ec58e07621cf2c74bf919f925b
hanyu.liang <hanyu.liang@zstack.io>
2024-05-08 17:44:42 +0800

    <fix>[sso-plugin]:scope field sets default value openid
    Resolves: ZSTAC-65442
    
    Change-Id: I6467644428676e6d666175747477786a71677963

--- conf/db/upgrade/V4.8.28__schema.sql
+++ conf/db/upgrade/V4.8.28__schema.sql
@@ -1,4 +1,6 @@
-ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `scope` varchar(255) default NULL;
+ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `scope` varchar(255) default 'openid';
 
 ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `identityProvider` varchar(16) default NULL;
+
+
 
