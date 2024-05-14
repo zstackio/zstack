6e2b1e758a57ad6eab362fad54857d1818ed144f
boce.wang <boce.wang@zstack.io>
2024-05-14 17:04:20 +0800

    update sql

--- conf/db/upgrade/V5.1.0__schema.sql
+++ conf/db/upgrade/V5.1.0__schema.sql
@@ -66,29 +66,3 @@ CREATE TABLE IF NOT EXISTS `zstack`.`ExponBlockVolumeVO` (
     PRIMARY KEY (`uuid`),
     CONSTRAINT fkExponBlockVolumeVOBlockVolumeVO FOREIGN KEY (uuid) REFERENCES BlockVolumeVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `zstack`.`SNSUniversalSmsEndpointVO`
-(
-    `uuid`               varchar(32)  NOT NULL UNIQUE,
-    `smsAccessKeyId`     varchar(128) NOT NULL,
-    `smsAccessKeySecret` varchar(128) NOT NULL,
-    `supplier`           varchar(32)  NOT NULL,
-    PRIMARY KEY (`uuid`),
-    CONSTRAINT fkSNSUniversalSmsEndpointVOSNSApplicationEndpointVO FOREIGN KEY (uuid) REFERENCES SNSApplicationEndpointVO (uuid) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `zstack`.`SNSEmaySmsEndpointVO`
-(
-    `uuid`       varchar(32)  NOT NULL UNIQUE,
-    `requestUrl` varchar(128) NOT NULL,
-    PRIMARY KEY (`uuid`),
-    CONSTRAINT fkSNSEmaySmsEndpointVOSNSApplicationEndpointVO FOREIGN KEY (uuid) REFERENCES SNSApplicationEndpointVO (uuid) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `zstack`.`SNSSmsEndpointVO` RENAME TO `zstack`.`SNSAliyunSmsEndpointVO`;
-ALTER TABLE `zstack`.`SNSAliyunSmsEndpointVO` DROP FOREIGN KEY fkSNSSmsEndpointVOSNSApplicationEndpointVO;
-ALTER TABLE `zstack`.`SNSAliyunSmsEndpointVO`
-    ADD CONSTRAINT fkSNSAliyunSmsEndpointVOSNSApplicationEndpointVO FOREIGN KEY (uuid) REFERENCES `zstack`.`SNSApplicationEndpointVO` (uuid) ON DELETE CASCADE;
-ALTER TABLE `zstack`.`SNSSmsReceiverVO` DROP FOREIGN KEY fkSNSSmsReceiverVOSNSSmsEndpointVO;
-ALTER TABLE `zstack`.`SNSSmsReceiverVO`
-    ADD CONSTRAINT fkSNSSmsReceiverVOSNSUniversalSmsEndpointVO FOREIGN KEY (endpointUuid) REFERENCES `zstack`.`SNSUniversalSmsEndpointVO` (uuid) ON DELETE CASCADE;
