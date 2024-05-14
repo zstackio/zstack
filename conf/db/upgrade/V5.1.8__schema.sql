3b33b91cae7dbc0e9fe251127076e764ace14ec8
hanyu.liang <hanyu.liang@zstack.io>
2024-05-14 13:59:27 +0800

    <fix>[sso]: resolve issue with adding third-party authentication server
    Resolves: ZSTAC-65726
    
    Change-Id: I637261488d6474716e6e6569666c737662636a76

--- conf/db/upgrade/V5.1.8__schema.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `scope` varchar(255) default 'openid';
-
-ALTER TABLE `zstack`.`OAuth2ClientVO` ADD COLUMN `identityProvider` varchar(16) default NULL;
