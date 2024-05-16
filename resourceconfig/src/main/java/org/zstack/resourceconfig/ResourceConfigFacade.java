7914ea43df6ee2c20c350a12a139b981e66b2874
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-05-16 14:59:18 +0800

    <chore>[resourceconfig]: add comments for method in ResourceConfigFacade
    Add comments for method
    ResourceConfigFacade.getResourceConfigValueByResourceType()
    
    Related: ZSV-5743
    
    Change-Id: I6c6e636d65706d717577616b687467716a616f71

--- resourceconfig/src/main/java/org/zstack/resourceconfig/ResourceConfigFacade.java
+++ resourceconfig/src/main/java/org/zstack/resourceconfig/ResourceConfigFacade.java
@@ -12,6 +12,15 @@ public interface ResourceConfigFacade {
 
     <T> T getResourceConfigValue(GlobalConfig gc, String resourceUuid, Class<T> clz);
 
+    /**
+     * <p>This method will get the resource configuration from the specified type.
+     *
+     * <p>If the type does not have a resource configuration set,
+     * it will return <code>null</code> and will NOT fetch
+     * the configuration of the upper level resource or the global configuration.
+     * </p>
+     * @since zsv 4.1.0
+     */
     <T> T getResourceConfigValueByResourceType(GlobalConfig gc, String resourceUuid, String type, Class<T> clz);
 
     <T> Map<String, T> getResourceConfigValues(GlobalConfig gc, List<String> resourceUuids, Class<T> clz);
