d2b4af641a44492875296701648f06d3afdcea1d
tao.gan <tao.gan@zstack.io>
2024-05-28 13:49:40 +0800

    <fix>[vm]: create vm from vm backup
    1 APICreateVmFromVmBackupMsg support specified cpuNum, memorySize,
      reservedMemorySize.
    2 APIRevertVmFromVmBackupMsg  support specified strategy
    
    APIImpact
    
    Resolves: ZSV-5764
    
    Change-Id: I75676f62616a71696d726f7a666b6368656c6873

--- sdk/src/main/java/org/zstack/heder/storage/volume/backup/CreateVmFromVmBackupAction.java
+++ sdk/src/main/java/org/zstack/heder/storage/volume/backup/CreateVmFromVmBackupAction.java
@@ -34,7 +34,7 @@ public class CreateVmFromVmBackupAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String backupStorageUuid;
 
-    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String instanceOfferingUuid;
 
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
@@ -72,6 +72,15 @@ public class CreateVmFromVmBackupAction extends AbstractAction {
 
     @Param(required = false, validValues = {"InstantStart","JustCreate","CreateStopped"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String strategy = "InstantStart";
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.Integer cpuNum;
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.Long memorySize;
+
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, numberRange = {0L,9223372036854775807L}, noTrim = false)
+    public java.lang.Long reservedMemorySize;
 
     @Param(required = false)
     public java.lang.String defaultL3NetworkUuid;
