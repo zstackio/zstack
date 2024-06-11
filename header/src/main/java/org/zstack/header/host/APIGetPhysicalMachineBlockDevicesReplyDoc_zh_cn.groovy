5d5a7f51d6d73ef81db99816c41c690eddb94ef2
tao.gan <tao.gan@zstack.io>
2024-06-11 14:26:25 +0800

    <feature>[host]: use server free disk
    Resolves: ZSV-5764
    
    Change-Id: I6d6c696677747a62746261777a72646c67616267

---
+++ header/src/main/java/org/zstack/header/host/APIGetPhysicalMachineBlockDevicesReplyDoc_zh_cn.groovy
@@ -0,0 +1,32 @@
+package org.zstack.header.host
+
+
+import org.zstack.header.errorcode.ErrorCode
+
+doc {
+
+	title "获取服务器磁盘信息返回"
+
+	ref {
+		name "blockDevices"
+		path "org.zstack.header.host.APIGetPhysicalMachineBlockDevicesReply.blockDevices"
+		desc "物理机磁盘信息"
+		type "BlockDevices"
+		since "zsv 4.3.0"
+		clz BlockDevicesParser.class
+	}
+	field {
+		name "success"
+		desc ""
+		type "boolean"
+		since "zsv 4.3.0"
+	}
+	ref {
+		name "error"
+		path "org.zstack.header.host.APIGetPhysicalMachineBlockDevicesReply.error"
+		desc "错误码，若不为null，则表示操作失败, 操作成功时该字段为null",false
+		type "ErrorCode"
+		since "zsv 4.3.0"
+		clz ErrorCode.class
+	}
+}
---
+++ header/src/main/java/org/zstack/header/host/BlockDevicesDoc_zh_cn.groovy
@@ -0,0 +1,25 @@
+package org.zstack.header.host
+
+import org.zstack.header.host.APIGetPhysicalMachineBlockDevicesReply.BlockDevices.BlockDevice
+
+doc {
+
+	title "物理机所有磁盘信息"
+
+	ref {
+		name "unusedBlockDevices"
+		path "org.zstack.header.host.APIGetPhysicalMachineBlockDevicesReply.BlockDevices.unusedBlockDevices"
+		desc "没有分区且没有挂载目录或者所有分区没有挂载目录的磁盘"
+		type "List"
+		since "zsv 4.3.0"
+		clz BlockDevice.class
+	}
+	ref {
+		name "usedBlockDevices"
+		path "org.zstack.header.host.APIGetPhysicalMachineBlockDevicesReply.BlockDevices.usedBlockDevices"
+		desc "已挂载目录或者有分区挂载目录的磁盘"
+		type "List"
+		since "zsv 4.3.0"
+		clz BlockDevice.class
+	}
+}
