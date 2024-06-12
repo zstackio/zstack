0d582b8815de9fa822ce92282a817633e327f42e
lianghy <hanyu.liang@zstack.io>
2024-06-12 15:46:43 +0800

    <fix>[sso]: support sso server
    Resolves: ZSTAC-66623
    
    Change-Id: I6467666238676e6d666175747477786a71677963

---
+++ sdk/src/main/java/org/zstack/sdk/TokenIntrospectionResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class TokenIntrospectionResult {
+    public boolean active;
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public boolean getActive() {
+        return this.active;
+    }
+
+}
