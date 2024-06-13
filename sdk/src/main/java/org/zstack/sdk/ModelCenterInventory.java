b74f256f99c5b81bbfe161fc6464fd3f5130c81f
Lei Liu <lei.liu@zstack.io>
2024-06-13 17:50:36 +0800

    <feature>[sdk]: Add sdk
    Change-Id: I797a7771746c736a7066706c7872666a78706b69

---
+++ sdk/src/main/java/org/zstack/sdk/ModelCenterInventory.java
@@ -0,0 +1,47 @@
+package org.zstack.sdk;
+
+
+
+public class ModelCenterInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String description;
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+    public java.lang.String getDescription() {
+        return this.description;
+    }
+
+    public java.lang.String url;
+    public void setUrl(java.lang.String url) {
+        this.url = url;
+    }
+    public java.lang.String getUrl() {
+        return this.url;
+    }
+
+    public java.lang.String parameters;
+    public void setParameters(java.lang.String parameters) {
+        this.parameters = parameters;
+    }
+    public java.lang.String getParameters() {
+        return this.parameters;
+    }
+
+}
