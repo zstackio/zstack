a505315d23dc870de0732db94147c459a96a791e
lin.ma <lin.ma@zstack.io>
2024-06-14 15:16:33 +0800

    <fix>[log]: dynamic load custom log configuration
    dynamic load custom log configuration
    
    Resolves: ZSTAC-65832
    
    Change-Id: I7377766f7769686e68667a67756669707375647a

--- core/src/main/java/org/zstack/core/log/LogGlobalConfig.java
+++ core/src/main/java/org/zstack/core/log/LogGlobalConfig.java
@@ -1,6 +1,7 @@
 package org.zstack.core.log;
 
 import org.zstack.core.config.GlobalConfig;
+import org.zstack.core.config.GlobalConfigDef;
 import org.zstack.core.config.GlobalConfigDefinition;
 import org.zstack.core.config.GlobalConfigValidation;
 
@@ -13,4 +14,8 @@ public class LogGlobalConfig {
 
     @GlobalConfigValidation
     public static GlobalConfig LOG_DELETE_ACCUMULATED_FILE_SIZE = new GlobalConfig(CATEGORY, "log.delete.accumulatedFileSize");
+
+    @GlobalConfigValidation(numberGreaterThan = 1)
+    @GlobalConfigDef(defaultValue = "30", type = Long.class, description = "sync custom log configuration interval")
+    public static GlobalConfig SYNC_CUSTOM_LOG_CONFIGURATION_TASK_INTERVAL = new GlobalConfig(CATEGORY, "log.syncCustomLogConfigurationTaskInterval");
 }
