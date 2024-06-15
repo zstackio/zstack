89d3d6ea18c61b650c6e0667531e32bdd7464445
ye.zou <ye.zou@zstack.io>
2024-06-15 12:27:45 +0800

    <fix>[sdk]: update sdk
    Change-Id: I6a6673667a76676a7265646e656a726770756862

---
+++ sdk/src/main/java/org/zstack/sdk/ModelServiceInstanceGroupInventory.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk;
+
+
+
+public class ModelServiceInstanceGroupInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String modelServiceUuid;
+    public void setModelServiceUuid(java.lang.String modelServiceUuid) {
+        this.modelServiceUuid = modelServiceUuid;
+    }
+    public java.lang.String getModelServiceUuid() {
+        return this.modelServiceUuid;
+    }
+
+    public java.util.List instances;
+    public void setInstances(java.util.List instances) {
+        this.instances = instances;
+    }
+    public java.util.List getInstances() {
+        return this.instances;
+    }
+
+}
