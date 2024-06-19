64ab6562287d1716f16de3f2e13c6290a971c894
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-06-19 14:40:28 +0800

    <fix>[ldap]: remove deprecated ldap system tags
    Related: ZSV-5531
    
    Change-Id: I77627a68766279626b6679726477667971686777

--- plugin/ldap/src/main/java/org/zstack/ldap/LdapSystemTags.java
+++ plugin/ldap/src/main/java/org/zstack/ldap/LdapSystemTags.java
@@ -8,39 +8,6 @@ import org.zstack.tag.PatternedSystemTag;
  */
 @TagDefinition
 public class LdapSystemTags {
-
-    // clean binding filter, filter to get users who don't need to be synchronized
-    @Deprecated
-    public static String LDAP_CLEAN_BINDING_FILTER_TOKEN = "ldapCleanBindingFilter";
-    @Deprecated
-    public static PatternedSystemTag LDAP_CLEAN_BINDING_FILTER = new PatternedSystemTag(String.format("ldapCleanBindingFilter::{%s}", LDAP_CLEAN_BINDING_FILTER_TOKEN), LdapServerVO.class);
-
-    // allow list filter, filter to get the users who need to be synchronized
-    @Deprecated
-    public static String LDAP_ALLOW_LIST_FILTER_TOKEN = "ldapAllowListFilter";
-    @Deprecated
-    public static PatternedSystemTag LDAP_ALLOW_LIST_FILTER = new PatternedSystemTag(String.format("ldapAllowListFilter::{%s}", LDAP_ALLOW_LIST_FILTER_TOKEN), LdapServerVO.class);
-
-    /**
-     *  Support Types：OpenLdap, WindowsAD
-     */
-    @Deprecated
-    public static String LDAP_SERVER_TYPE_TOKEN = "ldapServerType";
-    @Deprecated
-    public static PatternedSystemTag LDAP_SERVER_TYPE = new PatternedSystemTag(String.format("ldapServerType::{%s}", LDAP_SERVER_TYPE_TOKEN), LdapServerVO.class);
-
-    /**
-     * User Specifies a unique identifier for the User, Group in Ldap / AD (eg: cn / uid / mail  / ....)
-     *
-     * Application scenario：ZStackAccount-LdapUser(Group) binding，Ldap-User Login authentication
-     *
-     */
-    @Deprecated
-    public static String LDAP_USE_AS_LOGIN_NAME_TOKEN = "ldapUseAsLoginName";
-    @Deprecated
-    public static PatternedSystemTag LDAP_USE_AS_LOGIN_NAME = new PatternedSystemTag(String.format("ldapUseAsLoginName::{%s}", LDAP_USE_AS_LOGIN_NAME_TOKEN), LdapServerVO.class);
-
     public static String LDAP_URLS_TOKEN = "ldapUrls";
     public static PatternedSystemTag LDAP_URLS = new PatternedSystemTag(String.format("ldapUrls::{%s}", LDAP_URLS_TOKEN), LdapServerVO.class);
-
 }
