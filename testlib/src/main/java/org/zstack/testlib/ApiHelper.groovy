f0331e4bb135d42408a95c24b74e1a94fd4d27a9
AlanJager <ye.zou@zstack.io>
2024-06-19 14:47:05 +0800

    <test>[testlib]: api helper
    Change-Id: I7a69727a63756d76716269767864766f79746677

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -30383,6 +30383,35 @@ abstract class ApiHelper {
     }
 
 
+    def queryModelServiceInstanceGroup(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.QueryModelServiceInstanceGroupAction.class) Closure c) {
+        def a = new org.zstack.sdk.QueryModelServiceInstanceGroupAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+        a.conditions = a.conditions.collect { it.toString() }
+
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def queryMonitorTrigger(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.QueryMonitorTriggerAction.class) Closure c) {
         def a = new org.zstack.sdk.QueryMonitorTriggerAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
