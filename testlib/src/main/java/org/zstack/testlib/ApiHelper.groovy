f383b7962f7a1272f8399078c9a418a8ea9bd478
shenjin <jin.shen@zstack.io>
2024-07-05 13:32:21 +0800

    <feature>[conf]: inspect baremetal2 chassis hardware info by instance
    inspect baremetal2 chassis hareware info by instance
    
    Resolves: ZSTAC-61603
    
    Change-Id: I636c7a726365786570667267676971747761616e

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -6821,6 +6821,33 @@ abstract class ApiHelper {
     }
 
 
+    def checkStaticProvisionIp(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.CheckStaticProvisionIpAction.class) Closure c) {
+        def a = new org.zstack.sdk.CheckStaticProvisionIpAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def checkVipPortAvailability(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.CheckVipPortAvailabilityAction.class) Closure c) {
         def a = new org.zstack.sdk.CheckVipPortAvailabilityAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
@@ -24751,6 +24778,33 @@ abstract class ApiHelper {
 
     def inspectBareMetal2Chassis(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.InspectBareMetal2ChassisAction.class) Closure c) {
         def a = new org.zstack.sdk.InspectBareMetal2ChassisAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
+    def inspectBareMetal2ChassisByInstance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.InspectBareMetal2ChassisByInstanceAction.class) Closure c) {
+        def a = new org.zstack.sdk.InspectBareMetal2ChassisByInstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
         c.resolveStrategy = Closure.OWNER_FIRST
         c.delegate = a
