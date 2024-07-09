b3c1d8113e4a87b061e58e0667dd1355a349682c
Xingwei Yu <xingwei.yu@zstack.io>
2024-07-09 11:34:25 +0800

    <fix>[plugin]: enable ha of cbd
    Resolves: ZSTAC-66162
    
    Change-Id: I6472726f757077697667736f6670686c63687569

--- plugin/cbd/src/main/java/org/zstack/cbd/kvm/KvmCbdNodeServer.java
+++ plugin/cbd/src/main/java/org/zstack/cbd/kvm/KvmCbdNodeServer.java
@@ -122,12 +122,6 @@ public class KvmCbdNodeServer implements Component, KvmSetupSelfFencerExtensionP
                     final String __name__ = "activate-cbd-heartbeat-volume";
 
                     @Override
-                    public boolean skip(Map data) {
-                        // TODO: support HA
-                        return true;
-                    }
-
-                    @Override
                     public void run(FlowTrigger trigger, Map data) {
                         nodeSvc.activateHeartbeatVolume(host, new ReturnValueCompletion<HeartbeatVolumeTO>(trigger) {
                             @Override
@@ -146,12 +140,6 @@ public class KvmCbdNodeServer implements Component, KvmSetupSelfFencerExtensionP
 
                 flow(new NoRollbackFlow() {
                     final String __name__ = "setup-cbd-self-fencer-on-kvm";
-
-                    @Override
-                    public boolean skip(Map data) {
-                        // TODO: support HA
-                        return true;
-                    }
 
                     @Override
                     public void run(FlowTrigger trigger, Map data) {
