6463c0d549b5752827c8bce1834dfac7aaa44b20
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-07-17 10:39:14 +0800

    <fix>[mevoco]: configuring permissions for the Pro version
    Resolves: ZSV-6365
    
    Change-Id: I7976726866706d78787778726d6d71666567756f

--- plugin/account-import/src/main/java/org/zstack/identity/imports/PackageInfo.java
+++ plugin/account-import/src/main/java/org/zstack/identity/imports/PackageInfo.java
@@ -3,12 +3,15 @@ package org.zstack.identity.imports;
 import org.zstack.header.PackageAPIInfo;
 import org.zstack.header.rest.SDKPackage;
 
-import static org.zstack.header.PackageAPIInfo.PERMISSION_COMMUNITY_AVAILABLE;
-import static org.zstack.header.PackageAPIInfo.PERMISSION_ZSV_BASIC_AVAILABLE;
+import static org.zstack.header.PackageAPIInfo.*;
 
 @PackageAPIInfo(
-    APICategoryName = "账号",
-    permissions = {PERMISSION_COMMUNITY_AVAILABLE, PERMISSION_ZSV_BASIC_AVAILABLE}
+        APICategoryName = "账号",
+        permissions = {
+                PERMISSION_COMMUNITY_AVAILABLE,
+                PERMISSION_ZSV_BASIC_AVAILABLE,
+                PERMISSION_ZSV_PRO_AVAILABLE,
+        }
 )
 @SDKPackage(packageName = "org.zstack.sdk.identity.imports")
 public class PackageInfo {
--- plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/PackageInfo.java
+++ plugin/sharedMountPointPrimaryStorage/src/main/java/org/zstack/storage/primary/smp/PackageInfo.java
@@ -2,9 +2,15 @@ package org.zstack.storage.primary.smp;
 
 import org.zstack.header.PackageAPIInfo;
 
+import static org.zstack.header.PackageAPIInfo.*;
+
 @PackageAPIInfo(
         APICategoryName = "SMP主存储",
-        permissions = {PackageAPIInfo.PERMISSION_COMMUNITY_AVAILABLE, PackageAPIInfo.PERMISSION_ZSV_BASIC_AVAILABLE}
+        permissions = {
+                PERMISSION_COMMUNITY_AVAILABLE,
+                PERMISSION_ZSV_BASIC_AVAILABLE,
+                PERMISSION_ZSV_PRO_AVAILABLE,
+        }
 )
 public class PackageInfo {
 }
