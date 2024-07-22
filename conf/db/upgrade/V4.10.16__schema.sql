6a71b924ab4a36e20e9e117c689a8f67c1041a50
boce.wang <boce.wang@zstack.io>
2024-07-22 14:31:39 +0800

    <feature>[qga]: vm internal ip overwrite and conflict with ip range
    Resolves: ZSV-9443
    Related: ZSTAC-67476
    
    Change-Id: I6868776c7fee6441b8a8415d804469295b4b5cf3
    (cherry picked from commit 361ef8b6940dfe06f237aceca4b876938bd59d7d)

--- conf/db/upgrade/V4.10.16__schema.sql
+++ conf/db/upgrade/V4.10.16__schema.sql
@@ -27,3 +27,6 @@ CREATE TABLE IF NOT EXISTS `zstack`.`ReservedIpRangeVO` (
     PRIMARY KEY  (`uuid`),
     CONSTRAINT `fkReservedIpRangeVOL3NetworkEO` FOREIGN KEY (`l3NetworkUuid`) REFERENCES `L3NetworkEO` (`uuid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+update EventSubscriptionVO set name = 'VM NIC IP Changed (GuestTools Is Required)' where uuid='98536fa94e3f4481a38331a989132b7c';
+update EventSubscriptionVO set name = 'NIC IP Configured in VM has been Occupied or in the Reserved Range (GuestTools Is Required)' where uuid='4a3494bcdbac4eaab9e9e56e27d74a2a';
