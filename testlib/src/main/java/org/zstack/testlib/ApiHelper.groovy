79148c11493520464cafdcbe5e10ea49d3245ab5
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-07-25 10:12:15 +0800

    <fix>[build]: remove cas-plugin module
    Related: ZSV-5751
    
    Change-Id: I7067667362686a626d70726d796f6b6667747575

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -23857,33 +23857,6 @@ abstract class ApiHelper {
     }
 
 
-    def loginByCas(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.LoginByCasAction.class) Closure c) {
-        def a = new org.zstack.sdk.LoginByCasAction()
-        
-        c.resolveStrategy = Closure.OWNER_FIRST
-        c.delegate = a
-        c()
-        
-
-        if (System.getProperty("apipath") != null) {
-            if (a.apiId == null) {
-                a.apiId = Platform.uuid
-            }
-    
-            def tracker = new ApiPathTracker(a.apiId)
-            def out = errorOut(a.call())
-            def path = tracker.getApiPath()
-            if (!path.isEmpty()) {
-                Test.apiPaths[a.class.name] = path.join(" --->\n")
-            }
-        
-            return out
-        } else {
-            return errorOut(a.call())
-        }
-    }
-
-
     def mergeDataOnBackupStorage(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.MergeDataOnBackupStorageAction.class) Closure c) {
         def a = new org.zstack.sdk.MergeDataOnBackupStorageAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
