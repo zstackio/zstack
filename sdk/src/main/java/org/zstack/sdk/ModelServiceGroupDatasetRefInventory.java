9292ea3e9d24dfa3931bdea112850469a26aa683
Lei Liu <lei.liu@zstack.io>
2024-07-31 18:56:56 +0800

    <fix>[conf]: update sql & sdk
    DBImpact
    
    Change-Id: I6469666968696f656b666168707262766a6c6a6e

---
+++ sdk/src/main/java/org/zstack/sdk/ModelServiceGroupDatasetRefInventory.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk;
+
+
+
+public class ModelServiceGroupDatasetRefInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String datasetUuid;
+    public void setDatasetUuid(java.lang.String datasetUuid) {
+        this.datasetUuid = datasetUuid;
+    }
+    public java.lang.String getDatasetUuid() {
+        return this.datasetUuid;
+    }
+
+    public java.lang.String modelServiceInstanceGroupUuid;
+    public void setModelServiceInstanceGroupUuid(java.lang.String modelServiceInstanceGroupUuid) {
+        this.modelServiceInstanceGroupUuid = modelServiceInstanceGroupUuid;
+    }
+    public java.lang.String getModelServiceInstanceGroupUuid() {
+        return this.modelServiceInstanceGroupUuid;
+    }
+
+}
