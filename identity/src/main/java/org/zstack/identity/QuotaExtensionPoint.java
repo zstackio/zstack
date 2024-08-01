c03b29cd0f68ae49149e287e4d4dbd0aa2a27333
ye.zou <ye.zou@zstack.io>
2024-08-01 20:42:06 +0800

    <fix>[identoty]: fix extension point of update quota
    Resolves: ZSTAC-66285
    
    Change-Id: I62716b727069766d6865626d6c6b78656475786b
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- identity/src/main/java/org/zstack/identity/QuotaExtensionPoint.java
+++ identity/src/main/java/org/zstack/identity/QuotaExtensionPoint.java
@@ -3,5 +3,5 @@ package org.zstack.identity;
 import org.zstack.header.identity.QuotaVO;
 
 public interface QuotaExtensionPoint {
-    void beforeUpdateQuota(QuotaVO newQuotaVO, String accountUuid);
+    void beforeUpdateQuota(String quotaName, long newValue, String accountUuid);
 }
