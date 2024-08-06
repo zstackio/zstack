c25972bd59c0588f211aaff182bcd458d4c353c2
AlanJager <ye.zou@zstack.io>
2024-08-06 10:55:00 +0800

    <fix>[conf]: introduce service refs to model servies
    update V5.1.8.1__schema.sql and sdk
    
    DBImpact
    
    Resolves: ZSTAC-66285
    
    Change-Id: I746c7779646b736c696f636a6e72726674696766
    Signed-off-by: AlanJager <ye.zou@zstack.io>

--- sdk/src/main/java/org/zstack/sdk/AddModelServiceAction.java
+++ sdk/src/main/java/org/zstack/sdk/AddModelServiceAction.java
@@ -70,7 +70,7 @@ public class AddModelServiceAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String zoneUuid;
 
-    @Param(required = false, validValues = {"Endpoint","FineTune","App"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    @Param(required = false, validValues = {"Endpoint","FineTune","ModelEval","App"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String type = "Endpoint";
 
     @Param(required = false, validValues = {"Other","Bentoml","HuggingFace"}, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
---
+++ sdk/src/main/java/org/zstack/sdk/ModelEvalServiceInstanceGroupInventory.java
@@ -0,0 +1,63 @@
+package org.zstack.sdk;
+
+
+
+public class ModelEvalServiceInstanceGroupInventory extends org.zstack.sdk.ModelServiceInstanceGroupInventory {
+
+    public java.lang.Integer limits;
+    public void setLimits(java.lang.Integer limits) {
+        this.limits = limits;
+    }
+    public java.lang.Integer getLimits() {
+        return this.limits;
+    }
+
+    public java.lang.Float temperature;
+    public void setTemperature(java.lang.Float temperature) {
+        this.temperature = temperature;
+    }
+    public java.lang.Float getTemperature() {
+        return this.temperature;
+    }
+
+    public java.lang.Integer topK;
+    public void setTopK(java.lang.Integer topK) {
+        this.topK = topK;
+    }
+    public java.lang.Integer getTopK() {
+        return this.topK;
+    }
+
+    public java.lang.Float topP;
+    public void setTopP(java.lang.Float topP) {
+        this.topP = topP;
+    }
+    public java.lang.Float getTopP() {
+        return this.topP;
+    }
+
+    public java.lang.Integer maxLength;
+    public void setMaxLength(java.lang.Integer maxLength) {
+        this.maxLength = maxLength;
+    }
+    public java.lang.Integer getMaxLength() {
+        return this.maxLength;
+    }
+
+    public java.lang.Integer maxNewTokens;
+    public void setMaxNewTokens(java.lang.Integer maxNewTokens) {
+        this.maxNewTokens = maxNewTokens;
+    }
+    public java.lang.Integer getMaxNewTokens() {
+        return this.maxNewTokens;
+    }
+
+    public java.lang.Float repetitionPenalty;
+    public void setRepetitionPenalty(java.lang.Float repetitionPenalty) {
+        this.repetitionPenalty = repetitionPenalty;
+    }
+    public java.lang.Float getRepetitionPenalty() {
+        return this.repetitionPenalty;
+    }
+
+}
