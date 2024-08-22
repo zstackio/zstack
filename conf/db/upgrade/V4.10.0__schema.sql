619ddd4b9ea0cc37bfafa043db933b8ce1efcdd7
Zhang Wenhao <wenhao.zhang@zstack.io>
2024-08-22 14:24:33 +0800

    <fix>[conf]: drop IAM2 tables and delete IAM2 dummy account
    Related: ZSV-5751
    Related: ZSV-6559
    
    Change-Id: I77777065686e7a72796476646f6f716875686d78

--- conf/db/upgrade/V4.10.0__schema.sql
+++ conf/db/upgrade/V4.10.0__schema.sql
@@ -2,6 +2,36 @@ CALL ADD_COLUMN('SNSApplicationEndpointVO', 'connectionStatus', 'varchar(10)', 1
 
 -- Feature: IAM1 Role And Policy | ZSV-6559
 
+drop table if exists `IAM2TicketFlowCollectionVO`;
+drop table if exists `IAM2TicketFlowVO`;
+
+drop table if exists `IAM2ProjectVirtualIDGroupRefVO`;
+drop table if exists `IAM2ProjectVirtualIDRefVO`;
+drop table if exists `IAM2OrganizationProjectRefVO`;
+drop table if exists `IAM2GroupVirtualIDRefVO`;
+drop table if exists `IAM2VirtualIDRoleRefVO`;
+drop table if exists `IAM2VirtualIDOrganizationRefVO`;
+drop table if exists `IAM2VirtualIDGroupRoleRefVO`;
+drop table if exists `IAM2VirtualIDGroupRefVO`;
+drop table if exists `IAM2ProjectAccountRefVO`;
+drop table if exists `IAM2ProjectResourceRefVO`;
+
+drop table if exists `IAM2OrganizationAttributeVO`;
+drop table if exists `IAM2VirtualIDGroupAttributeVO`;
+drop table if exists `IAM2ProjectAttributeVO`;
+drop table if exists `IAM2VirtualIDAttributeVO`;
+drop table if exists `IAM2ProjectRoleVO`;
+drop table if exists `IAM2OrganizationVO`;
+drop table if exists `IAM2VirtualIDGroupVO`;
+drop table if exists `IAM2ProjectTemplateVO`;
+drop table if exists `IAM2ProjectVO`;
+drop table if exists `IAM2VirtualIDVO`;
+
+delete from `AccountResourceRefVO` where `accountUuid` = '2dce5dc485554d21a3796500c1db007a';
+delete from `QuotaVO` where `identityUuid` = '2dce5dc485554d21a3796500c1db007a';
+delete from `AccountVO` where `uuid` = '2dce5dc485554d21a3796500c1db007a';
+delete from `ResourceVO` where `uuid` = '2dce5dc485554d21a3796500c1db007a';
+
 ALTER TABLE `TwoFactorAuthenticationSecretVO` CHANGE COLUMN `userUuid` `accountUuid` char(32) not null;
 call DROP_COLUMN('TwoFactorAuthenticationSecretVO', 'userType');
 
