bb7677fe8098b116ea85d1352af1696179b8d19f
Xingwei Yu <xingwei.yu@zstack.io>
2024-09-29 16:17:06 +0800

    <fix>[storage]: update protocol of target volume during live migratation
    Resolves: ZSTAC-69987
    
    Change-Id: I6870726b78796a6b756970676e636f74676d6f64

--- storage/src/main/java/org/zstack/storage/volume/VolumeBase.java
+++ storage/src/main/java/org/zstack/storage/volume/VolumeBase.java
@@ -1412,6 +1412,7 @@ public class VolumeBase extends AbstractVolume implements Volume {
                                 .set(VolumeVO_.rootImageUuid, volume.getRootImageUuid())
                                 .set(VolumeVO_.primaryStorageUuid, volume.getPrimaryStorageUuid())
                                 .set(VolumeVO_.actualSize, volume.getActualSize())
+                                .set(VolumeVO_.protocol, volume.getProtocol())
                                 .update();
 
                         sql(VolumeVO.class)
@@ -1421,6 +1422,7 @@ public class VolumeBase extends AbstractVolume implements Volume {
                                 .set(VolumeVO_.rootImageUuid, transientVolume.getRootImageUuid())
                                 .set(VolumeVO_.primaryStorageUuid, transientVolume.getPrimaryStorageUuid())
                                 .set(VolumeVO_.actualSize, transientVolume.getActualSize())
+                                .set(VolumeVO_.protocol, transientVolume.getProtocol())
                                 .update();
                         flush();
                     }
