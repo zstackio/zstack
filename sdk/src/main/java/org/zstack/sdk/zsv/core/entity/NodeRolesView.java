9660a99963bcbcac772b3b0c15d6b65abe9e831d
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-03-04 13:34:46 +0800

    <feature>[sdk]: introduce APIGetNodeRolesMsg
    APIGetNodeRolesMsg use for check whether
    the management node is also a compute node (kvm host)
    
    Resolves: ZSV-8174
    
    Change-Id: I6b6a6b74646d7273667a7a666579676b6761796d

---
+++ sdk/src/main/java/org/zstack/sdk/zsv/core/entity/NodeRolesView.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk.zsv.core.entity;
+
+
+
+public class NodeRolesView  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String resourceType;
+    public void setResourceType(java.lang.String resourceType) {
+        this.resourceType = resourceType;
+    }
+    public java.lang.String getResourceType() {
+        return this.resourceType;
+    }
+
+    public java.util.List roles;
+    public void setRoles(java.util.List roles) {
+        this.roles = roles;
+    }
+    public java.util.List getRoles() {
+        return this.roles;
+    }
+
+}
