f40c0a960e4b04e2ecd4d5c6414ecac4bc0c1de5
shan.wu <shan.wu@zstack.io>
2025-03-05 11:29:27 +0800

    <fix>[ceph]: adding GC for some cases where ceph cannot delete images
    If children are still stored in the trash, it will prevent the parent from being completely deleted. At this time, add GC to continuously delete the parent. When child are deleted from the trash, the parent will also be deleted by GC.
    
    Resolves/Related: ZSTAC-73269
    Resolves/Related: ZSV-9206
    
    Change-Id: I900f6570657a65767978646d6d6e7365796e7990

---
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephDeleteVolumeChainGC.java
@@ -0,0 +1,61 @@
+package org.zstack.storage.ceph.primary;
+
+import org.zstack.core.Platform;
+import org.zstack.core.cloudbus.CloudBusCallBack;
+import org.zstack.core.gc.GC;
+import org.zstack.core.gc.GCCompletion;
+import org.zstack.core.gc.TimeBasedGarbageCollector;
+import org.zstack.header.message.MessageReply;
+import org.zstack.header.storage.primary.DeleteVolumeChainOnPrimaryStorageMsg;
+import org.zstack.header.storage.primary.DeleteVolumeChainOnPrimaryStorageReply;
+import org.zstack.header.storage.primary.PrimaryStorageConstant;
+import org.zstack.header.storage.primary.PrimaryStorageVO;
+import org.zstack.utils.CollectionUtils;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
+
+import java.util.List;
+
+public class CephDeleteVolumeChainGC extends TimeBasedGarbageCollector {
+    @GC
+    public String primaryStorageUuid;
+    @GC
+    public List<String> installPaths;
+    @GC
+    public String chainTop;
+
+    private static final CLogger logger = Utils.getLogger(CephDeleteVolumeChainGC.class);
+
+    @Override
+    protected void triggerNow(GCCompletion completion) {
+        if (!dbf.isExist(primaryStorageUuid, PrimaryStorageVO.class) || CollectionUtils.isEmpty(installPaths)) {
+            completion.cancel();
+            return;
+        }
+
+        DeleteVolumeChainOnPrimaryStorageMsg msg = new DeleteVolumeChainOnPrimaryStorageMsg();
+        msg.setPrimaryStorageUuid(primaryStorageUuid);
+        msg.setChainTop(chainTop);
+        msg.setInstallPaths(installPaths);
+        bus.makeTargetServiceIdByResourceUuid(msg, PrimaryStorageConstant.SERVICE_ID, primaryStorageUuid);
+        bus.send(msg, new CloudBusCallBack(completion) {
+            @Override
+            public void run(MessageReply reply) {
+                if (!reply.isSuccess()) {
+                    completion.fail(reply.getError());
+                    return;
+                }
+
+                DeleteVolumeChainOnPrimaryStorageReply r = reply.castReply();
+                if (CollectionUtils.isEmpty(r.getUndeletedInstallPaths())) {
+                    completion.success();
+                } else {
+                    installPaths = r.getUndeletedInstallPaths();
+                    updateContext();
+                    completion.fail(Platform.operr("delete volume chain error, continue to delete"));
+                }
+            }
+        });
+    }
+}
+
