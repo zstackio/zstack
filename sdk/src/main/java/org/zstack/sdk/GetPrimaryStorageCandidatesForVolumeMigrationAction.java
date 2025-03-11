3418d8afcfa662cadbe1343909721d458acd9b00
yingzhe.hu <yinghe.hu@zstack.io>
2025-03-11 15:46:04 +0800

    <fix>[sdk]: support data volume live storage migration sdk
    Resolves: ZSTAC-73538
    
    Change-Id: I61777962787a77777478776f6574616663776a6d

--- sdk/src/main/java/org/zstack/sdk/GetPrimaryStorageCandidatesForVolumeMigrationAction.java
+++ sdk/src/main/java/org/zstack/sdk/GetPrimaryStorageCandidatesForVolumeMigrationAction.java
@@ -28,6 +28,9 @@ public class GetPrimaryStorageCandidatesForVolumeMigrationAction extends Abstrac
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String volumeUuid;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.Boolean liveMigrate = false;
+
     @Param(required = false)
     public java.util.List systemTags;
 
