ef502d6c18f8eb8ac23fda2da6167bbb57c9b44b
AlanJager <ye.zou@zstack.io>
2025-03-20 11:12:52 +0800

    <chore>[sdk]: Update sdk
    Resolves: ZSTAC-73849
    
    Change-Id: I776f756867696b7963696a7173766f6b707a6b75

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -37452,6 +37452,33 @@ abstract class ApiHelper {
     }
 
 
+    def restartModelServiceGroups(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.RestartModelServiceGroupsAction.class) Closure c) {
+        def a = new org.zstack.sdk.RestartModelServiceGroupsAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def restartResourceStack(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.RestartResourceStackAction.class) Closure c) {
         def a = new org.zstack.sdk.RestartResourceStackAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
