5ddb6973fd412c9387671c9853826ef50029b051
shenjin <jin.shen@zstack.io>
2025-03-20 14:15:20 +0800

    <fix>[conf]: update 4.8.0.0 sql
    Resolves: ZSTAC-73799
    
    Change-Id: I77696a6a666b6a6774676772626a766d6a687178

--- conf/db/upgrade/V4.8.0.8__schema.sql
+++ conf/db/upgrade/V4.8.0.8__schema.sql
@@ -2,6 +2,10 @@ DELIMITER $$
 
 CREATE PROCEDURE UpdateBareMetal2InstanceProvisionNicVO()
 BEGIN
+    DECLARE instanceUuid_exists INT;
+    DECLARE isPrimaryProvisionNic_exists_in_ProvisionNicVO INT;
+    DECLARE isPrimaryProvisionNic_exists_in_ChassisNicVO INT;
+
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
@@ -10,38 +14,61 @@ BEGIN
 
     START TRANSACTION;
 
-    ALTER TABLE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    ADD COLUMN `instanceUuid` VARCHAR(32) NOT NULL DEFAULT '';
+    SELECT COUNT(*)
+        INTO instanceUuid_exists
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'zstack'
+          AND TABLE_NAME = 'BareMetal2InstanceProvisionNicVO'
+          AND COLUMN_NAME = 'instanceUuid';
 
-    UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    SET `instanceUuid` = `uuid`;
+    IF instanceUuid_exists = 0 THEN
+        CALL ADD_COLUMN('BareMetal2InstanceProvisionNicVO', 'instanceUuid', 'VARCHAR(32)', FALSE, '');
 
-    ALTER TABLE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    DROP FOREIGN KEY `fkBareMetal2InstanceProvisionNicVOInstanceVO`;
+        UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
+        SET `instanceUuid` = `uuid`;
 
-    ALTER TABLE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    ADD CONSTRAINT `fkBareMetal2InstanceProvisionNicVOInstanceVO`
-    FOREIGN KEY (`instanceUuid`) REFERENCES `BareMetal2InstanceVO` (`uuid`)
-    ON DELETE CASCADE;
+        ALTER TABLE `zstack`.`BareMetal2InstanceProvisionNicVO`
+        DROP FOREIGN KEY `fkBareMetal2InstanceProvisionNicVOInstanceVO`;
 
-    UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    SET `uuid` = REPLACE(UUID(), '-', '');
+        CALL ADD_CONSTRAINT('BareMetal2InstanceProvisionNicVO', 'fkBareMetal2InstanceProvisionNicVOInstanceVO',
+                                    'instanceUuid', 'BareMetal2InstanceVO', 'uuid', 'CASCADE');
 
-    ALTER TABLE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    ADD COLUMN `isPrimaryProvisionNic` BOOLEAN NOT NULL DEFAULT FALSE;
+        UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
+        SET `uuid` = REPLACE(UUID(), '-', '');
+    END IF;
 
-    UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
-    SET `isPrimaryProvisionNic` = TRUE;
+    SELECT COUNT(*)
+        INTO isPrimaryProvisionNic_exists_in_ProvisionNicVO
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'zstack'
+          AND TABLE_NAME = 'BareMetal2InstanceProvisionNicVO'
+          AND COLUMN_NAME = 'isPrimaryProvisionNic';
 
-    ALTER TABLE `zstack`.`BareMetal2ChassisNicVO`
-    ADD COLUMN `isPrimaryProvisionNic` BOOLEAN NOT NULL DEFAULT FALSE;
+    IF isPrimaryProvisionNic_exists_in_ProvisionNicVO = 0 THEN
+        CALL ADD_COLUMN('BareMetal2InstanceProvisionNicVO', 'isPrimaryProvisionNic', 'BOOLEAN', FALSE, FALSE);
 
-    UPDATE `zstack`.`BareMetal2ChassisNicVO`
-    SET `isPrimaryProvisionNic` = TRUE
-    WHERE `isProvisionNic` = TRUE;
+        UPDATE `zstack`.`BareMetal2InstanceProvisionNicVO`
+        SET `isPrimaryProvisionNic` = TRUE;
+    END IF;
+
+    SELECT COUNT(*)
+        INTO isPrimaryProvisionNic_exists_in_ChassisNicVO
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'zstack'
+          AND TABLE_NAME = 'BareMetal2ChassisNicVO'
+          AND COLUMN_NAME = 'isPrimaryProvisionNic';
+
+    IF isPrimaryProvisionNic_exists_in_ChassisNicVO = 0 THEN
+        CALL ADD_COLUMN('BareMetal2ChassisNicVO', 'isPrimaryProvisionNic', 'BOOLEAN', FALSE, FALSE);
+
+        UPDATE `zstack`.`BareMetal2ChassisNicVO`
+        SET `isPrimaryProvisionNic` = TRUE
+        WHERE `isProvisionNic` = TRUE;
+    END IF;
 
     COMMIT;
 END$$
 
 DELIMITER ;
 CALL UpdateBareMetal2InstanceProvisionNicVO();
+DROP PROCEDURE IF EXISTS UpdateBareMetal2InstanceProvisionNicVO;
