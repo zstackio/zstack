9124437730ff923b19c807056f8319911d2689ef
ye.zou <ye.zou@zstack.io>
2025-04-01 10:23:50 +0800

    <fix>[utils]: Introduce yaml loadAs by class
    Resolves: ZSTAC-74096
    
    Change-Id: I7079766d636b616a7076666b6666747a7a70657a
    Signed-off-by: ye.zou <ye.zou@zstack.io>

--- utils/src/main/java/org/zstack/utils/YamlUtils.java
+++ utils/src/main/java/org/zstack/utils/YamlUtils.java
@@ -2,6 +2,8 @@ package org.zstack.utils;
 
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.nodes.Tag;
+
 /**
  * @Author: qiuyu.zhang
  * @Date: 2024/4/8 14:17
@@ -16,8 +18,22 @@ public class YamlUtils {
         return yaml.dump(obj);
     }
 
+    public static String dumpAs(Object obj, Tag tag) {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        options.setDefaultScalarStyle(DumperOptions.ScalarStyle.PLAIN);
+        Yaml yaml = new Yaml(options);
+        return yaml.dumpAs(obj, tag, null);
+    }
+
     public static Object load(String str) {
         Yaml yaml = new Yaml();
         return yaml.load(str);
     }
+
+    public static <T> T load(String str, Class<T> clazz) {
+        Yaml yaml = new Yaml();
+        return yaml.loadAs(str, clazz);
+    }
+
 }
