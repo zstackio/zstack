f43fa1e4978d9347e0d80dda4046a328a414e17d
shixin.ruan <shixin.ruan@zstack.io>
2025-04-11 17:19:01 +0800

    <fix>[virtualRouterProvider]: fix ovn l2 network provider error<description>
    Resolves: ZSTAC-74197
    
    Change-Id: I6b74627a6d6a676a6961796e746d62686b776663

--- plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/FlatProviderFactory.java
+++ plugin/flatNetworkProvider/src/main/java/org/zstack/network/service/flat/FlatProviderFactory.java
@@ -10,6 +10,7 @@ import org.zstack.header.network.NetworkException;
 import org.zstack.header.network.l2.APICreateL2NetworkMsg;
 import org.zstack.header.network.l2.L2NetworkCreateExtensionPoint;
 import org.zstack.header.network.l2.L2NetworkInventory;
+import org.zstack.header.network.l2.VSwitchType;
 import org.zstack.header.network.service.*;
 import org.zstack.network.service.eip.EipConstant;
 import org.zstack.network.service.userdata.UserdataConstant;
@@ -115,6 +116,11 @@ public class FlatProviderFactory implements NetworkServiceProviderFactory, Prepa
 
     @Override
     public void afterCreateL2Network(L2NetworkInventory l2Network) {
+        VSwitchType vSwitchType = VSwitchType.valueOf(l2Network.getvSwitchType());
+        if (vSwitchType.getSdnControllerType() != null) {
+            return;
+        }
+
         NetworkServiceProviderL2NetworkRefVO ref = new NetworkServiceProviderL2NetworkRefVO();
         ref.setL2NetworkUuid(l2Network.getUuid());
         ref.setNetworkServiceProviderUuid(flatProvider.getUuid());
