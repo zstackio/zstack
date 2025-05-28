dac44d07bdf9312c78aae20000b8349203673162
hanyu.liang <hanyu.liang@zstack.io>
2025-05-28 04:54:01 -0400

    <fix>[sso]: migrate sso config storage from SystemTagVO to AttributesVO
    Resolves: ZSTAC-74198
    
    Change-Id: I637a74198f696e77766377626f78647269707470

---
+++ sdk/src/main/java/org/zstack/sdk/ExtendedAttribute.java
@@ -0,0 +1,23 @@
+package org.zstack.sdk;
+
+
+
+public class ExtendedAttribute extends org.zstack.sdk.iam2.api.Attribute {
+
+    public java.lang.String type;
+    public void setType(java.lang.String type) {
+        this.type = type;
+    }
+    public java.lang.String getType() {
+        return this.type;
+    }
+
+    public java.lang.String purpose;
+    public void setPurpose(java.lang.String purpose) {
+        this.purpose = purpose;
+    }
+    public java.lang.String getPurpose() {
+        return this.purpose;
+    }
+
+}
