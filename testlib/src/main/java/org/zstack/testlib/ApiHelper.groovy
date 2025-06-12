94344a59d574bff41322177bdc98207f2db06e3c
hanyu.liang <hanyu.liang@zstack.io>
2025-06-12 06:31:38 -0400

    <fix>[sdk]: support pushing user data to Keycloak
    Resolves: ZSTAC-74189
    
    Change-Id: I7375741896676741897574646678757a7a7a7578

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -35920,6 +35920,33 @@ abstract class ApiHelper {
     }
 
 
+    def ssoClientPushData(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SsoClientPushDataAction.class) Closure c) {
+        def a = new org.zstack.sdk.SsoClientPushDataAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def startBareMetal2Instance(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.StartBareMetal2InstanceAction.class) Closure c) {
         def a = new org.zstack.sdk.StartBareMetal2InstanceAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
