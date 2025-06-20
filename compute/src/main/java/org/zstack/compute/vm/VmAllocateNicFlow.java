9fd19103d82da56ad0fed9b08f814b4934f39903
boce.wang <boce.wang@zstack.io>
2025-06-20 16:22:00 +0800

    <fix>[compute]: fix nicManager setNicDriverType
    Resolves: ZSTAC-75732
    
    Change-Id: I6868776cf30600e03e664d6a9fe9147c7fa96aba

--- compute/src/main/java/org/zstack/compute/vm/VmAllocateNicFlow.java
+++ compute/src/main/java/org/zstack/compute/vm/VmAllocateNicFlow.java
@@ -136,8 +136,8 @@ public class VmAllocateNicFlow implements Flow {
             if (!StringUtils.isEmpty(nicSpec.getNicDriverType())) {
                 nic.setDriverType(nicSpec.getNicDriverType());
             } else {
-                boolean vmHasVirtio = VmSystemTags.VIRTIO.hasTag(spec.getVmInventory().getUuid());
-                nicManager.setNicDriverType(nic, vmHasVirtio,
+                boolean vmImageHasVirtio = spec.getImageSpec().getInventory().getVirtio();
+                nicManager.setNicDriverType(nic, vmImageHasVirtio,
                         ImagePlatform.valueOf(spec.getVmInventory().getPlatform()).isParaVirtualization(),
                         spec.getVmInventory());
             }
