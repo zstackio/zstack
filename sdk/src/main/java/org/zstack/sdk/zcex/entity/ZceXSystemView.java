ee90b50de1c66d821d5997b2b8585367f9a5054a
Zhang, Wenhao <wenhao.zhang@zstack.io>
2025-06-27 02:40:09 +0000

    <fix>[zce-x]: support to return system info in GetZceXCapabilityAction
    Resolves: ZSV-9115
    Resolves: ZSV-9124
    Related: ZSV-7444
    
    APIImpact
    
    Change-Id: I616f63747763716d7965696b68626e6c6f6b6d61

---
+++ sdk/src/main/java/org/zstack/sdk/zcex/entity/ZceXSystemView.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk.zcex.entity;
+
+
+
+public class ZceXSystemView  {
+
+    public java.lang.String adminUserName;
+    public void setAdminUserName(java.lang.String adminUserName) {
+        this.adminUserName = adminUserName;
+    }
+    public java.lang.String getAdminUserName() {
+        return this.adminUserName;
+    }
+
+    public java.lang.String adminUserId;
+    public void setAdminUserId(java.lang.String adminUserId) {
+        this.adminUserId = adminUserId;
+    }
+    public java.lang.String getAdminUserId() {
+        return this.adminUserId;
+    }
+
+    public java.lang.String version;
+    public void setVersion(java.lang.String version) {
+        this.version = version;
+    }
+    public java.lang.String getVersion() {
+        return this.version;
+    }
+
+}
