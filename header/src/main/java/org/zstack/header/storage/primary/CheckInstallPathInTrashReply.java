bf69e32ce5e08a1b98f6708f92b7e96f2664aa61
J M <jin.ma@zstack.io>
2025-07-01 18:22:21 +0800

    <fix>[storage]: fix migrate trash
    Resolves: ZSTAC-61914
    Resolves: ZSV-10515
    
    Change-Id: I706a7479756c766b6978626f6167736b67627562

--- header/src/main/java/org/zstack/header/storage/primary/CheckInstallPathInTrashReply.java
+++ header/src/main/java/org/zstack/header/storage/primary/CheckInstallPathInTrashReply.java
@@ -2,12 +2,15 @@ package org.zstack.header.storage.primary;
 
 import org.zstack.header.message.MessageReply;
 
+import java.util.List;
+
 /**
  * Created by mingjian.deng on 2018/12/25.
  */
 public class CheckInstallPathInTrashReply extends MessageReply {
     private Long trashId;
     private String resourceUuid;
+    private List<String> relatedTrashPaths;
 
     public Long getTrashId() {
         return trashId;
@@ -23,5 +26,13 @@ public class CheckInstallPathInTrashReply extends MessageReply {
 
     public void setResourceUuid(String resourceUuid) {
         this.resourceUuid = resourceUuid;
+    }
+
+    public List<String> getRelatedTrashPaths() {
+        return relatedTrashPaths;
+    }
+
+    public void setRelatedTrashPaths(List<String> relatedTrashPaths) {
+        this.relatedTrashPaths = relatedTrashPaths;
     }
 }
