b5cc66250ad1c81d49ca113942ed0a73cc75735c
yingzhe.hu <yingzhe.hu@zstack.io>
2025-07-10 14:55:11 +0800

    <feature>[volume]: support bm2 backup recovery
    Resolves: ZSTAC-75753
    
    Change-Id: I6f7678736a76657576726e766f7473616a6e7176

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
@@ -6001,7 +6001,7 @@ public class CephPrimaryStorageBase extends PrimaryStorageBase {
         return String.format("ceph://%s", originPath);
     }
 
-    protected String getTargetPoolNameFromAllocatedUrl(String allocatedUrl) {
+    public String getTargetPoolNameFromAllocatedUrl(String allocatedUrl) {
         if (allocatedUrl == null) {
             throw new OperationFailureException(operr("allocated url not found"));
         }
