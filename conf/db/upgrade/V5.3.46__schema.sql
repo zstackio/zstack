8b8bf2f54cb5a348ef5b084eeddef1d88c21e27e
ye.zou <ye.zou@zstack.io>
2025-08-08 15:56:50 +0800

    <feature>[conf]: Update PciDeviceSpecVO's column for new feature
    DBImpact
    
    Resolves: ZSTAC-76511
    
    Change-Id: I6a75756c736f6d68797a7672637863737666706b

--- conf/db/upgrade/V5.3.46__schema.sql
+++ conf/db/upgrade/V5.3.46__schema.sql
@@ -63,3 +63,5 @@ CREATE TABLE IF NOT EXISTS `zstack`.`H3cSdnSubnetIpRangeRefVO` (
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DELETE FROM UserTagVO WHERE uuid = 'a4de80903e57422699fb05bd367a3cb4';
+
+CALL ADD_COLUMN('PciDeviceSpecVO', 'allowResourceConfigWithMultipleDevices', 'tinyint(1)', 1, '1');
