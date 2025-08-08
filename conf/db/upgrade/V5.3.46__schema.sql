b0405b1fd9153e8d663ac4f54223d6a0e1faaff5
AlanJager <ye.zou@zstack.io>
2025-08-08 16:32:40 +0800

    <fix>[sdk]: Update sdk
    Resolves: ZSTAC-76511
    
    Change-Id: I71786b646769617575796364796c6265756c7875

--- conf/db/upgrade/V5.3.46__schema.sql
+++ conf/db/upgrade/V5.3.46__schema.sql
@@ -64,4 +64,4 @@ CREATE TABLE IF NOT EXISTS `zstack`.`H3cSdnSubnetIpRangeRefVO` (
 
 DELETE FROM UserTagVO WHERE uuid = 'a4de80903e57422699fb05bd367a3cb4';
 
-CALL ADD_COLUMN('PciDeviceSpecVO', 'allowResourceConfigWithMultipleDevices', 'tinyint(1)', 1, '1');
+CALL ADD_COLUMN('PciDeviceSpecVO', 'allowResourceConfigWithMultipleDevices', 'tinyint(1)', 0, '1');
