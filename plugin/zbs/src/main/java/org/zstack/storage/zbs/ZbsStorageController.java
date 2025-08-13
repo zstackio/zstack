eb7a2c706a93d00d03391dbadfb13a1ff6828f35
Xingwei Yu <xingwei.yu@zstack.io>
2025-08-13 17:11:52 +0800

    <fix>[plugin]: introducing get facts
    Resolves: ZSTAC-76752
    
    Change-Id: I666b7a6663657a6f7863707963696369706c7667

--- plugin/zbs/src/main/java/org/zstack/storage/zbs/ZbsStorageController.java
+++ plugin/zbs/src/main/java/org/zstack/storage/zbs/ZbsStorageController.java
@@ -176,8 +176,9 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
 
         DeployClientCmd cmd = new DeployClientCmd();
         cmd.setIp(h.getManagementIp());
-        cmd.setPassword(host.getPassword());
         cmd.setPort(host.getPort());
+        cmd.setUsername(host.getUsername());
+        cmd.setPassword(host.getPassword());
         httpCall(DEPLOY_CLIENT_PATH, cmd, DeployClientRsp.class, new ReturnValueCompletion<DeployClientRsp>(comp) {
             @Override
             public void success(DeployClientRsp returnValue) {
@@ -317,6 +318,7 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
                             @Override
                             public void success(GetFactsRsp returnValue) {
                                 ClusterInfo info = new ClusterInfo();
+                                info.setUuid(returnValue.getUuid());
                                 info.setVersion(returnValue.getVersion());
                                 newAddonInfo.setClusterInfo(info);
                                 trigger.next();
@@ -362,8 +364,9 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
 
                             DeployClientCmd cmd = new DeployClientCmd();
                             cmd.setIp(h.getManagementIp());
-                            cmd.setPassword(host.getPassword());
                             cmd.setPort(host.getPort());
+                            cmd.setUsername(host.getUsername());
+                            cmd.setPassword(host.getPassword());
                             httpCall(DEPLOY_CLIENT_PATH, cmd, DeployClientRsp.class, new ReturnValueCompletion<DeployClientRsp>(comp) {
                                 @Override
                                 public void success(DeployClientRsp returnValue) {
@@ -414,7 +417,7 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
         reloadDbInfo();
         final List<ZbsPrimaryStorageMdsBase> mds = CollectionUtils.transformAndRemoveNull(addonInfo.getMdsInfos(), ZbsPrimaryStorageMdsBase::new);
         new While<>(mds).each((m, comp) -> {
-            m.ping(new Completion(comp) {
+            m.ping(addonInfo.getClusterInfo(), new Completion(comp) {
                 @Override
                 public void success() {
                     m.getSelf().setStatus(MdsStatus.Connected);
@@ -1646,8 +1649,9 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
 
     public static class DeployClientCmd extends AgentCommand {
         private String ip;
-        private String password;
         private Integer port;
+        private String username;
+        private String password;
 
         public String getIp() {
             return ip;
@@ -1657,14 +1661,6 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
             this.ip = ip;
         }
 
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
         public Integer getPort() {
             return port;
         }
@@ -1672,10 +1668,35 @@ public class ZbsStorageController implements PrimaryStorageControllerSvc, Primar
         public void setPort(Integer port) {
             this.port = port;
         }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
     }
 
     public static class GetFactsRsp extends AgentResponse {
+        private String uuid;
         private String version;
+
+        public String getUuid() {
+            return uuid;
+        }
+
+        public void setUuid(String uuid) {
+            this.uuid = uuid;
+        }
 
         public String getVersion() {
             return version;
