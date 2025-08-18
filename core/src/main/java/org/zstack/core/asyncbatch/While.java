3b64459f53ffabf82e0689d1af50ca9d680e7d99
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-08-18 15:00:23 +0800

    <feature>[core]: While support progress report
    Use while.enableProgressReport() to enable
    progress report.
    
    Resolves: ZSV-9610
    
    Change-Id: I656175697973626e716b6370767578646e727161

--- core/src/main/java/org/zstack/core/asyncbatch/While.java
+++ core/src/main/java/org/zstack/core/asyncbatch/While.java
@@ -1,15 +1,22 @@
 package org.zstack.core.asyncbatch;
 
+import org.zstack.core.Platform;
+import org.zstack.core.progress.ProgressWhileProcessorFactory;
 import org.zstack.core.thread.ThreadGlobalProperty;
 import org.zstack.header.core.WhileDoneCompletion;
 import org.zstack.header.core.WhileCompletion;
 import org.zstack.header.errorcode.ErrorCode;
 import org.zstack.header.errorcode.ErrorCodeList;
+import org.zstack.utils.CollectionUtils;
 import org.zstack.utils.DebugUtils;
+import org.zstack.utils.Utils;
+import org.zstack.utils.logging.CLogger;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -19,6 +26,7 @@ import java.util.stream.Collectors;
  */
 
 public class While<T> {
+    private static final CLogger logger = Utils.getLogger(While.class);
     private Collection<T> items;
     private Do consumer;
 
@@ -29,6 +37,7 @@ public class While<T> {
     private AtomicInteger doneCount = new AtomicInteger(0);
 
     private ErrorCodeList errors = new ErrorCodeList();
+    private List<WhileProcessor> processors;
 
     public interface Do<T> {
         void accept(T item, WhileCompletion completion);
@@ -45,6 +54,37 @@ public class While<T> {
             array[i] = i + 1;
         }
         return new While<>(Arrays.stream(array).mapToObj(Integer::valueOf).collect(Collectors.toList()));
+    }
+
+    public While<T> enableProgressReport(String content) {
+        try {
+            ProgressWhileProcessorFactory factory = Platform.getComponentLoader().getComponent(ProgressWhileProcessorFactory.class);
+            if (processors == null) {
+                processors = new ArrayList<>();
+            }
+            processors.add(factory.create(content));
+        } catch (Exception e) {
+            logger.warn(String.format("unable to create progress while processor factory: %s", e.getMessage()));
+        }
+        return this;
+    }
+
+    private void callProcessorStart() {
+        if (processors != null) {
+            CollectionUtils.safeForEach(processors, p -> p.beforeStart(items.size()));
+        }
+    }
+
+    private void callProcessorDone() {
+        if (processors != null) {
+            CollectionUtils.safeForEach(processors, p -> p.afterDone(items.size() - doneCount.get()));
+        }
+    }
+
+    private void callProcessorAllDone() {
+        if (processors != null) {
+            CollectionUtils.safeForEach(processors, WhileProcessor::afterAllDone);
+        }
     }
 
     public While each(Do<T> consumer) {
@@ -71,6 +111,7 @@ public class While<T> {
 
     private void run(Iterator<T> it, WhileDoneCompletion completion) {
         if (!it.hasNext()) {
+            callProcessorAllDone();
             completion.done(errors);
             return;
         }
@@ -79,6 +120,7 @@ public class While<T> {
         consumer.accept(t, new WhileCompletion(completion) {
             @Override
             public void allDone() {
+                callProcessorAllDone();
                 completion.done(errors);
             }
 
@@ -107,7 +149,10 @@ public class While<T> {
 
     public void run(WhileDoneCompletion completion) {
         DebugUtils.Assert(consumer != null, "each() or all() or step() must be called before run()");
+        callProcessorStart();
+
         if (items.isEmpty()) {
+            callProcessorAllDone();
             completion.done(errors);
             return;
         }
@@ -159,6 +204,7 @@ public class While<T> {
 
             @Override
             public void done() {
+                callProcessorDone();
                 if (doneCount.decrementAndGet() == 0) {
                     doneCompletion(completion);
                     return;
@@ -170,6 +216,7 @@ public class While<T> {
 
     private void doneCompletion(WhileDoneCompletion completion) {
         if (isOver.compareAndSet(false, true)) {
+            callProcessorAllDone();
             completion.done(errors);
         }
     }
