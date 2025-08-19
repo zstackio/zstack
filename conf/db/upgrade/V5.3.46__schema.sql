09a59a03a596189c7a8575cd1dea0dab0e066ff5
ye.zou <ye.zou@zstack.io>
2025-08-19 11:41:53 +0800

    <fix>[conf]: Introduce new column nodeRank to ModelServiceInstanceVO
    DBImpact
    
    Resolves: ZSTAC-76166
    
    Change-Id: I676d6b706566616e6e7a7a7479676f666c747777

--- conf/db/upgrade/V5.3.46__schema.sql
+++ conf/db/upgrade/V5.3.46__schema.sql
@@ -3,3 +3,5 @@ DELETE FROM UserTagVO WHERE uuid = 'a4de80903e57422699fb05bd367a3cb4';
 CALL ADD_COLUMN('PciDeviceSpecVO', 'allowResourceConfigWithMultipleDevices', 'tinyint(1)', 0, '1');
 
 CALL ADD_COLUMN('GpuDeviceVO', 'opaque', 'MEDIUMTEXT', 1, NULL);
+
+CALL ADD_COLUMN('ModelServiceInstanceVO', 'nodeRank', 'int', 1, 0);
