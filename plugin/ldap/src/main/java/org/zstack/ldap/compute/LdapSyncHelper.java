73f7c660b2e0326b156d7c0abd688b73425bb5e7
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-08-20 13:59:36 +0800

    <fix>[ldap]: fix error in LdapSyncHelper progress report
    This patch is for zsv_4.10.28
    
    Related: ZSV-9610
    Related: ZSV-6173
    Related: ZSV-5531
    
    Change-Id: I7276787a77787065796f7178676e6363686c6774

--- plugin/ldap/src/main/java/org/zstack/ldap/compute/LdapSyncHelper.java
+++ plugin/ldap/src/main/java/org/zstack/ldap/compute/LdapSyncHelper.java
@@ -394,7 +394,7 @@ public class LdapSyncHelper {
 
     private void recordImportReply(ImportThirdPartyAccountMsg message, MessageReply rawReply) {
         int totalRecordExpect = message.getSpec().accountList.size();
-        if (!rawReply.isSuccess() && !(rawReply instanceof ImportThirdPartyAccountReply)) {
+        if (!rawReply.isSuccess() || !(rawReply instanceof ImportThirdPartyAccountReply)) {
             progress.appendFailCountInImportStage(totalRecordExpect);
             return;
         }
@@ -417,7 +417,7 @@ public class LdapSyncHelper {
 
     private void recordUnbindReply(UnbindThirdPartyAccountMsg message, MessageReply rawReply) {
         int totalRecordExpect = message.getSpec().getAccountUuidList().size();
-        if (!rawReply.isSuccess() && !(rawReply instanceof UnbindThirdPartyAccountReply)) {
+        if (!rawReply.isSuccess() || !(rawReply instanceof UnbindThirdPartyAccountReply)) {
             progress.appendFailCountInCleanStage(totalRecordExpect);
             return;
         }
