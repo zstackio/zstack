74f8311ecb01f170d652d212952caa288eff7190
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-08-21 13:52:37 +0800

    <fix>[header]: remove deprecated ProgressError and TaskProgressRange
    This patch is for zsv_4.10.28
    
    Related: ZSV-9610
    
    Change-Id: I656269726f6f70676c63776a6f676e78616b7864

--- header/src/main/java/org/zstack/header/core/progress/ProgressConstants.java
+++ header/src/main/java/org/zstack/header/core/progress/ProgressConstants.java
@@ -1,30 +1,10 @@
 package org.zstack.header.core.progress;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by mingjian.deng on 16/12/10.
  */
 public class ProgressConstants {
     public final static String PROGRESS_REPORT_PATH = "/progress/report";
-    public final static String PROGRESS_START_PATH = "/progress/start";
-    public final static String PROGRESS_FINISH_PATH = "/progress/finish";
 
-    public static final String ACTION_CATEGORY = "progress";
     public static final String SERVICE_ID = "core.progress";
-
-    public enum ProgressType {
-        AddImage,
-        LocalStorageMigrateVolume,
-        CreateRootVolumeTemplateFromRootVolume;
-
-        public static boolean contains(String type) {
-            List<String> types = new ArrayList<>();
-            for (ProgressType value: ProgressType.values()) {
-                types.add(value.name());
-            }
-            return types.contains(type);
-        }
-    }
 }
