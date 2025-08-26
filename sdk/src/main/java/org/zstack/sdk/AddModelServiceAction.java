a5f5cdb4e676bd5ed2720e86e48debbee11f5dcc
ye.zou <ye.zou@zstack.io>
2025-08-26 10:39:41 +0800

    <chore>[conf]: Add new column supportDistributed
    - Update sdk
    
    DBImpact
    
    Resolves: ZSTAC-77105
    
    Change-Id: I786d7a636a6979616668717963756f7377766367
    Signed-off-by: ye.zou <ye.zou@zstack.io>

--- sdk/src/main/java/org/zstack/sdk/AddModelServiceAction.java
+++ sdk/src/main/java/org/zstack/sdk/AddModelServiceAction.java
@@ -85,6 +85,9 @@ public class AddModelServiceAction extends AbstractAction {
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.util.List architectureImages;
 
+    @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.Boolean supportDistributed;
+
     @Param(required = false)
     public java.lang.String resourceUuid;
 
