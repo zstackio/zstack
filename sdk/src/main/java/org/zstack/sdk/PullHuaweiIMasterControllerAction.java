4f89cc23861d7c05b38812c6b616c24e410c2ebd
Shixin Ruan <shixin.ruan@zstack.io>
2025-09-07 19:04:38 +0800

    <fix>[sdk]: pull hardware switch
    Resolves: ZSTAC-77513
    
    Change-Id: I6376696773797a676867756c6d7a797277746861

--- sdk/src/main/java/org/zstack/sdk/PullHuaweiIMasterControllerAction.java
+++ sdk/src/main/java/org/zstack/sdk/PullHuaweiIMasterControllerAction.java
@@ -29,7 +29,7 @@ public class PullHuaweiIMasterControllerAction extends AbstractAction {
     public java.lang.String uuid;
 
     @Param(required = false, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
-    public boolean pullSwitch = false;
+    public boolean pullSwitch = true;
 
     @Param(required = false)
     public java.util.List systemTags;
