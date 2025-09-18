593c0eb93b9b90e69897464662a5ecbe1a681d6d
AlanJager <ye.zou@zstack.io>
2025-09-18 16:21:38 +0800

    <chore>[sdk]: Update sdk
    Resolves: ZSTAC-76891
    
    Change-Id: I776d68666c76797274647a617a746764636d6b78

---
+++ sdk/src/main/java/org/zstack/sdk/PodInventory.java
@@ -0,0 +1,15 @@
+package org.zstack.sdk;
+
+
+
+public class PodInventory extends org.zstack.sdk.VmInstanceInventory {
+
+    public java.lang.String status;
+    public void setStatus(java.lang.String status) {
+        this.status = status;
+    }
+    public java.lang.String getStatus() {
+        return this.status;
+    }
+
+}
