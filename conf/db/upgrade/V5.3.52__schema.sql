8d49d7f1745f3474d0aa7e8bf4a389d03203df21
ye.zou <ye.zou@zstack.io>
2025-09-18 13:59:29 +0800

    <fix>[conf]: Update schema to delete old pod info
    DBImpact
    
    Resolves: ZSTAC-76891
    
    Change-Id: I6a706679626173616265766a626c6872776a7963

--- conf/db/upgrade/V5.3.52__schema.sql
+++ conf/db/upgrade/V5.3.52__schema.sql
@@ -1,2 +1,11 @@
 CALL ADD_COLUMN('ModelServiceInstanceVO', 'name', 'VARCHAR(255)', 1, NULL);
 CALL ADD_COLUMN('ModelServiceInstanceVO', 'namespace', 'VARCHAR(255)', 1, NULL);
+
+-- Delete old vm records for pod and resync will be done after node started
+DELETE FROM `VmInstanceEO` where hypervisorType = 'Native';
+
+CREATE TABLE  `zstack`.`PodVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `status` varchar(64) NOT NULL,
+     PRIMARY KEY  (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
