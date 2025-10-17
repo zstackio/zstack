23dfad3d0fb91dbef2fdb5c99191d4a5ede2f4c5
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-10-17 16:32:19 +0800

    <feature>[managements]: add APIGetManagementNodesStatusMsg
    Resolves: ZSV-10249
    Related: ZSV-9734
    
    APIImpact
    
    Change-Id: I726667716c6e6c6a66666f6a6b65756a72737566
    (cherry picked from commit 1e769d0d41bbbce08fe900b6767a9e96aa9afffa)

---
+++ sdk/src/main/java/org/zstack/sdk/managements/common/ManagementsStatusView.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk.managements.common;
+
+
+
+public class ManagementsStatusView  {
+
+    public java.lang.String vip;
+    public void setVip(java.lang.String vip) {
+        this.vip = vip;
+    }
+    public java.lang.String getVip() {
+        return this.vip;
+    }
+
+    public java.lang.String uiHttpPath;
+    public void setUiHttpPath(java.lang.String uiHttpPath) {
+        this.uiHttpPath = uiHttpPath;
+    }
+    public java.lang.String getUiHttpPath() {
+        return this.uiHttpPath;
+    }
+
+    public java.util.List nodes;
+    public void setNodes(java.util.List nodes) {
+        this.nodes = nodes;
+    }
+    public java.util.List getNodes() {
+        return this.nodes;
+    }
+
+}
