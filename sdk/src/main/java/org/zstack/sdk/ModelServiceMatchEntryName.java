b69a072d7b5fdf7054bc360fe6d2c0e847bdef73
AlanJager <ye.zou@zstack.io>
2025-10-21 16:03:33 +0800

    <fix>[sdk]: Update sdk
    Resolves: ZSTAC-77238
    
    Change-Id: I6b657964756664796d6e727362796f6376657468

---
+++ sdk/src/main/java/org/zstack/sdk/ModelServiceMatchEntryName.java
@@ -0,0 +1,10 @@
+package org.zstack.sdk;
+
+public enum ModelServiceMatchEntryName {
+	TemplateExistence,
+	FrameworkCompatibility,
+	AcceleratorCompatibility,
+	FrameworkVersionCompatibility,
+	ArchitectureCompatibility,
+	QuantizationCompatibility,
+}
