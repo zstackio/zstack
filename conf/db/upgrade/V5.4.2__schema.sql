2c0c8885a0cf88853b2fda903d38dd2ce484beef
haoyu.ding <haoyu.ding@zstack.io>
2025-10-22 16:04:30 +0800

    <fix>[conf]: Use KubernetesServiceVO to store container service information
    DBImpact
    
    Resolves: ZSTAC-77444
    
    Change-Id: I76656f636c78646d677174756c776a7677637a63

--- conf/db/upgrade/V5.4.2__schema.sql
+++ conf/db/upgrade/V5.4.2__schema.sql
@@ -18,3 +18,17 @@ CALL ADD_COLUMN('ModelServiceTemplateVO', 'cudaVersion', 'varchar(255)', 1, NULL
 CALL ADD_COLUMN('ModelServiceTemplateVO', 'cannVersion', 'varchar(255)', 1, NULL);
 CALL ADD_COLUMN('ModelServiceTemplateVO', 'frameworkVersionSemver', 'varchar(255)', 1, NULL);
 CALL ADD_COLUMN('ModelServiceTemplateVO', 'gpuVendor', 'varchar(255)', 1, NULL);
+
+CALL ADD_COLUMN('PodVO', 'namespace', 'varchar(64)', 1, NULL);
+
+CREATE TABLE `zstack`.`KubernetesServiceVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(64) NOT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `namespace` varchar(64) NOT NULL,
+    `type` varchar(20) NOT NULL,
+    `clusterIp` varchar(64) DEFAULT NULL,
+    `externalIp` varchar(64) DEFAULT NULL,
+    `ports` text,
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
