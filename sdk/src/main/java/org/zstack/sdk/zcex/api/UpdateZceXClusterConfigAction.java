3be93c1a891de4c635faee087dfb6362e00e38c3
tao.gan <tao.gan@zstack.io>
2025-10-26 14:29:53 +0800

    <fix>[zcex]: maintain storage package initialization status
    Resolves: ZSV-9928
    
    Change-Id: I6a6267617661796b647662776c636d6d727a6d6c

--- sdk/src/main/java/org/zstack/sdk/zcex/api/UpdateZceXClusterConfigAction.java
+++ sdk/src/main/java/org/zstack/sdk/zcex/api/UpdateZceXClusterConfigAction.java
@@ -28,6 +28,9 @@ public class UpdateZceXClusterConfigAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String uuid;
 
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String softwarePackageUuid;
+
     @Param(required = false, nonempty = false, nullElements = false, emptyString = false, noTrim = false)
     public java.lang.String managementIp;
 
--- sdk/src/main/java/org/zstack/sdk/zstone/api/UpdateZStoneClusterConfigAction.java
+++ sdk/src/main/java/org/zstack/sdk/zstone/api/UpdateZStoneClusterConfigAction.java
@@ -28,6 +28,9 @@ public class UpdateZStoneClusterConfigAction extends AbstractAction {
     @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String uuid;
 
+    @Param(required = true, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String softwarePackageUuid;
+
     @Param(required = true, maxLength = 128, nonempty = false, nullElements = false, emptyString = false, noTrim = false)
     public java.lang.String clusterName;
 
