7b7dff420a38c0387a573b17b76911e6fcb562ee
ye.zou <ye.zou@zstack.io>
2025-10-31 17:27:36 +0800

    <fix>[conf]: Update schema for model/service's new fields
    DBImpact
    
    Resolves: ZSTAC-77238
    
    Change-Id: I63766176647164627161756a7069756b74746e77

--- conf/db/upgrade/V5.4.2__schema.sql
+++ conf/db/upgrade/V5.4.2__schema.sql
@@ -1,10 +1,19 @@
 ALTER TABLE `zstack`.`MdevDeviceSpecVO` modify column name varchar(128) NOT NULL;
 ALTER TABLE `zstack`.`MdevDeviceVO` modify column name varchar(128) NOT NULL;
 
-CALL ADD_COLUMN('ModelVO', 'versionSemver', 'varchar(255)', 1, 'NULL');
+CALL ADD_COLUMN('ModelVO', 'framework', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelVO', 'versionSemver', 'varchar(255)', 1, NULL);
 CALL ADD_COLUMN('ModelVO', 'isLatestVersion', 'tinyint(1)', 1, '0');
-CALL ADD_COLUMN('ModelVO', 'artifactChecksum', 'varchar(255)', 1, 'NULL');
+CALL ADD_COLUMN('ModelVO', 'artifactChecksum', 'varchar(255)', 1, NULL);
 CALL ADD_COLUMN('ModelVO', 'artifactSizeBytes', 'bigint', 1, '0');
-CALL ADD_COLUMN('ModelVO', 'architectureType', 'varchar(255)', 1, 'NULL');
-CALL ADD_COLUMN('ModelVO', 'frameworkVersion', 'varchar(255)', 1, 'NULL');
-CALL ADD_COLUMN('ModelVO', 'requiredAccelerator', 'varchar(255)', 1, 'NULL');
+CALL ADD_COLUMN('ModelVO', 'architectureType', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelVO', 'frameworkVersion', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelVO', 'requiredAccelerator', 'varchar(255)', 1, NULL);
+
+CALL RENAME_TABLE('ModelServiceImageVO', 'ModelServiceTemplateVO');
+
+CALL ADD_COLUMN('ModelServiceTemplateVO', 'pythonVersionSemver', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelServiceTemplateVO', 'cudaVersion', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelServiceTemplateVO', 'cannVersion', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelServiceTemplateVO', 'frameworkVersionSemver', 'varchar(255)', 1, NULL);
+CALL ADD_COLUMN('ModelServiceTemplateVO', 'gpuVendor', 'varchar(255)', 1, NULL);
