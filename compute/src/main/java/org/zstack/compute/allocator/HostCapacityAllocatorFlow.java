ca44fe4c31bcd4234b7bf7c4336fae3d7bc4e221
zhangjianjun <jianjun.zhang@zstack.io>
2025-11-03 11:22:39 +0800

    <fix>[ovn]: sub hugepage memory used by ovs-dpdk
    Resolves: ZSTAC-78381
    
    Change-Id: I75796262676c64656861746c6e7065676271677a
    
    Signed-off-by: zhangjianjun <jianjun.zhang@zstack.io>

--- compute/src/main/java/org/zstack/compute/allocator/HostCapacityAllocatorFlow.java
+++ compute/src/main/java/org/zstack/compute/allocator/HostCapacityAllocatorFlow.java
@@ -38,29 +38,8 @@ public class HostCapacityAllocatorFlow extends AbstractHostAllocatorFlow {
             }
         }
 
-        // aggregate reserved huge page bytes from extensions (non-overlapping, non-negative)
-        long hugePageMemReservedForSysCom = 0L;
-        final List<HugePageMemoryUsageExtensionPoint> extps =
-                pluginRgty.getExtensionList(HugePageMemoryUsageExtensionPoint.class);
-        for (HugePageMemoryUsageExtensionPoint extp : extps) {
-            try {
-                long usage = Math.max(0L, extp.getHugePageMemoryUsage(hvo.getUuid()));
-                hugePageMemReservedForSysCom = Math.addExact(hugePageMemReservedForSysCom, usage);
-            } catch (ArithmeticException ae) {
-                logger.warn(String.format("reserved huge page bytes overflow on host[%s], cap to Long.MAX_VALUE",
-                        hvo.getUuid()), ae);
-                hugePageMemReservedForSysCom = Long.MAX_VALUE;
-                break;
-            } catch (Exception e) {
-                logger.warn(String.format("failed to get huge page usage from %s on host[%s], ignore this extension",
-                        extp.getClass().getSimpleName(), hvo.getUuid()), e);
-            }
-        }
-        long available = hvo.getCapacity().getAvailableMemory();
-        long availableAfter = Math.max(0L, available - hugePageMemReservedForSysCom);
-        return ratioMgr.calculateHostAvailableMemoryByRatio(hvo.getUuid(), availableAfter) >= vmMemSize;
+        return ratioMgr.calculateHostAvailableMemoryByRatio(hvo.getUuid(), hvo.getCapacity().getAvailableMemory()) >= vmMemSize;
     }
-
 
     private List<HostVO> allocate(List<HostVO> vos, long cpu, long memory, long oldMemory) {
         return vos.parallelStream()
