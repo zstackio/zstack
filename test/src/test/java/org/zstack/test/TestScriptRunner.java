af76b9b23f565f5d50d78608ffa87fdc98899c0f
Zhang Wenhao <wenhao.zhang@zstack.io>
2025-11-11 14:11:35 +0800

    <fix>[utils]: enhance Ssh tools
    ScriptRunner is also a SshRunner
    
    Related: ZSV-9923
    
    Change-Id: I7372726375776768756b796f636a646b706b6f70

--- test/src/test/java/org/zstack/test/TestScriptRunner.java
+++ test/src/test/java/org/zstack/test/TestScriptRunner.java
@@ -1,21 +1,31 @@
 package org.zstack.test;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.zstack.utils.path.PathUtil;
 import org.zstack.utils.ssh.Ssh;
 import org.zstack.utils.ssh.SshResult;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 import static org.zstack.utils.CollectionDSL.e;
 import static org.zstack.utils.CollectionDSL.map;
+import static org.zstack.utils.StringDSL.s;
 
 /**
  * Created by frank on 4/22/2015.
  */
 public class TestScriptRunner {
     @Test
-    public void test() {
+    public void test() throws Exception {
+        String scriptPath = PathUtil.findFileOnClassPath("scripts/check-public-dns-name.sh", true).getAbsolutePath();
+        String contents = FileUtils.readFileToString(new File(scriptPath), StandardCharsets.UTF_8);
+        String scriptContent = s(contents).formatByMap(map(e("dnsCheckList", "google.com")));
+
         SshResult ret = new Ssh().setHostname("localhost")
                 .setUsername("root").setPassword("password")
-                .script("scripts/check-public-dns-name.sh", map(e("dnsCheckList", "google.com"))).runAndClose();
+                .shell(scriptContent).runAndClose();
         ret.raiseExceptionIfFailed();
     }
 }
