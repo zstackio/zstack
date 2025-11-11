e54f8178fe91349f98660cdaec84790b5b3bfb32
AlanJager <ye.zou@zstack.io>
2025-11-11 10:53:26 +0800

    <chore>[sdk]: Update sdk
    Resolves: ZSTAC-79795
    
    Change-Id: I69686b786862746f717364646d7a656b64676173

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -20024,6 +20024,33 @@ abstract class ApiHelper {
     }
 
 
+    def generateModelMetadata(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GenerateModelMetadataAction.class) Closure c) {
+        def a = new org.zstack.sdk.GenerateModelMetadataAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def generateSeMdevDevices(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GenerateSeMdevDevicesAction.class) Closure c) {
         def a = new org.zstack.sdk.GenerateSeMdevDevicesAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
