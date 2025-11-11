3aa0b4b92983d4b2ca01d6eb666985ec127d9dd2
shan.wu <shan.wu@zstack.io>
2025-11-11 14:22:29 +0800

    <fix>[sharedblock]: convert memory snapshot install path from absolute path to protocol path
    convert memory snapshot install path from absolute path to protocol path
    
    Resolves/Related: ZSTAC-79756
    
    Change-Id: I6e626d68626461627a737765786a676e6b617064
    (cherry picked from commit 7b302df4bc696b7f8f5266c3fdad9c139eb25cc5)

--- conf/db/upgrade/V4.8.38__schema.sql
+++ conf/db/upgrade/V4.8.38__schema.sql
@@ -2,3 +2,10 @@ CALL ADD_COLUMN('LicenseHistoryVO', 'quotaType', 'varchar(64)', 1, 'None');
 CALL ADD_COLUMN('HostCapacityVO', 'cpuCoreNum', 'int unsigned', 0, '0');
 
 CREATE INDEX idx_schedType_createDate ON `zstack`.`VmSchedHistoryVO` (schedType, createDate);
+
+UPDATE VolumeSnapshotVO AS sp, PrimaryStorageVO AS ps
+SET sp.primaryStorageInstallPath = REPLACE(sp.primaryStorageInstallPath, '/dev/', 'sharedblock://')
+WHERE sp.primaryStorageUuid = ps.uuid
+  AND ps.type = 'SharedBlock'
+  AND sp.volumeType = 'Memory'
+  AND sp.primaryStorageInstallPath LIKE '/dev/%';
