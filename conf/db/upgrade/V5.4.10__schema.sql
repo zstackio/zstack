dafd9816feca24b7908eb226eab941e4f53d437b
shan.wu <shan.wu@zstack.io>
2025-11-11 14:22:29 +0800

    <fix>[sharedblock]: convert memory snapshot install path from absolute path to protocol path
    convert memory snapshot install path from absolute path to protocol path
    
    Resolves/Related: ZSTAC-79756
    
    Change-Id: I6e626d68626461627a737765786a676e6b617064

--- conf/db/upgrade/V5.4.10__schema.sql
+++ conf/db/upgrade/V5.4.10__schema.sql
@@ -1,3 +1,7 @@
 -- ZSTAC-86476: Add normalizedModelName column for GPU spec dedup
 CALL ADD_COLUMN('GpuDeviceSpecVO', 'normalizedModelName', 'VARCHAR(255)', 1, NULL);
 CALL CREATE_INDEX('GpuDeviceSpecVO', 'idx_gpu_spec_normalized_model', 'normalizedModelName');
+
+UPDATE VolumeSnapshotVO AS sp, PrimaryStorageVO AS ps
+SET sp.primaryStorageInstallPath = REPLACE(sp.primaryStorageInstallPath, '/dev/', 'sharedblock://')
+WHERE sp.primaryStorageUuid = ps.uuid AND ps.type = 'SharedBlock' AND sp.volumeType = 'Memory' AND sp.primaryStorageInstallPath LIKE '/dev/%';
