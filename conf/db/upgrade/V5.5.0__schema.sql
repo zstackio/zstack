4f7be587a2908ae437ec0c59941936042cb9bf41
hanyu.liang <hanyu.liang@zstack.io>
2025-11-18 15:30:21 +0800

    <fix>[mevoco]: correct ARM and VMware add-ons not consuming main license quota
    Resolves: ZSTAC-79237
    
    Change-Id: I646365206a7923767067706e7a6e667a796f686d

--- conf/db/upgrade/V5.5.0__schema.sql
+++ conf/db/upgrade/V5.5.0__schema.sql
@@ -1,4 +1,6 @@
 CALL ADD_COLUMN('VmVfNicVO', 'secondaryPciDeviceUuid', 'VARCHAR(32)', 1, NULL);
 ALTER TABLE `zstack`.`VmVfNicVO` ADD CONSTRAINT `fkVmVfNicVOSecondaryPciDeviceVO` FOREIGN KEY (`secondaryPciDeviceUuid`) REFERENCES `zstack`.`PciDeviceVO` (`uuid`) ON DELETE SET NULL;
 
+CALL ADD_COLUMN('LicenseAuthorizedCapacityVO', 'resourceInfo', 'text', 1, NULL);
+
 CALL ADD_COLUMN('PciDeviceVO', 'dependentDevices', 'varchar(255)', 1, NULL);
