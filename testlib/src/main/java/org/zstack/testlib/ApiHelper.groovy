e701e82824296c4f9cbf8d04c5ef0582b5cbb862
lin.ma <lin.ma@zstack.io>
2025-12-15 22:50:02 +0800

    <fix>[hygon]: Fix APIGenerateHygonMdevDevicesMsg to ungenerate before generate
    Use FlowChain to ensure ungenerateHygonMdevDevices completes before
    generateHygonMdevDevices. Update interceptor to check running VMs.
    
    Resolves: ZSTAC-79099
    
    Change-Id: I777774646773776a6a696a71786b77756c6e6d69

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -39527,6 +39527,33 @@ abstract class ApiHelper {
     }
 
 
+    def setVmInstanceHygonMdev(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SetVmInstanceHygonMdevAction.class) Closure c) {
+        def a = new org.zstack.sdk.SetVmInstanceHygonMdevAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def setVmMonitorNumber(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.SetVmMonitorNumberAction.class) Closure c) {
         def a = new org.zstack.sdk.SetVmMonitorNumberAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
