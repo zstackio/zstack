cf8f4407303ca2786a0e2f7542181f78d9318273
zhangjianjun <jianjun.zhang@zstack.io>
2025-12-16 11:37:52 +0800

    <fix>[compute]: batch get vnic qos config
    Resolves: ZSTAC-80581
    
    Change-Id: I726b707863746875777165747362677279667070
    
    Signed-off-by: zhangjianjun <jianjun.zhang@zstack.io>

--- compute/src/main/java/org/zstack/compute/vm/VmNicQosConfigBackend.java
+++ compute/src/main/java/org/zstack/compute/vm/VmNicQosConfigBackend.java
@@ -1,10 +1,19 @@
 package org.zstack.compute.vm;
 
+import java.util.List;
+
 public interface VmNicQosConfigBackend {
     String getVmInstanceType();
     void addNicQos(String vmUuid, String vmNicUuid, Long outboundBandwidth, Long inboundBandwidth);
     void deleteNicQos(String vmUuid, String vmNicUuid,String direction);
     VmNicQosStruct getNicQos(String vmUuid, String vmNicUuid);
+
+    /**
+     * Batch version: return QoS struct for provided NIC UUIDs in the same order.
+     */
+    default List<VmNicQosStruct> getNicQosBatch(List<String> vmNicUuids) {
+        throw new UnsupportedOperationException("getNicQosBatch is not supported");
+    }
 
     void addVmQos(String vmUuid, Long outboundBandwidth, Long inboundBandwidth);
     void deleteVmQos(String vmUuid, String direction);
