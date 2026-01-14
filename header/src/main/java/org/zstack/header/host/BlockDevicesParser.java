5ce64bd76170e84023e8be2e41754367cf4b412c
tao.gan <tao.gan@zstack.io>
2026-01-14 16:18:10 +0800

    <fix>[host]: filter out disks of type loop and rom
    Resolves: ZSV-10987
    
    Change-Id: I7277616f7378676d696772796b766676766c706b

--- header/src/main/java/org/zstack/header/host/BlockDevicesParser.java
+++ header/src/main/java/org/zstack/header/host/BlockDevicesParser.java
@@ -136,9 +136,8 @@ public class BlockDevicesParser {
             "   ]\n" +
             "}\n" +
             "===\r\n" +
-            "/dev/sr0:unknown\r\n\r\n" +
             "/dev/vda:\r\n" +
-            "/dev/vdb:loop\r\n\r\n" +
+            "/dev/vdb:msdos\r\n" +
             "/dev/vdc:unknown\r\n";
 
     public static List<BlockDevice> parse(String blockDevices) {
@@ -157,7 +156,7 @@ public class BlockDevicesParser {
             }
             blockDevicePartitionTable.put(blockDeviceAndPartitionTable.get(0), blockDeviceAndPartitionTable.get(1));
         });
-        allBlockDevices.forEach(blockDevice -> blockDevice.setPartitionTable(blockDevicePartitionTable.get(blockDevice.getName())));
+        allBlockDevices.forEach(blockDevice -> blockDevice.setPartitionTable(blockDevicePartitionTable.getOrDefault(blockDevice.getName(), null)));
 
         return allBlockDevices;
     }
@@ -173,7 +172,7 @@ public class BlockDevicesParser {
 
     public static String getBlockDevicesCommand() {
         String blockDevicesCommand = "lsblk -p -b -o NAME,TYPE,SIZE,PHY-SEC,LOG-SEC,MOUNTPOINT -J";
-        String partitionTableInfoCommand = "for disk in $(lsblk -d -p -n -o NAME); do echo -n \"$disk:\"; " +
+        String partitionTableInfoCommand = "for disk in $(lsblk -d -p -n -o NAME,TYPE | awk '$2!~/loop|rom/ {print $1}'); do echo -n \"$disk:\"; " +
                 "parted -s $disk print 2>/dev/null | awk '/Partition Table/ {print $3} END{print \"\"}'; done";
         return String.format("%s ; echo === ; %s", blockDevicesCommand, partitionTableInfoCommand);
     }
