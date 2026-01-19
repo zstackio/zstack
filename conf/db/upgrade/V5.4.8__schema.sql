b5fcc6672d93c18e3c6327fa6bc93b64317f400c
yaohua.wu <yaohua.wu@zstack.io>
2026-01-19 13:59:31 +0800

    <feature>[conf]: support resnotify webhook
    DBImpact
    
    Resolves: ZSTAC-80472
    
    Change-Id: I6c6f66796372726772746d74616c6f63776e7270

--- conf/db/upgrade/V5.4.8__schema.sql
+++ conf/db/upgrade/V5.4.8__schema.sql
@@ -8,3 +8,26 @@ CREATE TABLE IF NOT EXISTS `zstack`.`ExternalServiceConfigurationVO` (
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`ResNotifySubscriptionVO` (
+    `uuid` VARCHAR(32) NOT NULL UNIQUE,
+    `name` VARCHAR(255) DEFAULT NULL,
+    `description` VARCHAR(2048) DEFAULT NULL,
+    `resourceTypes` TEXT NOT NULL,
+    `eventTypes` VARCHAR(256) DEFAULT NULL,
+    `type` VARCHAR(32) NOT NULL DEFAULT 'WEBHOOK',
+    `state` VARCHAR(32) NOT NULL DEFAULT 'Enabled',
+    `lastOpDate` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` TIMESTAMP,
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`ResNotifyWebhookRefVO` (
+    `uuid` VARCHAR(32) NOT NULL UNIQUE,
+    `webhookUrl` TEXT NOT NULL,
+    `secret` VARCHAR(256) DEFAULT NULL,
+    `customHeaders` TEXT,
+    PRIMARY KEY (`uuid`),
+    CONSTRAINT `fk_ResNotifyWebhookRefVO_ResNotifySubscriptionVO` 
+        FOREIGN KEY (`uuid`) REFERENCES `ResNotifySubscriptionVO`(`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- core/src/main/java/org/zstack/core/db/DatabaseFacade.java
+++ core/src/main/java/org/zstack/core/db/DatabaseFacade.java
@@ -13,23 +13,23 @@ import java.util.List;
 
 public interface DatabaseFacade {
     EntityManager getEntityManager();
-    
+
     <T> T findById(long id, Class<T> entityClass);
-    
+
     <T> T findByUuid(String uuid, Class<T> entityClass);
 
     <T> T find(Query q);
-    
+
     <T> T persist(T entity);
 
     void persistCollection(Collection entities);
-    
+
     <T> T persistAndRefresh(T entity);
-    
+
     <T> void update(T entity);
-    
+
     <T> T updateAndRefresh(T entity);
-    
+
     <T> T reload(T entity);
 
     void updateCollection(Collection entities);
@@ -44,26 +44,26 @@ public interface DatabaseFacade {
 
     void hardDeleteCollectionSelectedBySQL(String sql, Class entityClass);
 
-    CriteriaBuilder  getCriteriaBuilder();
-    
+    CriteriaBuilder getCriteriaBuilder();
+
     <T> SimpleQuery<T> createQuery(Class<T> entityClass);
 
     long count(Class<?> entityClass);
-    
-    void entityForTranscationCallback(Operation op, Class<?>...entityClass);
-    
+
+    void entityForTranscationCallback(Operation op, Class<?>... entityClass);
+
     long generateSequenceNumber(Class<?> seqTable);
 
     <T> List<T> listAll(Class<T> clazz);
-    
+
     <T> List<T> listAll(int offset, int length, Class<T> clazz);
-    
+
     <T> List<T> listByPrimaryKeys(Collection priKeys, Class<T> clazz);
-    
+
     <T> List<T> listByPrimaryKeys(Collection priKeys, int offset, int length, Class<T> clazz);
 
     <T> List<T> listByApiMessage(APIListMessage msg, Class<T> clazz);
-    
+
     boolean isExist(Object id, Class<?> clazz);
 
     /**
@@ -84,4 +84,6 @@ public interface DatabaseFacade {
     String getDbVersion();
 
     void installEntityLifeCycleCallback(Class entityClass, EntityEvent evt, EntityLifeCycleCallback cb);
+
+    void uninstallEntityLifeCycleCallback(Class entityClass, EntityEvent evt, EntityLifeCycleCallback cb);
 }
