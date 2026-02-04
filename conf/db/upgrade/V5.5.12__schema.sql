0bfbf4544c99462ef85235d83c834b4b9b965780
lin.ma <lin.ma@zstack.io>
2026-02-04 18:06:11 +0800

    <fix>[zwatch]: VPC Router CPU alarm use external monitoring
    Resolves: ZSTAC-81171
    
    Change-Id: I7778676171646874706164777869707279776172

--- conf/db/upgrade/V5.5.12__schema.sql
+++ conf/db/upgrade/V5.5.12__schema.sql
@@ -48,3 +48,18 @@ CREATE TABLE IF NOT EXISTS `zstack`.`ExternalServiceConfigurationVO` (
 UPDATE VolumeSnapshotVO AS sp, PrimaryStorageVO AS ps
 SET sp.primaryStorageInstallPath = REPLACE(sp.primaryStorageInstallPath, '/dev/', 'sharedblock://')
 WHERE sp.primaryStorageUuid = ps.uuid AND ps.type = 'SharedBlock' AND sp.volumeType = 'Memory' AND sp.primaryStorageInstallPath LIKE '/dev/%';
+
+UPDATE `zstack`.`ActiveAlarmTemplateVO`
+SET `metricName` = 'CPUUsedUtilization'
+WHERE `uuid` = 'c9e6cdca107140bea62b4ca919ff9e88'
+  AND `metricName` = 'VRouterCPUAverageUsedUtilization';
+
+UPDATE `zstack`.`AlarmVO`
+SET `metricName` = 'CPUUsedUtilization'
+WHERE `uuid` IN (
+    SELECT `alarmUuid` FROM `zstack`.`ActiveAlarmVO`
+    WHERE `templateUuid` = 'c9e6cdca107140bea62b4ca919ff9e88'
+)
+  AND `metricName` = 'VRouterCPUAverageUsedUtilization';
+
+
