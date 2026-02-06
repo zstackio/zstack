4cfe4a49e08c2ff1d02ce6c0ad4168b4b93e995c
tao.yang <tao.yang@zstack.io>
2026-02-06 19:55:28 +0800

    <feature>[kms]: support kms
    DBImpact
    
    Resolves: ZSV-11331
    
    Change-Id: I786f686371626e6674636772676c68747768716a

---
+++ sdk/src/main/java/org/zstack/sdk/NkpRestoreInfo.java
@@ -0,0 +1,63 @@
+package org.zstack.sdk;
+
+
+
+public class NkpRestoreInfo  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String description;
+    public void setDescription(java.lang.String description) {
+        this.description = description;
+    }
+    public java.lang.String getDescription() {
+        return this.description;
+    }
+
+    public java.lang.String kdf;
+    public void setKdf(java.lang.String kdf) {
+        this.kdf = kdf;
+    }
+    public java.lang.String getKdf() {
+        return this.kdf;
+    }
+
+    public java.lang.String saltPolicy;
+    public void setSaltPolicy(java.lang.String saltPolicy) {
+        this.saltPolicy = saltPolicy;
+    }
+    public java.lang.String getSaltPolicy() {
+        return this.saltPolicy;
+    }
+
+    public java.lang.String encryptedMasterSeed;
+    public void setEncryptedMasterSeed(java.lang.String encryptedMasterSeed) {
+        this.encryptedMasterSeed = encryptedMasterSeed;
+    }
+    public java.lang.String getEncryptedMasterSeed() {
+        return this.encryptedMasterSeed;
+    }
+
+    public java.lang.Integer currentVersion;
+    public void setCurrentVersion(java.lang.Integer currentVersion) {
+        this.currentVersion = currentVersion;
+    }
+    public java.lang.Integer getCurrentVersion() {
+        return this.currentVersion;
+    }
+
+}
