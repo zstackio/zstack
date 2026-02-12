a84a36e2515e6b6bdc69f80420e62364e0832a90
ye.zou <ye.zou@zstack.io>
2026-02-12 16:12:49 +0800

    <fix>[ceph]: apply over-provisioning ratio when releasing snapshot capacity
    Resolves: ZSTAC-79709
    
    Change-Id: I45a2133bbb8c51c25ae3549d59e588976192a08d

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
@@ -5446,7 +5446,7 @@ public class CephPrimaryStorageBase extends PrimaryStorageBase {
         httpCall(DELETE_SNAPSHOT_PATH, cmd, DeleteSnapshotRsp.class, new ReturnValueCompletion<DeleteSnapshotRsp>(msg) {
             @Override
             public void success(DeleteSnapshotRsp returnValue) {
-                osdHelper.releaseAvailableCapacity(msg.getSnapshot().getPrimaryStorageInstallPath(), msg.getSnapshot().getSize());
+                osdHelper.releaseAvailableCapWithRatio(msg.getSnapshot().getPrimaryStorageInstallPath(), msg.getSnapshot().getSize());
                 bus.reply(msg, reply);
                 completion.done();
             }
