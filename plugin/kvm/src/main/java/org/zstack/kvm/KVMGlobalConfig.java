fc39d0fe1b0150e2d8f31508024d023ff5471b39
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-02-12 14:40:34 +0800

    <fix>[kvm]: continue to update KVMGlobalConfig default value to "None"
    Resolves: ZSV-11340
    Related: ZSV-11010
    
    Change-Id: I63646f6264697877707967746f69756e776f6c73

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMGlobalConfig.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMGlobalConfig.java
@@ -11,6 +11,8 @@ import org.zstack.header.cluster.ClusterVO;
 import org.zstack.header.host.HostVO;
 import org.zstack.header.zone.ZoneVO;
 
+import static org.zstack.kvm.KVMConstant.EDK_VERSION_NONE;
+
 /**
  */
 @GlobalConfigDefinition
@@ -166,7 +168,7 @@ public class KVMGlobalConfig {
     @BindResourceConfig({HostVO.class, ClusterVO.class})
     public static GlobalConfig MINIMUM_MEMORY_SIZE_BEFORE_START_VM = new GlobalConfig(CATEGORY, "min.free.memory.size");
 
-    @GlobalConfigDef(defaultValue = "None",
+    @GlobalConfigDef(defaultValue = EDK_VERSION_NONE,
             description = "Specify the EDK version to be used for the next VM startup. None indicates the use of the system's default EDK version")
     @BindResourceConfig(value = {VmInstanceVO.class, ClusterVO.class})
     public static GlobalConfig VM_EDK_VERSION_CONFIG = new GlobalConfig(CATEGORY, "vm.edk.version");
