a648c38953ebb3bf24fabc6f105aaf488ff9963c
ye.zou <ye.zou@zstack.io>
2026-02-19 15:45:07 +0800

    <fix>[utils]: add ORG_ZSTACK_AI_10134 error code for GPU count validation
    Resolves: ZSTAC-80991
    
    Change-Id: I7677ddc25c8859e35e8ba80fd3105406bc761a76

--- utils/src/main/java/org/zstack/utils/clouderrorcode/CloudOperationsErrorCode.java
+++ utils/src/main/java/org/zstack/utils/clouderrorcode/CloudOperationsErrorCode.java
@@ -14804,6 +14804,8 @@ public class CloudOperationsErrorCode {
 
     public static final String ORG_ZSTACK_AI_10133 = "ORG_ZSTACK_AI_10133";
 
+    public static final String ORG_ZSTACK_AI_10134 = "ORG_ZSTACK_AI_10134";
+
     public static final String ORG_ZSTACK_CORE_CLOUDBUS_10000 = "ORG_ZSTACK_CORE_CLOUDBUS_10000";
 
     public static final String ORG_ZSTACK_CORE_CLOUDBUS_10001 = "ORG_ZSTACK_CORE_CLOUDBUS_10001";
