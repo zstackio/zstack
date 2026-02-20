68791ea939110216bd7089392e940f427e357dec
ye.zou <ye.zou@zstack.io>
2026-02-20 16:33:01 +0800

    <fix>[ai]: add error codes for AI and PCI
    Add ORG_ZSTACK_AI_10134 for ModelCenter disconnected
    check and ORG_ZSTACK_PCIDEVICE_10077 for SR-IOV bond
    validation.
    
    Resolves: ZSTAC-72783
    
    Change-Id: I504a415a6e822513df955be600188ae88e2e1058

--- utils/src/main/java/org/zstack/utils/clouderrorcode/CloudOperationsErrorCode.java
+++ utils/src/main/java/org/zstack/utils/clouderrorcode/CloudOperationsErrorCode.java
@@ -11588,6 +11588,8 @@ public class CloudOperationsErrorCode {
 
     public static final String ORG_ZSTACK_PCIDEVICE_10076 = "ORG_ZSTACK_PCIDEVICE_10076";
 
+    public static final String ORG_ZSTACK_PCIDEVICE_10077 = "ORG_ZSTACK_PCIDEVICE_10077";
+
     public static final String ORG_ZSTACK_CAS_DRIVER_DONGHAI_10000 = "ORG_ZSTACK_CAS_DRIVER_DONGHAI_10000";
 
     public static final String ORG_ZSTACK_CAS_DRIVER_DONGHAI_10001 = "ORG_ZSTACK_CAS_DRIVER_DONGHAI_10001";
@@ -14803,6 +14805,8 @@ public class CloudOperationsErrorCode {
     public static final String ORG_ZSTACK_AI_10132 = "ORG_ZSTACK_AI_10132";
 
     public static final String ORG_ZSTACK_AI_10133 = "ORG_ZSTACK_AI_10133";
+
+    public static final String ORG_ZSTACK_AI_10134 = "ORG_ZSTACK_AI_10134";
 
     public static final String ORG_ZSTACK_CORE_CLOUDBUS_10000 = "ORG_ZSTACK_CORE_CLOUDBUS_10000";
 
