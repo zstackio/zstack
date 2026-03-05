a5906f07501e6b6806134807338ee28148e4bcff
hanyu.liang <hanyu.liang@zstack.io>
2026-03-05 15:16:05 +0800

    <fix>[accesskey]: support AccessKey type distinction
    Add type attribute for AccessKey to distinguish between
    user and system types.
    
    Resolves: ZSTAC-82022
    
    Change-Id: I6f706c6b777068657a627774746365626c6b636a

--- conf/db/upgrade/V5.5.12__schema.sql
+++ conf/db/upgrade/V5.5.12__schema.sql
@@ -1,3 +1,5 @@
+CALL ADD_COLUMN('AccessKeyVO', 'type', 'varchar(32)', 0, 'User');
+
 -- ZSTAC-75319: Add normalizedModelName column for GPU spec dedup
 CALL ADD_COLUMN('GpuDeviceSpecVO', 'normalizedModelName', 'VARCHAR(255)', 1, NULL);
 CALL CREATE_INDEX('GpuDeviceSpecVO', 'idx_gpu_spec_normalized_model', 'normalizedModelName');
