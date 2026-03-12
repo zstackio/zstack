736ad28af30cd6ce6f50268d1e38c8945157002c
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-03-12 16:03:33 +0800

    <feature>[kvm]: introduce VmHostBackupFileVO
    Resolves: ZSV-11439
    Related: ZSV-11310
    
    Change-Id: I63706e6a7a75616366716574646a677a6e646276

---
+++ header/src/main/java/org/zstack/header/vm/additions/VmHostBackupFileVO_.java
@@ -0,0 +1,15 @@
+package org.zstack.header.vm.additions;
+
+import org.zstack.header.vo.ResourceVO_;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import java.sql.Timestamp;
+
+@StaticMetamodel(VmHostBackupFileVO.class)
+public class VmHostBackupFileVO_ extends ResourceVO_ {
+    public static volatile SingularAttribute<VmHostBackupFileVO, String> vmInstanceUuid;
+    public static volatile SingularAttribute<VmHostBackupFileVO, VmHostFileType> type;
+    public static volatile SingularAttribute<VmHostBackupFileVO, Timestamp> createDate;
+    public static volatile SingularAttribute<VmHostBackupFileVO, Timestamp> lastOpDate;
+}
