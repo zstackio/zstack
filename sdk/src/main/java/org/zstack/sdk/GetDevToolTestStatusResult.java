dcda4a8f67839b008f85254dbfee415e2bec1d33
ye.zou <ye.zou@zstack.io>
2026-03-17 13:55:47 +0800

    <chore>[devtool]: add SDK files for GetDevToolTestStatus API
    Resolves: ZSTAC-0
    
    Change-Id: Ib479b185506e061d001b9eaa95cbddfe58427db9

---
+++ sdk/src/main/java/org/zstack/sdk/GetDevToolTestStatusResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class GetDevToolTestStatusResult {
+    public java.lang.String status;
+    public void setStatus(java.lang.String status) {
+        this.status = status;
+    }
+    public java.lang.String getStatus() {
+        return this.status;
+    }
+
+}
