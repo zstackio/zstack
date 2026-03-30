0849eac8da1e6a01136f28fc47214612b86186b9
xinhao.huang <xinhao.huang@zstack.io>
2026-03-30 19:13:53 +0800

    <fix>[conf]: add error codes for APIGetGpuDeviceCandidatesMsg
    Resolves: ZSTAC-83779
    
    Change-Id: I7771726f74616c72777a706a74736b697a6d6f61

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -22508,6 +22508,33 @@ abstract class ApiHelper {
     }
 
 
+    def getGpuDeviceCandidates(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetGpuDeviceCandidatesAction.class) Closure c) {
+        def a = new org.zstack.sdk.GetGpuDeviceCandidatesAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getGpuDeviceSpecCandidates(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetGpuDeviceSpecCandidatesAction.class) Closure c) {
         def a = new org.zstack.sdk.GetGpuDeviceSpecCandidatesAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
