c07c1d782757cd4dc856eb379ad9eebe58e64e6f
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-04-11 17:45:18 +0800

    <fix>[kvm]: mark TPM VM host files changed on start/shutdown
    When a VM with TPM starts or shuts down, NvRam/TpmState
    data must have changed. Preemptively set changeDate on the
    corresponding VmHostFileVO so the periodic tracker knows
    to sync them, even if the direct sync fails.
    
    Also add ResourceDestinationMaker check to all VM canonical
    event handlers in KvmSecureBootManager to ensure only the
    owning management node processes each event.
    
    Resolves: ZSV-11779
    Related: ZSV-11310
    
    Change-Id: I6e6d6c6c647175716669756b75756a6f72657277

--- compute/src/main/java/org/zstack/compute/vm/devices/VmTpmManager.java
+++ compute/src/main/java/org/zstack/compute/vm/devices/VmTpmManager.java
@@ -1,20 +1,28 @@
 package org.zstack.compute.vm.devices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.compute.vm.VmSystemTags;
 import org.zstack.core.Platform;
 import org.zstack.core.db.DatabaseFacade;
 import org.zstack.core.db.Q;
 import org.zstack.header.image.ImageBootMode;
 import org.zstack.header.tpm.entity.TpmVO;
 import org.zstack.header.tpm.entity.TpmVO_;
+import org.zstack.header.vm.additions.VmHostFileType;
 import org.zstack.resourceconfig.ResourceConfig;
 import org.zstack.resourceconfig.ResourceConfigFacade;
 import org.zstack.utils.Utils;
 import org.zstack.utils.logging.CLogger;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import static org.zstack.compute.vm.VmGlobalConfig.ENABLE_UEFI_SECURE_BOOT;
+import static org.zstack.header.vm.additions.VmHostFileType.NvRam;
+import static org.zstack.header.vm.additions.VmHostFileType.TpmState;
+import static org.zstack.utils.CollectionDSL.list;
 
 public class VmTpmManager {
     private static final CLogger logger = Utils.getLogger(VmTpmManager.class);
@@ -58,13 +66,27 @@ public class VmTpmManager {
     }
 
     public boolean needRegisterNvRam(String vmUuid) {
-        boolean tpmExists = Q.New(TpmVO.class)
+        return needRegister(NvRam, vmUuid);
+    }
+
+    public Set<VmHostFileType> vmHostFileTypeNeedRegisterForVm(String vmUuid) {
+        String bootMode = VmSystemTags.BOOT_MODE.getTokenByResourceUuid(vmUuid, VmSystemTags.BOOT_MODE_TOKEN);
+        if (!isUefiBootMode(bootMode)) {
+            return Collections.emptySet();
+        }
+
+        boolean hasTpm = Q.New(TpmVO.class)
                 .eq(TpmVO_.vmInstanceUuid, vmUuid)
                 .isExists();
-        if (tpmExists) {
-            return true;
+        if (hasTpm) {
+            return new HashSet<>(list(NvRam, TpmState));
         }
         ResourceConfig resourceConfig = resourceConfigFacade.getResourceConfig(ENABLE_UEFI_SECURE_BOOT.getIdentity());
-        return resourceConfig.getResourceConfigValue(vmUuid, Boolean.class);
+        return resourceConfig.getResourceConfigValue(vmUuid, Boolean.class) == Boolean.TRUE ?
+                new HashSet<>(list(NvRam)) : Collections.emptySet();
+    }
+
+    public boolean needRegister(VmHostFileType type, String vmUuid) {
+        return vmHostFileTypeNeedRegisterForVm(vmUuid).contains(type);
     }
 }
--- plugin/kvm/src/main/java/org/zstack/kvm/efi/KvmVmHostFileFactory.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/efi/KvmVmHostFileFactory.java
@@ -1,15 +1,26 @@
 package org.zstack.kvm.efi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.compute.vm.devices.VmTpmManager;
 import org.zstack.header.vm.additions.VmHostBackupFileVO;
+import org.zstack.header.vm.additions.VmHostFileType;
 import org.zstack.header.vm.additions.VmHostFileVO;
 import org.zstack.kvm.tpm.TpmStateVmHostBackupFileBase;
 import org.zstack.kvm.tpm.TpmStateVmHostFileBase;
 import org.zstack.kvm.vmfiles.AbstractVmHostBackupFileBase;
 import org.zstack.kvm.vmfiles.AbstractVmHostFileBase;
+import org.zstack.resourceconfig.ResourceConfigFacade;
+
+import java.util.Set;
 
 import static org.zstack.core.Platform.operr;
 
 public class KvmVmHostFileFactory {
+    @Autowired
+    private ResourceConfigFacade resourceConfigFacade;
+    @Autowired
+    private VmTpmManager vmTpmManager;
+
     public AbstractVmHostFileBase createBase(VmHostFileVO file) {
         switch (file.getType()) {
         case NvRam: return new NvRamVmHostFileBase(file);
@@ -24,5 +35,13 @@ public class KvmVmHostFileFactory {
         case TpmState: return new TpmStateVmHostBackupFileBase(backupFile);
         default: throw operr("invalid VM host file type: " + backupFile.getType()).toException();
         }
+    }
+
+    public Set<VmHostFileType> vmHostFileTypeNeedRegisterForVm(String vmUuid) {
+        return vmTpmManager.vmHostFileTypeNeedRegisterForVm(vmUuid);
+    }
+
+    public boolean needRegister(VmHostFileType type, String vmUuid) {
+        return vmTpmManager.needRegister(type, vmUuid);
     }
 }
