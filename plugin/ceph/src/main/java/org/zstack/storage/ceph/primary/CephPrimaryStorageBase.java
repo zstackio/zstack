8ca173f5d0767b330159d42098c5d6c77e9b30da
yaohua.wu <yaohua.wu@zstack.io>
2026-04-13 17:10:24 +0800

    Revert "Merge branch 'fix/ZSTAC-79709' into '5.5.12'"
    This reverts merge request !9162

--- plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
+++ plugin/ceph/src/main/java/org/zstack/storage/ceph/primary/CephPrimaryStorageBase.java
@@ -5448,7 +5448,7 @@ public class CephPrimaryStorageBase extends PrimaryStorageBase {
         httpCall(DELETE_SNAPSHOT_PATH, cmd, DeleteSnapshotRsp.class, new ReturnValueCompletion<DeleteSnapshotRsp>(msg) {
             @Override
             public void success(DeleteSnapshotRsp returnValue) {
-                osdHelper.releaseAvailableCapWithRatio(msg.getSnapshot().getPrimaryStorageInstallPath(), msg.getSnapshot().getSize());
+                osdHelper.releaseAvailableCapacity(msg.getSnapshot().getPrimaryStorageInstallPath(), msg.getSnapshot().getSize());
                 bus.reply(msg, reply);
                 completion.done();
             }
