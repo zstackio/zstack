6c7a9cfcdaba23d873a34cb855eff208a5a2b366
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-04-21 19:06:56 +0800

    <test>[core]: add I18nCase
    Resolves: ZSV-10444
    
    Change-Id: I736366796c616a726b6f7875776a717762726a64

--- test/src/test/groovy/org/zstack/test/integration/core/I18nCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/core/I18nCase.groovy
@@ -1,11 +1,12 @@
 package org.zstack.test.integration.core
 
-import com.google.common.collect.Lists
 import org.zstack.core.Platform
+import org.zstack.core.errorcode.ErrorFacade
+import org.zstack.header.errorcode.ErrorCode
+import org.zstack.header.core.I18nMessage
 import org.zstack.testlib.SubCase
 
 class I18nCase extends SubCase {
-
 
     @Override
     void clean() {
@@ -24,22 +25,30 @@ class I18nCase extends SubCase {
 
     @Override
     void test() {
+        logger.info("Test 001: Platform.i18m(\"%s\", errorCode): raw details use getDetails(), i18n details use getI18nDetails()")
+        ErrorCode cause = Platform.operr("cause %s", "C1")
+        I18nMessage im = Platform.i18m("%s", cause)
+        assert im.getDetails() == cause.getDetails()
+        assert im.getI18nDetails() == cause.getI18nDetails()
 
-        // prevent the case failing when some annotations are deleted
-        // cancel annotation when testing i18n messsages
+        logger.info("Test 002")
+        I18nMessage im2 = I18nMessage.valueOf("hello", "你好")
+        ErrorCode cause2 = Platform.operr("test-002 %s", im2)
+        assert cause2.getDetails() == "test-002 hello"
+        assert cause2.getI18nDetails() == "test-002 你好"
 
-        // Locale en = new Locale("en_US")
+        logger.info("Test 003: ErrorFacadeImpl.instantiateErrorCode(..., i18m(...)): details formatted correctly and i18nDetails not be destroyed")
+        ErrorFacade errf = bean(ErrorFacade.class)
+        ErrorCode e = errf.instantiateErrorCode("SYS.1006", "parent %s", im)
+        assert e.details == String.format("parent %s", cause.getDetails())
+        assert e.opaque
+        assert e.opaque["arg.0"] instanceof I18nMessage
+        assert ((I18nMessage)e.opaque["arg.0"]).getI18nDetails() == im.getI18nDetails()
 
-        // test single quote
-        // logger.info(Platform.toI18nString("not dest host found in db, can\u0027t send change password cmd to the host!", en, Lists.newArrayList()))
-        // logger.info(Platform.toI18nString("ecs instance [%s] start isn\\u0027t finish, status is still [%s]", en, "aaaaaa", "bbbbbb"))
-
-        // test %d
-        // logger.info(Platform.toI18nString("invalid volume IOPS[%s] is larger than %d", en, "cccccc",123))
-        // logger.info(Platform.toI18nString("unable to connect to KVM[ip:%s, username:%s, sshPort:%d ] to do DNS check, please check if username/password is wrong; %s", en, "dddddd", "eeeeee", 456, "ffffff"))
-
-        // test zh_CN
-        // logger.info(Platform.toI18nString("modify-ecs-[%s]-console-vnc-password-failed, due to [code: %s, details: %s]", null, "gggggg", "hhhhhh", "iiiiii"))
-
+        logger.info("Test 004: ErrorCode as I18nMessage participates in formatting: ensure behavior preserved")
+        ErrorCode c4 = Platform.operr("error %s", "E2")
+        I18nMessage im4 = Platform.i18m("wrap[%s]", c4)
+        assert im4.getDetails() == String.format("wrap[%s]", c4.getDetails())
+        assert im4.getI18nDetails() == String.format("wrap[%s]", c4.getI18nDetails())
     }
 }
