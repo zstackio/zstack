e742258472d91a07ee6f24ecb827435380aeb061
hanyu.liang <hanyu.liang@zstack.io>
2026-04-25 01:24:35 +0800

    <refactor>[iam2]: add ZIAM_DEFAULT_SCOPE constant
    Add ZIAM_DEFAULT_SCOPE = "openid profile email" to OAuth2PluginConstants
    for use by IAM2RopcLoginBackend instead of hardcoded string.
    
    Resolves: ZCF-3009
    
    Change-Id: I7638e4bb3231fa75b1ad3460863040d43970d9f5

--- abstraction/src/main/java/org/zstack/abstraction/sso/OAuth2PluginConstants.java
+++ abstraction/src/main/java/org/zstack/abstraction/sso/OAuth2PluginConstants.java
@@ -48,4 +48,5 @@ public interface OAuth2PluginConstants {
     String MAX_KEY_PROVIDER_NAME = "max_key";
     String ZIAM_PROVIDER_NAME = "ziam";
     String LOGIN_TYPE_IAM2_ROPC = "iam2_ropc";
+    String ZIAM_DEFAULT_SCOPE = "openid profile email";
 }
