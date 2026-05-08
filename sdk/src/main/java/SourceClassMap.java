3ba508562561d7fe87bdc685af5aab1e1faef9c4
AlanJager <ye.zou@zstack.io>
2026-05-08 23:56:30 +0800

    <fix>[sdk]: Update sdk and schema
    DBImpact
    
    Change-Id: I68707570767978766a64737265706f737a65736e

--- sdk/src/main/java/SourceClassMap.java
+++ sdk/src/main/java/SourceClassMap.java
@@ -10,6 +10,7 @@ public class SourceClassMap {
 			put("org.zstack.accessKey.AccessKeyInventory", "org.zstack.sdk.AccessKeyInventory");
 			put("org.zstack.accessKey.AccessKeyState", "org.zstack.sdk.AccessKeyState");
 			put("org.zstack.accessKey.AccessKeyType", "org.zstack.sdk.AccessKeyType");
+			put("org.zstack.ai.CdnTemplateEntry", "org.zstack.sdk.CdnTemplateEntry");
 			put("org.zstack.ai.NginxRedirectRule", "org.zstack.sdk.NginxRedirectRule");
 			put("org.zstack.ai.entity.ApplicationDevelopmentServiceInventory", "org.zstack.sdk.ApplicationDevelopmentServiceInventory");
 			put("org.zstack.ai.entity.DatasetInventory", "org.zstack.sdk.DatasetInventory");
@@ -1010,6 +1011,7 @@ public class SourceClassMap {
 			put("org.zstack.sdk.CbtTaskResourceRefInventory", "org.zstack.header.cbt.CbtTaskResourceRefInventory");
 			put("org.zstack.sdk.CbtTaskStatus", "org.zstack.header.cbt.CbtTaskStatus");
 			put("org.zstack.sdk.CdRomTO", "org.zstack.kvm.KVMAgentCommands$CdRomTO");
+			put("org.zstack.sdk.CdnTemplateEntry", "org.zstack.ai.CdnTemplateEntry");
 			put("org.zstack.sdk.CdpPolicyInventory", "org.zstack.header.storage.cdp.CdpPolicyInventory");
 			put("org.zstack.sdk.CdpPolicyState", "org.zstack.header.storage.cdp.CdpPolicyState");
 			put("org.zstack.sdk.CdpTaskInventory", "org.zstack.header.storage.cdp.CdpTaskInventory");
---
+++ sdk/src/main/java/org/zstack/sdk/QueryCdnModelServiceTemplateListResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk;
+
+
+
+public class QueryCdnModelServiceTemplateListResult {
+    public java.util.List templates;
+    public void setTemplates(java.util.List templates) {
+        this.templates = templates;
+    }
+    public java.util.List getTemplates() {
+        return this.templates;
+    }
+
+}
