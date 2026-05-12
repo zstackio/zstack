0aa7f73ed72ca398f6ee53f30c39df6572ed5bbd
ye.zou <ye.zou@zstack.io>
2026-05-12 15:20:04 +0800

    <Cloud>[ZSTAC-84025]: Add CDN template schema and SDK inventory
    The persistent CDN template inventory needs an upgrade table and SDK mapping so APIQuery and sync results expose the same typed inventory to clients.
    
    Constraint: feature-5.5.22-aios upgrades need the V5.5.22 schema file updated with the new VO table.
    
    Confidence: high
    
    Scope-risk: narrow
    
    Tested: Docker ./runMavenProfile premium
    
    Tested: mvn -pl sdk -DskipTests package
    
    Not-tested: Full SDK regeneration pipeline

---
+++ sdk/src/main/java/org/zstack/sdk/CdnModelServiceTemplateInventory.java
@@ -0,0 +1,111 @@
+package org.zstack.sdk;
+
+
+
+public class CdnModelServiceTemplateInventory  {
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String version;
+    public void setVersion(java.lang.String version) {
+        this.version = version;
+    }
+    public java.lang.String getVersion() {
+        return this.version;
+    }
+
+    public java.lang.String platform;
+    public void setPlatform(java.lang.String platform) {
+        this.platform = platform;
+    }
+    public java.lang.String getPlatform() {
+        return this.platform;
+    }
+
+    public java.lang.Long size;
+    public void setSize(java.lang.Long size) {
+        this.size = size;
+    }
+    public java.lang.Long getSize() {
+        return this.size;
+    }
+
+    public java.lang.String projectId;
+    public void setProjectId(java.lang.String projectId) {
+        this.projectId = projectId;
+    }
+    public java.lang.String getProjectId() {
+        return this.projectId;
+    }
+
+    public java.lang.String projectName;
+    public void setProjectName(java.lang.String projectName) {
+        this.projectName = projectName;
+    }
+    public java.lang.String getProjectName() {
+        return this.projectName;
+    }
+
+    public java.lang.String downloadUrl;
+    public void setDownloadUrl(java.lang.String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+    public java.lang.String getDownloadUrl() {
+        return this.downloadUrl;
+    }
+
+    public boolean installed;
+    public void setInstalled(boolean installed) {
+        this.installed = installed;
+    }
+    public boolean getInstalled() {
+        return this.installed;
+    }
+
+    public java.lang.String modelServiceUuid;
+    public void setModelServiceUuid(java.lang.String modelServiceUuid) {
+        this.modelServiceUuid = modelServiceUuid;
+    }
+    public java.lang.String getModelServiceUuid() {
+        return this.modelServiceUuid;
+    }
+
+    public long usingServiceCount;
+    public void setUsingServiceCount(long usingServiceCount) {
+        this.usingServiceCount = usingServiceCount;
+    }
+    public long getUsingServiceCount() {
+        return this.usingServiceCount;
+    }
+
+    public java.sql.Timestamp createDate;
+    public void setCreateDate(java.sql.Timestamp createDate) {
+        this.createDate = createDate;
+    }
+    public java.sql.Timestamp getCreateDate() {
+        return this.createDate;
+    }
+
+    public java.sql.Timestamp lastOpDate;
+    public void setLastOpDate(java.sql.Timestamp lastOpDate) {
+        this.lastOpDate = lastOpDate;
+    }
+    public java.sql.Timestamp getLastOpDate() {
+        return this.lastOpDate;
+    }
+
+}
