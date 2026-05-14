d4eaeb6c7663f410894693e815b39dd7ee6272b6
haidong.pang <haidong.pang@zstack.io>
2026-05-14 16:24:15 +0800

    <fix>[volume-cache]: rename GC pool in-use cache field
    Rename GCPoolCmd retain list from volumes to inUseCacheUuids.
    
    Resolves: ZSTAC-85164
    
    Change-Id: Ia72f68775819e5193375ba29f87a4efdfb45430c

--- plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KVMAgentCommands.java
@@ -5417,7 +5417,7 @@ public class KVMAgentCommands {
         public String poolUuid;
         public String mountPoint;
         public boolean force;
-        public List<VolumeTO> volumes;
+        public List<String> inUseCacheUuids;
     }
 
     public static class AllocateCacheCmd extends AgentCommand {
