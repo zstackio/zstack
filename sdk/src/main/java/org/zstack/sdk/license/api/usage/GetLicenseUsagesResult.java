6b73e336e896381c2de4ccd84337658422f47750
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-05-21 00:57:29 +0800

    <feature>[sdk]: Add GetLicenseUsages API for multi-dimension usage
    Introduce APIGetLicenseUsagesMsg / APIGetLicenseUsagesReply
    so callers can query platform and addon license usage
    across CPUSocket, CPUCore, Host (and VM where applicable) in one request.
    
    Resolves: ZSV-12284
    Related: ZSV-12274
    
    Change-Id: I786a656d6b656c76776f78746e617a646e707a6c

---
+++ sdk/src/main/java/org/zstack/sdk/license/api/usage/GetLicenseUsagesResult.java
@@ -0,0 +1,14 @@
+package org.zstack.sdk.license.api.usage;
+
+
+
+public class GetLicenseUsagesResult {
+    public java.util.List results;
+    public void setResults(java.util.List results) {
+        this.results = results;
+    }
+    public java.util.List getResults() {
+        return this.results;
+    }
+
+}
--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -37233,6 +37233,33 @@ abstract class ApiHelper {
     }
 
 
+    def getLicenseUsages(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.license.api.usage.GetLicenseUsagesAction.class) Closure c) {
+        def a = new org.zstack.sdk.license.api.usage.GetLicenseUsagesAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getManagementNodesStatus(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.managements.common.GetManagementNodesStatusAction.class) Closure c) {
         def a = new org.zstack.sdk.managements.common.GetManagementNodesStatusAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
