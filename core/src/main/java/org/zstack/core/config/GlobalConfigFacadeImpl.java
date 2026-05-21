8457962b5315f92a7e77e05931b20542aef5930e
shixin.ruan <shixin@shixin.ruan>
2026-05-21 22:53:53 +0900

    <fix>[mgt-ipv6]: wire global config event facade
    Avoid relying only on Configurable injection for GlobalConfig instances
    created during GlobalConfigFacadeImpl startup.
    
    Resolves: ZSTAC-79206
    
    Change-Id: I3f975fc23240d88ba08dc870220277cc065f7cfb

--- core/src/main/java/org/zstack/core/config/GlobalConfigFacadeImpl.java
+++ core/src/main/java/org/zstack/core/config/GlobalConfigFacadeImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.zstack.core.asyncbatch.While;
 import org.zstack.core.cloudbus.CloudBus;
 import org.zstack.core.cloudbus.CloudBusCallBack;
+import org.zstack.core.cloudbus.EventFacade;
 import org.zstack.core.cloudbus.MessageSafe;
 import org.zstack.core.componentloader.PluginRegistry;
 import org.zstack.core.db.DatabaseFacade;
@@ -54,6 +55,8 @@ public class GlobalConfigFacadeImpl extends AbstractService implements GlobalCon
     private CloudBus bus;
     @Autowired
     private DatabaseFacade dbf;
+    @Autowired
+    private EventFacade evtf;
     @Autowired
     private ErrorFacade errf;
     @Autowired
@@ -336,6 +339,7 @@ public class GlobalConfigFacadeImpl extends AbstractService implements GlobalCon
 
             private void initAllConfig() {
                 for (GlobalConfig config : configsFromXml.values()) {
+                    config.wire(dbf, evtf);
                     config.init();
                 }
             }
