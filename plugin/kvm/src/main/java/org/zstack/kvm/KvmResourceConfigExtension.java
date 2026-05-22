f68c9d256d7deab9617dc20fe3f023b3dc616a37
haoyu.ding <haoyu.ding@zstack.io>
2026-05-22 14:19:40 +0800

    <fix>[kvm]: Add KVM VM CPU hardware virtualization config
    DBImpact
    GlobalConfigImpact
    
    Resolves: ZSTAC-83181
    
    Change-Id: I69756d697976666c78736a7165656975676b6969
    
    
    (cherry picked from commit abe052c23ae587275a52ff66b526f9baa0b8e354)

--- plugin/kvm/src/main/java/org/zstack/kvm/KvmResourceConfigExtension.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/KvmResourceConfigExtension.java
@@ -1,12 +1,17 @@
 package org.zstack.kvm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.core.db.Q;
+import org.zstack.header.image.ImagePlatform;
 import org.zstack.header.vm.ArchiveResourceConfigBundle;
 import org.zstack.header.vm.ResourceConfigMemorySnapshotExtensionPoint;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.zstack.header.vm.VmInstanceVO;
+import org.zstack.header.vm.VmInstanceVO_;
 import org.zstack.resourceconfig.ResourceConfigFacade;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class KvmResourceConfigExtension implements ResourceConfigMemorySnapshotExtensionPoint {
     @Autowired
@@ -20,6 +25,30 @@ public class KvmResourceConfigExtension implements ResourceConfigMemorySnapshotE
         bundle.setIdentity(KVMGlobalConfig.NESTED_VIRTUALIZATION.getIdentity());
         bundle.setValue(rcf.getResourceConfigValue(KVMGlobalConfig.NESTED_VIRTUALIZATION, resourceUuid, String.class));
         bundleList.add(bundle);
+
+        if (isWindowsVm(resourceUuid)) {
+            Boolean cpuHardwareVirtualization = rcf.getResourceConfigValue(KVMGlobalConfig.VM_CPU_HARDWARE_VIRTUALIZATION,
+                    resourceUuid, Boolean.class);
+            ArchiveResourceConfigBundle.ResourceConfigBundle cpuHardwareVirtualizationBundle =
+                    new ArchiveResourceConfigBundle.ResourceConfigBundle();
+            cpuHardwareVirtualizationBundle.setResourceUuid(resourceUuid);
+            cpuHardwareVirtualizationBundle.setIdentity(KVMGlobalConfig.VM_CPU_HARDWARE_VIRTUALIZATION.getIdentity());
+            cpuHardwareVirtualizationBundle.setValue(cpuHardwareVirtualization.toString());
+            bundleList.add(cpuHardwareVirtualizationBundle);
+        }
         return bundleList;
+    }
+
+    private boolean isWindowsVm(String vmUuid) {
+        VmInstanceVO vm = Q.New(VmInstanceVO.class).eq(VmInstanceVO_.uuid, vmUuid).find();
+        if (vm == null) {
+            return false;
+        }
+
+        if (ImagePlatform.isType(vm.getPlatform(), ImagePlatform.Windows, ImagePlatform.WindowsVirtio)) {
+            return true;
+        }
+
+        return vm.getGuestOsType() != null && vm.getGuestOsType().toLowerCase(Locale.ROOT).contains("windows");
     }
 }
