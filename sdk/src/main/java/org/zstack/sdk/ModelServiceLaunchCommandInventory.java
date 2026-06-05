ca5faad5a3c8cd7bdb0a32fcd28c0ffa4f367436
ye.zou <ye.zou@zstack.io>
2026-06-05 14:56:24 +0800

    <feature>[sdk]: expose launch command inventory
    Add the ModelServiceInstanceVO.launchCommand schema column and regenerate the Java SDK so ModelServiceInstanceInventory exposes the synced launch command metadata. The standalone launch-command action is intentionally removed because callers should use the existing group query inventory.
    
    Tested: ./runMavenProfile sdk in privileged PR docker
    
    Resolves: ZSTAC-84940
    
    Change-Id: I9a36942edeeae26ce82900060f53a8990648dbb9

---
+++ sdk/src/main/java/org/zstack/sdk/ModelServiceLaunchCommandInventory.java
@@ -0,0 +1,119 @@
+package org.zstack.sdk;
+
+
+
+public class ModelServiceLaunchCommandInventory  {
+
+    public java.lang.String adapter;
+    public void setAdapter(java.lang.String adapter) {
+        this.adapter = adapter;
+    }
+    public java.lang.String getAdapter() {
+        return this.adapter;
+    }
+
+    public java.lang.String framework;
+    public void setFramework(java.lang.String framework) {
+        this.framework = framework;
+    }
+    public java.lang.String getFramework() {
+        return this.framework;
+    }
+
+    public java.lang.String modelDir;
+    public void setModelDir(java.lang.String modelDir) {
+        this.modelDir = modelDir;
+    }
+    public java.lang.String getModelDir() {
+        return this.modelDir;
+    }
+
+    public java.lang.String modelId;
+    public void setModelId(java.lang.String modelId) {
+        this.modelId = modelId;
+    }
+    public java.lang.String getModelId() {
+        return this.modelId;
+    }
+
+    public java.lang.Integer backendPort;
+    public void setBackendPort(java.lang.Integer backendPort) {
+        this.backendPort = backendPort;
+    }
+    public java.lang.Integer getBackendPort() {
+        return this.backendPort;
+    }
+
+    public java.lang.String startupMode;
+    public void setStartupMode(java.lang.String startupMode) {
+        this.startupMode = startupMode;
+    }
+    public java.lang.String getStartupMode() {
+        return this.startupMode;
+    }
+
+    public java.lang.String source;
+    public void setSource(java.lang.String source) {
+        this.source = source;
+    }
+    public java.lang.String getSource() {
+        return this.source;
+    }
+
+    public java.lang.String commandLine;
+    public void setCommandLine(java.lang.String commandLine) {
+        this.commandLine = commandLine;
+    }
+    public java.lang.String getCommandLine() {
+        return this.commandLine;
+    }
+
+    public java.util.List commandArgs;
+    public void setCommandArgs(java.util.List commandArgs) {
+        this.commandArgs = commandArgs;
+    }
+    public java.util.List getCommandArgs() {
+        return this.commandArgs;
+    }
+
+    public java.lang.String executedCommand;
+    public void setExecutedCommand(java.lang.String executedCommand) {
+        this.executedCommand = executedCommand;
+    }
+    public java.lang.String getExecutedCommand() {
+        return this.executedCommand;
+    }
+
+    public java.lang.Boolean shell;
+    public void setShell(java.lang.Boolean shell) {
+        this.shell = shell;
+    }
+    public java.lang.Boolean getShell() {
+        return this.shell;
+    }
+
+    public java.lang.Integer pid;
+    public void setPid(java.lang.Integer pid) {
+        this.pid = pid;
+    }
+    public java.lang.Integer getPid() {
+        return this.pid;
+    }
+
+    public java.lang.String logFile;
+    public void setLogFile(java.lang.String logFile) {
+        this.logFile = logFile;
+    }
+    public java.lang.String getLogFile() {
+        return this.logFile;
+    }
+
+    public java.lang.String generatedAt;
+    public void setGeneratedAt(java.lang.String generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+    public java.lang.String getGeneratedAt() {
+        return this.generatedAt;
+    }
+
+}
