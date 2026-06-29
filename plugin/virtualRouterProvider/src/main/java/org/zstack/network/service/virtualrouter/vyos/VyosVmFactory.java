90a7f4c00d91f5ab08ea65cd2c746ae769465671
shixin.ruan <shixin.ruan@zstack.io>
2026-06-29 20:56:26 +0800

    <fix>[virtualrouter]: reserve management VIP ports
    Detect the actual virtual router management NIC IP through UsedIpVO so both IPv4 and IPv6 management addresses reserve appliance service ports.
    
    Test: mvn -pl plugin/virtualRouterProvider -am -DskipTests compile
    
    Resolves: ZSTAC-86300
    
    Change-Id: I9b9262737a57a5366d66b82ad4a43128ce901fb0

--- plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/vyos/VyosVmFactory.java
+++ plugin/virtualRouterProvider/src/main/java/org/zstack/network/service/virtualrouter/vyos/VyosVmFactory.java
@@ -6,17 +6,24 @@ import org.zstack.appliancevm.ApplianceVmVO;
 import org.zstack.appliancevm.ApvmCascadeFilterExtensionPoint;
 import org.zstack.core.cascade.CascadeAction;
 import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.Q;
 import org.zstack.header.network.l3.IpRangeVO;
 import org.zstack.header.network.l3.L3NetworkVO;
 import org.zstack.header.network.l3.UsedIpInventory;
-import org.zstack.header.network.service.NetworkServiceType;
+import org.zstack.header.network.l3.UsedIpVO;
+import org.zstack.header.network.l3.UsedIpVO_;
 import org.zstack.header.vm.VmNicInventory;
+import org.zstack.header.vm.VmNicVO;
+import org.zstack.header.vm.VmNicVO_;
 import org.zstack.network.service.lb.LoadBalancerConstants;
 import org.zstack.network.service.vip.VipGetUsedPortRangeExtensionPoint;
 import org.zstack.network.service.vip.VipVO;
 import org.zstack.network.service.virtualrouter.VirtualRouterGlobalConfig;
 import org.zstack.network.service.virtualrouter.VirtualRouterGlobalProperty;
 import org.zstack.network.service.virtualrouter.VirtualRouterManagerImpl;
+import org.zstack.network.service.virtualrouter.VirtualRouterNicMetaData;
+import org.zstack.network.service.virtualrouter.VirtualRouterVmVO;
+import org.zstack.network.service.virtualrouter.VirtualRouterVmVO_;
 import org.zstack.utils.RangeSet;
 import org.zstack.utils.Utils;
 import org.zstack.utils.VipUseForList;
@@ -24,6 +31,7 @@ import org.zstack.utils.logging.CLogger;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by weiwang on 19/09/2017
@@ -49,15 +57,13 @@ public class VyosVmFactory extends VyosVmBaseFactory implements VipGetUsedPortRa
         portRangeList.setRanges(portRanges);
 
         VipVO vipVO = dbf.findByUuid(vipUuid, VipVO.class);
-        /* system vip is the vip of public ip of vpc or vpc ha group */
-        boolean hasSnat = vipVO.getServicesTypes().contains(NetworkServiceType.SNAT.toString());
-        if (vipVO.isSystem()) {
-            if (protocol.equalsIgnoreCase(LoadBalancerConstants.LB_PROTOCOL_UDP) && hasSnat){
+        if (isVirtualRouterManagementNicIp(vipVO)) {
+            if (protocol.equalsIgnoreCase(LoadBalancerConstants.LB_PROTOCOL_UDP)){
                 portRanges.add(new RangeSet.Range(VyosConstants.DNS_PORT, VyosConstants.DNS_PORT, true));
                 portRanges.add(new RangeSet.Range(VyosConstants.NTP_PORT, VyosConstants.NTP_PORT, true));
             }
 
-            if (protocol.equalsIgnoreCase(LoadBalancerConstants.LB_PROTOCOL_TCP) && hasSnat){
+            if (!protocol.equalsIgnoreCase(LoadBalancerConstants.LB_PROTOCOL_UDP)){
                 portRanges.add(new RangeSet.Range(VyosConstants.DNS_PORT, VyosConstants.DNS_PORT, true));
 
                 int sshPort = VirtualRouterGlobalConfig.SSH_PORT.value(Integer.class);
@@ -69,6 +75,42 @@ public class VyosVmFactory extends VyosVmBaseFactory implements VipGetUsedPortRa
         }
 
         return portRangeList;
+    }
+
+    private boolean isVirtualRouterManagementNicIp(VipVO vipVO) {
+        List<String> nicUuids = new ArrayList<>();
+        UsedIpVO vipUsedIp = vipVO.getUsedIpUuid() == null ? null :
+                Q.New(UsedIpVO.class).eq(UsedIpVO_.uuid, vipVO.getUsedIpUuid()).find();
+        if (vipUsedIp != null
+                && Objects.equals(vipUsedIp.getIp(), vipVO.getIp())
+                && Objects.equals(vipUsedIp.getL3NetworkUuid(), vipVO.getL3NetworkUuid())
+                && vipUsedIp.getVmNicUuid() != null) {
+            nicUuids.add(vipUsedIp.getVmNicUuid());
+        } else {
+            nicUuids.addAll(Q.New(UsedIpVO.class)
+                    .select(UsedIpVO_.vmNicUuid)
+                    .eq(UsedIpVO_.ip, vipVO.getIp())
+                    .eq(UsedIpVO_.l3NetworkUuid, vipVO.getL3NetworkUuid())
+                    .notNull(UsedIpVO_.vmNicUuid)
+                    .listValues());
+        }
+
+        if (nicUuids.isEmpty()) {
+            return false;
+        }
+
+        List<VmNicVO> managementNics = Q.New(VmNicVO.class)
+                .in(VmNicVO_.uuid, nicUuids)
+                .in(VmNicVO_.metaData, VirtualRouterNicMetaData.MANAGEMENT_NIC_MASK_STRING_LIST)
+                .list();
+
+        for (VmNicVO nic : managementNics) {
+            if (Q.New(VirtualRouterVmVO.class).eq(VirtualRouterVmVO_.uuid, nic.getVmInstanceUuid()).isExists()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
