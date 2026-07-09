3ee585eb47fb30f2b9174503b77f2fa20bf8a75d
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-07-09 18:25:40 +0800

    <fix>[sdk]: add APIQueryLicenseAuthorizedNodeMsg
    Resolves: ZSV-12642
    Related: ZSV-12274
    
    Change-Id: I79716c616e677a666a71786b77686b6a72686a6e

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -37397,6 +37397,33 @@ abstract class ApiHelper {
     }
 
 
+    def queryLicenseAuthorizedNode(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.license.api.client.QueryLicenseAuthorizedNodeAction.class) Closure c) {
+        def a = new org.zstack.sdk.license.api.client.QueryLicenseAuthorizedNodeAction()
+        a.sessionId = Test.currentEnvSpec?.session?.uuid
+        c.resolveStrategy = Closure.OWNER_FIRST
+        c.delegate = a
+        c()
+        
+
+        if (System.getProperty("apipath") != null) {
+            if (a.apiId == null) {
+                a.apiId = Platform.uuid
+            }
+    
+            def tracker = new ApiPathTracker(a.apiId)
+            def out = errorOut(a.call())
+            def path = tracker.getApiPath()
+            if (!path.isEmpty()) {
+                Test.apiPaths[a.class.name] = path.join(" --->\n")
+            }
+        
+            return out
+        } else {
+            return errorOut(a.call())
+        }
+    }
+
+
     def getLicenseAuthorizedCapacity(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.license.api.server.GetLicenseAuthorizedCapacityAction.class) Closure c) {
         def a = new org.zstack.sdk.license.api.server.GetLicenseAuthorizedCapacityAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
