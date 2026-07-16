38fde38e4d28a19ac5ebd8cd9afac395e424ca1d
Chen, Taiyue <taiyue.chen@zstack.io>
2026-07-16 17:07:02 +0800

    <fix>[platformservice]: pass ZCenter deployment language
    Expose the ZCenter deployment language in the generated SDK action.
    
    Related: ZSV-12702
    
    Change-Id: I6e61727a7566657a636e6a746f6c72666462706a

--- sdk/src/main/java/org/zstack/sdk/platformservice/DeployPlatformServiceAction.java
+++ sdk/src/main/java/org/zstack/sdk/platformservice/DeployPlatformServiceAction.java
@@ -64,6 +64,9 @@ public class DeployPlatformServiceAction extends AbstractAction {
     @Param(required = false, maxLength = 255, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
     public java.lang.String allocatorStrategy;
 
+    @Param(required = false, validValues = {"zh_CN","en_US"}, maxLength = 5, nonempty = false, nullElements = false, emptyString = true, noTrim = false)
+    public java.lang.String language;
+
     @Param(required = false)
     public java.util.List systemTags;
 
