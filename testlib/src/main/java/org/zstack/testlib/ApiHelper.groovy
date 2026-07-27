51cd04d84bba3f88d953644a1d62fb33d31d9b0c
tao.gan <tao.gan@zstack.io>
2026-07-27 16:53:20 +0800

    Revert "Merge branch 'ZSV-12509@@2' into 'zsv_5.1.0'"
    This reverts merge request !10524

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -17216,33 +17216,6 @@ abstract class ApiHelper {
     }
 
 
-    def getPrimaryStorageCandidatesForVmVolumeMigration(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetPrimaryStorageCandidatesForVmVolumeMigrationAction.class) Closure c) {
-        def a = new org.zstack.sdk.GetPrimaryStorageCandidatesForVmVolumeMigrationAction()
-        a.sessionId = Test.currentEnvSpec?.session?.uuid
-        c.resolveStrategy = Closure.OWNER_FIRST
-        c.delegate = a
-        c()
-
-
-        if (System.getProperty("apipath") != null) {
-            if (a.apiId == null) {
-                a.apiId = Platform.uuid
-            }
-
-            def tracker = new ApiPathTracker(a.apiId)
-            def out = errorOut(a.call())
-            def path = tracker.getApiPath()
-            if (!path.isEmpty()) {
-                Test.apiPaths[a.class.name] = path.join(" --->\n")
-            }
-
-            return out
-        } else {
-            return errorOut(a.call())
-        }
-    }
-
-
     def getPrimaryStorageCandidatesForVolumeMigration(@DelegatesTo(strategy = Closure.OWNER_FIRST, value = org.zstack.sdk.GetPrimaryStorageCandidatesForVolumeMigrationAction.class) Closure c) {
         def a = new org.zstack.sdk.GetPrimaryStorageCandidatesForVolumeMigrationAction()
         a.sessionId = Test.currentEnvSpec?.session?.uuid
