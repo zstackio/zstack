a428ac8dbee9faad9c998efbd4f816fe013c4435
Zhang Wenhao <wenhao.zhang@zstack.io>
2026-07-28 17:29:29 +0800

    <fix>[zcenter]: allowed zcenter-account API for basic license
    Resolves: ZSV-12820
    Related: ZSV-12257
    
    Change-Id: I6477626c65767164626465687766697a6f716d75

--- agents/zcenter-accounts/src/main/java/org/zstack/zcenter/accounts/RBACInfo.java
+++ agents/zcenter-accounts/src/main/java/org/zstack/zcenter/accounts/RBACInfo.java
@@ -16,6 +16,7 @@ public class RBACInfo implements RBACDescription {
                 .adminOnlyForAll()
                 .communityAvailable()
                 .zsvProAvailable()
+                .zsvBasicAvailable()
                 .build();
     }
 }
