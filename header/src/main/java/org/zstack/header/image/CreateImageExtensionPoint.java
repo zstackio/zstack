24ac3f2364dececd3d053868807627e32c809dfd
zhong.zhou <zhong.zhou@zstack.io>
2026-07-30 11:48:40 +0800

    <fix>[storage]: add image commit addon extension
    Resolves: ZSV-12473
    
    Change-Id: I79906e3d2d9c044aaad8c8f709081e1c76e478c1

--- header/src/main/java/org/zstack/header/image/CreateImageExtensionPoint.java
+++ header/src/main/java/org/zstack/header/image/CreateImageExtensionPoint.java
@@ -1,5 +1,9 @@
 package org.zstack.header.image;
 
+import org.zstack.header.message.Message;
+
+import java.util.Map;
+
 /**
  * Created by mingjian.deng on 2017/9/14.
  */
@@ -7,4 +11,7 @@ public interface CreateImageExtensionPoint {
     void beforeCreateImage(ImageInventory img, String bsUuid, String psUuid);
     void beforeSyncImage(ImageInventory img, String bsUuid);
     String getImageDescription(ImageInventory img);
+
+    default void addAddon(Message msg, String hostUuid, Map<String, Object> addons) {
+    }
 }
