d0dc9227ff4f4ae4dc4707d8c0c3763c9317c902
shixin.ruan <shixin.ruan@zstack.io>
2026-08-08 16:07:01 +0800

    <feature>[network]: add l3 create context
    Additive L3 and IP range context overloads for CP-002.
    
    Resolves: ZCF-5485
    
    Change-Id: I6bd6f7ca57ad17d65f70215f5ec71bdda1709e2c

--- header/src/main/java/org/zstack/header/network/l3/SdnControllerL3.java
+++ header/src/main/java/org/zstack/header/network/l3/SdnControllerL3.java
@@ -1,12 +1,19 @@
 package org.zstack.header.network.l3;
 
 import org.zstack.header.core.Completion;
+import org.zstack.header.network.l2.NetworkCreateContext;
 
 import java.util.List;
 
 public interface SdnControllerL3 {
     void createL3Network(L3NetworkInventory inv, List<String> systemTags, Completion completion);
+    default void createL3Network(L3NetworkInventory inv, List<String> systemTags, NetworkCreateContext context, Completion completion) {
+        createL3Network(inv, systemTags, completion);
+    }
     void deleteL3Network(L3NetworkInventory inv, Completion completion);
     void createIpRange(IpRangeInventory inv, Completion completion);
+    default void createIpRange(IpRangeInventory inv, NetworkCreateContext context, Completion completion) {
+        createIpRange(inv, completion);
+    }
     void deleteIpRange(IpRangeInventory inv, Completion completion);
 }
