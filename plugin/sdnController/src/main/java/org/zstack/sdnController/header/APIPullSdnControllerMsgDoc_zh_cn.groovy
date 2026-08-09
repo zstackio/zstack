e9d2f95ddcac028a42f22714676c670080fe4759
shixin.ruan <shixin.ruan@zstack.io>
2026-08-09 18:22:00 +0800

    <fix>[sdnController]: sync generated api doc
    Keep generated API documentation byte-for-byte consistent.
    
    Resolves: ZCF-5485
    
    Change-Id: I5fd4e3d7dfe6093a7eed11930ba8abeab6f99fb4

--- plugin/sdnController/src/main/java/org/zstack/sdnController/header/APIPullSdnControllerMsgDoc_zh_cn.groovy
+++ plugin/sdnController/src/main/java/org/zstack/sdnController/header/APIPullSdnControllerMsgDoc_zh_cn.groovy
@@ -11,63 +11,63 @@ doc {
 
     rest {
         request {
-            url "PUT /v1/sdn-controllers/{uuid}/resources/actions"
+			url "PUT /v1/sdn-controllers/{uuid}/resources/actions"
 
-            header (Authorization: 'OAuth the-session-uuid')
+			header (Authorization: 'OAuth the-session-uuid')
 
             clz APIPullSdnControllerMsg.class
 
             desc """"""
+            
+			params {
 
-            params {
-
-                column {
-                    name "uuid"
-                    enclosedIn "pullSdnController"
-                    desc "资源的UUID，唯一标示该资源"
-                    location "url"
-                    type "String"
-                    optional false
-                    since "5.5.38"
-                }
-                column {
-                    name "resourceType"
-                    enclosedIn "pullSdnController"
-                    desc ""
-                    location "body"
-                    type "String"
-                    optional false
-                    since "5.5.38"
-                    values ("Segment","TenantRouter")
-                }
-                column {
-                    name "resourceUuids"
-                    enclosedIn "pullSdnController"
-                    desc ""
-                    location "body"
-                    type "List"
-                    optional true
-                    since "5.5.38"
-                }
-                column {
-                    name "systemTags"
-                    enclosedIn ""
-                    desc "系统标签"
-                    location "body"
-                    type "List"
-                    optional true
-                    since "5.5.38"
-                }
-                column {
-                    name "userTags"
-                    enclosedIn ""
-                    desc "用户标签"
-                    location "body"
-                    type "List"
-                    optional true
-                    since "5.5.38"
-                }
-            }
+				column {
+					name "uuid"
+					enclosedIn "pullSdnController"
+					desc "资源的UUID，唯一标示该资源"
+					location "url"
+					type "String"
+					optional false
+					since "5.5.38"
+				}
+				column {
+					name "resourceType"
+					enclosedIn "pullSdnController"
+					desc ""
+					location "body"
+					type "String"
+					optional false
+					since "5.5.38"
+					values ("Segment","TenantRouter")
+				}
+				column {
+					name "resourceUuids"
+					enclosedIn "pullSdnController"
+					desc ""
+					location "body"
+					type "List"
+					optional true
+					since "5.5.38"
+				}
+				column {
+					name "systemTags"
+					enclosedIn ""
+					desc "系统标签"
+					location "body"
+					type "List"
+					optional true
+					since "5.5.38"
+				}
+				column {
+					name "userTags"
+					enclosedIn ""
+					desc "用户标签"
+					location "body"
+					type "List"
+					optional true
+					since "5.5.38"
+				}
+			}
         }
 
         response {
