023a94e923c4846b54ae9cc0e2286d1b2f09a7ab
shixin.ruan <shixin@shixin.ruan>
2026-09-02 17:03:20 +0900

    <fix>[sdk]: trim remaining generated whitespace
    Resolves: ZSTAC-86959
    
    Change-Id: I0bad5fb0166a200d8c841210b29799cf150fa679

--- testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
+++ testlib/src/main/java/org/zstack/testlib/ApiHelper.groovy
@@ -36226,14 +36226,14 @@ abstract class ApiHelper {
             if (a.apiId == null) {
                 a.apiId = Platform.uuid
             }
-    
+
             def tracker = new ApiPathTracker(a.apiId)
             def out = errorOut(a.call())
             def path = tracker.getApiPath()
             if (!path.isEmpty()) {
                 Test.apiPaths[a.class.name] = path.join(" --->\n")
             }
-        
+
             return out
         } else {
             return errorOut(a.call())
@@ -36247,7 +36247,7 @@ abstract class ApiHelper {
         c.resolveStrategy = Closure.OWNER_FIRST
         c.delegate = a
         c()
-        
+
         a.conditions = a.conditions.collect { it.toString() }
 
 
