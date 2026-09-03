652960f64f9874268f4a4a47c22ce6262cc274fc
shixin.ruan <shixin.ruan@zstack.io>
2026-09-04 02:44:43 +0800

    <fix>[zns]: sync generated zns sdk
    Resolves: ZSTAC-86959
    
    Change-Id: I5c094ddb4aaf1a46db311434aab9b8446e7381bf

--- sdk/src/main/java/org/zstack/sdk/network/zns/GetZnsIntegrationEffectiveOfferAction.java
+++ sdk/src/main/java/org/zstack/sdk/network/zns/GetZnsIntegrationEffectiveOfferAction.java
@@ -17,10 +17,10 @@ public class GetZnsIntegrationEffectiveOfferAction extends AbstractAction {
         public Result throwExceptionIfError() {
             if (error != null) {
                 throw new ApiException(
-                    String.format("error[code: %s, description: %s, details: %s, globalErrorCode: %s]", error.code, error.description, error.details, error.globalErrorCode)
+                    String.format("error[code: %s, description: %s, details: %s, globalErrorCode: %s]", error.code, error.description, error.details, error.globalErrorCode)    
                 );
             }
-
+            
             return this;
         }
     }
@@ -62,9 +62,9 @@ public class GetZnsIntegrationEffectiveOfferAction extends AbstractAction {
             ret.error = res.error;
             return ret;
         }
-
+        
         org.zstack.sdk.network.zns.GetZnsIntegrationEffectiveOfferResult value = res.getResult(org.zstack.sdk.network.zns.GetZnsIntegrationEffectiveOfferResult.class);
-        ret.value = value == null ? new org.zstack.sdk.network.zns.GetZnsIntegrationEffectiveOfferResult() : value;
+        ret.value = value == null ? new org.zstack.sdk.network.zns.GetZnsIntegrationEffectiveOfferResult() : value; 
 
         return ret;
     }
