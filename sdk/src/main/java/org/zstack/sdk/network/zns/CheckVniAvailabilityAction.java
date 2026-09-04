c6f5212c2919bfa633c109ef714628bf205de3a2
shixin.ruan <shixin@shixin.ruan>
2026-09-05 04:22:15 +0900

    <fix>[zns]: generate VNI API clients
    Add generated SDK and ApiHelper support for VNI availability. Allocate
    the dedicated malformed-response ErrorCode.
    
    Resolves: ZSTAC-86959
    
    Change-Id: I3f28a1c20a99cf57bf7b377d33ee1121bb379512

--- sdk/src/main/java/org/zstack/sdk/network/zns/CheckVniAvailabilityAction.java
+++ sdk/src/main/java/org/zstack/sdk/network/zns/CheckVniAvailabilityAction.java
@@ -17,10 +17,10 @@ public class CheckVniAvailabilityAction extends AbstractAction {
         public Result throwExceptionIfError() {
             if (error != null) {
                 throw new ApiException(
-                    String.format("error[code: %s, description: %s, details: %s, globalErrorCode: %s]", error.code, error.description, error.details, error.globalErrorCode)    
+                    String.format("error[code: %s, description: %s, details: %s, globalErrorCode: %s]", error.code, error.description, error.details, error.globalErrorCode)
                 );
             }
-            
+
             return this;
         }
     }
@@ -56,9 +56,9 @@ public class CheckVniAvailabilityAction extends AbstractAction {
             ret.error = res.error;
             return ret;
         }
-        
+
         org.zstack.sdk.network.zns.CheckVniAvailabilityResult value = res.getResult(org.zstack.sdk.network.zns.CheckVniAvailabilityResult.class);
-        ret.value = value == null ? new org.zstack.sdk.network.zns.CheckVniAvailabilityResult() : value; 
+        ret.value = value == null ? new org.zstack.sdk.network.zns.CheckVniAvailabilityResult() : value;
 
         return ret;
     }
