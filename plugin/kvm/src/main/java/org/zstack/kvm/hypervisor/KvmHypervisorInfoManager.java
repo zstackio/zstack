0bb1ac82571a3114edda2af2c44a59d13f79e057
shixin.ruan <shixin.ruan@zstack.io>
2026-09-10 21:19:50 +0800

    Revert "Merge branch 'shixin-ZSTAC-88484' into '5.5.38'"
    This reverts merge request !10897

--- plugin/kvm/src/main/java/org/zstack/kvm/hypervisor/KvmHypervisorInfoManager.java
+++ plugin/kvm/src/main/java/org/zstack/kvm/hypervisor/KvmHypervisorInfoManager.java
@@ -3,21 +3,10 @@ package org.zstack.kvm.hypervisor;
 import static org.zstack.kvm.KVMAgentCommands.GetVirtualizerInfoRsp;
 import static org.zstack.kvm.KVMAgentCommands.VirtualizerInfoTO;
 
-import java.util.List;
-
 public interface KvmHypervisorInfoManager {
     void save(GetVirtualizerInfoRsp rsp);
     void saveHostInfo(VirtualizerInfoTO info);
     void saveVmInfo(VirtualizerInfoTO info);
-
-    /**
-     * Saves the reported hypervisor information on the management node that owns the host.
-     *
-     * @param hostUuid the host uuid used to route the message, must not be null
-     * @param hostInfo the host hypervisor information, null when only vm information is reported
-     * @param vmInfoList the vm hypervisor information list, null when only host information is reported
-     */
-    void saveOnHostOwnerNode(String hostUuid, VirtualizerInfoTO hostInfo, List<VirtualizerInfoTO> vmInfoList);
 
     void clean(String uuid);
 
