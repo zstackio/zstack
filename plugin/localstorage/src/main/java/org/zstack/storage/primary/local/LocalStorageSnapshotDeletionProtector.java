e5e8351975d3bc782210528146a92cfb24ad4207
tao.gan <tao.gan@zstack.io>
2026-09-11 13:25:20 +0800

    <fix>[localstorage]: fix registered volume snapshot cleanup
    Resolves: ZSV-13115
    
    Change-Id: I787a72666a63767273727069647669746268716b

--- plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSnapshotDeletionProtector.java
+++ plugin/localstorage/src/main/java/org/zstack/storage/primary/local/LocalStorageSnapshotDeletionProtector.java
@@ -1,16 +1,16 @@
 package org.zstack.storage.primary.local;
 
 import org.zstack.header.core.Completion;
-import org.zstack.header.errorcode.OperationFailureException;
 import org.zstack.header.storage.snapshot.VolumeSnapshotDeletionProtector;
 import org.zstack.header.storage.snapshot.VolumeSnapshotInventory;
-import org.zstack.storage.snapshot.VolumeSnapshotSystemTags;
+import org.zstack.storage.volume.VolumeSystemTags;
 
 import static org.zstack.core.Platform.inerr;
-import static org.zstack.core.Platform.operr;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LocalStorageSnapshotDeletionProtector implements VolumeSnapshotDeletionProtector {
     @Override
@@ -20,12 +20,26 @@ public class LocalStorageSnapshotDeletionProtector implements VolumeSnapshotDele
 
     @Override
     public void protect(VolumeSnapshotInventory snapshot, Completion completion) {
-        Path path = Paths.get(snapshot.getPrimaryStorageInstallPath());
-        if (!path.getParent().toString().contains(snapshot.getVolumeUuid())) {
+        List<String> volumeUuids = getUsedVolumeUuids(snapshot);
+        volumeUuids.add(snapshot.getVolumeUuid());
+
+        if (!isSnapshotPathOwnedByVolume(snapshot.getPrimaryStorageInstallPath(), volumeUuids)) {
             completion.fail(inerr("the snapshot[name:%s, uuid:%s, path: %s] seems not belong to the volume[uuid:%s]",
                     snapshot.getName(), snapshot.getUuid(), snapshot.getPrimaryStorageInstallPath(), snapshot.getVolumeUuid()));
             return;
         }
         completion.success();
+    }
+
+    private List<String> getUsedVolumeUuids(VolumeSnapshotInventory snapshot) {
+        return VolumeSystemTags.OVERWRITED_VOLUME.getTags(snapshot.getVolumeUuid()).stream()
+                .map(it -> VolumeSystemTags.OVERWRITED_VOLUME.getTokenByTag(
+                        it, VolumeSystemTags.OVERWRITED_VOLUME_TOKEN))
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isSnapshotPathOwnedByVolume(String installPath, List<String> volumeUuids) {
+        Path path = Paths.get(installPath);
+        return volumeUuids.stream().anyMatch(it -> path.getParent().toString().contains(it));
     }
 }
